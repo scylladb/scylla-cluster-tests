4e2b6e442f3f446e573deb9247854d41290b2eb0
Lucas Meneghel Rodrigues <lmr@scylladb.com>
2016-01-19 17:49:24 -0200

    scylla_longevity.py: Use ChaosMonkey in longevity tests
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@scylladb.com>

--- scylla_longevity.py
+++ scylla_longevity.py
@@ -3,7 +3,7 @@
 from avocado import main
 
 from sdcm.tester import ClusterTester
-from sdcm.nemesis import StopStartMonkey
+from sdcm.nemesis import ChaosMonkey
 
 
 class LongevityTest(ClusterTester):
@@ -18,15 +18,15 @@ class LongevityTest(ClusterTester):
         """
         Run cassandra-stress on a cluster for 20 min (smoke check).
         """
-        self.db_cluster.add_nemesis(StopStartMonkey)
-        self.db_cluster.start_nemesis(interval=5)
+        self.db_cluster.add_nemesis(ChaosMonkey)
+        self.db_cluster.start_nemesis(interval=10)
         self.run_stress(duration=20)
 
     def test_12_hours(self):
         """
         Run cassandra-stress on a cluster for 12 hours.
         """
-        self.db_cluster.add_nemesis(StopStartMonkey)
+        self.db_cluster.add_nemesis(ChaosMonkey)
         self.db_cluster.start_nemesis(interval=30)
         self.run_stress(duration=60 * 12)
 
@@ -34,7 +34,7 @@ class LongevityTest(ClusterTester):
         """
         Run cassandra-stress on a cluster for 24 hours.
         """
-        self.db_cluster.add_nemesis(StopStartMonkey)
+        self.db_cluster.add_nemesis(ChaosMonkey)
         self.db_cluster.start_nemesis(interval=30)
         self.run_stress(duration=60 * 24)
 
@@ -42,7 +42,7 @@ class LongevityTest(ClusterTester):
         """
         Run cassandra-stress on a cluster for 1 week.
         """
-        self.db_cluster.add_nemesis(StopStartMonkey)
+        self.db_cluster.add_nemesis(ChaosMonkey)
         self.db_cluster.start_nemesis(interval=30)
         self.run_stress(duration=60 * 24 * 7)
 
