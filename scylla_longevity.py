498d9062d6c347c4b69665fc90c54e29a7c2caad
Lucas Meneghel Rodrigues <lmr@scylladb.com>
2016-01-19 17:38:13 -0200

    scylla_longevity.py: Update test docstrings
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@scylladb.com>

--- scylla_longevity.py
+++ scylla_longevity.py
@@ -17,23 +17,32 @@ class LongevityTest(ClusterTester):
 
     def test_20_minutes(self):
         """
-        Run a very short test, as a config/code sanity check.
+        Run cassandra-stress on a cluster for 20 min (smoke check).
         """
         self.db_cluster.add_nemesis(StopStartMonkey)
         self.db_cluster.start_nemesis(interval=5)
         self.run_stress(duration=20)
 
     def test_12_hours(self):
+        """
+        Run cassandra-stress on a cluster for 12 hours.
+        """
         self.db_cluster.add_nemesis(StopStartMonkey)
         self.db_cluster.start_nemesis(interval=30)
         self.run_stress(duration=60 * 12)
 
     def test_1_day(self):
+        """
+        Run cassandra-stress on a cluster for 24 hours.
+        """
         self.db_cluster.add_nemesis(StopStartMonkey)
         self.db_cluster.start_nemesis(interval=30)
         self.run_stress(duration=60 * 24)
 
     def test_1_week(self):
+        """
+        Run cassandra-stress on a cluster for 1 week.
+        """
         self.db_cluster.add_nemesis(StopStartMonkey)
         self.db_cluster.start_nemesis(interval=30)
         self.run_stress(duration=60 * 24 * 7)
