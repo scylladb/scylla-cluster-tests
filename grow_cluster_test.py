88274efdcfe472a3aa54bc11034dc16014de1183
Lucas Meneghel Rodrigues <lmr@scylladb.com>
2016-01-21 13:05:39 -0200

    Add grow_cluster_test.py: Grow cluster testing
    Add a new test that picks a 3 node cluster, and keeps
    adding new nodes to it until we get to the magic
    number of 30 nodes, while running cassandra-stress
    on the loader node.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@scylladb.com>

---
+++ grow_cluster_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import threading
+
+from avocado import main
+
+from sdcm.tester import ClusterTester
+
+
+class GrowClusterTest(ClusterTester):
+
+    """
+    Test scylla cluster growth (adding nodes after an initial cluster size).
+
+    :avocado: enable
+    """
+
+    def setUp(self):
+        self.credentials = None
+        self.db_cluster = None
+        self.loaders = None
+        self.init_resources(n_db_nodes=3, n_loader_nodes=1)
+        self.loaders.wait_for_init()
+        self.db_cluster.wait_for_init()
+        self.stress_thread = None
+
+    def test_grow_one_to_30(self):
+        """
+        1) Start a 1 node cluster
+        2) Start cassandra-stress on the loader node
+        3) Add a new node
+        4) Keep repeating 3) until we get to the target number of 30 nodes
+        """
+        self.stress_thread = threading.Thread(target=self.run_stress,
+                                              kwargs={'duration': 120})
+        self.stress_thread.start()
+        target_size = 30
+        while len(self.db_cluster.nodes) < target_size:
+            new_nodes = self.db_cluster.add_nodes(count=1)
+            self.db_cluster.wait_for_init(node_list=new_nodes)
+        self.stress_thread.join()
+
+
+if __name__ == '__main__':
+    main()
