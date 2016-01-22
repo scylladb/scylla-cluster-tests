c32bd3363124d98ba40358fb87131adeb30b371d
Lucas Meneghel Rodrigues <lmr@scylladb.com>
2016-01-22 15:32:26 -0200

    grow_cluster_test: Give more time to c-s to run
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@scylladb.com>

--- grow_cluster_test.py
+++ grow_cluster_test.py
@@ -31,8 +31,11 @@ class GrowClusterTest(ClusterTester):
         3) Add a new node
         4) Keep repeating 3) until we get to the target number of 30 nodes
         """
+        # Let's estimate 15 minutes of c-s for each new node
+        # They initialize in ~ 5 minutes average
+        duration = 15 * 27
         self.stress_thread = threading.Thread(target=self.run_stress,
-                                              kwargs={'duration': 120})
+                                              kwargs={'duration': duration})
         self.stress_thread.start()
         target_size = 30
         while len(self.db_cluster.nodes) < target_size:
