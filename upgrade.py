25e343466763420feb8996d97f966f2c5b8db09a
Benoît Canet <benoit@scylladb.com>
2016-05-02 16:43:59 +0000

    Create a way to test upgrades
    Signed-off-by: Benoît Canet <benoit@scylladb.com>

---
+++ upgrade.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright (c) 2016 ScyllaDB
+
+
+from avocado import main
+
+from sdcm.tester import ClusterTester
+
+from sdcm.nemesis import UpgradeNemesis
+
+
+class UpgradeTest(ClusterTester):
+
+    """
+    Test a Scylla cluster upgrade on a short period.
+
+    :avocado: enable
+    """
+
+    default_params = {'timeout': 650000}
+
+    def test_20_minutes(self):
+        """
+        Run cassandra-stress on a cluster for 12 hours.
+        """
+        self.db_cluster.add_nemesis(UpgradeNemesis)
+        self.db_cluster.start_nemesis(interval=10)
+        self.run_stress(duration=20)
+
+if __name__ == '__main__':
+    main()
