558c3b6b70abcc1ba354152dcbd55488884840c6
Amos Kong <amos@scylladb.com>
2016-07-12 15:26:22 +0800

    perf regression: save result to xml for reporting to jenkins
    This patch saves performance results to xml file, it will be
    parsed by jenkins plugin (PerfPublisher) in the end of job,
    then we can check and compare history results from jenkins.
    
    Reference:
    https://wiki.jenkins-ci.org/display/JENKINS/PerfPublisher+Plugin
    
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- performance_regression_test.py
+++ performance_regression_test.py
@@ -38,6 +38,52 @@ class PerformanceRegressionTest(ClusterTester):
                                           result['latency 95th percentile'],
                                           result['latency 99th percentile'],
                                           result['latency 99.9th percentile']))
+        f = open('jenkins_perf_PerfPublisher.xml', 'w')
+        content = """<report name="simple_regression_test report" categ="none">
+
+  <test name="simple_regression_test" executed="yes">
+    <description>"simple regression test"</description>
+    <targets>
+      <target threaded="yes">target-%s</target>
+      <ami_id>%s</ami_id>
+      <stress_modes>%s</stress_modes>
+    </targets>
+    <platform name="scylla_platform">
+      <instance_type_db>%s</instance_type_db>
+    </platform>
+
+    <result>
+      <success passed="yes" state="1"/>
+      <performance unit="kbs" mesure="%s" isRelevant="true" />
+      <metrics>
+        <op-rate unit="op/s" mesure="%s" isRelevant="true" />
+        <partition-rate unit="pk/s" mesure="%s" isRelevant="true" />
+        <row-rate unit="row/s" mesure="%s" isRelevant="true" />
+        <latency-mean unit="mean" mesure="%s" isRelevant="true" />
+        <latency-median unit="med" mesure="%s" isRelevant="true" />
+        <l-95th-pct unit=".95" mesure="%s" isRelevant="true" />
+        <l-99th-pct unit=".99" mesure="%s" isRelevant="true" />
+        <l-99.9th-pct unit=".999" mesure="%s" isRelevant="true" />
+      </metrics>
+    </result>
+  </test>
+</report>
+""" % (self.params.get('ami_id_db_scylla'),
+            self.params.get('ami_id_db_scylla'),
+            self.params.get('stress_modes'),
+            self.params.get('instance_type_db'),
+            result['op rate'],
+            result['op rate'],
+            result['partition rate'],
+            result['row rate'],
+            result['latency mean'],
+            result['latency median'],
+            result['latency 95th percentile'],
+            result['latency 99th percentile'],
+            result['latency 99.9th percentile'])
+
+        f.write(content)
+        f.close()
 
     def display_results(self, results):
         self.log.info(self.str_pattern % ('op-rate', 'partition-rate',
