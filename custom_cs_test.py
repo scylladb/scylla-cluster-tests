adf099860eeed6ab157d04a6fa71be673c9d7908
Lucas Meneghel Rodrigues <lmr@scylladb.com>
2016-07-15 17:27:17 -0300

    Add custom_cs_test.py
    This is a quick and simple to modify test for people
    seeking to automate running a customized cassandra-stress
    command, using a YAML configuration file. Details on
    how to construct and create such a file can be found on:
    
    http://www.datastax.com/dev/blog/improved-cassandra-2-1-stress-tool-benchmark-any-schema
    
    The included example tries to test at the same time
    wide partition support and DateTieredCompactionStrategy
    support. Each user may want to customize the file to
    fulfill their needs.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@scylladb.com>

---
+++ custom_cs_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright (c) 2016 ScyllaDB
+
+
+from avocado import main
+
+from sdcm.tester import ClusterTester
+from sdcm.data_path import get_data_path
+
+
+class CustomCsTest(ClusterTester):
+
+    """
+    Run a custom c-s workload (yaml) on a cluster.
+
+    :avocado: enable
+    """
+
+    default_params = {'timeout': 650000}
+
+    def test_write_mode(self):
+        """
+        Run cassandra-stress with params defined in data_dir/scylla.yaml
+        """
+        cs_custom_config = get_data_path('cassandra-stress-custom.yaml')
+        with open(cs_custom_config, 'r') as cs_custom_config_file:
+            self.log.info('Using custom cassandra-stress config:')
+            self.log.info(cs_custom_config_file.read())
+        for node in self.loaders.nodes:
+            node.remoter.send_files(cs_custom_config,
+                                    '/tmp/cassandra-stress-custom.yaml',
+                                    verbose=True)
+        ip = self.db_cluster.get_node_private_ips()[0]
+        cs_command = ('cassandra-stress user '
+                      'profile=/tmp/cassandra-stress-custom.yaml '
+                      'ops\(insert=1\) -node %s' % ip)
+        self.run_stress(stress_cmd=cs_command)
+
+
+if __name__ == '__main__':
+    main()
