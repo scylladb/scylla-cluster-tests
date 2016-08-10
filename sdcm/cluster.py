6a77e2ed1e539d6f1e2ae7961298ec034f379e88
Shlomi Livne <shlomi@scylladb.com>
2016-08-10 14:54:00 +0300

    Do not start multiple c-s at the same time
    To avoid schema management issues - we avoid starting multiple loaders
    c-s at the same time and force 30 seconds between them
    
    Signed-off-by: Shlomi Livne <shlomi@scylladb.com>

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -1113,6 +1113,7 @@ class BaseLoaderSet(object):
                                             args=(loader,))
             setup_thread.daemon = True
             setup_thread.start()
+            time.sleep(30)
 
         return queue
 
