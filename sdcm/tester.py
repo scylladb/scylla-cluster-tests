scylladb/scylla-cluster-tests

commit 576f28cc2e67cb4ab9b3c88ae444815584e3b59d

sdcm/test: use libvirt uri from configure file
Current code is using fixed libvirt uri ('qemu:///system'), this patch
changed the code to use uri from configure file.

Signed-off-by: Amos Kong <amos@scylladb.com>

sdcm/tester.py
@@ -274,7 +274,7 @@ class ClusterTester(Test):
 
         user_prefix = self.params.get('user_prefix', None)
 
-        hypervisor = libvirt.open('qemu:///system')
+        hypervisor = libvirt.open(self.params.get('libvirt_uri', 'qemu:///system'))
 
         if self.params.get('db_type') == 'scylla':
             self.db_cluster = ScyllaLibvirtCluster(domain_info=db_info,

commit bb7ae56e962a27e8720c821021aa11352333d3e7

sdcm.tester: Fix problem with libvirt uri
Getting from 'libvirt_uri' was being called in the
expectation that it's a default, and it's not (comes
from the multiplexer, which has a slightly different
interface).

The problem was introduced in 576f28cc2e67cb4ab9b3c88ae444815584e3b59d,
this patch fixes it.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@scylladb.com>

sdcm/tester.py
@@ -278,7 +278,10 @@ class ClusterTester(Test):
 
         user_prefix = self.params.get('user_prefix', None)
 
-        hypervisor = libvirt.open(self.params.get('libvirt_uri', 'qemu:///system'))
+        libvirt_uri = self.params.get('libvirt_uri')
+        if libvirt_uri is None:
+            libvirt_uri = 'qemu:///system'
+        hypervisor = libvirt.open(libvirt_uri)
 
         if self.params.get('db_type') == 'scylla':
             self.db_cluster = ScyllaLibvirtCluster(domain_info=db_info,