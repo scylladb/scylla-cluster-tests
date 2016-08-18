scylladb/scylla-cluster-tests

commit eafb16ff008eb5f957895130d3d0d3234db21d39

sdcm/cluster: use configurable libvirt uri to clean domain
Signed-off-by: Amos Kong <amos@scylladb.com>

sdcm/cluster.py
@@ -58,6 +58,7 @@ CREDENTIALS = []
 EC2_INSTANCES = []
 LIBVIRT_DOMAINS = []
 LIBVIRT_IMAGES = []
+LIBVIRT_URI = 'qemu:///system'
 DEFAULT_USER_PREFIX = getpass.getuser()
 # Test duration (min). Parameter used to keep instances produced by tests that
 # are supposed to run longer than 24 hours from being killed
@@ -70,10 +71,11 @@ def set_duration(duration):
 
 
 def clean_domain(domain_name):
-    process.run('virsh -c qemu:///system destroy %s' % domain_name,
+    global LIBVIRT_URI
+    process.run('virsh -c %s destroy %s' % domain_name, LIBVIRT_URI,
                 ignore_status=True)
 
-    process.run('virsh -c qemu:///system undefine %s' % domain_name,
+    process.run('virsh -c %s undefine %s' % domain_name, LIBVIRT_URI,
                 ignore_status=True)
 
 
@@ -1364,12 +1366,14 @@ class LibvirtCluster(BaseCluster):
         del user_data
         global LIBVIRT_DOMAINS
         global LIBVIRT_IMAGES
+        global LIBVIRT_URI
         nodes = []
         os_type = self._domain_info['os_type']
         os_variant = self._domain_info['os_variant']
         memory = self._domain_info['memory']
         bridge = self._domain_info['bridge']
         uri = self._domain_info['uri']
+        LIBVIRT_URI = uri
         image_parent_dir = os.path.dirname(self._domain_info['image'])
         for index in range(len(self.nodes), len(self.nodes) + count):
             index += 1

commit b3c2cff69764d340b94de0e850b05658e459a275

sdcm: Fix cleanup of libvirt domains
Broken with commit eafb16ff008eb5f957895130d3d0d3234db21d39.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@scylladb.com>

sdcm/cluster.py
@@ -70,12 +70,17 @@ def set_duration(duration):
     TEST_DURATION = duration
 
 
+def set_libvirt_uri(libvirt_uri):
+    global LIBVIRT_URI
+    LIBVIRT_URI = libvirt_uri
+
+
 def clean_domain(domain_name):
     global LIBVIRT_URI
-    process.run('virsh -c %s destroy %s' % domain_name, LIBVIRT_URI,
+    process.run('virsh -c %s destroy %s' % (LIBVIRT_URI, domain_name),
                 ignore_status=True)
 
-    process.run('virsh -c %s undefine %s' % domain_name, LIBVIRT_URI,
+    process.run('virsh -c %s undefine %s' % (LIBVIRT_URI, domain_name),
                 ignore_status=True)
 
 

sdcm/tester.py
@@ -282,6 +282,7 @@ class ClusterTester(Test):
         if libvirt_uri is None:
             libvirt_uri = 'qemu:///system'
         hypervisor = libvirt.open(libvirt_uri)
+        cluster.set_libvirt_uri(libvirt_uri)
 
         if self.params.get('db_type') == 'scylla':
             self.db_cluster = ScyllaLibvirtCluster(domain_info=db_info,