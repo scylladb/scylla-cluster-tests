970e51d57c113c40ccec288a562e70092c4cac4c
Amos Kong <amos@scylladb.com>
2016-12-22 07:33:53 +0800

    kill c_s_exporter to avoid SCT stuck when c-s finishes
    cassandra_stress_exporter keeps watching the pipe when c-s finished,
    and `tee` try to copy data from pipe and send back to SSH client,
    so the SSH command won't return, it caused SCT stuck. This patch
    kills c_s_exporter to release pipe, the regression bug is solved.
    
    https://github.com/scylladb/scylla-cluster-tests/issues/210
    
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -1433,7 +1433,7 @@ class BaseLoaderSet(object):
         self.log.debug('Setup duration -> %s s', int(time_elapsed))
 
     def run_stress_thread(self, stress_cmd, timeout, output_dir, stress_num=1):
-        stress_cmd = "mkfifo /tmp/cs_pipe_$1; cat /tmp/cs_pipe_$1|python /usr/bin/cassandra_stress_exporter & " + stress_cmd + "|tee /tmp/cs_pipe_$1"
+        stress_cmd = "mkfifo /tmp/cs_pipe_$1; cat /tmp/cs_pipe_$1|python /usr/bin/cassandra_stress_exporter & " + stress_cmd + "|tee /tmp/cs_pipe_$1; pkill -P $$ -f cassandra_stress_exporter; rm -f /tmp/cs_pipe_$1"
         # We'll save a script with the last c-s command executed on loaders
         stress_script = script.TemporaryScript(name='run_cassandra_stress.sh',
                                                content='%s\n' % stress_cmd)
