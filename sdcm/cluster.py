66516f716e17973763257044fdc1305e3fb6148f
Amos Kong <amos@scylladb.com>
2017-08-01 19:45:36 +0800

    disable set_tc by default
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -3113,7 +3113,8 @@ class ScyllaGCECluster(GCECluster, BaseScyllaCluster):
         for node in node_list:
             dst_nodes = [n for n in node_list if n.dc_idx != node.dc_idx]
             local_nodes = [n for n in node_list if n.dc_idx == node.dc_idx and n != node]
-            self.set_tc(node, dst_nodes, local_nodes)
+            if self.params.get('enable_tc').lower() == 'true':
+                self.set_tc(node, dst_nodes, local_nodes)
 
     def destroy(self):
         self.stop_nemesis()
