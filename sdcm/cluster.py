fab0df635220379d9391e86e7a83857697c58c22
Amos Kong <amos@scylladb.com>
2017-08-08 07:18:58 +0800

    fix for enable_tc lost in config
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -3113,7 +3113,8 @@ class ScyllaGCECluster(GCECluster, BaseScyllaCluster):
         for node in node_list:
             dst_nodes = [n for n in node_list if n.dc_idx != node.dc_idx]
             local_nodes = [n for n in node_list if n.dc_idx == node.dc_idx and n != node]
-            if self.params.get('enable_tc').lower() == 'true':
+            enable_tc = self.params.get('enable_tc')
+            if enable_tc and enable_tc.lower() == 'true':
                 self.set_tc(node, dst_nodes, local_nodes)
 
     def destroy(self):
