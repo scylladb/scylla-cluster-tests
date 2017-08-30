7660b5bc8f3966ae197c4318aa681fb6cbdbb980
Amos Kong <amos@scylladb.com>
2017-08-30 10:27:13 +0800

    longevity: start mixed load when write load finishes
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- longevity_test.py
+++ longevity_test.py
@@ -47,6 +47,7 @@ class LongevityTest(ClusterTester):
         if prepare_write_cmd:
             stress_queue = self.run_stress_thread(stress_cmd=prepare_write_cmd, stress_num=2)
             results = self.get_stress_results(queue=stress_queue, stress_num=2)
+            self.get_stress_results(queue=stress_queue, stress_num=2)
 
         for stress_cmd in self.params.get('stress_cmd'):
             params = {'stress_cmd': stress_cmd}
