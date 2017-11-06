88f7b57a7a060355a2a5311fa0222c279e3d2bc1
Roy Dahan <roy@scylladb.com>
2017-11-06 15:20:04 +0200

    Update longevity_test.py

--- longevity_test.py
+++ longevity_test.py
@@ -42,7 +42,7 @@ class LongevityTest(ClusterTester):
                                     test_id=self.test_id)
         stress_queue = list()
 
-        # prepare wirte workload
+        # prepare write workload
         prepare_write_cmd = self.params.get('prepare_write_cmd')
         if prepare_write_cmd:
             stress_queue = self.run_stress_thread(stress_cmd=prepare_write_cmd, stress_num=2)
