5af974829c1385a53e2ad7ce80cb2d40410b0937
Larisa Ustalov <larisau@scylladb.com>
2017-11-29 16:32:04 +0200

    wait function: changed to have an option to use it with timeout

--- sdcm/wait.py
+++ sdcm/wait.py
@@ -16,11 +16,46 @@ Wait functions appropriate for tests that have high timing variance.
 """
 import time
 import logging
+from avocado.utils import wait
 
-LOGGER = logging.getLogger('avocado.test')
+LOGGER = logging.getLogger('sdcm.wait')
 
 
-def wait_for(func, step=1, text=None, *args, **kwargs):
+class WaitTimeoutExpired(Exception):
+    pass
+
+
+def wait_for(func, step=1, text=None, timeout=None, throw_exc=False, **kwargs):
+    """
+    Wrapper function to wait with timeout option.
+    If timeout received, avocado 'wait_for' method will be used.
+    Otherwise the below function will be called.
+
+    :param func: Function to evaluate.
+    :param step: Time to sleep between attempts in seconds
+    :param text: Text to print while waiting, for debug purposes
+    :param timeout: Timeout in seconds
+    :param throw_exc: Raise exception if timeout expired, but func result is not True
+    :param kwargs: Keyword arguments to func
+    :return: Return value of func.
+    """
+    if not timeout:
+        return forever_wait_for(func, step, text, **kwargs)
+    if kwargs:
+        def func_wrap():
+            return func(**kwargs)
+        res = wait.wait_for(func_wrap, timeout=timeout, step=step, text=text)
+    else:
+        res = wait.wait_for(func, timeout=timeout, step=step, text=text)
+    if res is not True:
+        err = 'Wait for: {}: timeout - {} seconds - expired'.format(text, timeout)
+        LOGGER.warning(err)
+        if throw_exc:
+            raise WaitTimeoutExpired(err)
+    return res
+
+
+def forever_wait_for(func, step=1, text=None, **kwargs):
     """
     Wait indefinitely until func evaluates to True.
 
@@ -30,14 +65,13 @@ def wait_for(func, step=1, text=None, *args, **kwargs):
     :param func: Function to evaluate.
     :param step: Amount of time to sleep before another try.
     :param text: Text to log, for debugging purposes.
-    :param args: Positional arguments to func
     :param kwargs: Keyword arguments to func
     :return: Return value of func.
     """
     ok = False
     start_time = time.time()
     while not ok:
-        ok = func(*args, **kwargs)
+        ok = func(**kwargs)
         time.sleep(step)
         time_elapsed = time.time() - start_time
         if text is not None:
