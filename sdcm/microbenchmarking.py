edfa301bcf22d1a5b27a39cf0258efb0af3fa691
Bentsi Magidovich <bentsi@scylladb.com>
2018-06-20 21:17:11 +0300

    Set default debug level for the microbenchmarking to DEBUG

--- sdcm/microbenchmarking.py
+++ sdcm/microbenchmarking.py
@@ -12,10 +12,10 @@ from es import ES
 from results_analyze import BaseResultsAnalyzer
 
 logger = logging.getLogger("microbenchmarking")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.INFO)
+ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
