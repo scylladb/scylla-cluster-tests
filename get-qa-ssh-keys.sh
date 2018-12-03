1abfba04f1f0cae98d08e9e856b2fd17315e63c9
Bentsi Magidovich <bentsi@scylladb.com>
2018-12-03 18:10:40 +0200

    Using GCE from keystore: updated yaml, readme and keys retrieval script

--- get-qa-ssh-keys.sh
+++ get-qa-ssh-keys.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 set -e
 SSH_KEYS_DIR=~/.ssh
-QA_PRIVATE_KEY_NAME=scylla-qa-ec2
-QA_PRIVATE_KEY_PATH=${SSH_KEYS_DIR}/${QA_PRIVATE_KEY_NAME}
-
-if [ -e ${QA_PRIVATE_KEY_PATH} ]; then
-    echo "QA private key ${QA_PRIVATE_KEY_PATH} exists. Nothing to update."
-else
-    echo "QA private key ${QA_PRIVATE_KEY_PATH} doesn't exist. Retrieving..."
-    mkdir -p ${SSH_KEYS_DIR}
-    aws s3 cp s3://scylla-qa-keystore/${QA_PRIVATE_KEY_NAME} ${QA_PRIVATE_KEY_PATH}
-    chmod 600 ${QA_PRIVATE_KEY_PATH}
-    echo "Done."
-fi
+QA_PRIVATE_KEY_NAMES="scylla-qa-ec2 scylla-test"
+echo "Obtaining QA SSH keys..."
+for pkey_name in ${QA_PRIVATE_KEY_NAMES}
+do
+    QA_PRIVATE_KEY_PATH=${SSH_KEYS_DIR}/${pkey_name}
+    if [ -e ${QA_PRIVATE_KEY_PATH} ]; then
+        echo "QA private key '${QA_PRIVATE_KEY_PATH}' exists. Nothing to update."
+    else
+        echo "QA private key '${QA_PRIVATE_KEY_PATH}' doesn't exist. Retrieving..."
+        mkdir -p ${SSH_KEYS_DIR}
+        aws s3 cp s3://scylla-qa-keystore/${pkey_name} ${QA_PRIVATE_KEY_PATH}
+        chmod 600 ${QA_PRIVATE_KEY_PATH}
+        echo "Done."
+    fi
+done
+echo "QA SSH keys obtained."
