a06dba55b57a36b986363b0fb863dc338c62a8a4
Bentsi Magidovich <bentsi@scylladb.com>
2018-12-19 12:33:36 +0200

    Added support to add monitor root device name in test YAML

--- sdcm/tester.py
+++ sdcm/tester.py
@@ -409,15 +409,18 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
         if monitor_info['type'] is None:
             monitor_info['type'] = self.params.get('instance_type_monitor')
         if monitor_info['disk_size'] is None:
-            monitor_info['disk_size'] = self.params.get('aws_root_disk_size_monitor', default=10)
+            monitor_info['disk_size'] = self.params.get('aws_root_disk_size_monitor', default=None)
         if monitor_info['device_mappings'] is None:
-            monitor_info['device_mappings'] = [{
-                "DeviceName": "/dev/sda1",  # Root device
-                "Ebs": {
-                    "VolumeSize": monitor_info['disk_size'],
-                    "VolumeType": "gp2"
-                }
-            }]
+            if monitor_info['disk_size']:
+                monitor_info['device_mappings'] = [{
+                    "DeviceName": self.params.get("aws_root_disk_name_monitor", "/dev/sda1"),
+                    "Ebs": {
+                        "VolumeSize": monitor_info['disk_size'],
+                        "VolumeType": "gp2"
+                    }
+                }]
+            else:
+                monitor_info['device_mappings'] = []
         user_prefix = self.params.get('user_prefix', None)
 
         user_credentials = self.params.get('user_credentials_path', None)
