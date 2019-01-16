scylladb/scylla-cluster-tests

commit 48e77d5d0133ae085507c353fe4250692587360e

Fixing store_results_in_elasticsearch paramter for different stress results methods

sdcm/tester.py
@@ -168,6 +168,8 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
         self.connections = []
         logging.getLogger('botocore').setLevel(logging.CRITICAL)
         logging.getLogger('boto3').setLevel(logging.CRITICAL)
+        if self.create_stats:
+            self.create_test_stats()
         self.init_resources()
         self.db_cluster.wait_for_init()
         if self.cs_db_cluster:
@@ -179,8 +181,6 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
         # cancel reuse cluster - for new nodes added during the test
         cluster.Setup.reuse_cluster(False)
 
-        if self.create_stats:
-            self.create_test_stats()
 
     def get_nemesis_class(self):
         """
@@ -197,7 +197,7 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
         Allow access db stats object to external objects like Nemesis - for updates
         :return: db_stats.Stats object or None if param store_results_in_elasticsearch set to False in yaml.
         """
-        if self.params.get(key='store_results_in_elasticsearch',default=True):
+        if self.create_stats:
             return db_stats.Stats(test_index=self._test_index,
                                   test_id=self._test_id)
 
@@ -668,12 +668,13 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
 
     @clean_resources_on_exception
     def run_stress_thread(self, stress_cmd, duration=None, stress_num=1, keyspace_num=1, profile=None, prefix='',
-                          keyspace_name='', round_robin=False):
+                          keyspace_name='', round_robin=False, stats_aggregate_cmds=True):
         # stress_cmd = self._cs_add_node_flag(stress_cmd)
         if duration is None:
             duration = self.params.get('test_duration')
         timeout = duration * 60 + 600
-        self.update_stress_cmd_details(stress_cmd, prefix)
+        if self.create_stats:
+            self.update_stress_cmd_details(stress_cmd, prefix, stresser="cassandra-stress", aggregate=stats_aggregate_cmds)
         return self.loaders.run_stress_thread(stress_cmd, timeout,
                                               self.outputdir,
                                               stress_num=stress_num,
@@ -684,11 +685,12 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
                                               round_robin=round_robin)
 
     @clean_resources_on_exception
-    def run_stress_thread_bench(self, stress_cmd, duration=None):
+    def run_stress_thread_bench(self, stress_cmd, duration=None, stats_aggregate_cmds=True):
         if duration is None:
             duration = self.params.get('test_duration')
         timeout = duration * 60 + 600
-        self.update_bench_stress_cmd_details(stress_cmd)
+        if self.create_stats:
+            self.update_stress_cmd_details(stress_cmd, stresser="scylla-bench", aggregate=stats_aggregate_cmds)
         return self.loaders.run_stress_thread_bench(stress_cmd, timeout,
                                               self.outputdir,
                                               node_list=self.db_cluster.nodes)
@@ -707,7 +709,7 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
         # to run out of memory when writing the XML report. Since
         # the error message is merely informational, let's simply
         # use the last 5 lines for the final error message.
-        if results:
+        if results and self.create_stats:
             self.update_stress_results(results)
         else:
             self.log.warning('There is no stress results, probably stress thread has failed.')
@@ -719,14 +721,15 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
     @clean_resources_on_exception
     def get_stress_results(self, queue, store_results=True):
         results = self.loaders.get_stress_results(queue)
-        if store_results:
+        if store_results and self.create_stats:
             self.update_stress_results(results)
         return results
 
     @clean_resources_on_exception
     def get_stress_results_bench(self, queue):
         results = self.loaders.get_stress_results_bench(queue)
-        self.update_stress_results(results)
+        if self.create_stats:
+            self.update_stress_results(results)
         return results
 
     def get_auth_provider(self, user, password):
@@ -1037,8 +1040,7 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
                     cr.destroy()
                 self.credentials = []
 
-        if self.create_stats:
-            self.update_test_details(db_cluster_errors, db_cluster_coredumps)
+        self.update_test_details(db_cluster_errors, db_cluster_coredumps)
 
         if db_cluster_coredumps:
             self.fail('Found coredumps on DB cluster nodes: %s' %

performance_regression_test.py
@@ -37,7 +37,6 @@ class PerformanceRegressionTest(ClusterTester):
 
     def __init__(self, *args, **kwargs):
         super(PerformanceRegressionTest, self).__init__(*args, **kwargs)
-        self.create_stats = False
 
     def display_single_result(self, result):
         self.log.info(self.str_pattern % (result['op rate'],
@@ -151,7 +150,7 @@ class PerformanceRegressionTest(ClusterTester):
         if save_stats:
             self.create_test_stats(sub_type=sub_type)
         stress_queue = self.run_stress_thread(stress_cmd=stress_cmd, stress_num=stress_num, keyspace_num=keyspace_num,
-                                              prefix=prefix)
+                                              prefix=prefix, stats_aggregate_cmds=False)
         results = self.get_stress_results(queue=stress_queue, store_results=True)
         if save_stats:
             self.update_test_details(scylla_conf=True)
@@ -212,7 +211,8 @@ class PerformanceRegressionTest(ClusterTester):
         base_cmd_w = self.params.get('stress_cmd_w')
         self.create_test_stats()
         # run a workload
-        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_w, stress_num=2, keyspace_num=1)
+        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_w, stress_num=2, keyspace_num=1,
+                                              stats_aggregate_cmds=False)
         results = self.get_stress_results(queue=stress_queue)
 
         self.update_test_details(scylla_conf=True)
@@ -278,13 +278,14 @@ class PerformanceRegressionTest(ClusterTester):
 
         self.create_test_stats(sub_type='write-prepare')
         # run a write workload
-        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_w, stress_num=2, prefix='preload-')
+        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_w, stress_num=2, prefix='preload-',
+                                              stats_aggregate_cmds=False)
         self.get_stress_results(queue=stress_queue, store_results=False)
         self.update_test_details()
 
         # run a read workload
         self.create_test_stats()
-        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_r, stress_num=2)
+        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_r, stress_num=2, stats_aggregate_cmds=False)
         results = self.get_stress_results(queue=stress_queue)
 
         self.update_test_details(scylla_conf=True)
@@ -344,13 +345,14 @@ class PerformanceRegressionTest(ClusterTester):
 
         self.create_test_stats(sub_type='write-prepare')
         # run a write workload as a preparation
-        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_w, stress_num=2, prefix='preload-')
+        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_w, stress_num=2, prefix='preload-',
+                                              stats_aggregate_cmds=False)
         self.get_stress_results(queue=stress_queue, store_results=False)
         self.update_test_details()
 
         # run a mixed workload
         self.create_test_stats()
-        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_m, stress_num=2)
+        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_m, stress_num=2, stats_aggregate_cmds=False)
         results = self.get_stress_results(queue=stress_queue)
 
         self.update_test_details(scylla_conf=True)
@@ -388,13 +390,14 @@ class PerformanceRegressionTest(ClusterTester):
                     params.update({'stress_cmd': stress_cmd})
 
                     # Run all stress commands
+                    params.update(dict(stats_aggregate_cmds=False))
                     self.log.debug('RUNNING stress cmd: {}'.format(stress_cmd))
                     stress_queue.append(self.run_stress_thread(**params))
 
             # One stress cmd command
             else:
                     stress_queue.append(self.run_stress_thread(stress_cmd=prepare_write_cmd, stress_num=1,
-                                                               prefix='preload-'))
+                                                               prefix='preload-', stats_aggregate_cmds=False))
 
             for stress in stress_queue:
                 self.get_stress_results(queue=stress, store_results=False)
@@ -405,7 +408,7 @@ class PerformanceRegressionTest(ClusterTester):
 
         # Run WRITE workload
         self.create_test_stats(sub_type='write')
-        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_w, stress_num=1)
+        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_w, stress_num=1, stats_aggregate_cmds=False)
         results = self.get_stress_results(queue=stress_queue)
         self.update_test_details()
         # TEMP check if possible
@@ -416,7 +419,7 @@ class PerformanceRegressionTest(ClusterTester):
 
         # Run READ workload
         self.create_test_stats(sub_type='read')
-        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_r, stress_num=1)
+        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_r, stress_num=1, stats_aggregate_cmds=False)
         results = self.get_stress_results(queue=stress_queue)
         self.update_test_details()
         self.display_results(results, test_name='test_latency')
@@ -426,7 +429,7 @@ class PerformanceRegressionTest(ClusterTester):
 
         # run MIXED workload
         self.create_test_stats(sub_type='mixed')
-        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_m, stress_num=1)
+        stress_queue = self.run_stress_thread(stress_cmd=base_cmd_m, stress_num=1, stats_aggregate_cmds=False)
         results = self.get_stress_results(queue=stress_queue)
         self.update_test_details(scylla_conf=True)
         self.display_results(results, test_name='test_latency')
@@ -443,7 +446,7 @@ class PerformanceRegressionTest(ClusterTester):
                       "32 -concurrency 512 -replication-factor 3")
 
         self.create_test_stats()
-        stress_queue = self.run_stress_thread_bench(stress_cmd=base_cmd_r)
+        stress_queue = self.run_stress_thread_bench(stress_cmd=base_cmd_r, stats_aggregate_cmds=False)
         results = self.get_stress_results_bench(queue=stress_queue)
 
         self.update_test_details(scylla_conf=True)
@@ -489,7 +492,7 @@ class PerformanceRegressionTest(ClusterTester):
 
         self.create_test_stats(sub_type='write')
         self._scylla_bench_prepare_table()
-        self.run_stress_thread_bench(stress_cmd=cmd_w)
+        self.run_stress_thread_bench(stress_cmd=cmd_w, stats_aggregate_cmds=False)
         start_timestamp = int(time.time())
         self.db_cluster.wait_total_space_used_per_node(700 * KB * KB * KB, 'scylla_bench.test')  # 700GB
 
@@ -498,7 +501,7 @@ class PerformanceRegressionTest(ClusterTester):
                  "-rows-per-request=1000000 -no-lower-bound -start-timestamp=%s -duration=60m" % start_timestamp)
 
         self.create_test_stats(sub_type='read')
-        stress_queue = self.run_stress_thread_bench(stress_cmd=cmd_r)
+        stress_queue = self.run_stress_thread_bench(stress_cmd=cmd_r, stats_aggregate_cmds=False)
         results = self.get_stress_results_bench(queue=stress_queue)
         self.update_test_details()
         self.display_results(results, test_name='test_timeseries_read_bench')
@@ -519,7 +522,8 @@ class PerformanceRegressionTest(ClusterTester):
             self.log.debug('Run stress test with user profile {}'.format(user_profile))
             assert os.path.exists(user_profile), 'File not found: {}'.format(user_profile)
             self.log.debug('Stress cmd: {}'.format(stress_cmd))
-            stress_queue = self.run_stress_thread(stress_cmd=stress_cmd, stress_num=1, profile=user_profile)
+            stress_queue = self.run_stress_thread(stress_cmd=stress_cmd, stress_num=1, profile=user_profile,
+                                                  stats_aggregate_cmds=False)
             results = self.get_stress_results(queue=stress_queue)
             self.update_test_details(scylla_conf=True)
             self.display_results(results, test_name=test_name)

sdcm/db_stats.py
@@ -202,7 +202,7 @@ class Stats(object):
         if not self._test_id:
             super(Stats, self).__init__(*args, **kwargs)
 
-    def _create(self):
+    def create(self):
         es.ES().create(self._test_index, ES_DOC_TYPE, self._test_id, self._stats)
 
     def update(self, data):
@@ -288,6 +288,7 @@ class TestStatsMixin(Stats):
         test_details['test_duration'] = self.params.get(key='test_duration', default=60)
         test_details['start_time'] = time.time()
         test_details['grafana_snapshot'] = ""
+        test_details['prometheus_report'] = ""
         return test_details
 
     def create_test_stats(self, sub_type=None):
@@ -302,25 +303,21 @@ class TestStatsMixin(Stats):
         else:
             self._stats['test_details']['test_name'] = self.params.id.name
         self._stats['results']['throughput'] = defaultdict(dict)
-        self._create()
+        self.create()
 
-    def update_stress_cmd_details(self, cmd, prefix=''):
-        section = '{0}cassandra-stress'.format(prefix)
+    def update_stress_cmd_details(self, cmd, prefix='', stresser="cassandra-stress", aggregate=True):
+        section = '{prefix}{stresser}'.format(**locals())
         if section not in self._stats['test_details']:
-            self._stats['test_details'][section] = [] if self.create_stats else {}
-        cmd_params = get_stress_cmd_params(cmd)
+            self._stats['test_details'][section] = [] if aggregate else {}
+        if stresser == "cassandra-stress":
+            cmd_params = get_stress_cmd_params(cmd)
+        elif stresser == "scylla-bench":
+            cmd_params = get_stress_bench_cmd_params(cmd)
+        else:
+            cmd_params = None
+            self.log.warning("Unknown stresser: %s" % stresser)
         if cmd_params:
-            self._stats['test_details'][section].append(cmd_params) if self.create_stats else\
-                self._stats['test_details'][section].update(cmd_params)
-            self.update(dict(test_details=self._stats['test_details']))
-
-    def update_bench_stress_cmd_details(self, cmd, prefix=''):
-        section = '{0}scylla-bench'.format(prefix)
-        if section not in self._stats['test_details']:
-            self._stats['test_details'][section] = [] if self.create_stats else {}
-        cmd_params = get_stress_bench_cmd_params(cmd)
-        if cmd_params:
-            self._stats['test_details'][section].append(cmd_params) if self.create_stats else\
+            self._stats['test_details'][section].append(cmd_params) if aggregate else\
                 self._stats['test_details'][section].update(cmd_params)
             self.update(dict(test_details=self._stats['test_details']))
 
@@ -385,30 +382,31 @@ class TestStatsMixin(Stats):
             self._stats['results']['stats_total'] = total_stats
 
     def update_test_details(self, errors=None, coredumps=None, scylla_conf=False):
-        self._stats['test_details']['time_completed'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        if self.monitors:
-            test_start_time = self._stats['test_details']['start_time']
-            self.get_scylla_throughput()
-            self._stats['test_details']['grafana_snapshot'] = self.monitors.get_grafana_screenshot(test_start_time)
-            self._stats['test_details']['prometheus_report'] = self.monitors.download_monitor_data()
+        if self.create_stats:
+            self._stats['test_details']['time_completed'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+            if self.monitors:
+                test_start_time = self._stats['test_details']['start_time']
+                self.get_scylla_throughput()
+                self._stats['test_details']['grafana_snapshot'] = self.monitors.get_grafana_screenshot(test_start_time)
+                self._stats['test_details']['prometheus_report'] = self.monitors.download_monitor_data()
 
-        if scylla_conf and 'scylla_args' not in self._stats['test_details'].keys():
-            node = self.db_cluster.nodes[0]
-            res = node.remoter.run('grep ^SCYLLA_ARGS /etc/sysconfig/scylla-server', verbose=True)
-            self._stats['test_details']['scylla_args'] = res.stdout.strip()
-            res = node.remoter.run('cat /etc/scylla.d/io.conf', verbose=True)
-            self._stats['test_details']['io_conf'] = res.stdout.strip()
-            res = node.remoter.run('cat /etc/scylla.d/cpuset.conf', verbose=True)
-            self._stats['test_details']['cpuset_conf'] = res.stdout.strip()
+            if self.db_cluster and scylla_conf and 'scylla_args' not in self._stats['test_details'].keys():
+                node = self.db_cluster.nodes[0]
+                res = node.remoter.run('grep ^SCYLLA_ARGS /etc/sysconfig/scylla-server', verbose=True)
+                self._stats['test_details']['scylla_args'] = res.stdout.strip()
+                res = node.remoter.run('cat /etc/scylla.d/io.conf', verbose=True)
+                self._stats['test_details']['io_conf'] = res.stdout.strip()
+                res = node.remoter.run('cat /etc/scylla.d/cpuset.conf', verbose=True)
+                self._stats['test_details']['cpuset_conf'] = res.stdout.strip()
 
-        self._stats['status'] = self.status
-        update_data = {'status': self._stats['status'], 'test_details': self._stats['test_details'],
-                       'results': {'throughput': self._stats['results']['throughput']}}
-        if errors:
-            update_data.update({'errors': errors})
-        if coredumps:
-            update_data.update({'coredumps': coredumps})
-        self.update(update_data)
+            self._stats['status'] = self.status
+            update_data = {'status': self._stats['status'], 'test_details': self._stats['test_details'],
+                           'results': {'throughput': self._stats['results']['throughput']}}
+            if errors:
+                update_data.update({'errors': errors})
+            if coredumps:
+                update_data.update({'coredumps': coredumps})
+            self.update(update_data)
 
     def check_regression(self):
         ra = PerformanceResultsAnalyzer(es_index=self._test_index, es_doc_type=ES_DOC_TYPE,

commit fec62b2eb544843d030156cc4b394ceb1ae273ce

verify_stress_thread: only warn when results is empty
If we disable store_results_in_elasticsearch, we can always see warning:

 WARNI| There is no stress results, probably stress thread has failed.

The problem was introduced by 48e77d5d0133ae085507c353fe4250692587360e

Signed-off-by: Amos Kong <amos@scylladb.com>

sdcm/tester.py
@@ -719,7 +719,7 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
         # use the last 5 lines for the final error message.
         if results and self.create_stats:
             self.update_stress_results(results)
-        else:
+        if not results:
             self.log.warning('There is no stress results, probably stress thread has failed.')
         errors = errors[-5:]
         if errors: