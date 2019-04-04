e2121e5e54879fd371077d1d7e57552f8827bc47
ALeksandr Bykov <alex.bykov@scylladb.com>
2019-04-04 18:09:05 +0700

    Add shared snapshots for grafana
    Save and store on external service interactive grafana snapshots

--- data_dir/share_snapshot.js
+++ data_dir/share_snapshot.js
@@ -10,39 +10,39 @@ page.viewportSize = { width: 1920, height: 1080 };
 var address = system.args[1];
 
 page.open(address, function(status) {
-	if (status !== "success") {
-		console.log("Error to load page " + address);
-		phantom.exit();
-	}
-	window.setTimeout(function () {
-		page.evaluate(function() {
-			l = document.getElementsByClassName("fa-share-square-o");
-			var ev = document.createEvent("MouseEvents");
-        	ev.initEvent("click", true, true);
-        	l[0].parentElement.dispatchEvent(ev);
-		});
+    if (status !== "success") {
+        console.log("Error to load page " + address);
+        phantom.exit();
+    }
+    window.setTimeout(function () {
+        page.evaluate(function() {
+            l = document.getElementsByClassName("fa-share-square-o");
+            var ev = document.createEvent("MouseEvents");
+            ev.initEvent("click", true, true);
+            l[0].parentElement.dispatchEvent(ev);
+        });
     }, 15000);
-	window.setTimeout(function() {
-		page.evaluate(function() {
-			l = document.getElementsByClassName("icon-gf-snapshot");
-			var ev = document.createEvent("MouseEvents");
-        	ev.initEvent("click", true, true);
-        	l[0].parentElement.dispatchEvent(ev);
-		});
-	}, 20000);
-	window.setTimeout(function() {
-		page.evaluate(function() {
-			l = document.getElementsByClassName("fa-cloud-upload");
-			var ev = document.createEvent("MouseEvents");
-        	ev.initEvent("click", true, true);
-        	l[1].dispatchEvent(ev);
-		});
-	}, 25000);
-	window.setTimeout(function() {
-		page.evaluate(function() {
-			l = document.getElementsByClassName("fa-external-link-square");
-        	console.log(l[0].parentElement.lastChild.nodeValue.trim());
-		});
-		phantom.exit();
-	}, 60000);
+    window.setTimeout(function() {
+        page.evaluate(function() {
+            l = document.getElementsByClassName("icon-gf-snapshot");
+            var ev = document.createEvent("MouseEvents");
+            ev.initEvent("click", true, true);
+            l[0].parentElement.dispatchEvent(ev);
+        });
+    }, 20000);
+    window.setTimeout(function() {
+        page.evaluate(function() {
+            l = document.getElementsByClassName("fa-cloud-upload");
+            var ev = document.createEvent("MouseEvents");
+            ev.initEvent("click", true, true);
+            l[1].dispatchEvent(ev);
+        });
+    }, 25000);
+    window.setTimeout(function() {
+        page.evaluate(function() {
+            l = document.getElementsByClassName("fa-external-link-square");
+            console.log(l[0].parentElement.lastChild.nodeValue.trim());
+        });
+        phantom.exit();
+    }, 60000);
 });
