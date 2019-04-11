scylladb/scylla-cluster-tests

commit dd716c863ad3cf4cba1899a3ec3f500ec6150198

Remove the avocado.utils.process from code
remove process.run and process.CmdError

next clusters are still use avocado.util.process
- libvirt
- docker

sdcm/nemesis.py
@@ -24,8 +24,9 @@ import string
 import threading
 import re
 
-from avocado.utils import process
 from cassandra import InvalidRequest
+
+from invoke.exceptions import UnexpectedExit, Failure
 
 from sdcm.cluster_aws import ScyllaAWSCluster
 from sdcm.cluster import SCYLLA_YAML_PATH, NodeSetupTimeout, NodeSetupFailed, Setup
@@ -142,7 +143,7 @@ class Nemesis(object):
             self.log.debug("Command '%s' duration -> %s s", result.command,
                            result.duration)
             return result
-        except process.CmdError, details:
+        except (UnexpectedExit, Failure):
             err = ("nodetool command '%s' failed on node %s: %s" %
                    (cmd, node, details.result))
             self.error_list.append(err)

performance_regression_in_memory_test.py
@@ -1,4 +1,4 @@
-from avocado.utils.process import CmdError
+from invoke.exceptions import UnexpectedExit, Failure
 
 from performance_regression_test import PerformanceRegressionTest
 from sdcm.utils import log_run_info, retrying
@@ -16,7 +16,7 @@ class InMemoryPerformanceRegressionTest(PerformanceRegressionTest):
         super(InMemoryPerformanceRegressionTest, self).__init__(*args, **kwargs)
 
     @log_run_info
-    @retrying(n=3, sleep_time=15, allowed_exceptions=(CmdError,))  # retrying since SSH can fail with 255
+    @retrying(n=3, sleep_time=15, allowed_exceptions=(UnexpectedExit, Failure))  # retrying since SSH can fail with 255
     def run_compaction_on_all_nodes(self):
         for node in self.db_cluster.nodes:
             node.run_nodetool("compact")

sdcm/cluster.py
@@ -31,18 +31,19 @@ from sdcm.mgmt import ScyllaManagerError
 from sdcm.prometheus import start_metrics_server
 from textwrap import dedent
 from avocado.utils import path
-from avocado.utils import process
 from avocado.utils import script
 from datetime import datetime
 from .log import SDCMAdapter
-from .remote import Remote, RemoteFabric
-from .remote import disable_master_ssh
+from .remote import RemoteCmdRunner, LocalCmdRunner
 from . import wait
 from .utils import log_run_info, retrying, get_data_dir_path, Distro, verify_scylla_repo_file, S3Storage
 from .loader import CassandraStressExporterSetup
 from .db_stats import PrometheusDBStats
 
 from avocado.utils import runtime as avocado_runtime
+
+from invoke.exceptions import UnexpectedExit, Failure
+
 
 SCYLLA_CLUSTER_DEVICE_MAPPINGS = [{"DeviceName": "/dev/xvdb",
                                    "Ebs": {"VolumeSize": 40,
@@ -74,6 +75,7 @@ SCYLLA_DIR = "/var/lib/scylla"
 
 
 logger = logging.getLogger(__name__)
+localrunner = LocalCmdRunner()
 
 
 def set_ip_ssh_connections(ip_type):
@@ -84,8 +86,6 @@ def set_ip_ssh_connections(ip_type):
 def set_duration(duration):
     global TEST_DURATION
     TEST_DURATION = duration
-    if TEST_DURATION >= 3 * 60:
-        disable_master_ssh()
 
 
 def set_libvirt_uri(libvirt_uri):
@@ -95,11 +95,11 @@ def set_libvirt_uri(libvirt_uri):
 
 def clean_domain(domain_name):
     global LIBVIRT_URI
-    process.run('virsh -c %s destroy %s' % (LIBVIRT_URI, domain_name),
-                ignore_status=True)
+    localrunner.run('virsh -c %s destroy %s' % (LIBVIRT_URI, domain_name),
+                       ignore_status=True)
 
-    process.run('virsh -c %s undefine %s' % (LIBVIRT_URI, domain_name),
-                ignore_status=True)
+    localrunner.run('virsh -c %s undefine %s' % (LIBVIRT_URI, domain_name),
+                       ignore_status=True)
 
 
 def remove_if_exists(file_path):
@@ -161,7 +161,6 @@ class Setup(object):
     REUSE_CLUSTER = False
     MIXED_CLUSTER = False
     MULTI_REGION = False
-    REMOTE = Remote
 
     _test_id = None
 
@@ -200,11 +199,6 @@ class Setup(object):
     @classmethod
     def tags(cls, key, value):
         cls.TAGS[key] = value
-
-    @classmethod
-    def set_remote_runner(cls, val='Remote'):
-        if 'fabric' in val.lower():
-            cls.REMOTE = RemoteFabric
 
 
 def create_common_tags():
@@ -320,7 +314,7 @@ class BaseNode(object):
         self._private_ip_address = None
         ssh_login_info['hostname'] = self.ssh_address
 
-        self.remoter = Setup.REMOTE(**ssh_login_info)
+        self.remoter = RemoteCmdRunner(**ssh_login_info)
         self._ssh_login_info = ssh_login_info
         logger = logging.getLogger('avocado.test')
         self.log = SDCMAdapter(logger, extra={'prefix': str(self)})
@@ -610,7 +604,7 @@ class BaseNode(object):
         Get coredump backtraces.
 
         :param last: Whether to only show the last backtrace.
-        :return: process.CmdResult output
+        :return: fabric.Result output
         """
         try:
             backtrace_cmd = 'sudo coredumpctl info'
@@ -911,7 +905,7 @@ class BaseNode(object):
         wait.wait_for(keyspace_available, step=60, text='Waiting until keyspace {} is available'.format(keyspace))
         try:
             result = self.remoter.run('nodetool cfstats {}'.format(keyspace))
-        except process.CmdError:
+        except (Failure, UnexpectedExit):
             self.log.error('nodetool error - see tcpdump thread uuid %s for '
                            'debugging info', tcpdump_id)
             raise
@@ -2917,7 +2911,7 @@ class BaseLoaderSet(object):
                 if loader_node.termination_event.isSet():
                     break
                 db_node = random.choice(db_nodes)
-                if db_node.remoter and not db_node.remoter.is_up():
+                if hasattr(db_node, 'remoter') and db_node.remoter and not db_node.remoter.is_up():
                     self.log.warning("Chosen node is down. Choose new one")
                     continue
                 self.log.info('Fullscan start on node {} from loader {}'.format(db_node.name, loader_node.name))
@@ -2984,7 +2978,7 @@ class BaseMonitorSet(object):
         self.configure_scylla_monitoring(node)
         try:
             self.start_scylla_monitoring(node)
-        except process.CmdError:
+        except (Failure, UnexpectedExit):
             self.reconfigure_scylla_monitoring()
         # The time will be used in url of Grafana monitor,
         # the data from this point to the end of test will
@@ -3082,7 +3076,7 @@ class BaseMonitorSet(object):
                 yaml.safe_dump(templ_yaml, fo, default_flow_style=False)  # to remove tag !!python/unicode
             node.remoter.run("sudo chmod 777 %s" % prometheus_yaml_template)
             node.send_files(src=local_template, dst=prometheus_yaml_template, delete_dst=True, verbose=True)
-            process.run("rm -rf {temp_dir}".format(**locals()))
+            localrunner.run("rm -rf {temp_dir}".format(**locals()))
         self._monitoring_targets = " ".join(db_targets_list)
         configure_script = dedent("""
             cd {0.monitor_install_path}
@@ -3121,7 +3115,7 @@ class BaseMonitorSet(object):
             node.remoter.run("chmod 777 %s" % f.name)
             node.remoter.run("sudo bash -ce 'cat %s >> %s'" % (f.name, alertmanager_conf_file))
 
-    @retrying(n=5, sleep_time=10, allowed_exceptions=(process.CmdError,),
+    @retrying(n=5, sleep_time=10, allowed_exceptions=(Failure, UnexpectedExit),
               message="Waiting for reconfiguring scylla monitoring")
     def reconfigure_scylla_monitoring(self):
         for node in self.nodes:
@@ -3156,9 +3150,9 @@ class BaseMonitorSet(object):
         def _register_grafana_json(json_filename):
             url = "http://{0.public_ip_address}:{1.grafana_port}/api/dashboards/db".format(node, self)
             json_path = get_data_dir_path(json_filename)
-            result = process.run('curl -XPOST -i %s --data-binary @%s -H "Content-Type: application/json"' %
-                                 (url, json_path), ignore_status=True)
-            return result.exit_status == 0
+            result = localrunner.run('curl -XPOST -i %s --data-binary @%s -H "Content-Type: application/json"' %
+                                     (url, json_path))
+            return result.exited == 0
 
         wait.wait_for(_register_grafana_json, step=10,
                       text="Waiting to register 'data_dir/%s'..." % sct_dashboard_json,
@@ -3195,10 +3189,10 @@ class BaseMonitorSet(object):
                 curl {phantomjs_url} -o {phantomjs_tar} -L
                 tar xvfj {phantomjs_tar}
             """.format(**locals()))
-            process.run("bash -ce '%s'" % install_phantom_js_script)
-            process.run("cd phantomjs-2.1.1-linux-x86_64 && "
-                        "sed -e 's/200);/10000);/' examples/rasterize.js |grep -v 'use strict' > r.js",
-                        shell=True)
+            localrunner.run("bash -ce '%s'" % install_phantom_js_script)
+            localrunner.run("cd phantomjs-2.1.1-linux-x86_64 && "
+                            "sed -e 's/200);/10000);/' examples/rasterize.js |grep -v 'use strict' > r.js",
+                            shell=True)
             self.phantomjs_installed = True
         else:
             self.log.debug("PhantomJS is already installed!")
@@ -3235,9 +3229,8 @@ class BaseMonitorSet(object):
                     datetime_now = datetime.now().strftime("%Y%m%d_%H%M%S")
                     snapshot_path = os.path.join(node.logdir,
                                                  "grafana-screenshot-%s-%s-%s.png" % (name, datetime_now, n))
-                    process.run("cd phantomjs-2.1.1-linux-x86_64 && "
-                                "bin/phantomjs r.js \"%s\" \"%s\" 1920px" % (
-                                    grafana_url, snapshot_path), shell=True)
+                    localrunner.run("cd phantomjs-2.1.1-linux-x86_64 && "
+                                    "bin/phantomjs r.js \"%s\" \"%s\" 1920px" % (grafana_url, snapshot_path), shell=True)
                 # since there is only one monitoring node returning here
                     screenshots.append(S3Storage.upload_file(snapshot_path, prefix=Setup.test_id()))
                 return screenshots

sdcm/docker.py
@@ -2,12 +2,13 @@ import json
 import time
 import logging
 import os
-from avocado.utils import process
 
 import cluster
 from sdcm.utils import retrying
+from remote import LocalCmdRunner
 
 logger = logging.getLogger(__name__)
+localrunner = LocalCmdRunner()
 
 BASE_NAME = 'db-node'
 LOADER_NAME = 'loader-node'
@@ -31,7 +32,7 @@ class CannotFindContainers(Exception):
 
 
 def _cmd(cmd, timeout=10, sudo=False):
-    res = process.run('docker {}'.format(cmd), ignore_status=True, timeout=timeout, sudo=sudo)
+    res = localrunner.run('docker {}'.format(cmd), ignore_status=True, timeout=timeout, sudo=sudo)
     if res.exit_status:
         if 'No such container:' in res.stderr:
             raise DockerContainerNotExists(res.stderr)

sdcm/mgmt.py
@@ -2,7 +2,8 @@
 from enum import Enum
 from textwrap import dedent
 
-from avocado.utils import process
+from invoke.exceptions import UnexpectedExit, Failure
+
 import requests
 import logging
 import json
@@ -625,7 +626,7 @@ class SCTool(object):
         logger.debug("Issuing: 'sctool {}'".format(cmd))
         try:
             res = self._remoter_run(cmd='sudo sctool {}'.format(cmd))
-        except process.CmdError as e:
+        except (UnexpectedExit, Failure) as e:
             raise ScyllaManagerError("Encountered an error on sctool command: {}: {}".format(cmd, e))
 
         if is_verify_errorless_result:

sdcm/sct_config.py
@@ -129,14 +129,6 @@ class SCTConfiguration(dict):
                 Default: Use public IPs to connect to instances (public)
                 Use private IPs to connect to instances (private)
              """),
-        dict(name="ssh_remote", env="SCT_SSH_REMOTE", default=None, type=str, required=False,
-             help="""
-             Define which module to use to connect and execute commands on remote unit.
-
-             In transition period to values are allowed:
-             'RemoteFabric' - define and use the fabric module to connect and execute command on remote host
-             ''(empty, not set) - use old functionality SSHSubprocess to connect and execute command on remote ost
-             """),
 
         dict(name="scylla_repo", env="SCT_SCYLLA_REPO",
              default=None, type=str,

sdcm/tester.py
@@ -21,7 +21,6 @@ import boto3.session
 import libvirt
 import shutil
 from avocado import Test
-from avocado.utils.process import CmdError
 from cassandra import ConsistencyLevel
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import Cluster as ClusterDriver
@@ -57,6 +56,7 @@ from . import db_stats
 from db_stats import PrometheusDBStats
 from results_analyze import PerformanceResultsAnalyzer
 from sdcm.sct_config import SCTConfiguration
+from invoke.exceptions import UnexpectedExit, Failure
 
 try:
     from botocore.vendored.requests.packages.urllib3.contrib.pyopenssl import extract_from_urllib3
@@ -171,7 +171,6 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
         elif cluster_backend == 'gce':
             cluster.Setup.set_multi_region(len(self.params.get('gce_datacenter').split()) > 1)
 
-        cluster.Setup.set_remote_runner(self.params.get('ssh_remote', default='Remote'))
 
         version_tag = self.params.get('version_tag')
         if version_tag:
@@ -1289,7 +1288,7 @@ class ClusterTester(db_stats.TestStatsMixin, Test):
         # if all are zeros the result will be False, otherwise we are still sending
         return any([float(v[1]) for v in results[0]["values"]])
 
-    @retrying(n=30, sleep_time=60, allowed_exceptions=(AssertionError, CmdError))
+    @retrying(n=30, sleep_time=60, allowed_exceptions=(AssertionError, UnexpectedExit, Failure))
     def wait_for_hints_to_be_sent(self, node, num_dest_nodes):
         num_shards = self.get_num_shards(node)
         hints_after_send_completed = num_shards * num_dest_nodes

commit 6d0b3f22c4b6b71c84386b19387a1d2d373507b9

_run_nodetool: fix NameError
NameError: global name 'details' is not defined
This issue was introduced by commit dd716c863ad3cf4cba1899a3ec3f500ec6150198

Signed-off-by: Amos Kong <amos@scylladb.com>

sdcm/nemesis.py
@@ -144,6 +144,7 @@ class Nemesis(object):
                            result.duration)
             return result
         except (UnexpectedExit, Failure):
+        except (UnexpectedExit, Failure) as details:
             err = ("nodetool command '%s' failed on node %s: %s" %
                    (cmd, node, details.result))
             self.error_list.append(err)