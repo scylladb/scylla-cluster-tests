930ba88e4a7e48e276977d6057cda14977970f89
Israel Fruchter <fruch@scylladb.com>
2019-06-06 15:01:08 +0300

    Add `nodetool upgradesstables` to upgrade_test.py
    * Fix rolling upgrade ubuntu versions in pipelines

---
+++ vars/supportedUpgradeFromVersions.groovy
@@ -0,0 +1,20 @@
+#!groovy
+
+def call(String git_branch, List base_versions_list) {
+    def clean_branch_name = git_branch - 'origin/' - 'branch-'
+    if (is_enterprise_version(clean_branch_name)) {
+        return base_versions_list
+    } else {
+        return base_versions_list.findAll{ ! is_enterprise_version(it) }
+    }
+}
+
+def is_enterprise_version(String version) {
+    def first_version = 0
+    try {
+        first_version = version.tokenize('.')[0] as Integer
+    } catch (java.lang.NumberFormatException ex) {
+        println "WARN: non formal/versioned branch '$version', assuming opensource version"
+    }
+    return first_version > 2000
+}
