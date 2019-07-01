0805ed671e8fb3b76093d051af0cda022a4daa54
Israel Fruchter <fruch@scylladb.com>
2019-07-01 14:39:16 +0300

    Add longevity-counters-4days-multidc.yaml to `/test-cases`

--- vars/getJenkinsLabels.groovy
+++ vars/getJenkinsLabels.groovy
@@ -1,6 +1,15 @@
 #!groovy
+import groovy.json.JsonSlurper
 
 def call(String backend, String aws_region=null) {
+    try {
+        regionList = new JsonSlurper().parseText(aws_region)
+        aws_region = regionList[0]
+    } catch(Exception) {
+
+    }
+
+
     def jenkins_labels = ['aws-eu-west-1': 'aws-sct-builders-eu-west-1',
                           'aws-us-east-1' : 'aws-sct-builders-us-east-1',
                           'gce': 'gce-sct-builders']
