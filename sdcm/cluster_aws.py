d7cc3f1f5b65d95e8c5675f86fc8abf9188dbbeb
Bentsi Magidovich <bentsi@scylladb.com>
2019-07-22 11:30:11 +0300

    Fixing reuse cluster following changes to list_instances_aws

--- sdcm/cluster_aws.py
+++ sdcm/cluster_aws.py
@@ -237,7 +237,7 @@ class AWSCluster(cluster.BaseCluster):
         ec2 = ec2_client.EC2Client(region_name=self.region_names[dc_idx],
                                    spot_max_price_percentage=self.params.get('spot_max_price', default=0.60))
         results = list_instances_aws(tags_dict={'TestId': test_id, 'NodeType': self.node_type},
-                                     region_name=self.region_names[dc_idx])
+                                     region_name=self.region_names[dc_idx], group_as_region=True)
         instances = results[self.region_names[dc_idx]]
 
         def sort_by_index(item):
