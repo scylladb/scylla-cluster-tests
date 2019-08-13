765838be72ef179577c645080b2e04db73c55130
Shlomi Balalis <Balalis@scylladb.com>
2019-08-13 17:09:11 +0300

    Changed the default monitor branch to branch-3.0
    and adjusted the monitor node's installtion and configuration to match the new branch

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -3240,7 +3240,7 @@ class BaseMonitorSet(object):
         self.local_metrics_addr = start_metrics_server()  # start prometheus metrics server locally and return local ip
         self.sct_ip_port = self.set_local_sct_ip()
         self.grafana_port = 3000
-        self.monitor_branch = self.params.get('monitor_branch', default='branch-2.4')
+        self.monitor_branch = self.params.get('monitor_branch')
         self._monitor_install_path_base = None
         self.phantomjs_installed = False
 
@@ -3359,9 +3359,11 @@ class BaseMonitorSet(object):
                 yum install -y epel-release
                 yum clean all
                 rm -rf /var/cache/yum/*
-                yum install -y python-pip unzip wget
-                pip install --upgrade pip
-                pip install pyyaml
+                yum install -y unzip wget
+                yum install -y python36
+                yum install -y python36-pip
+                python3 -m pip install --upgrade pip
+                python3 -m pip install pyyaml
                 curl -fsSL get.docker.com -o get-docker.sh
                 sh get-docker.sh
                 systemctl start docker
@@ -3372,10 +3374,10 @@ class BaseMonitorSet(object):
                 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
                 sudo apt-get update
                 sudo apt-get install -y docker docker.io
+                apt-get install python36
                 apt-get install -y python-setuptools unzip wget
-                easy_install pip
-                pip install --upgrade pip
-                pip install pyyaml
+                python3 -m pip install --upgrade pip
+                python3 -m pip install pyyaml
                 systemctl start docker
             """)
         elif node.is_debian8():
@@ -3389,12 +3391,12 @@ class BaseMonitorSet(object):
             node.remoter.run(cmd="sudo apt-get install -y curl")
             node.remoter.run(cmd="sudo apt-get install software-properties-common -y")
             node.remoter.run(cmd="sudo apt-get update", ignore_status=True)
+            node.remoter.run(cmd="apt-get install python36")
             node.remoter.run(cmd="sudo apt-get install -y python-setuptools wget")
             node.remoter.run(cmd="sudo apt-get install -y unzip")
             prereqs_script = dedent("""
-                easy_install pip
-                pip install --upgrade pip
-                pip install pyyaml
+                python3 -m pip install --upgrade pip
+                python3 -m pip install pyyaml
                 systemctl start docker
             """)
         else:
@@ -3453,7 +3455,7 @@ class BaseMonitorSet(object):
         configure_script = dedent("""
             cd {0.monitor_install_path}
             mkdir -p {0.monitoring_conf_dir}
-            ./genconfig.py -s -n -d {0.monitoring_conf_dir} {0._monitoring_targets}
+            python3 genconfig.py -s -n -d {0.monitoring_conf_dir} {0._monitoring_targets}
         """.format(self))
         node.remoter.run("bash -ce '%s'" % configure_script, verbose=True)
         if alert_manager:
@@ -3504,11 +3506,13 @@ class BaseMonitorSet(object):
             configure_script = dedent("""
                         cd {0.monitor_install_path}
                         mkdir -p {0.monitoring_conf_dir}
-                        ./genconfig.py -s -n -d {0.monitoring_conf_dir} {0._monitoring_targets}
+                        python3 genconfig.py -s -n -d {0.monitoring_conf_dir} {0._monitoring_targets}
                     """.format(self))
             node.remoter.run("sudo bash -ce '%s'" % configure_script, verbose=True)
 
     def start_scylla_monitoring(self, node):
+        node.remoter.run("cp {0.monitor_install_path}/prometheus/scylla_manager_servers.example.yml"
+                         " {0.monitor_install_path}/prometheus/scylla_manager_servers.yml".format(self))
         run_script = dedent("""
             cd {0.monitor_install_path}
             mkdir -p {0.monitoring_data_dir}
