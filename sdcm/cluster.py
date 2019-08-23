3c4543407918718a27baf0e83276b1bfd7bf83ae
Amos Kong <amos@scylladb.com>
2019-08-23 17:30:51 +0800

    loader/monitor: update yum repo cache at the beginning stage with retry
    Signed-off-by: Amos Kong <amos@scylladb.com>
    (cherry picked from commit 7eb891a7668759d14504abf86903357b78b26a06)

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -1961,6 +1961,11 @@ class BaseLoaderSet(object):
             scylla_repo_loader = self.params.get('scylla_repo')
         node.download_scylla_repo(scylla_repo_loader)
         if node.is_rhel_like():
+            result = node.remoter.run('ls /etc/yum.repos.d/epel.repo', ignore_status=True)
+            if result.exit_status == 0:
+                node.remoter.run('sudo yum update -y --skip-broken --disablerepo=epel', retry=3)
+            else:
+                node.remoter.run('sudo yum update -y --skip-broken', retry=3)
             node.remoter.run('sudo yum install -y {}-tools'.format(node.scylla_pkg()))
         else:
             node.remoter.run('sudo apt-get update')
@@ -2436,6 +2441,11 @@ class BaseMonitorSet(object):
 
     def install_scylla_monitoring_prereqs(self, node):
         if node.is_rhel_like():
+            result = node.remoter.run('ls /etc/yum.repos.d/epel.repo', ignore_status=True)
+            if result.exit_status == 0:
+                node.remoter.run('sudo yum update -y --skip-broken --disablerepo=epel', retry=3)
+            else:
+                node.remoter.run('sudo yum update -y --skip-broken', retry=3)
             prereqs_script = dedent("""
                 yum install -y epel-release
                 yum clean all
