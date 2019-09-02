777422b8afaacc9073d47612ce4819098fcecaf8
Israel Fruchter <fruch@scylladb.com>
2019-09-02 11:02:30 +0300

    Handle interlaced reactor stalls, to avoid backtrace events

--- unit_tests/test_cluster.py
+++ unit_tests/test_cluster.py
@@ -3,9 +3,11 @@ import tempfile
 import logging
 import shutil
 import os.path
+import json
 
 from sdcm.cluster import BaseNode
 from sdcm.sct_events import start_events_device, stop_events_device
+from sdcm.sct_events import EVENTS_PROCESSES
 
 
 class DummyNode(BaseNode):
@@ -57,23 +59,24 @@ class TestBaseNode(unittest.TestCase):
                              base_logdir=cls.temp_dir, ssh_login_info=dict(key_file='~/.ssh/scylla-test'))
         cls.node.remoter = DummyRemote()
 
-        cls.node.database_log = os.path.join(os.path.dirname(__file__), 'test_data', 'database.log')
-
     @classmethod
     def tearDownClass(cls):
         stop_events_device()
         shutil.rmtree(cls.temp_dir)
 
+    def setUp(self):
+        self.node.database_log = os.path.join(os.path.dirname(__file__), 'test_data', 'database.log')
+
     def test_search_database_log(self):
         critical_errors = self.node.search_database_log()
-        self.assertEqual(len(critical_errors), 4)
+        self.assertEqual(len(critical_errors), 34)
 
         for line_number, line in critical_errors:
             print line
 
     def test_search_database_log_teardown(self):
         critical_errors = self.node.search_database_log(start_from_beginning=True, publish_events=False)
-        self.assertEqual(len(critical_errors), 4)
+        self.assertEqual(len(critical_errors), 36)
 
         for line_number, line in critical_errors:
             print line
@@ -84,3 +87,23 @@ class TestBaseNode(unittest.TestCase):
 
         for line_number, line in errors:
             print line_number, line
+
+    def test_search_database_interlace_reactor_stall(self):
+        self.node.database_log = os.path.join(os.path.dirname(__file__), 'test_data', 'database_interlace_stall.log')
+
+        _ = self.node.search_database_log()
+
+        events_file = open(EVENTS_PROCESSES['MainDevice'].raw_events_filename, 'r')
+        events = []
+        for line in events_file.readlines():
+
+            events.append(json.loads(line))
+
+        event_a, event_b = events[-2], events[-1]
+        print event_a
+        print event_b
+
+        assert event_a["type"] == "REACTOR_STALLED"
+        assert event_a["line_number"] == 1
+        assert event_b["type"] == "REACTOR_STALLED"
+        assert event_b["line_number"] == 4
