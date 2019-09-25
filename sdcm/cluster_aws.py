ee4e4e65ef55ba6e6e7e930490802892128cb3df
Amos Kong <amos@scylladb.com>
2019-09-26 05:24:51 +0800

    cluster_aws: fix permmsion problem in writing scylla.yaml
    Command: 'sudo echo replace_address_first_boot: 172.30.0.160 >> /etc/scylla/scylla.yaml'
    Exit code: 1
    
    Stderr:
    bash: /etc/scylla/scylla.yaml: Permission denied
    
    Signed-off-by: Amos Kong <amos@scylladb.com>
    (cherry picked from commit 45b122617058c81ccbe1fb63c27ffeafe51714b5)

--- sdcm/cluster_aws.py
+++ sdcm/cluster_aws.py
@@ -419,7 +419,7 @@ class AWSNode(cluster.BaseNode):
             self.remoter.run("sudo bash -cxe '%s'" % clean_script)
             output = self.remoter.run('sudo grep replace_address: /etc/scylla/scylla.yaml', ignore_status=True)
             if 'replace_address_first_boot:' not in output.stdout:
-                self.remoter.run('sudo echo replace_address_first_boot: %s >> /etc/scylla/scylla.yaml' %
+                self.remoter.run('echo replace_address_first_boot: %s |sudo tee --append /etc/scylla/scylla.yaml' %
                                  self._instance.private_ip_address)
         self._instance.stop()
         self._instance_wait_safe(self._instance.wait_until_stopped)
