1d93abb4c6d3fe3a5c12d67752c930a209aa9314
ALeksandr Bykov <alex.bykov@scylladb.com>
2019-10-03 18:29:49 +0700

    Fixed the creation of grafana livesnapshots for monitoring v3.0

--- data_dir/share_snapshot.js
+++ data_dir/share_snapshot.js
@@ -10,39 +10,39 @@ page.viewportSize = { width: 1920, height: 1080 };
 var address = system.args[1];
 
 page.open(address, function(status) {
-	if (status !== "success") {
-		console.log("Error to load page " + address);
-		phantom.exit();
-	}
-	window.setTimeout(function () {
-		page.evaluate(function() {
-			var ev = document.createEvent('MouseEvent');
-			ev.initMouseEvent('click', true, true);
-			var l = document.getElementsByClassName("navbar-button--share");
-        	l[0].dispatchEvent(ev)
-		});
+    if (status !== "success") {
+        console.log("Error to load page " + address);
+        phantom.exit();
+    }
+    window.setTimeout(function () {
+        page.evaluate(function() {
+            var ev = document.createEvent('MouseEvent');
+            ev.initMouseEvent('click', true, true);
+            var l = document.getElementsByClassName("navbar-button--share");
+            l[0].dispatchEvent(ev)
+        });
     }, 15000);
-	window.setTimeout(function() {
-		page.evaluate(function() {
-			var ev = document.createEvent('MouseEvent');
-			ev.initMouseEvent('click', true, true);
-			l = document.getElementsByClassName("gf-tabs-item");
-        	l[1].firstElementChild.dispatchEvent(ev);
-		});
-	}, 20000);
-	window.setTimeout(function() {
-		page.evaluate(function() {
-			var ev = document.createEvent('MouseEvent');
-			ev.initMouseEvent('click', true, true);
-			l = document.getElementsByClassName("fa-cloud-upload");
-        	l[0].dispatchEvent(ev);
-		});
-	}, 25000);
-	window.setTimeout(function() {
-		page.evaluate(function() {
-			l = document.getElementsByClassName("fa-external-link-square");
-        	console.log(l[0].parentElement.lastChild.nodeValue.trim());
-		});
-		phantom.exit();
-	}, 60000);
+    window.setTimeout(function() {
+        page.evaluate(function() {
+            var ev = document.createEvent('MouseEvent');
+            ev.initMouseEvent('click', true, true);
+            l = document.getElementsByClassName("gf-tabs-item");
+            l[1].firstElementChild.dispatchEvent(ev);
+        });
+    }, 20000);
+    window.setTimeout(function() {
+        page.evaluate(function() {
+            var ev = document.createEvent('MouseEvent');
+            ev.initMouseEvent('click', true, true);
+            l = document.getElementsByClassName("gf-form-button-row");
+            l[0].children[1].dispatchEvent(ev);
+        });
+    }, 25000);
+    window.setTimeout(function() {
+        page.evaluate(function() {
+            l = document.getElementsByClassName("fa-external-link-square");
+            console.log(l[0].parentElement.lastChild.nodeValue.trim());
+        });
+        phantom.exit();
+    }, 60000);
 });
