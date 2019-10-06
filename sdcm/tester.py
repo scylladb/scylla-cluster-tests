94d8f1bda04551b21556fb3a91e4cab3acca4c8e
Israel Fruchter <fruch@scylladb.com>
2019-10-06 10:32:48 +0300

    Fix `wait_ami_available` code failing for scylla-cloud-run
    It should have used default='', since `split()` can be run on None

--- sdcm/tester.py
+++ sdcm/tester.py
@@ -531,7 +531,7 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
             services.append(service)
             self.credentials.append(UserRemoteCredentials(key_file=user_credentials))
 
-        ami_ids = self.params.get('ami_id_db_scylla').split()
+        ami_ids = self.params.get('ami_id_db_scylla', default='').split()
         for idx, service in enumerate(services):
             wait_ami_available(service.meta.client, ami_ids[idx])
 
