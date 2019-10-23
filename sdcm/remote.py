6638d103563a4338f7890ca75a6cecd99e486339
fgelcer <32832995+fgelcer@users.noreply.github.com>
2019-10-23 17:28:23 +0300

    add load tests to reach admission control feature (Reads and Writes) (#1339)
    * add load tests to reach admission control feature (Reads and Writes)
    
    * Workaround to avoid scp issues
    
    * it happens because c-s user profile is sent for
    * each job on each node, making it run (in my case)
    * more than 80 times per test, and it cause issues.
    * there is a ticket in trello to refactor this scp
    * command
    
    * Fixed pylint issues on sdcm/remote.py
    
    (cherry picked from commit e194b352b437076ad33d6c993dfde48ae4c0854d)

--- sdcm/remote.py
+++ sdcm/remote.py
@@ -311,14 +311,24 @@ class RemoteCmdRunner(CommandRunner):
                                                           escape=False)
                 local_dest = six.moves.shlex_quote(dst)
                 scp = self._make_scp_cmd([remote_source], local_dest)
-                result = self.connection.local(scp, hide=True)
-                self.log.info("Command {} with status {}".format(result.command, result.exited))
-                if result.exited:
-                    files_received = False
-                # Avoid "already printed" message without real error
-                if result.stderr:
-                    self.log.info("Stderr: {}".format(result.stderr))
-                    files_received = False
+                try:
+                    result = self.connection.local(scp, hide=True)
+                    self.log.info("Command {} with status {}".format(result.command, result.exited))
+
+                    if result.exited:
+                        files_received = False
+
+                    # Avoid "already printed" message without real error
+                    if result.stderr:
+                        self.log.info("Stderr: {}".format(result.stderr))
+                        files_received = False
+                except UnexpectedExit as details:
+                    if not details.result.exited and not details.result.stderr:
+                        pass
+                    else:
+                        pass
+                else:
+                    self.log.info("Command {} with status {}".format(result.command, result.exited))
 
         if not preserve_perm:
             # we have no way to tell scp to not try to preserve the
@@ -416,10 +426,17 @@ class RemoteCmdRunner(CommandRunner):
             local_sources = self._make_rsync_compatible_source(src, True)
             if local_sources:
                 scp = self._make_scp_cmd(local_sources, remote_dest)
-                result = self.connection.local(scp)
-                self.log.info('Command {} with status {}'.format(result.command, result.exited))
-                if result.exited:
-                    files_sent = False
+                try:
+                    result = self.connection.local(scp)
+                except UnexpectedExit as details:
+                    if not details.result.exited and not details.result.stderr:
+                        pass
+                    else:
+                        pass
+                else:
+                    self.log.info('Command {} with status {}'.format(result.command, result.exited))
+                    if result.exited:
+                        files_sent = False
         return files_sent
 
     def use_rsync(self):
