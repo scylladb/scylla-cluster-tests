f269acbfae484ffca43820ec1d656432843ed834
Israel Fruchter <israel.fruchter@gmail.com>
2019-10-27 11:46:02 +0200

    Revert "Add support to fail test on thread failures (#1361)"
    This reverts commit 4348223651e1ac9fe5d24fdbd58b6904d90be7ab.

--- longevity_in_memory_test.py
+++ longevity_in_memory_test.py
@@ -2,6 +2,9 @@ from longevity_test import LongevityTest
 
 
 class InMemoryLongevetyTest(LongevityTest):
+    def __init__(self, *args, **kwargs):
+        super(InMemoryLongevetyTest, self).__init__(*args, **kwargs)
+
     def test_in_mem_longevity(self):
         self._pre_create_schema(in_memory=True)
         self.test_custom_time()
--- longevity_large_partition_test.py
+++ longevity_large_partition_test.py
@@ -2,6 +2,8 @@ from longevity_test import LongevityTest
 
 
 class LargePartitionLongevetyTest(LongevityTest):
+    def __init__(self, *args, **kwargs):
+        super(LargePartitionLongevetyTest, self).__init__(*args, **kwargs)
 
     def test_large_partition_longevity(self):
         self._pre_create_schema()
