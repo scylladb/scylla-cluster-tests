92c392bbbcbad228d43af30e608903239435170a
Amos Kong <amos@scylladb.com>
2019-11-21 16:29:47 +0800

    clean old ssl_conf files in configuring client encryption
    Currently we call BaseScyllaCluster.node_config_setup() to change one kind of
    configuration, but it will re-setup all configuration again.
    
    For cleaning replace_address_first_boot parameter in scylla.yaml, we call
    BaseScyllaCluster.node_config_setup(), client_encrypt will be re-configed
    again, it fails in re-copy ssl_conf files.
    
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -1590,6 +1590,7 @@ server_encryption_options:
             mkdir -p ~/.cassandra/
             cp /tmp/ssl_conf/client/cqlshrc ~/.cassandra/
             sudo mkdir -p /etc/scylla/
+            sudo rm -rf /etc/scylla/ssl_conf/
             sudo mv -f /tmp/ssl_conf/ /etc/scylla/
         """)
         self.remoter.run('bash -cxe "%s"' % setup_script)
