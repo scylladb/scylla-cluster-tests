scylladb/scylla-cluster-tests

commit c529efa57f9903788a60ecbed65a51660468eed3

Port SCT to python3
* The codebase won't suport python2.7 anymore
* Remove all six usage

sdcm/cluster_aws.py
@@ -101,7 +101,7 @@ class AWSCluster(cluster.BaseCluster):  # pylint: disable=too-many-instance-attr
 
     def _create_on_demand_instances(self, count, interfaces, ec2_user_data, dc_idx=0, tags_list=None):  # pylint: disable=too-many-arguments
         ami_id = self._ec2_ami_id[dc_idx]
-        self.log.debug("Creating {count} on-demand instances using AMI id '{ami_id}'... ".format(**locals()))
+        self.log.debug(f"Creating {count} on-demand instances using AMI id '{ami_id}'... ")
         instances = self._ec2_services[dc_idx].create_instances(ImageId=ami_id,
                                                                 UserData=ec2_user_data,
                                                                 MinCount=count,
@@ -161,7 +161,7 @@ class AWSCluster(cluster.BaseCluster):  # pylint: disable=too-many-instance-attr
                     instances_i = ec2.create_spot_instances(**spot_params)
                 instances.extend(instances_i)
             except ClientError as cl_ex:
-                if ec2_client.MAX_SPOT_EXCEEDED_ERROR in cl_ex.message:
+                if ec2_client.MAX_SPOT_EXCEEDED_ERROR in str(cl_ex):
                     self.log.debug('Cannot create spot instance(-s): %s.'
                                    'Creating on demand instance(-s) instead.', cl_ex)
                     instances_i = self._create_on_demand_instances(
@@ -362,11 +362,13 @@ class AWSCluster(cluster.BaseCluster):  # pylint: disable=too-many-instance-attr
             post_boot_script += self.network_config_ipv6_workaround_script()
 
         if isinstance(ec2_user_data, dict):
-            ec2_user_data['post_configuration_script'] = base64.b64encode(post_boot_script).decode()
+            ec2_user_data['post_configuration_script'] = base64.b64encode(
+                post_boot_script.encode('utf-8')).decode('acsii')
             ec2_user_data = json.dumps(ec2_user_data)
         else:
             if 'clustername' in ec2_user_data:
-                ec2_user_data += " --base64postscript={0}".format(base64.b64encode(post_boot_script))
+                ec2_user_data += " --base64postscript={0}".format(
+                    base64.b64encode(post_boot_script.encode('utf-8')).decode('ascii'))
             else:
                 ec2_user_data = post_boot_script
 
@@ -711,7 +713,7 @@ class AWSNode(cluster.BaseNode):
 
         if not imagedata:
             self.log.warning('Some error during getting console screenshot')
-        return imagedata
+        return imagedata.encode('ascii')
 
     def traffic_control(self, tcconfig_params=None):
         """

.pre-commit-config.yaml
@@ -25,7 +25,7 @@ repos:
         - --ignore=E226,E24,W50,W690,E402
 
 -   repo: https://github.com/pre-commit/mirrors-pylint
-    rev: 'v1.9.1'  # Use the sha / tag you want to point at
+    rev: 'v2.4.2'  # Use the sha / tag you want to point at
     hooks:
     -   id: pylint
         additional_dependencies:

docker/env/Dockerfile
@@ -2,6 +2,14 @@ FROM centos:7
 
 ADD install-prereqs.sh install-prereqs.sh
 RUN ./install-prereqs.sh docker
+RUN yum update -y \
+    && yum install -y https://centos7.iuscommunity.org/ius-release.rpm \
+    && yum install -y python36u python36u-libs python36u-devel python36u-pip
+
 ADD requirements-python.txt requirements-python.txt
-RUN pip install setuptools==40.8.0 pip==19.1.1
-RUN pip install -r requirements-python.txt
+RUN pip3 install setuptools==40.8.0 pip==19.1.1
+RUN pip3 install -r requirements-python.txt
+
+ENV LANG en_US.UTF-8
+ENV LC_ALL en_US.UTF-8
+ENV PYTHONWARNINGS ignore:unclosed

docker/env/version
@@ -1,1 +1,1 @@
-0.51
+0.52

grow_cluster_test.py
@@ -167,7 +167,7 @@ class GrowClusterTest(ClusterTester):
                 for _ in range(rm_cnt):
                     decommision_nemesis = nemesis.DecommissionMonkey(
                         tester_obj=self, termination_event=self.db_cluster.termination_event)
-                    decommision_nemesis.disrupt(add_node=False)
+                    decommision_nemesis.disrupt_nodetool_decommission(add_node=False)
             duration = (datetime.datetime.now() - start).seconds / 60  # current duration in minutes
             self.log.info('Count of nodes in cluster: %s', len(self.db_cluster.nodes))
 

longevity_test.py
@@ -131,7 +131,7 @@ class LongevityTest(ClusterTester):
             # the loaders (round_robin).
             if keyspace_num > 1 and self.params.get('round_robin'):
                 self.log.debug("Using round_robin for multiple Keyspaces...")
-                for i in xrange(1, keyspace_num + 1):
+                for i in range(1, keyspace_num + 1):
                     keyspace_name = self._get_keyspace_name(i)
                     self._run_all_stress_cmds(write_queue, params={'stress_cmd': prepare_write_cmd,
                                                                    'keyspace_name': keyspace_name,
@@ -186,7 +186,7 @@ class LongevityTest(ClusterTester):
             # Stress: Same as in prepare_write - allow the load to be spread across all loaders when using multi ks
             if keyspace_num > 1 and self.params.get('round_robin'):
                 self.log.debug("Using round_robin for multiple Keyspaces...")
-                for i in xrange(1, keyspace_num + 1):
+                for i in range(1, keyspace_num + 1):
                     keyspace_name = self._get_keyspace_name(i)
                     params = {'keyspace_name': keyspace_name, 'round_robin': True, 'stress_cmd': stress_cmd}
 
@@ -340,7 +340,7 @@ class LongevityTest(ClusterTester):
         """
         def chunks(_list, chunk_size):
             """Yield successive n-sized chunks from _list."""
-            for i in xrange(0, len(_list), chunk_size):
+            for i in range(0, len(_list), chunk_size):
                 yield _list[i:i + chunk_size], i, i+chunk_size, len(_list) + i * 2
 
         for batch, _, _, extra_tables_idx in list(chunks(stress_params_list, batch_size)):
@@ -370,8 +370,8 @@ class LongevityTest(ClusterTester):
                                     scylla_encryption_options=self.params.get('scylla_encryption_options', None))
 
         num_of_batches = int(total_stress / batch_size)
-        for batch in xrange(0, num_of_batches):
-            for i in xrange(1 + batch * batch_size, (batch + 1) * batch_size + 1):
+        for batch in range(0, num_of_batches):
+            for i in range(1 + batch * batch_size, (batch + 1) * batch_size + 1):
                 keyspace_name = self._get_keyspace_name(i)
                 self._run_all_stress_cmds(stress_queue, params={'stress_cmd': stress_cmd,
                                                                 'keyspace_name': keyspace_name, 'round_robin': True})
@@ -421,7 +421,7 @@ class LongevityTest(ClusterTester):
         """
 
         self.log.debug('Pre Creating Schema for c-s with {} keyspaces'.format(keyspace_num))
-        for i in xrange(1, keyspace_num+1):
+        for i in range(1, keyspace_num+1):
             keyspace_name = 'keyspace{}'.format(i)
             self.create_keyspace(keyspace_name=keyspace_name, replication_factor=3)
             self.log.debug('{} Created'.format(keyspace_name))

mgmt_cli_test.py
@@ -286,5 +286,5 @@ class MgmtCliTest(ClusterTester):
             table_repair_percentage = float(table_repair_progress.replace('%', ''))
             return table_repair_percentage
         except ScyllaManagerError as err:
-            assert "not found in" in err.message, "Unexpected error: {}".format(err.message)
+            assert "not found in" in str(err), "Unexpected error: {}".format(str(err))
             return None

commit 0233f3c7eaa988d76affc78bf6b8884ab7731a16

terminate_and_replace_node: fix problem in cleaning replace_address parameter (#1457)
* clean old ssl_conf files in configuring client encryption

Currently we call BaseScyllaCluster.node_config_setup() to change one kind of
configuration, but it will re-setup all configuration again.

For cleaning replace_address_first_boot parameter in scylla.yaml, we call
BaseScyllaCluster.node_config_setup(), client_encrypt will be re-configed
again, it fails in re-copy ssl_conf files.

* clean_replacement_node_ip: clean replace parameter by sed

Currently we call BaseScyllaCluster.node_config_setup() in
clean_replacement_node_ip() for cleaning replace_address_first_boot parameter
in scylla.yaml, but it will re-setup all related configuration again. The
repeated configuration of client encryption will fails in re-copy ssl_conf
files.

This patch changed to clean the parameter by sed, it won't effect other
configuration.

* i3 restart: only clean effective replace_address_first_boot parameter

* fix a typo introduced by c529efa57

node.disable_client_encrypt() actually isn't called. It won't work for this
issue.

* sdcm/cluster: drop _param_enabled()

We had corrected the type of bool parameters, then it's safe to just use
self.params.get()

sdcm/cluster.py
@@ -1590,6 +1590,7 @@ server_encryption_options:
             mkdir -p ~/.cassandra/
             cp /tmp/ssl_conf/client/cqlshrc ~/.cassandra/
             sudo mkdir -p /etc/scylla/
+            sudo rm -rf /etc/scylla/ssl_conf/
             sudo mv -f /tmp/ssl_conf/ /etc/scylla/
         """)
         self.remoter.run('bash -cxe "%s"' % setup_script)
@@ -2418,17 +2419,6 @@ class BaseCluster():  # pylint: disable=too-many-instance-attributes
                 errors.update({node.name: node_errors})
         return errors
 
-    def _param_enabled(self, param):
-        param = self.params.get(param)
-        if isinstance(param, str):
-            return bool(param and param.lower() == 'true')
-        elif isinstance(param, bool):
-            return param
-        elif param is None:
-            return False
-        else:
-            raise ValueError('Unsupported type: {}'.format(type(param)))
-
     def destroy(self):
         self.log.info('Destroy nodes')
         for node in self.nodes:
@@ -2933,10 +2923,10 @@ class BaseScyllaCluster():  # pylint: disable=too-many-public-methods
 
     def node_config_setup(self, node, seed_address=None, endpoint_snitch=None, murmur3_partitioner_ignore_msb_bits=None, client_encrypt=None):  # pylint: disable=too-many-arguments,invalid-name
         node.config_setup(seed_address=seed_address, cluster_name=self.name,  # pylint: disable=no-member
-                          enable_exp=self._param_enabled('experimental'), endpoint_snitch=endpoint_snitch,
+                          enable_exp=self.params.get('experimental'), endpoint_snitch=endpoint_snitch,
                           authenticator=self.params.get('authenticator'),
-                          server_encrypt=self._param_enabled('server_encrypt'),
-                          client_encrypt=client_encrypt if client_encrypt is None else self._param_enabled(
+                          server_encrypt=self.params.get('server_encrypt'),
+                          client_encrypt=client_encrypt if client_encrypt is not None else self.params.get(
                               'client_encrypt'),
                           append_scylla_yaml=self.params.get('append_scylla_yaml'), append_scylla_args=self.get_scylla_args(),
                           hinted_handoff=self.params.get('hinted_handoff'),
@@ -2995,14 +2985,16 @@ class BaseScyllaCluster():  # pylint: disable=too-many-public-methods
 
         node.wait_db_up(timeout=timeout)
         node.wait_jmx_up()
-        self.clean_replacement_node_ip(node, seed_address, endpoint_snitch)
+        self.clean_replacement_node_ip(node)
 
-    def clean_replacement_node_ip(self, node, seed_address, endpoint_snitch):
+    @staticmethod
+    def clean_replacement_node_ip(node):
         if node.replacement_node_ip:
             # If this is a replacement node, we need to set back configuration in case
             # when scylla-server process will be restarted
             node.replacement_node_ip = None
-            self.node_config_setup(node, seed_address, endpoint_snitch)
+            node.remoter.run(
+                'sudo sed -i -e "s/^replace_address_first_boot:/# replace_address_first_boot:/g" /etc/scylla/scylla.yaml')
 
     @staticmethod
     def verify_logging_from_nodes(nodes_list):

sdcm/cluster_aws.py
@@ -620,7 +620,7 @@ class AWSNode(cluster.BaseNode):
 
                 self.start_scylla_server(verify_up=False)
                 self.remoter.run(
-                    'sudo sed -i -e "s/replace_address_first_boot:/# replace_address_first_boot:/g" /etc/scylla/scylla.yaml')
+                    'sudo sed -i -e "s/^replace_address_first_boot:/# replace_address_first_boot:/g" /etc/scylla/scylla.yaml')
                 self.remoter.run("sudo sed -e '/auto_bootstrap:.*/s/True/False/g' -i /etc/scylla/scylla.yaml")
             finally:
                 if event_filters:
@@ -803,11 +803,11 @@ class ScyllaAWSCluster(cluster.BaseScyllaCluster, AWSCluster):
 
     def node_config_setup(self, node, seed_address=None, endpoint_snitch=None, murmur3_partitioner_ignore_msb_bits=None, client_encrypt=None):  # pylint: disable=too-many-arguments
         setup_params = dict(
-            enable_exp=self._param_enabled('experimental'),
+            enable_exp=self.params.get('experimental'),
             endpoint_snitch=endpoint_snitch,
             authenticator=self.params.get('authenticator'),
-            server_encrypt=self._param_enabled('server_encrypt'),
-            client_encrypt=client_encrypt if client_encrypt is not None else self._param_enabled('client_encrypt'),
+            server_encrypt=self.params.get('server_encrypt'),
+            client_encrypt=client_encrypt if client_encrypt is not None else self.params.get('client_encrypt'),
             append_scylla_args=self.get_scylla_args(),
             authorizer=self.params.get('authorizer'),
             hinted_handoff=self.params.get('hinted_handoff'),
@@ -884,7 +884,7 @@ class ScyllaAWSCluster(cluster.BaseScyllaCluster, AWSCluster):
 
         node.wait_db_up(verbose=verbose, timeout=timeout)
         node.check_nodes_status()
-        self.clean_replacement_node_ip(node, seed_address, endpoint_snitch)
+        self.clean_replacement_node_ip(node)
 
     def destroy(self):
         self.stop_nemesis()