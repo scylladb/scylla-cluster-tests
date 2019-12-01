3cb3d0ab239353abeedd786740462e6e0798be24
Amos Kong <amos@scylladb.com>
2019-12-01 20:09:32 +0800

    cluster_aws: retry to stop scylla-server after instance restart (#1469)
    * cluster_aws: retry to stop scylla-server after instance restart
    
    Currently we stop scylla-server when ssh is up after instance restarts.
    It works well most of time, but in some special case, the instance network is
    unavailable again in stopping scylla-server, it causes the nemesis fails and
    leave a problem instance (no RAID setup).
    
    This patch changed to retry to stop scylla-server if NoValidConnectionsError is
    touched.
    
    
    * stop_ssh_up_thread: avoid to repeatedly stop same thread
    
    > Traceback (most recent call last):
    >   File "/sct/sdcm/cluster.py", line 2489, in node_setup
    >     cl_inst.node_setup(node, **setup_kwargs)
    >   File "/sct/sdcm/cluster.py", line 3490, in node_setup
    >     self.install_scylla_monitoring(node)
    >   File "/sct/sdcm/utils/common.py", line 136, in inner
    >     res = func(*args, **kwargs)
    >   File "/sct/sdcm/cluster.py", line 3753, in install_scylla_monitoring
    >     self.install_scylla_monitoring_prereqs(node)
    >   File "/sct/sdcm/cluster.py", line 3597, in install_scylla_monitoring_prereqs
    >     node.remoter.reconnect()
    >   File "/sct/sdcm/utils/common.py", line 94, in inner
    >     return func(*args, **kwargs)
    >   File "/sct/sdcm/remote.py", line 198, in reconnect
    >     self.stop_ssh_up_thread()
    >   File "/sct/sdcm/remote.py", line 273, in stop_ssh_up_thread
    >     self._ssh_up_thread.join(5)
    > AttributeError: 'NoneType' object has no attribute 'join'
    
    (cherry picked from commit 5b931f10cad64145f41d2d7eff9f745570b4c74d)

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -33,6 +33,7 @@ from invoke.exceptions import UnexpectedExit, Failure, CommandTimedOut
 import yaml
 import requests
 from paramiko import SSHException
+from paramiko.ssh_exception import NoValidConnectionsError
 
 
 from sdcm.collectd import ScyllaCollectdSetup
@@ -2022,7 +2023,7 @@ server_encryption_options:
         if verify_up:
             self.wait_jmx_up(timeout=timeout)
 
-    @retrying(n=3, sleep_time=5, allowed_exceptions=(CommandTimedOut,),
+    @retrying(n=3, sleep_time=5, allowed_exceptions=(CommandTimedOut, NoValidConnectionsError,),
               message="Failed to stop scylla.server, retrying...")
     def stop_scylla_server(self, verify_up=False, verify_down=True, timeout=300, ignore_status=False):
         if verify_up:
