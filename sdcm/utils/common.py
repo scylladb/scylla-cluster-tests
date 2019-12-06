ad9864a7cdf9810d22400d218d359270a37ed945
Dmitry Kropachev <dmitry.kropachev@scylladb.com>
2019-12-06 14:30:49 +0700

    Fix to skip sending message on first retry
    (cherry picked from commit ba6140d9320d530cf7610c1e3753b30e55c0094e)

--- sdcm/utils/common.py
+++ sdcm/utils/common.py
@@ -90,7 +90,7 @@ class retrying(object):  # pylint: disable=invalid-name,too-few-public-methods
                 return func(*args, **kwargs)
             for i in xrange(self.n):
                 try:
-                    if self.message:
+                    if self.message and i > 0:
                         LOGGER.info("%s [try #%s]", self.message, i)
                     return func(*args, **kwargs)
                 except self.allowed_exceptions as ex:
