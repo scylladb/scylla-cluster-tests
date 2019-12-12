0325b34b461daf6dac2855d941e84fc2338588df
aleksbykov <44991177+aleksbykov@users.noreply.github.com>
2019-12-12 15:36:55 +0700

    Fix for issue #1462: SCT failed to clean the resources at the end of the job (#1550)
    Trello: https://trello.com/c/40wn98PT
    Fix issue #1462 #1462
    minor enhancement

--- sdcm/utils/common.py
+++ sdcm/utils/common.py
@@ -1344,7 +1344,10 @@ def clean_aws_instances_according_post_behavior(params, config, logdir):  # pyli
         if action == 'destroy':
             instances_ids = [instance['InstanceId'] for instance in instances]
             LOGGER.info('Clean next instances %s', instances_ids)
-            client.terminate_instances(InstanceIds=instances_ids)
+            try:
+                client.terminate_instances(InstanceIds=instances_ids)
+            except Exception as details:  # pylint: disable=broad-except
+                LOGGER.error("Error during instance termination: %s", details)
         elif action == 'keep-on-failure':
             if status:
                 LOGGER.info('Run failed. Leave instances running')
@@ -1375,7 +1378,7 @@ def clean_gce_instances_according_post_behavior(params, config, logdir):  # pyli
 
     def apply_action(instances, action):
         if action == 'destroy':
-            for instance in filtered_instances['db_nodes']:
+            for instance in instances:
                 LOGGER.info('Destroying instance: %s', instance.name)
                 instance.destroy()
                 LOGGER.info('Destroyed instance: %s', instance.name)
