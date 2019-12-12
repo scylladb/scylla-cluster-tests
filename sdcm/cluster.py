scylladb/scylla-cluster-tests

commit ae7c6ce4f473ed60c7d0ebb8f21054b4decf0538

Fix for a small issue during reuse_cluster (#1542)
issue reported at #1536

sdcm/cluster.py
@@ -3501,6 +3501,7 @@ class BaseMonitorSet():  # pylint: disable=too-many-public-methods,too-many-inst
         node.wait_ssh_up()
         # update repo cache and system after system is up
         node.update_repo_cache()
+        self.mgmt_auth_token = kwargs["auth_token"]  # pylint: disable=attribute-defined-outside-init
 
         if Setup.REUSE_CLUSTER:
             self.configure_scylla_monitoring(node)
@@ -3524,10 +3525,9 @@ class BaseMonitorSet():  # pylint: disable=too-many-public-methods,too-many-inst
         else:
             node.remoter.run('sudo apt-get install screen -y')
         if self.params.get("use_mgmt", default=None):
-            self.install_scylla_manager(node, auth_token=kwargs["auth_token"])
+            self.install_scylla_manager(node, auth_token=self.mgmt_auth_token)
 
     def install_scylla_manager(self, node, auth_token):
-        self.mgmt_auth_token = auth_token  # pylint: disable=attribute-defined-outside-init
         if self.params.get('use_mgmt', default=None):
             node.install_mgmt(scylla_repo=self.params.get('scylla_repo_m'),
                               scylla_mgmt_repo=self.params.get('scylla_mgmt_repo'), auth_token=auth_token,

commit a49b8680d0f8393ec3a824375411caddd35de85c

Fix #1546: mgmt_auth_token breaking the node_setup of monitor
was introduced by #1542

sdcm/cluster.py
@@ -3501,7 +3501,7 @@ class BaseMonitorSet():  # pylint: disable=too-many-public-methods,too-many-inst
         node.wait_ssh_up()
         # update repo cache and system after system is up
         node.update_repo_cache()
-        self.mgmt_auth_token = kwargs["auth_token"]  # pylint: disable=attribute-defined-outside-init
+        self.mgmt_auth_token = kwargs.get("auth_token", default=None)  # pylint: disable=attribute-defined-outside-init
 
         if Setup.REUSE_CLUSTER:
             self.configure_scylla_monitoring(node)