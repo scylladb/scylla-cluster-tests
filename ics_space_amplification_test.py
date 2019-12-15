af46d91ecdba324dbfc100cc363e6abb5161c9fc
Yaron Gilor <yarongilor@scylladb.com>
2019-12-15 18:44:29 +0200

    Adjust python3 -> python2 backward compatibility

--- ics_space_amplification_test.py
+++ ics_space_amplification_test.py
@@ -17,19 +17,25 @@ AVAIL_SIZE_METRIC_OLD = 'node_filesystem_avail'
 
 class IcsSpaceAmplificationTest(IcsLongevetyTest):
 
-    def _get_used_capacity_gb(self, node) -> float:  # pylint: disable=too-many-locals
-        # node_filesystem_size_bytes{mountpoint="/var/lib/scylla", instance=~".*?10.0.79.46.*?"}-node_filesystem_avail_bytes{mountpoint="/var/lib/scylla", instance=~".*?10.0.79.46.*?"}
-        filesystem_capacity_query = f'{FS_SIZE_METRIC}{generate_capacity_query_postfix(node)}'
-        used_capacity_query = f'{filesystem_capacity_query}-{AVAIL_SIZE_METRIC}{generate_capacity_query_postfix(node)}'
-        self.log.debug(f"filesystem_capacity_query: {filesystem_capacity_query}")
+    def _get_used_capacity_gb(self, node):  # pylint: disable=too-many-locals
+        # example: node_filesystem_size_bytes{mountpoint="/var/lib/scylla", instance=~".*?10.0.79.46.*?"}-node_filesystem_avail_bytes{mountpoint="/var/lib/scylla", instance=~".*?10.0.79.46.*?"}
+        node_capacity_query_postfix = generate_node_capacity_query_postfix(node)
+        filesystem_capacity_query = '{FS_SIZE_METRIC}{node_capacity_query_postfix}'.format(
+            **dict(locals(), **globals()))
+        used_capacity_query = '{filesystem_capacity_query}-{AVAIL_SIZE_METRIC}{node_capacity_query_postfix}'.format(
+            **dict(locals(), **globals()))
+        self.log.debug("filesystem_capacity_query: {filesystem_capacity_query}".format(**dict(locals(), **globals())))
 
         fs_size_res = self.prometheus_db.query(query=filesystem_capacity_query, start=int(time.time()) - 5,
                                                end=int(time.time()))
         assert fs_size_res, "No results from Prometheus"
         if not fs_size_res[0]:  # if no returned values - try the old metric names.
-            filesystem_capacity_query = f'{FS_SIZE_METRIC_OLD}{generate_capacity_query_postfix(node)}'
-            used_capacity_query = f'{filesystem_capacity_query}-{AVAIL_SIZE_METRIC_OLD}{generate_capacity_query_postfix(node)}'
-            self.log.debug(f"filesystem_capacity_query: {filesystem_capacity_query}")
+            filesystem_capacity_query = '{FS_SIZE_METRIC_OLD}{node_capacity_query_postfix}'.format(
+                **dict(locals(), **globals()))
+            used_capacity_query = '{filesystem_capacity_query}-{AVAIL_SIZE_METRIC_OLD}{node_capacity_query_postfix}'.format(
+                **dict(locals(), **globals()))
+            self.log.debug("filesystem_capacity_query: {filesystem_capacity_query}".format(
+                **dict(locals(), **globals())))
             fs_size_res = self.prometheus_db.query(query=filesystem_capacity_query, start=int(time.time()) - 5,
                                                    end=int(time.time()))
 
@@ -39,7 +45,7 @@ class IcsSpaceAmplificationTest(IcsLongevetyTest):
                                                 end=int(time.time()))
         assert used_cap_res, "No results from Prometheus"
         used_size_mb = float(used_cap_res[0]["values"][0][1]) / float(MB_SIZE)
-        used_size_gb = float(used_size_mb / 1024)
+        used_size_gb = round(float(used_size_mb / 1024), 2)
         self.log.debug(
             "The used filesystem capacity on node {} is: {} MB/ {} GB".format(node.public_ip_address, used_size_mb,
                                                                               used_size_gb))
@@ -47,15 +53,17 @@ class IcsSpaceAmplificationTest(IcsLongevetyTest):
 
     def _get_filesystem_available_size_list(self, node, start_time):
         """
-        :param node:
-        :return:
+        :returns a list of file-system free-capacity on point in time from 'start_time' up to now.
         """
-        available_capacity_query = f'{AVAIL_SIZE_METRIC}{generate_capacity_query_postfix(node)}'
+        node_capacity_query_postfix = generate_node_capacity_query_postfix(node)
+        available_capacity_query = '{AVAIL_SIZE_METRIC}{node_capacity_query_postfix}'.format(
+            **dict(locals(), **globals()))
         available_size_res = self.prometheus_db.query(query=available_capacity_query, start=int(start_time),
                                                       end=int(time.time()))
         assert available_size_res, "No results from Prometheus"
         if not available_size_res[0]:  # if no returned values - try the old metric names.
-            available_capacity_query = f'{AVAIL_SIZE_METRIC_OLD}{generate_capacity_query_postfix(node)}'
+            available_capacity_query = '{AVAIL_SIZE_METRIC_OLD}{node_capacity_query_postfix}'.format(
+                **dict(locals(), **globals()))
             available_size_res = self.prometheus_db.query(query=available_capacity_query, start=int(start_time),
                                                           end=int(time.time()))
         assert available_size_res[0], "Could not resolve available-size query result."
@@ -68,13 +76,17 @@ class IcsSpaceAmplificationTest(IcsLongevetyTest):
         """
         fs_size_metric = 'node_filesystem_size_bytes'
         fs_size_metric_old = 'node_filesystem_size'
-        filesystem_capacity_query = f'{fs_size_metric}{generate_capacity_query_postfix(node)}'
+        node_capacity_query_postfix = generate_node_capacity_query_postfix(node)
+        filesystem_capacity_query = '{fs_size_metric}{node_capacity_query_postfix}'.format(
+            **dict(locals(), **globals()))
         fs_size_res = self.prometheus_db.query(query=filesystem_capacity_query, start=int(time.time()) - 5,
                                                end=int(time.time()))
         assert fs_size_res, "No results from Prometheus"
         if not fs_size_res[0]:  # if no returned values - try the old metric names.
-            filesystem_capacity_query = f'{fs_size_metric_old}{generate_capacity_query_postfix(node)}'
-            self.log.debug(f"filesystem_capacity_query: {filesystem_capacity_query}")
+            filesystem_capacity_query = '{fs_size_metric_old}{node_capacity_query_postfix}'.format(
+                **dict(locals(), **globals()))
+            self.log.debug("filesystem_capacity_query: {filesystem_capacity_query}".format(
+                **dict(locals(), **globals())))
             fs_size_res = self.prometheus_db.query(query=filesystem_capacity_query, start=int(time.time()) - 5,
                                                    end=int(time.time()))
         assert fs_size_res[0], "Could not resolve capacity query result."
@@ -101,29 +113,32 @@ class IcsSpaceAmplificationTest(IcsLongevetyTest):
         return max_used_capacity_gb
 
     def _get_nodes_space_ampl_over_time_gb(self, dict_nodes_initial_capacity, start_time,
-                                           written_data_size_gb=0) -> dict:
+                                           written_data_size_gb=0):
         dict_nodes_space_amplification = {}
         dict_nodes_used_capacity = self._get_nodes_used_capacity()
         for node in self.db_cluster.nodes:
+            node_initial_capacity = dict_nodes_initial_capacity[node.private_ip_address]
+            node_used_capacity = dict_nodes_used_capacity[node.private_ip_address]
             node_max_used_capacity_gb = self._get_max_used_capacity_over_time_gb(node=node,
                                                                                  start_time=start_time)
-            dict_nodes_space_amplification[node.private_ip_address] = node_max_used_capacity_gb - \
-                written_data_size_gb - \
-                dict_nodes_initial_capacity[
-                node.private_ip_address]
+            dict_nodes_space_amplification[node.private_ip_address] = round(node_max_used_capacity_gb -
+                                                                            written_data_size_gb - node_initial_capacity, 2)
             self.log.info(
-                f"Node {node.private_ip_address} used capacity changed from {dict_nodes_initial_capacity[node.private_ip_address]} to {dict_nodes_used_capacity[node.private_ip_address]}.")
-            self.log.info(f"Space amplification is: {dict_nodes_space_amplification[node.private_ip_address]} GB")
+                "Node {node.private_ip_address} used capacity changed from {node_initial_capacity} to {node_used_capacity}.".format(
+                    **dict(locals(), **globals())))
+            self.log.info("Space amplification is: {} GB".format(
+                dict_nodes_space_amplification[node.private_ip_address]))
         return dict_nodes_space_amplification
 
     def _get_nodes_space_amplification_after_write(self, dict_nodes_initial_capacity, written_data_size_gb,
-                                                   start_time) -> dict:
-        self.log.info(f"Space amplification results after a write of: {written_data_size_gb} are:")
+                                                   start_time):
+        self.log.info("Space amplification results after a write of: {written_data_size_gb} are:".format(
+            **dict(locals(), **globals())))
         return self._get_nodes_space_ampl_over_time_gb(dict_nodes_initial_capacity=dict_nodes_initial_capacity,
                                                        written_data_size_gb=written_data_size_gb,
                                                        start_time=start_time)
 
-    def _get_nodes_used_capacity(self) -> dict:
+    def _get_nodes_used_capacity(self):
         """
 
         :rtype: dictionary with capacity per node-ip
@@ -138,30 +153,39 @@ class IcsSpaceAmplificationTest(IcsLongevetyTest):
         compaction_query = "sum(scylla_compaction_manager_compactions{})"
         now = time.time()
         results = self.prometheus_db.query(query=compaction_query, start=now - 60, end=now)
-        self.log.debug(f"scylla_compaction_manager_compactions: {results}")
+        self.log.debug("scylla_compaction_manager_compactions: {results}".format(
+            **dict(locals(), **globals())))
         # if all are zeros the result will be False, otherwise there are still compactions
         if results:
             assert any([float(v[1]) for v in results[0]["values"]]) is False, \
                 "Waiting until all compactions settle down"
 
     def test_ics_space_amplification(self):  # pylint: disable=too-many-locals
+        """
+        Includes 3 space amplification test scenarios for ICS:
+        (1) writing new data.
+        (2) over-writing existing data.
+        (3) running a major compaction.
+        """
         self._pre_create_schema_with_compaction()
         stress_queue = list()
         write_queue = list()
         verify_queue = list()
         column_size = 200
         num_of_columns = 5
+        # The number of writes for the total new data to test. impacts 'total_new_data_to_write_gb'.
         ops_num = 200200300
-        overwrite_ops_num = ops_num // 2
-        total_new_data_to_write_gb = ops_num * column_size * num_of_columns / (1024 ** 3)
-        total_data_to_overwrite_gb = overwrite_ops_num * column_size * num_of_columns / (1024 ** 3)
+        overwrite_ops_num = ops_num // 2  # A half of the data size is used for the over-write data test.
+        # The total data size to test. should be large enough to clearly identify space amplification issue.
+        total_new_data_to_write_gb = round(ops_num * column_size * num_of_columns / (1024 ** 3), 2)
+        total_data_to_overwrite_gb = round(overwrite_ops_num * column_size * num_of_columns / (1024 ** 3), 2)
         keyspace_num = 1
 
         self.log.debug('Test Space-amplification on writing new data')
         prepare_write_cmd = "cassandra-stress write cl=ALL n={ops_num}  -schema 'replication(factor=3)" \
                             " compaction(strategy=LeveledCompactionStrategy)' -port jmx=6868 -mode cql3 native" \
                             " -rate threads=1000 -col 'size=FIXED({column_size}) n=FIXED({num_of_columns})'" \
-                            " -pop seq=1..{ops_num} -log interval=15".format(**locals())
+                            " -pop seq=1..{ops_num} -log interval=15".format(**dict(locals(), **globals()))
         dict_nodes_initial_capacity = self._get_nodes_used_capacity()
         start_time = time.time()
         self._run_all_stress_cmds(write_queue, params={'stress_cmd': prepare_write_cmd,
@@ -180,7 +204,7 @@ class IcsSpaceAmplificationTest(IcsLongevetyTest):
         self.log.debug('Test Space-amplification on over-write data')
         prepare_overwrite_cmd = "cassandra-stress write cl=ALL  n={overwrite_ops_num} -schema 'replication(factor=3) compaction(strategy=LeveledCompactionStrategy)' -port jmx=6868 -mode cql3 native" \
                                 " -rate threads=1000 -col 'size=FIXED({column_size}) n=FIXED({num_of_columns})' -pop 'dist=uniform(1..{overwrite_ops_num})' ".format(
-                                    **locals())
+                                    **dict(locals(), **globals()))
 
         verify_overwrite_queue = list()
         self.log.debug('Total data to write per cycle is: {} GB '.format(total_data_to_overwrite_gb))
@@ -215,8 +239,12 @@ class IcsSpaceAmplificationTest(IcsLongevetyTest):
 def verify_nodes_space_amplification(dict_nodes_space_amplification):
     for node_ip, space_amplification_gb in dict_nodes_space_amplification.items():
         assert space_amplification_gb < MAX_ICS_SPACE_AMPLIFICATION_ALLOWED_GB, \
-            f'Node {node_ip} space amplification of: {space_amplification_gb} exceeds the maximum allowed ({MAX_ICS_SPACE_AMPLIFICATION_ALLOWED_GB})'
+            'Node {node_ip} space amplification of: {space_amplification_gb} exceeds the maximum allowed ({MAX_ICS_SPACE_AMPLIFICATION_ALLOWED_GB})'.format(
+                **dict(locals(), **globals()))
 
 
-def generate_capacity_query_postfix(node):
-    return f'{{mountpoint="{SCYLLA_DIR}", instance=~".*?{node.private_ip_address}.*?"}}'
+def generate_node_capacity_query_postfix(node):
+    node_ip = node.private_ip_address  # due to pylint bug
+    scylla_dir = SCYLLA_DIR  # due to pylint bug
+    return '{{mountpoint="{scylla_dir}", instance=~".*?{node_ip}.*?"}}'.format(
+        **dict(locals(), **globals()))
