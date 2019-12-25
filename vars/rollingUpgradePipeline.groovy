b769dbb8d24c38db4eb1c10c5a5d49481c9ad827
Israel Fruchter <fruch@scylladb.com>
2019-12-25 16:35:34 +0200

    Fix #1616: error handling in rolling upgrade pipeline (#1617)
    (cherry picked from commit f3c3ce6e7c64e7823edcc79e316734f28b502d46)

--- vars/rollingUpgradePipeline.groovy
+++ vars/rollingUpgradePipeline.groovy
@@ -54,40 +54,104 @@ def call(Map pipelineParams) {
                                     withEnv(["AWS_ACCESS_KEY_ID=${env.AWS_ACCESS_KEY_ID}",
                                              "AWS_SECRET_ACCESS_KEY=${env.AWS_SECRET_ACCESS_KEY}",]) {
 
-                                        wrap([$class: 'BuildUser']) {
-                                            dir('scylla-cluster-tests') {
-                                                checkout scm
+                                        stage("Upgrade from ${base_version}") {
+                                            catchError(stageResult: 'FAILURE') {
+                                                wrap([$class: 'BuildUser']) {
+                                                    dir('scylla-cluster-tests') {
+                                                        checkout scm
+                                                        sh """
+                                                        #!/bin/bash
+                                                        set -xe
+                                                        env
+                                                        export SCT_NEW_CONFIG=yes
+                                                        export SCT_CLUSTER_BACKEND=gce
 
-                                                sh """
-                                                #!/bin/bash
-                                                set -xe
-                                                env
-                                                export SCT_NEW_CONFIG=yes
-                                                export SCT_CLUSTER_BACKEND=gce
+                                                        export SCT_CONFIG_FILES=${pipelineParams.test_config}
+                                                        export SCT_SCYLLA_VERSION=${base_version}
+                                                        export SCT_NEW_SCYLLA_REPO=${pipelineParams.params.new_scylla_repo}
 
-                                                export SCT_CONFIG_FILES=${pipelineParams.test_config}
-                                                export SCT_SCYLLA_VERSION=${base_version}
-                                                export SCT_NEW_SCYLLA_REPO=${pipelineParams.params.new_scylla_repo}
+                                                        export SCT_POST_BEHAVIOR_DB_NODES="${params.post_behavior_db_nodes}"
+                                                        export SCT_POST_BEHAVIOR_LOADER_NODES="${params.post_behavior_loader_nodes}"
+                                                        export SCT_POST_BEHAVIOR_MONITOR_NODES="${params.post_behavior_monitor_nodes}"
+                                                        export SCT_INSTANCE_PROVISION=${pipelineParams.params.get('provision_type', '')}
+                                                        export SCT_AMI_ID_DB_SCYLLA_DESC=\$(echo \$GIT_BRANCH | sed -E 's+(origin/|origin/branch-)++')
+                                                        export SCT_AMI_ID_DB_SCYLLA_DESC=\$(echo \$SCT_AMI_ID_DB_SCYLLA_DESC | tr ._ - | cut -c1-8 )
 
-                                                export SCT_POST_BEHAVIOR_DB_NODES="${params.post_behavior_db_nodes}"
-                                                export SCT_POST_BEHAVIOR_LOADER_NODES="${params.post_behavior_loader_nodes}"
-                                                export SCT_POST_BEHAVIOR_MONITOR_NODES="${params.post_behavior_monitor_nodes}"
-                                                export SCT_INSTANCE_PROVISION=${pipelineParams.params.get('provision_type', '')}
-                                                export SCT_AMI_ID_DB_SCYLLA_DESC=\$(echo \$GIT_BRANCH | sed -E 's+(origin/|origin/branch-)++')
-                                                export SCT_AMI_ID_DB_SCYLLA_DESC=\$(echo \$SCT_AMI_ID_DB_SCYLLA_DESC | tr ._ - | cut -c1-8 )
+                                                        export SCT_GCE_IMAGE_DB=${pipelineParams.gce_image_db}
+                                                        export SCT_SCYLLA_LINUX_DISTRO=${pipelineParams.linux_distro}
+                                                        export SCT_AMI_ID_DB_SCYLLA_DESC="\$SCT_AMI_ID_DB_SCYLLA_DESC-\$SCT_SCYLLA_LINUX_DISTRO"
 
-                                                export SCT_GCE_IMAGE_DB=${pipelineParams.gce_image_db}
-                                                export SCT_SCYLLA_LINUX_DISTRO=${pipelineParams.linux_distro}
-                                                export SCT_AMI_ID_DB_SCYLLA_DESC="\$SCT_AMI_ID_DB_SCYLLA_DESC-\$SCT_SCYLLA_LINUX_DISTRO"
+                                                        export SCT_WORKAROUND_KERNEL_BUG_FOR_IOTUNE=${pipelineParams.workaround_kernel_bug_for_iotune}
 
-                                                export SCT_WORKAROUND_KERNEL_BUG_FOR_IOTUNE=${pipelineParams.workaround_kernel_bug_for_iotune}
+                                                        echo "start test ......."
+                                                        ./docker/env/hydra.sh run-test ${pipelineParams.test_name} --backend ${params.backend}  --logdir /sct
+                                                        echo "end test ....."
+                                                        """
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        stage("Collect logs for Upgrade from ${base_version}") {
+                                            catchError(stageResult: 'FAILURE') {
+                                                wrap([$class: 'BuildUser']) {
+                                                    dir('scylla-cluster-tests') {
+                                                        def test_config = groovy.json.JsonOutput.toJson(pipelineParams.test_config)
+                                                        sh """
+                                                        #!/bin/bash
 
-                                                export SCT_COLLECT_LOGS=true
-                                                export SCT_EXECUTE_POST_BEHAVIOR=true
-                                                echo "start test ......."
-                                                ./docker/env/hydra.sh run-test ${pipelineParams.test_name} --backend ${params.backend}  --logdir /sct
-                                                echo "end test ....."
-                                                """
+                                                        set -xe
+                                                        env
+
+                                                        export SCT_CLUSTER_BACKEND=gce
+                                                        export SCT_CONFIG_FILES=${pipelineParams.test_config}
+
+                                                        echo "start collect logs ..."
+                                                        ./docker/env/hydra.sh collect-logs --logdir /sct --backend gce
+                                                        echo "end collect logs"
+                                                        """
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        stage("Clean resources for Upgrade from ${base_version}") {
+                                            catchError(stageResult: 'FAILURE') {
+                                                wrap([$class: 'BuildUser']) {
+                                                    dir('scylla-cluster-tests') {
+                                                        def test_config = groovy.json.JsonOutput.toJson(pipelineParams.test_config)
+                                                        sh """
+                                                        #!/bin/bash
+
+                                                        set -xe
+                                                        env
+
+                                                        export SCT_POST_BEHAVIOR_DB_NODES="${params.post_behavior_db_nodes}"
+                                                        export SCT_POST_BEHAVIOR_LOADER_NODES="${params.post_behavior_loader_nodes}"
+                                                        export SCT_POST_BEHAVIOR_MONITOR_NODES="${params.post_behavior_monitor_nodes}"
+
+                                                        echo "start clean resources ..."
+                                                        ./docker/env/hydra.sh clean-resources --logdir /sct --backend gce
+                                                        echo "end clean resources"
+                                                        """
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        stage("Send email for Upgrade from ${base_version}") {
+                                            catchError(stageResult: 'FAILURE') {
+                                                wrap([$class: 'BuildUser']) {
+                                                    dir('scylla-cluster-tests') {
+                                                        sh """
+                                                        #!/bin/bash
+
+                                                        set -xe
+                                                        env
+
+                                                        echo "Start send email ..."
+                                                        ./docker/env/hydra.sh send-email --logdir /sct --email-recipients "${email_recipients}"
+                                                        echo "Email sent"
+                                                        """
+                                                    }
+                                                }
                                             }
                                         }
                                     }
