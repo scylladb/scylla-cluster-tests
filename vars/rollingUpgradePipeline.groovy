8f79c1d3aa476f1bd28294fed486d4c3fcd31137
Aleksandr Bykov <alex.bykov@scylladb.com>
2019-12-25 20:20:22 +0700

    fixing stage result for rolling upgrade pipeline #1616

--- vars/rollingUpgradePipeline.groovy
+++ vars/rollingUpgradePipeline.groovy
@@ -62,7 +62,7 @@ def call(Map pipelineParams) {
                                             wrap([$class: 'BuildUser']) {
                                                 dir('scylla-cluster-tests') {
                                                     checkout scm
-                                                    try {
+                                                    catchError(stageResult: 'FAILURE') {
                                                         sh """
                                                         #!/bin/bash
                                                         set -xe
@@ -91,9 +91,6 @@ def call(Map pipelineParams) {
                                                         ./docker/env/hydra.sh run-test ${pipelineParams.test_name} --backend ${params.backend}  --logdir /sct
                                                         echo "end test ....."
                                                         """
-                                                    } catch (error) {
-                                                        echo "FAILED"
-                                                        sh """echo ${error}"""
                                                     }
                                                 }
                                             }
@@ -102,7 +99,7 @@ def call(Map pipelineParams) {
                                             wrap([$class: 'BuildUser']) {
                                                 dir('scylla-cluster-tests') {
                                                     def test_config = groovy.json.JsonOutput.toJson(pipelineParams.test_config)
-                                                    try {
+                                                    catchError(stageResult: 'FAILURE') {
                                                         sh """
                                                         #!/bin/bash
 
@@ -116,8 +113,6 @@ def call(Map pipelineParams) {
                                                         ./docker/env/hydra.sh collect-logs --logdir /sct --backend gce
                                                         echo "end collect logs"
                                                         """
-                                                    } catch (error) {
-                                                        sh """echo ${error}"""
                                                     }
                                                 }
                                             }
@@ -126,7 +121,7 @@ def call(Map pipelineParams) {
                                             wrap([$class: 'BuildUser']) {
                                                 dir('scylla-cluster-tests') {
                                                     def test_config = groovy.json.JsonOutput.toJson(pipelineParams.test_config)
-                                                    try {
+                                                    catchError(stageResult: 'FAILURE') {
                                                         sh """
                                                         #!/bin/bash
 
@@ -141,8 +136,6 @@ def call(Map pipelineParams) {
                                                         ./docker/env/hydra.sh clean-resources --logdir /sct --backend gce
                                                         echo "end clean resources"
                                                         """
-                                                    } catch (error) {
-                                                        sh """echo ${error}"""
                                                     }
                                                 }
                                             }
@@ -150,7 +143,7 @@ def call(Map pipelineParams) {
                                         stage("Send email for Upgrade from ${base_version}") {
                                             wrap([$class: 'BuildUser']) {
                                                 dir('scylla-cluster-tests') {
-                                                    try {
+                                                    catchError(stageResult: 'FAILURE') {
                                                         sh """
                                                         #!/bin/bash
 
@@ -161,8 +154,6 @@ def call(Map pipelineParams) {
                                                         ./docker/env/hydra.sh send-email --logdir /sct --email-recipients "${email_recipients}"
                                                         echo "Email sent"
                                                         """
-                                                    } catch (error) {
-                                                        sh """echo ${error}"""
                                                     }
                                                 }
                                             }
