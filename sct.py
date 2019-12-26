08f248f7b548fa4250d1de86e8f78298ebbff417
Aleksandr Bykov <alex.bykov@scylladb.com>
2019-12-26 19:21:04 +0700

    fix issue #1622
    Add to sct.py send_email checks:
    if email-recipients is empty, don't send email

--- sct.py
+++ sct.py
@@ -456,6 +456,10 @@ def send_email(test_id=None, email_recipients=None, logdir=None):
     from sdcm.send_email import (GeminiEmailReporter, LongevityEmailReporter,
                                  get_running_instances_for_email_report,
                                  read_email_data_from_file)
+    LOGGER.info(email_recipients)
+    if not email_recipients:
+        LOGGER.warning("No email recipients. Email will not be sent")
+        return
 
     if not logdir:
         logdir = os.path.expanduser('~/sct-results')
