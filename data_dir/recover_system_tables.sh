2961d7d45bbea102c4e71157c77c96d976722624
Amos Kong <amos@scylladb.com>
2019-12-26 13:26:25 +0800

    clean sstables before recovering the system tables
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- data_dir/recover_system_tables.sh
+++ data_dir/recover_system_tables.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-DIR='/var/lib/scylla/data/system';
-for i in `sudo ls $DIR`;
-do
-    sudo test -e $DIR/$i/snapshots/$1 && sudo find $DIR/$i/snapshots/$1 -type f -exec sudo /bin/cp {} $DIR/$i/ \;
+for DIR in '/var/lib/scylla/data/system' '/var/lib/scylla/data/system_schema'; do
+    for i in `sudo ls $DIR`;
+    do
+        sudo test -e $DIR/$i/snapshots/$1 && (sudo rm -f $DIR/$i/* || true) && sudo find $DIR/$i/snapshots/$1 -type f -exec sudo -u scylla /bin/cp {} $DIR/$i/ \;
+    done
 done
+
 
 # Some new table may not exist in old version, `test -e` might failed (return -1),
 # it might be return to this script.
