a88f1f0a1ab954f90a2a6a9a9bf7469cf65657e4
Israel Fruchter <fruch@scylladb.com>
2020-01-08 08:59:35 +0200

    Add support using scylla-cloud-image (#1655)

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -697,6 +697,7 @@ class BaseNode():  # pylint: disable=too-many-instance-attributes,too-many-publi
                 # Here we're assuming that journalctl systems are Scylla images
                 db_services_log_cmd = (f'sudo journalctl -f --no-tail --no-pager --utc {since}'
                                        '-u scylla-ami-setup.service '
+                                       '-u scylla-image-setup.service '
                                        '-u scylla-io-setup.service '
                                        '-u scylla-server.service '
                                        '-u scylla-jmx.service')
