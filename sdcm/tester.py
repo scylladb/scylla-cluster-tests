d1196105ced65d3755877eda7ab7fa4ca9e1ca3f
Roy Dahan <roy@scylladb.com>
2020-01-09 19:28:21 +0200

    Replace the current version tagging of resources (#1670)
    Till now the version tag was based on GIT_BRANCH which created a mess when people used their private branches.
    We would like to have it better tracked in our AWS reports and track a version cost.
    In order to do that, we will use the root dir of job name to identify which job it's related to.
    For example,
    JOB_NAME=scylla-master/longevity/longevity-50gb-4days
    The job will be tagged with version scylla-master.
    In case of a missing JOB_NAME (can happen when one run test locally and doesn't pass the JOB_NAME param), it'll be tagged as "unknown".

--- sdcm/tester.py
+++ sdcm/tester.py
@@ -178,9 +178,6 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
         cluster.Setup.set_intra_node_comm_public(self.params.get(
             'intra_node_comm_public') or cluster.Setup.MULTI_REGION)
 
-        version_tag = self.params.get('ami_id_db_scylla_desc')
-        if version_tag:
-            cluster.Setup.tags('version', version_tag)
         # for saving test details in DB
         self.create_stats = self.params.get(key='store_results_in_elasticsearch', default=True)
         self.scylla_dir = SCYLLA_DIR
