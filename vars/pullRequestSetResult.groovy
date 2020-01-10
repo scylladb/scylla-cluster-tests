6773497f1a0dd06142994eef8be64f02ed900c4e
Dmitry Kropachev <dmitry.kropachev@scylladb.com>
2020-01-10 14:19:26 +0700

    Add test-provision-gce

---
+++ vars/pullRequestSetResult.groovy
@@ -0,0 +1,13 @@
+#!groovy
+
+def call(String status, String context, String description){
+	if (env.CHANGE_ID) {
+		pullRequest.createStatus(status: status,
+			context: context,
+			description: description,
+			targetUrl: "${env.JOB_URL}/workflow-stage")
+	}
+	if (status == 'failure') {
+		currentBuild.result = 'FAILURE'
+	}
+}
