3a972274c6dc49247c78e905b07a93cdf56ac438
piodul <4950936+piodul@users.noreply.github.com>
2020-01-14 23:25:21 +0100

    Wait for target node to be UN before waiting for hints in ValidateHintedHandoffShortDowntime (#1668)
    * sdcm/cluster: allow to specify verification_node for UN check
    
    * nemesis: wait for node to be seen as UN before waiting for hints
    
    A node won't start sending hints to a target node until it sees it as
    UN. This change modifies ValidateHintedHandoffShortDowntime nemesis so
    that it waits for this condition to become true before starting to wait
    for hints to complete sending.
    
    Co-authored-by: Bentsi <bentsi@scylladb.com>
    (cherry picked from commit 193f4764a5e1868afc8ce8e5e3f00a69858ad2cb)

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -2878,11 +2878,11 @@ class BaseScyllaCluster(object):  # pylint: disable=too-many-public-methods
         ClusterHealthValidatorEvent(type='done', name='ClusterHealthCheck', status=Severity.NORMAL,
                                     message='Cluster health check finished')
 
-    def check_nodes_up_and_normal(self, nodes=None):
+    def check_nodes_up_and_normal(self, nodes=None, verification_node=None):
         """Checks via nodetool that node joined the cluster and reached 'UN' state"""
         if not nodes:
-            nodes = self.nodes  # pylint: disable=no-member
-        status = self.get_nodetool_status()
+            nodes = self.nodes
+        status = self.get_nodetool_status(verification_node=verification_node)
         up_statuses = []
         for node in nodes:
             for dc_status in status.values():
@@ -2896,8 +2896,8 @@ class BaseScyllaCluster(object):  # pylint: disable=too-many-public-methods
 
     @retrying(n=30, sleep_time=3, allowed_exceptions=(ClusterNodesNotReady,),
               message="Waiting for nodes to join the cluster")
-    def wait_for_nodes_up_and_normal(self, nodes):
-        self.check_nodes_up_and_normal(nodes=nodes)
+    def wait_for_nodes_up_and_normal(self, nodes, verification_node=None):
+        self.check_nodes_up_and_normal(nodes=nodes, verification_node=verification_node)
 
     def get_scylla_version(self):
         if not self.nodes[0].scylla_version:  # pylint: disable=no-member
