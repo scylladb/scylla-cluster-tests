464c49fddf451887fb373cefe099190d47c051f5
Dmitry Kropachev <dmitry.kropachev@scylladb.com>
2020-01-17 10:46:49 +0700

    fix set_hostname: Remove single quotes from command

--- sdcm/cluster_aws.py
+++ sdcm/cluster_aws.py
@@ -461,9 +461,13 @@ class AWSNode(cluster.BaseNode):
         if result.ok:
             self.log.debug('Hostname has been changed succesfully. Apply')
             apply_hostname_change_script = dedent(f"""
-                systemctl restart rsyslog
+                if ! grep "\\$LocalHostname {self.name}" /etc/rsyslog.conf; then
+                    echo "" >> /etc/rsyslog.conf
+                    echo "\\$LocalHostname {self.name}" >> /etc/rsyslog.conf
+                fi
                 grep -P "127.0.0.1[^\\\\n]+{self.name}" || sed -ri "s/(127.0.0.1[ \\t]+localhost[^\\n]*)$/\\1\\t{self.name}/" /etc/hosts
                 grep "preserve_hostname: true" /etc/cloud/cloud.cfg 1>/dev/null 2>&1 || echo "preserve_hostname: true" >> /etc/cloud/cloud.cfg
+                systemctl restart rsyslog
             """)
             self.remoter.run(f"sudo bash -cxe '{apply_hostname_change_script}'")
             self.log.debug('Continue node %s set up', self.name)
