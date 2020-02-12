6cdabdd6f0e4c8b1da0ff6aa1886e7a954800d8f
Israel Fruchter <fruch@scylladb.com>
2020-02-12 15:37:00 +0200

    feature(alternator): a way to tag write_isolation for dynamodb tables (#1807)
    new SCT config `alternator_write_isolation` that can be controlled to tag
    alternator table is using or not LWT
    
    Ref: [alternator write-isolation-policies docs](
    https://github.com/scylladb/scylla/blob/master/
    docs/alternator/alternator.md#write-isolation-policies)

--- sdcm/utils/alternator.py
+++ sdcm/utils/alternator.py
@@ -8,6 +8,7 @@ LOGGER = logging.getLogger(__name__)
 
 def create_table(endpoint_url, test_params):
     dynamodb_primarykey_type = test_params.get('dynamodb_primarykey_type', 'HASH')
+    write_isolation = test_params.get('alternator_write_isolation')
 
     try:
         dynamodb = boto3.resource('dynamodb', endpoint_url=endpoint_url)
@@ -40,6 +41,21 @@ def create_table(endpoint_url, test_params):
         waiter.config.max_attempts = 100
         waiter.wait(TableName=name)
 
+        if write_isolation:
+            set_write_isolation(table, write_isolation)
+
     except ClientError as ex:
         LOGGER.warning(str(ex))
         assert 'already exists' in str(ex)
+
+
+def set_write_isolation(table, isolation):
+    got = table.meta.client.describe_table(TableName=table.name)['Table']
+    arn = got['TableArn']
+    tags = [
+        {
+            'Key': 'system:write_isolation',
+            'Value': isolation
+        }
+    ]
+    table.meta.client.tag_resource(ResourceArn=arn, Tags=tags)
