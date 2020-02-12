208028f4567e67ca8b325ee06441524cfa2e4963
Israel Fruchter <fruch@scylladb.com>
2020-02-12 10:44:41 +0200

    feature(alternator): a way to tag write_isolation for dynamodb tables
    new SCT config `alternator_write_isolation` that can be controlled to tag
    alternator table is using or not LWT
    
    Ref: [alternator write-isolation-policies docs](
    https://github.com/scylladb/scylla/blob/master/
    docs/alternator/alternator.md#write-isolation-policies)

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -64,8 +64,8 @@ def docker_scylla():
     entryfile_path = Path(base_dir) if base_dir else Path(__file__).parent.parent
     entryfile_path = entryfile_path.joinpath('./docker/scylla-sct/entry.sh')
 
-    scylla = RemoteDocker(LocalNode(), image_name="scylladb/scylla:3.2.0",
-                          command_line="--smp 1 --alternator-port 8000", extra_docker_opts=f'-p 8000 --cpus="1" -v {entryfile_path}:/entry.sh --entrypoint /entry.sh')
+    scylla = RemoteDocker(LocalNode(), image_name="scylladb/scylla-nightly:666.development-202002120417",
+                          command_line="--smp 1 --alternator-port 8000 --experimental 1", extra_docker_opts=f'-p 8000 --cpus="1" -v {entryfile_path}:/entry.sh --entrypoint /entry.sh')
 
     def db_up():
         try:
