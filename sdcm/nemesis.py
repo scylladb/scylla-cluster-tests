63f6fce4d2ca224611431543b13516c2cf70c310
Yaron Gilor <yarongilor@scylladb.com>
2020-03-04 12:14:29 +0200

    fix(nemesis.py): increase node init timeout
    	Since in 3.3 node bootstrap takes more than 3 hours , this will prevent nemesis from fail.
    
    	Fixes #1821
    
    (cherry picked from commit f81a3c580f60beffa2c575ffd91053bdbd7cd523)

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -73,6 +73,8 @@ class Nemesis():  # pylint: disable=too-many-instance-attributes,too-many-public
     disruptive = False
     run_with_gemini = True
     networking = False
+    MINUTE_IN_SEC = 60
+    HOUR_IN_SEC = 60 * MINUTE_IN_SEC
 
     def __init__(self, tester_obj, termination_event):
         self.tester = tester_obj  # ClusterTester object
@@ -415,7 +417,7 @@ class Nemesis():  # pylint: disable=too-many-instance-attributes,too-many-public
             self.target_node.start_scylla_server(verify_up=True, verify_down=False)
 
     @retrying(n=3, sleep_time=60, allowed_exceptions=(NodeSetupFailed, NodeSetupTimeout))
-    def _add_and_init_new_cluster_node(self, old_node_ip=None, timeout=10800):
+    def _add_and_init_new_cluster_node(self, old_node_ip=None, timeout=HOUR_IN_SEC * 6):
         """When old_node_private_ip is not None replacement node procedure is initiated"""
         self.log.info("Adding new node to cluster...")
         new_node = self.cluster.add_nodes(count=1, dc_idx=self.target_node.dc_idx, enable_auto_bootstrap=True)[0]
