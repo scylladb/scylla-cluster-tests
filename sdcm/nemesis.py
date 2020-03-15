266604675192ff6638097e8455ad01fb722596d2
Roy Dahan <roy@scylladb.com>
2020-03-15 20:29:27 +0200

    fix(nemesis): Change Networking nemesis to be 'Disruptive' as they should
    (cherry picked from commit 415b53304e4240caa13a789c6b253b15f79ecc88)

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -2155,7 +2155,7 @@ class MgmtRepair(Nemesis):
 
 
 class AbortRepairMonkey(Nemesis):
-    disruptive = True
+    disruptive = False
 
     @log_time_elapsed_and_status
     def disrupt(self):
@@ -2203,7 +2203,7 @@ class TopPartitions(Nemesis):
 
 
 class RandomInterruptionNetworkMonkey(Nemesis):
-    disruptive = False
+    disruptive = True
     networking = True
     run_with_gemini = False
 
@@ -2213,7 +2213,7 @@ class RandomInterruptionNetworkMonkey(Nemesis):
 
 
 class BlockNetworkMonkey(Nemesis):
-    disruptive = False
+    disruptive = True
     networking = True
     run_with_gemini = False
 
@@ -2223,7 +2223,7 @@ class BlockNetworkMonkey(Nemesis):
 
 
 class RejectInterNodeNetworkMonkey(Nemesis):
-    disruptive = False
+    disruptive = True
     networking = True
     run_with_gemini = False
 
@@ -2233,7 +2233,7 @@ class RejectInterNodeNetworkMonkey(Nemesis):
 
 
 class RejectNodeExporterNetworkMonkey(Nemesis):
-    disruptive = False
+    disruptive = True
     networking = True
     run_with_gemini = False
 
@@ -2243,7 +2243,7 @@ class RejectNodeExporterNetworkMonkey(Nemesis):
 
 
 class RejectThriftNetworkMonkey(Nemesis):
-    disruptive = False
+    disruptive = True
     networking = True
     run_with_gemini = False
 
@@ -2253,7 +2253,7 @@ class RejectThriftNetworkMonkey(Nemesis):
 
 
 class StopStartInterfacesNetworkMonkey(Nemesis):
-    disruptive = False
+    disruptive = True
     networking = True
     run_with_gemini = False
 
