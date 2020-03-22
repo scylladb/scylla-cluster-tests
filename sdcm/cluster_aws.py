2921c8db8b71f3e81a68e58c902d4883803f8180
Israel Fruchter <fruch@scylladb.com>
2020-03-22 15:33:16 +0200

    feature(cluster_aws): read user_data_format_version from AMI tags
    renaming the old `sci_version` tag to `user_data_format_version`
    
    Ref: scylladb/scylla-machine-image#21
    (cherry picked from commit 1b99e85151d23582489fff8dd6fc816546681c3e)

--- sdcm/cluster_aws.py
+++ sdcm/cluster_aws.py
@@ -758,9 +758,12 @@ class ScyllaAWSCluster(cluster.BaseScyllaCluster, AWSCluster):
         shortid = str(cluster_uuid)[:8]
         name = '%s-%s' % (cluster_prefix, shortid)
 
-        scylla_cloud_image_version = get_ami_tags(ec2_ami_id[0], region_name=params.get(
-            'region_name').split()[0]).get('sci_version', '1')
-        if LooseVersion(scylla_cloud_image_version) >= LooseVersion('2'):
+        ami_tags = get_ami_tags(ec2_ami_id[0], region_name=params.get('region_name').split()[0])
+        # TODO: remove once all other related code is merged in scylla-pkg and scylla-machine-image
+        user_data_format_version = ami_tags.get('sci_version', '1')
+        user_data_format_version = ami_tags.get('user_data_format_version', user_data_format_version)
+
+        if LooseVersion(user_data_format_version) >= LooseVersion('2'):
             user_data = dict(scylla_yaml=dict(cluster_name=name), start_scylla_on_first_boot=False)
         else:
             user_data = ('--clustername %s '
