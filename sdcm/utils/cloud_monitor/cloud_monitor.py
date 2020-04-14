d34933b2b7afee90fe0fb9c9cb0a5b6d0ad025ee
Bentsi Magidovich <bentsi@scylladb.com>
2020-04-14 21:43:39 +0300

    feature(cloud-monitor): added keep alive information
    add keep alive data to the email and to the attached html

--- sdcm/utils/cloud_monitor/cloud_monitor.py
+++ sdcm/utils/cloud_monitor/cloud_monitor.py
@@ -2,14 +2,14 @@ from datetime import datetime
 from logging import getLogger
 from sdcm.send_email import Email
 from sdcm.utils.common import list_instances_aws, list_instances_gce, aws_tags_to_dict, gce_meta_to_dict
-from sdcm.utils.cloud_monitor.report import GeneralReport, DetailedReport
+from sdcm.utils.cloud_monitor.report import GeneralReport, DetailedReport, NA
 
 
 LOGGER = getLogger(__name__)
 
 
 class CloudInstance:  # pylint: disable=too-few-public-methods,too-many-instance-attributes
-    def __init__(self, cloud, name, region_az, state, lifecycle, instance_type, owner, create_time):  # pylint: disable=too-many-arguments
+    def __init__(self, cloud, name, region_az, state, lifecycle, instance_type, owner, create_time, keep):  # pylint: disable=too-many-arguments
         self.cloud = cloud
         self.name = name
         self.region_az = region_az
@@ -18,6 +18,7 @@ class CloudInstance:  # pylint: disable=too-few-public-methods,too-many-instance
         self.instance_type = instance_type
         self.owner = owner.lower()
         self.create_time = create_time
+        self.keep = keep  # keep alive
 
 
 class CloudInstances:
@@ -31,19 +32,32 @@ class CloudInstances:
     def __getitem__(self, item):
         return self.instances[item]
 
+    @staticmethod
+    def get_keep_alive_gce_instance(instance):
+        # checking labels
+        labels = instance.extra["labels"]
+        if labels:
+            return labels.get("keep", labels.get("keep-alive", ""))
+        # checking tags
+        tags = instance.extra["tags"]
+        if tags:
+            return "alive" if 'alive' in tags or 'keep-alive' in tags or 'keep' in tags else ""
+        return ""
+
     def get_aws_instances(self):
         aws_instances = list_instances_aws(verbose=True)
         for instance in aws_instances:
             tags = aws_tags_to_dict(instance.get('Tags'))
             cloud_instance = CloudInstance(
                 cloud="aws",
-                name=tags.get("Name", "N/A"),
+                name=tags.get("Name", NA),
                 region_az=instance["Placement"]["AvailabilityZone"],
                 state=instance["State"]["Name"],
                 lifecycle="spot" if instance.get("SpotInstanceRequestId") else "on-demand",
                 instance_type=instance["InstanceType"],
-                owner=tags.get("RunByUser", tags.get("Owner", "N/A")),
+                owner=tags.get("RunByUser", tags.get("Owner", NA)),
                 create_time=instance['LaunchTime'].ctime(),
+                keep=tags.get("keep", ""),
             )
             self.instances["aws"].append(cloud_instance)
         self.all_instances += self.instances["aws"]
@@ -59,8 +73,9 @@ class CloudInstances:
                 state=instance.state,
                 lifecycle="spot" if instance.extra["scheduling"]["preemptible"] else "on-demand",
                 instance_type=instance.size,
-                owner=tags.get('RunByUser', 'N/A') if tags else "N/A",
+                owner=tags.get("RunByUser", NA) if tags else NA,
                 create_time=instance.extra['creationTimestamp'],
+                keep=self.get_keep_alive_gce_instance(instance)
             )
             self.instances["gce"].append(cloud_instance)
         self.all_instances += self.instances["gce"]
@@ -74,7 +89,7 @@ class CloudInstances:
 def notify_by_email(general_report: GeneralReport, detailed_report: DetailedReport, recipients: list):
     email_client = Email()
     LOGGER.info("Sending email to '%s'", recipients)
-    email_client.send(subject="Cloud resources report - {}".format(datetime.now()),
+    email_client.send(subject="Cloud resources: usage report - {}".format(datetime.now()),
                       content=general_report.to_html(),
                       recipients=recipients,
                       html=True,
