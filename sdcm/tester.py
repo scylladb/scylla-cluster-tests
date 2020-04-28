2f17fdaa10aafb21c467516168477639ad3ae764
juliayakovlev <julia.yakovlev@scylladb.com>
2020-04-28 10:17:24 +0300

    fix(full scan): set warning event if full scan failed with timeout
    If Full Scan because of 'Client request timeout.', set WARNING event
    instead of ERROR
    
    (cherry picked from commit 91b685d81566d7d09f505071706e6f664724a38f)

--- sdcm/tester.py
+++ sdcm/tester.py
@@ -867,7 +867,8 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
             # 'unpack requires a string argument of length 4' error is received when cassandra.connection return
             # "Error decoding response from Cassandra":
             # failure like: Operation failed for keyspace1.standard1 - received 0 responses and 1 failures from 1 CL=ONE
-            if 'timed out' in str(details) or 'unpack requires' in str(details) or db_node.running_nemesis:
+            if 'timed out' in str(details) or 'unpack requires' in str(details) or 'timeout' in str(details)\
+                    or db_node.running_nemesis:
                 severity = Severity.WARNING
             else:
                 severity = Severity.ERROR
