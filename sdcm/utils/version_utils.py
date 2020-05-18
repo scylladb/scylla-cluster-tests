852aea3718b8d0e429f08dc3d4563a772d9be056
Oren Efraimov <oren.efraimov@scylladb.com>
2020-05-18 16:43:19 +0300

    improvement(version_utils): Extract version for any URL
    * version_utils.py:
       - Moving all constants to top of the file
       - Creating multiple new methods:
         - get_branch_version - Find all URL patterns
         - extract_all_urls_from_scylla_patterns - Create
            for each pattern all URLs options
         - validate_url_scylla_version_file - Validate
            for each URL that the file exists and not
            empty
         - extract_all_scylla_urls_from_file - Extract
            from PRM/DEV URL file all Scylla version URLs
    * Adding validation for all URLs in `sct_config`
    * Fixing files yaml:
       * complex_test_case_with_version.yaml - Used Scylla manager version 2.0
       * multi_region_dc_test_case.yaml - Remove useless variables:
         - scylla_repo_m
         - scylla_mgmt_repo
         - scylla_mgmt_upgrade_to_repo
    * Fix multiple Pylint issues: E501, E126 and E265
    
    Trello url: https://trello.com/c/y2WdQpkz/1777-verify-links-in-repo-and-list-files

--- sdcm/utils/version_utils.py
+++ sdcm/utils/version_utils.py
@@ -1,68 +1,150 @@
 import re
+from collections import namedtuple
+from enum import Enum, auto
+from string import Template
 
 import requests
 from pkg_resources import parse_version
 from repodataParser.RepoParser import Parser
 
+from sdcm.utils.common import ParallelObject
 
 # Examples of ScyllaDB version strings:
 #   - 666.development-0.20200205.2816404f575
 #   - 3.3.rc1-0.20200209.0d0c1d43188
 #   - 2019.1.4-0.20191217.b59e92dbd
+
+# gemini version 1.0.1, commit ef7c6f422c78ef6b84a6f3bccf52ea9ec846bba0, date 2019-05-16T09:56:16Z
+GEMINI_VERSION_RE = re.compile(r'\s(?P<gemini_version>([\d]+\.[\d]+\.[\d]+)?),')
+REPO_VERSIONS_REGEX = re.compile(r'Version: (.*?)\n', re.DOTALL)
+
 SCYLLA_VERSION_RE = re.compile(r"\d+(\.\d+)?\.[\d\w]+([.~][\d\w]+)?")
+PRIMARY_XML_GZ_REGEX = re.compile(r'="(.*?primary.xml.gz)"')
+
+REPOMD_XML_PATH = "repodata/repomd.xml"
+SCYLLA_URL_REGEX = re.compile(".*(?P<url>http.*scylladb.com.*).*")
+
+SCYLLA_URL_RESPONSE_TIMEOUT = 30
+SUPPORTED_XML_EXTENSIONS = ("xml", "xml.gz")
+SUPPORTED_FILE_EXTENSIONS = ("list", "repo", "Packages", "gz") + SUPPORTED_XML_EXTENSIONS
+VERSION_NOT_FOUND_ERROR = "The URL not supported, only Debian and Yum are supported"
+
+
+class ScyllaFileType(Enum):
+    DEBIAN = auto()
+    YUM = auto()
+
+
+FILE_REGEX_DICT = {
+    ScyllaFileType.DEBIAN: [
+        (re.compile(r"deb\s+\[arch=(?P<arch>.*?)\]\s(?P<url>http.*?)\s(?P<version_code_name>.*?)\s(?P<component>.*?)$"),
+         "{url}/dists/{version_code_name}/{component}/binary-{arch}/Packages"),
+    ],
+    ScyllaFileType.YUM:
+        [
+            (re.compile(r"baseurl=(?P<url>http.*scylladb.com.*)"), "{url}" + REPOMD_XML_PATH),
+            (re.compile(r"deb\s+(?P<url>http.*?)\s(?P<version_code_name>.*?)\s(?P<component>.*?)$"),
+             "{url}/dists/{version_code_name}/{component}/binary-amd64/Packages.gz"),
+    ],
+}
+
+# The variable "type" indices the type of URL (Debian or Yum)
+# The variable "urls" contains all urls using the Scylla pattern
+RepositoryDetails = namedtuple("RepositoryDetails", ["type", "urls"])
+
+
+def get_url_content(url, return_url_data=True):
+    response = requests.get(url=url)
+    if response.status_code != 200:
+        raise ValueError(f"The following repository URL '{url}' is incorrect")
+    response_data = response.text
+    if not response_data:
+        raise ValueError(f"The repository URL '{url}' not contains any content")
+    if return_url_data:
+        return response_data.split('\n')
+    # To overcome on Pylint's "inconsistent-return-statements", a value must be returned    return []
+    return []
+
+
+def get_scylla_urls_from_repository(repo_details):
+    urls = set()
+    for url in repo_details.urls:
+        for (url_regex, url_format) in FILE_REGEX_DICT[repo_details.type]:
+            match = url_regex.match(url)
+            if not match:
+                # Continue until find the correct Regex
+                continue
+
+            full_url = url_format.format(**match.groupdict())
+            # for scylla-manager we never used a noarch key
+            basearch_list = ["x86_64"] if 'scylla-manager' in full_url else ["x86_64", "noarch"]
+            for basearch in basearch_list:
+                urls.add(Template(full_url).substitute(basearch=basearch, releasever='7'))
+            # We found the correct regex and we can continue to next URL
+            break
+
+    urls = list(urls)
+    ParallelObject(objects=urls, timeout=SCYLLA_URL_RESPONSE_TIMEOUT).run(func=lambda _url: get_url_content(
+        url=_url, return_url_data=False))
+    return urls
+
+
+def get_branch_version_from_debian_repository(urls):
+    def get_version(url):
+        data = '\n'.join(get_url_content(url=url))
+        # Get only the major version (i.e. "2019.1.1-0.20190709.9f724fedb-1~stretch", get only "2019.1.1")
+        major_versions = [version.split('-', maxsplit=1)[0] for version in REPO_VERSIONS_REGEX.findall(data)]
+        if not major_versions:
+            return ""
+        return max(set(major_versions), key=major_versions.count)
+
+    threads = ParallelObject(objects=urls, timeout=SCYLLA_URL_RESPONSE_TIMEOUT).run(func=get_version)
+    result = [thread.result for thread in threads]
+    return max(result, key=result.count)
+
+
+def get_branch_version_from_centos_repository(urls):
+    def get_version(url):
+        data = '\n'.join(get_url_content(url=url))
+        primary_path = PRIMARY_XML_GZ_REGEX.search(data).groups()[0]
+        xml_url = url.replace(REPOMD_XML_PATH, primary_path)
+
+        parser = Parser(url=xml_url)
+        major_versions = [package['version'][1]['ver'] for package in parser.getList()]
+        return max(set(major_versions), key=major_versions.count)
+
+    threads = ParallelObject(objects=urls, timeout=SCYLLA_URL_RESPONSE_TIMEOUT).run(func=get_version)
+    result = [thread.result for thread in threads]
+    return max(result, key=result.count)
+
+
+def get_repository_details(url):
+    urls = list({line for line in get_url_content(url=url) if SCYLLA_URL_REGEX.match(line)})
+
+    for file_type, regex_list in FILE_REGEX_DICT.items():
+        for _url in urls:
+            for (url_regex, _) in regex_list:
+                match = url_regex.match(_url)
+                if match:
+                    return RepositoryDetails(type=file_type, urls=urls)
+    raise ValueError(VERSION_NOT_FOUND_ERROR)
 
 
 def get_branch_version(url):
-    try:
-        return get_branch_version_from_list(url)
-    except ValueError:
-        pass
+    repo_details = get_repository_details(url=url)
+    urls = get_scylla_urls_from_repository(repo_details=repo_details)
 
-    try:
-        return get_branch_version_from_repo(url)
-    except ValueError:
-        raise ValueError("url isn't a correct deb list or yum repo\n\turl:[{}]".format(url))
-
-
-def get_branch_version_from_list(url):
-    page = requests.get(url).text
-    list_regex = re.compile(
-        r'deb\s+\[arch=(?P<arch>.*?)\]\s(?P<url>http.*?)\s(?P<version_code_name>.*?)\s(?P<component>.*?)\n', re.DOTALL)
-    match = list_regex.search(page)
-    if not match:
-        raise ValueError("url isn't a correct deb list\n\turl:[{}] ".format(url))
-    params = list_regex.search(page).groupdict()
-
-    packages_url = "{url}/dists/{version_code_name}/{component}/binary-{arch}/Packages".format(**params)
-    page = requests.get(packages_url).text
-
-    versions_regex = re.compile(r'Version: (.*?)\n', re.DOTALL)
-    major_versions = [m.groups()[0].split('-')[0] for m in versions_regex.finditer(page)]
-
-    return max(set(major_versions), key=major_versions.count)
+    if repo_details.type == ScyllaFileType.DEBIAN:
+        return get_branch_version_from_debian_repository(urls=urls)
+    elif repo_details.type == ScyllaFileType.YUM:
+        return get_branch_version_from_centos_repository(urls=urls)
+    # To overcome on Pylint's "inconsistent-return-statements", a value must be returned
+    return []
 
 
-def get_branch_version_from_repo(url):
-    page = requests.get(url).text
-    repo_regex = re.compile(r'baseurl=(http.*?)\$basearch')
-
-    match = repo_regex.search(page)
-    if not match:
-        raise ValueError("url isn't a correct yum repo\n\turl:[{}] ".format(url))
-
-    repo_base_url = match.groups()[0]
-    url = "{}x86_64/repodata/repomd.xml".format(repo_base_url)
-    page = requests.get(url).text
-
-    primary_regex = re.compile(r'="(.*?primary.xml.gz)"')
-    primary_path = primary_regex.search(page).groups()[0]
-
-    url = "{}x86_64/{}".format(repo_base_url, primary_path)
-
-    parser = Parser(url=url)
-    major_versions = [package['version'][1]['ver'] for package in parser.getList()]
-
-    return max(set(major_versions), key=major_versions.count)
+def get_branch_version_for_multiple_repositories(urls):
+    threads = ParallelObject(objects=urls, timeout=SCYLLA_URL_RESPONSE_TIMEOUT).run(func=get_branch_version)
+    return [thread.result for thread in threads]
 
 
 def is_enterprise(version):
@@ -73,12 +155,7 @@ def is_enterprise(version):
     return parse_version(version) > parse_version('2000')
 
 
-GEMINI_VERSION_RE = re.compile(r'\s(?P<gemini_version>([\d]+\.[\d]+\.[\d]+)?),')
-# gemini version 1.0.1, commit ef7c6f422c78ef6b84a6f3bccf52ea9ec846bba0, date 2019-05-16T09:56:16Z
-
-
 def get_gemini_version(output: str):
-
     # take only version number - 1.0.1
     result = GEMINI_VERSION_RE.search(output)
 
--- unit_tests/test_version_utils.py
+++ unit_tests/test_version_utils.py
@@ -1,16 +1,19 @@
 from __future__ import absolute_import
+
 import unittest
 
-from sdcm.utils.version_utils import get_branch_version_from_list, get_branch_version_from_repo, get_branch_version, is_enterprise
+from sdcm.utils.version_utils import get_branch_version, \
+    is_enterprise, VERSION_NOT_FOUND_ERROR, get_branch_version_for_multiple_repositories
 
-# pylint: disable=invalid-name
-
-DEB_URL = 'https://s3.amazonaws.com/downloads.scylladb.com/enterprise/deb/unstable/stretch/'\
-          '9f724fedb93b4734fcfaec1156806921ff46e956-2bdfa9f7ef592edaf15e028faf3b7f695f39ebc1'\
-          '-525a0255f73d454f8f97f32b8bdd71c8dec35d3d/68/scylladb-2019.1/scylla.list'
-RPM_URL = 'https://s3.amazonaws.com/downloads.scylladb.com/enterprise/rpm/unstable/centos/'\
-          '9f724fedb93b4734fcfaec1156806921ff46e956-2bdfa9f7ef592edaf15e028faf3b7f695f39ebc1'\
-          '-525a0255f73d454f8f97f32b8bdd71c8dec35d3d-a6b2b2355c666b1893f702a587287da978aeec22/71/scylla.repo'
+BASE_S3_DOWNLOAD_URL = 'https://s3.amazonaws.com/downloads.scylladb.com'
+DEB_URL = \
+    f'{BASE_S3_DOWNLOAD_URL}/enterprise/deb/unstable/stretch/9f724fedb93b4734fcfaec1156806921ff46e956-2bdfa9f7e' \
+    f'f592edaf15e028faf3b7f695f39ebc1-525a0255f73d454f8f97f32b8bdd71c8dec35d3d/68/scylladb-2019.1/' \
+    f'scylla.list'
+RPM_URL = \
+    f'{BASE_S3_DOWNLOAD_URL}/enterprise/rpm/unstable/centos/9f724fedb93b4734fcfaec1156806921ff46e956-2bdfa9f7e' \
+    f'f592edaf15e028faf3b7f695f39ebc1-525a0255f73d454f8f97f32b8bdd71c8dec35d3d-a6b2b2355c666b1893f702a587287da' \
+    f'978aeec22/71/scylla.repo'
 
 ISSUE_5288_RPM_URL = 'http://scratch.scylladb.com/amos/yum_repos/master/piotrj_issue5288_v3/scylla.repo'
 
@@ -18,21 +21,30 @@ BROKEN_URL = 'https://www.google.com'
 
 
 class TestVersionUtils(unittest.TestCase):
+
+    def check_multiple_urls(self, urls):
+        expected_versions, repo_urls = [], []
+        for (url, expected_version) in urls:
+            repo_urls.append(url)
+            expected_versions.append(expected_version)
+        for branch_version, expected_version in zip(get_branch_version_for_multiple_repositories(urls=repo_urls),
+                                                    expected_versions):
+            self.assertEqual(branch_version, expected_version)
+
     def test_01_get_branch_version_from_list(self):
-        self.assertEqual(get_branch_version_from_list(DEB_URL), '2019.1.1')
-
-    def test_02_get_branch_version_from_repo(self):
-        self.assertEqual(get_branch_version_from_repo(RPM_URL), '2019.1.1')
-        self.assertEqual(get_branch_version_from_repo(ISSUE_5288_RPM_URL), '666.development')
-
-    def test_03_get_branch_version(self):
-        self.assertEqual(get_branch_version(RPM_URL), '2019.1.1')
         self.assertEqual(get_branch_version(DEB_URL), '2019.1.1')
 
+    def test_02_get_branch_version_from_repo(self):
+        self.check_multiple_urls(urls=[(RPM_URL, "2019.1.1"), (ISSUE_5288_RPM_URL, "666.development")])
+
+    def test_03_get_branch_version(self):
+        self.check_multiple_urls(urls=[(RPM_URL, "2019.1.1"), (DEB_URL, "2019.1.1")])
+
     def test_04_get_branch_version_failed(self):
-        self.assertRaisesRegex(ValueError, "url isn't a correct", get_branch_version, BROKEN_URL)
-        self.assertRaisesRegex(ValueError, "url isn't a correct", get_branch_version_from_list, BROKEN_URL)
-        self.assertRaisesRegex(ValueError, "url isn't a correct", get_branch_version_from_repo, BROKEN_URL)
+        msg_error = VERSION_NOT_FOUND_ERROR
+        self.assertRaisesRegex(ValueError, msg_error, get_branch_version, BROKEN_URL)
+        self.assertRaisesRegex(ValueError, msg_error, get_branch_version, BROKEN_URL)
+        self.assertRaisesRegex(ValueError, msg_error, get_branch_version, BROKEN_URL)
 
     def test_05_is_enterprise(self):
         self.assertEqual(is_enterprise('2019.1.1'), True)
