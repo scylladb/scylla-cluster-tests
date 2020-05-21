b86e038df65c5ace48a10ab55d79d2ef77ffed8a
Amos Kong <amos@scylladb.com>
2020-05-22 07:04:19 +0800

    fix(download_scylla_repo): retry to download scylla repo
    Private repo isn't stable sometimes, the downloaded repo content isn't
    valid, it's not easy to fix by Amnon, that might be caused by network
    problem between micro service and s3.
    
    Signed-off-by: Amos Kong <amos@scylladb.com>
    (cherry picked from commit d2f0ec94d564fdc67ba0a0ff27d7445171b32033)

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -1752,6 +1752,7 @@ class BaseNode(AutoSshContainerMixin, WebDriverContainerMixin):  # pylint: disab
         """)
         self.remoter.run('bash -cxe "%s"' % setup_script)
 
+    @retrying(n=3, sleep_time=10, allowed_exceptions=(AssertionError,), message="Retrying on getting scylla repo")
     def download_scylla_repo(self, scylla_repo):
         if not scylla_repo:
             self.log.error("Scylla YUM repo file url is not provided, it should be defined in configuration YAML!!!")
