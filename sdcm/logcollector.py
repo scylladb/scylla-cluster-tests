a123a8026212d329199ff1eb33644b1822f576c1
Aleksandr Bykov <alex.bykov@scylladb.com>
2020-05-22 15:44:46 +0700

    fix(collector): Add log file of cdctressor for collecting
    new log files on loader. Log collector for loader nodes
    
    (cherry picked from commit 8c62b82eb5c7957fcda9895935f9790352222c76)

--- sdcm/logcollector.py
+++ sdcm/logcollector.py
@@ -795,6 +795,8 @@ class LoaderLogCollector(LogCollector):
         FileLog(name='*gemini-l*.log',
                 search_locally=True),
         FileLog(name='gemini_result*.log',
+                search_locally=True),
+        FileLog(name='cdclogreader*.log',
                 search_locally=True)
     ]
 
