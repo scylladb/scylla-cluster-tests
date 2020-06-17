d44143b02eb2e9fe6602ffeac13f9b8d25a8a440
Israel Fruchter <fruch@scylladb.com>
2020-06-17 09:09:49 +0300

    fix(rsyslog): support the case of possible IP change in AWS
    Since external IP addresses can change in multi-dc setup
    we failed to restart the tunneling docker of autossh:rsyslogd in those cases
    
    create a new function `BaseNode.refresh_ip_address` to handle those.
    
    (cherry picked from commit 4f8ee2d7bf9adac7746228826a54f41075b58e2b)

--- sdcm/cluster_aws.py
+++ sdcm/cluster_aws.py
@@ -616,7 +616,8 @@ class AWSNode(cluster.BaseNode):
         self._wait_public_ip()
         self.log.debug('Got new public IP %s',
                        self._instance.public_ip_address)
-        self.remoter.hostname = self.external_address
+
+        self.refresh_ip_address()
         self.wait_ssh_up()
 
         if any(ss in self._instance.instance_type for ss in ['i3', 'i2']):
