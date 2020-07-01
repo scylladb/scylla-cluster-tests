f9b59cf3b8dd50dad8d064cf73525fa9af416d72
Israel Fruchter <fruch@scylladb.com>
2020-07-01 18:22:38 +0300

    fix(test_cases_lint): assort fixes for the test_case linting
    Seems like this is broken for some time now, and recent change to
    how we run the linting, is exposing them
    
    * few wrong configuration files, missing parameters
    * need to skip some files which are partial configurations,
      used only to mask specific params in other yamls
    * removed some params default from code (`grow_cluster_test.py`)
    * printing the exceptions from the `hydra conf` command, so it would
      be clear where the failure is originating

--- sct.py
+++ sct.py
@@ -356,6 +356,7 @@ def conf(config_file, backend):
         config.verify_configuration()
         config.check_required_files()
     except Exception as ex:  # pylint: disable=broad-except
+        logging.exception(str(ex))
         click.secho(str(ex), fg='red')
         sys.exit(1)
     else:
