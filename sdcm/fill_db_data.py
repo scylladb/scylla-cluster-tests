e63a3208455a005aacd6731badf3cbe8b8d48150
Amos Kong <amos@scylladb.com>
2020-07-08 10:56:06 +0800

    skip non-frozen test for 2019.1 upgrade
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- sdcm/fill_db_data.py
+++ sdcm/fill_db_data.py
@@ -2641,7 +2641,7 @@ class FillDatabaseData(ClusterTester):
                         "[[OrderedMapSerializedKey([('home', home_address(street='...', city='SF', zip_code=94102, phones=SortedSet(['123456'])))])]]"],
             'min_version': '',
             'max_version': '',
-            'skip': ''},
+            'skip': 'skip non-frozen test for 2019.1 upgrade'},
         # more_user_types_test
         {
             'create_tables': ["""
