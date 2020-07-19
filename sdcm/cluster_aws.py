2feef5d99ea7a1e50089c8101aa0ce04c37cbd5b
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2020-07-19 08:57:11 +0000

    fix(pylint): disable arguments-differ for wait_for_init()
    (cherry picked from commit 60e4d5498c699f23fddc2e48b5b4156098a9a797)
    (cherry picked from commit df9aed36a89316a25cdeddca625d9141f2809215)

--- sdcm/cluster_aws.py
+++ sdcm/cluster_aws.py
@@ -940,7 +940,7 @@ class CassandraAWSCluster(ScyllaAWSCluster):
         node.remoter.run('sudo apt-get install -y openjdk-6-jdk')
 
     @cluster.wait_for_init_wrap
-    def wait_for_init(self, node_list=None, verbose=False, timeout=None):
+    def wait_for_init(self, node_list=None, verbose=False, timeout=None):  # pylint: disable=arguments-differ
         self.get_seed_nodes()
 
 
