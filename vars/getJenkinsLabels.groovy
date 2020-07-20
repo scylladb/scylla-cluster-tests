75deaf85a640bb86e4111e928bbf22541a360a8b
Bentsi Magidovich <bentsi@scylladb.com>
2020-07-20 20:55:46 +0300

    feature(region-selection): randomly select AWS region + added eu-north1 (Stockholm) region

--- vars/getJenkinsLabels.groovy
+++ vars/getJenkinsLabels.groovy
@@ -9,19 +9,26 @@ def call(String backend, String aws_region=null) {
 
     }
 
-
     def jenkins_labels = ['aws-eu-west-1': 'aws-sct-builders-eu-west-1',
                           'aws-eu-west-2': 'aws-sct-builders-eu-west-2',
+                          'aws-eu-north-1': 'aws-sct-builders-eu-north-1',
                           'aws-us-east-1' : 'aws-sct-builders-us-east-1',
                           'gce': 'gce-sct-builders',
                           'docker': 'sct-builders']
-
+    println("Finding builder for region: " + aws_region)
     if (backend == 'aws' && aws_region)
     {
-        return jenkins_labels["${backend}-${aws_region}"]
+        if (aws_region == "random"){
+            def aws_supported_regions = ["eu-west-2", "eu-north-1"]
+            Collections.shuffle(aws_supported_regions)
+            aws_region = aws_supported_regions[0]
+        }
+
+        return [ "label": jenkins_labels["${backend}-${aws_region}"], "region": aws_region ]
+
     }
     else
     {
-        return jenkins_labels[backend]
+        return [ "label": jenkins_labels[backend] ]
     }
 }
