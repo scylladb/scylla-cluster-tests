053d6004d3865ff9b54d8ad5aa0f8a1a3cc4c6ec
Fabio Gelcer <fabio@scylladb.com>
2020-08-03 22:23:16 +0300

    fix(getjenkinslabels): on master, moved to use new builders
    as old builders are in a different VPC, and there is no
    way to move the VPC of an existing instance, 2 new builders
    were created on us-east-1 and eu-west-1 to suply to master
    jobs a builder in the new VPC, but leave the old builders
    as they are, to continue to run all other branches on those
    regions, without the need to merge the new builder and runner
    code.

--- vars/getJenkinsLabels.groovy
+++ vars/getJenkinsLabels.groovy
@@ -9,11 +9,11 @@ def call(String backend, String aws_region=null) {
 
     }
 
-    def jenkins_labels = ['aws-eu-west-1': 'aws-sct-builders-eu-west-1',
+    def jenkins_labels = ['aws-eu-west-1': 'aws-sct-builders-eu-west-1-new',
                           'aws-eu-west-2': 'aws-sct-builders-eu-west-2',
                           'aws-eu-north-1': 'aws-sct-builders-eu-north-1',
                           'aws-eu-central-1': 'aws-sct-builders-eu-central-1',
-                          'aws-us-east-1' : 'aws-sct-builders-us-east-1',
+                          'aws-us-east-1' : 'aws-sct-builders-us-east-1-new',
                           'gce': 'gce-sct-builders',
                           'docker': 'sct-builders']
 
