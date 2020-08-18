7499f7b02c206d540f7e2458a2c0d5d685ec52c8
Oren Efraimov <oren.efraimov@scylladb.com>
2020-08-18 18:35:19 +0300

    fix(alternator/api.py): Creating Alternator resources with "None" region name
    * Changing the value of "region_name" to "None" because we used custom endpoint
       and the value of the region is never mind
    
    (cherry picked from commit 92d9b52283013f278534c8c20d2e4170fb7d9417)

--- sdcm/utils/alternator/api.py
+++ sdcm/utils/alternator/api.py
@@ -30,7 +30,8 @@ class Alternator:
         endpoint_url = endpoint_url or self.endpoint_url
         if endpoint_url not in self.alternator_apis:
             aws_params = dict(endpoint_url=endpoint_url, aws_access_key_id=self.aws_access_key_id,
-                              aws_secret_access_key=self.aws_secret_access_key)
+                              aws_secret_access_key=self.aws_secret_access_key,
+                              region_name="None")
             resource: DynamoDBServiceResource = boto3.resource('dynamodb', **aws_params)
             client: DynamoDBClient = boto3.client('dynamodb', **aws_params)
             self.alternator_apis[endpoint_url] = AlternatorApi(resource=resource, client=client)
