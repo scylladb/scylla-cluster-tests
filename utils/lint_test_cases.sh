bd6b2ff0a371f3f78d902168a2995d6c2936f9b8
Israel Fruchter <fruch@scylladb.com>
2020-09-06 12:15:28 +0300

    fix(lint_test_case): ignore gce base yaml
    since those yaml should run with gce as backend, since they
    don't have the needed parameters for running with aws as backend
    
    (cherry picked from commit 74bf92e89f141651bd90edad5b96e1d0f087ba7e)

--- utils/lint_test_cases.sh
+++ utils/lint_test_cases.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for f in `find ./test-cases/ \\( -iname "*.yaml" ! -iname "*multi-dc.yaml" ! -iname *multiDC*.yaml ! -iname *multiple-dc*.yaml ! -iname *rolling* ! -iregex .*docker.* ! -iregex .*artifacts.* !  -iregex .*private-repo.* ! -iregex .*ics/long.* ! -iregex .*scylla-operator/.* \\)` ; do
+for f in `find ./test-cases/ \\( -iname "*.yaml" ! -iname "*multi-dc.yaml" ! -iname *multiDC*.yaml ! -iname *multiple-dc*.yaml ! -iname *rolling* ! -iregex .*docker.* ! -iregex .*artifacts.* !  -iregex .*private-repo.* ! -iregex .*ics/long.* ! -iregex .*scylla-operator/.* ! -iregex .*gce.* \\)` ; do
     echo "---- linting: $f -----"
     RES=$( script --flush --quiet --return /tmp/test-case.txt --command "SCT_SCYLLA_VERSION=4.0.0 python3 ./sct.py conf $f" )
     if [[ "$?" == "1" ]]; then
@@ -9,9 +9,9 @@ for f in `find ./test-cases/ \\( -iname "*.yaml" ! -iname "*multi-dc.yaml" ! -in
     fi
 done
 
-for f in `find ./test-cases/ \\( -iname *multi-dc.yaml -or -iname *multiDC*.yaml -or -iname *multiple-dc*.yaml -or -iname *rolling* -or -iregex .*artifacts.*yaml -or -iregex .*private-repo.*\.yaml \\) ! -name docker.yaml`; do
+for f in `find ./test-cases/ \\( -iname *multi-dc.yaml -or -iname *multiDC*.yaml -or -iname *multiple-dc*.yaml -or -iname *rolling* -or -iregex .*artifacts.*yaml -or -iregex .*private-repo.*\.yaml -or -iregex .*gce.* \\) ! -name docker.yaml`; do
     echo "---- linting: $f -----"
-    RES=$( script --flush --quiet --return /tmp/test-case.txt --command "SCT_SCYLLA_REPO=http://repositories.scylladb.com/scylla/repo/qa-test/centos/scylladb-2019.1.repo python3 ./sct.py conf --backend gce $f" )
+    RES=$( script --flush --quiet --return /tmp/test-case.txt --command "SCT_SCYLLA_REPO=http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2019.1.repo python3 ./sct.py conf --backend gce $f" )
     if [[ "$?" == "1" ]]; then
         cat /tmp/test-case.txt
         exit 1;
