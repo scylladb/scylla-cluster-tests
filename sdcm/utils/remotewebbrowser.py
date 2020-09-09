c9ba82ac40b444d89623dd1437334f987167fa48
Israel Fruchter <fruch@scylladb.com>
2020-09-09 10:11:01 +0300

    fix(DockerClient): support ipv6 when passing base_url
    In manager ipv6 I've seen failures like this, coming from `DockerClient.__init__`:
    
    ```
    2020-09-08 21:27:53.828: (TestFrameworkEvent Severity.ERROR),
    source=MgmtCliTest.update_test_with_errors() message=update_test_with_errors
    (silenced) failed with:
    exception=Port could not be cast to integer value as 'd018:eb8:3100:fa28:ae78:d480:22'
    ```

--- sdcm/utils/remotewebbrowser.py
+++ sdcm/utils/remotewebbrowser.py
@@ -20,7 +20,7 @@ from docker import DockerClient  # pylint: disable=wrong-import-order
 from selenium.webdriver import Remote, ChromeOptions
 
 from sdcm.utils.docker_utils import ContainerManager, DOCKER_API_CALL_TIMEOUT
-from sdcm.utils.common import get_free_port, wait_for_port
+from sdcm.utils.common import get_free_port, wait_for_port, normalize_ipv6_url
 from sdcm.utils.ssh_agent import SSHAgent
 
 
@@ -44,7 +44,7 @@ class WebDriverContainerMixin:
         if not self.ssh_login_info:
             return None
         SSHAgent.add_keys((self.ssh_login_info["key_file"], ))
-        return DockerClient(base_url=f"ssh://{self.ssh_login_info['user']}@{self.ssh_login_info['hostname']}",
+        return DockerClient(base_url=f"ssh://{self.ssh_login_info['user']}@{normalize_ipv6_url(self.ssh_login_info['hostname'])}",
                             timeout=DOCKER_API_CALL_TIMEOUT)
 
 
