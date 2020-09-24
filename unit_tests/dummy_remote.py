2ea60c30ac46b147e7d8b5a4086e94a11977b12e
juliayakovlev <julia.yakovlev@scylladb.com>
2020-09-24 17:00:43 +0300

    fix(coredump thread): Coredump thread should not retry forever on unknown exception
    If exceptions will happen 10 times during getting backtraces - fail the thread and don't
    run it forever

--- unit_tests/dummy_remote.py
+++ unit_tests/dummy_remote.py
@@ -16,6 +16,7 @@
 import os
 import shutil
 import logging
+from typing import Optional
 
 from sdcm.remote import LocalCmdRunner
 
@@ -23,9 +24,12 @@ from sdcm.remote import LocalCmdRunner
 class DummyOutput:
     def __init__(self, stdout):
         self.stdout = stdout
+        self.exit_status = 0
+        self.stderr = ""
 
 
 class DummyRemote:
+
     @staticmethod
     def run(*args, **kwargs):
         logging.info(args, kwargs)
@@ -39,6 +43,34 @@ class DummyRemote:
     def receive_files(src, dst):
         shutil.copy(src, dst)
         return True
+
+
+class DummyRemoteWithException(DummyRemote):
+
+    def __init__(self) -> None:
+        # Hold exception object that should be raised
+        self.test_exception: Optional[Exception] = None
+        # How many time return exception from run
+        self.exceptions_amount: int = 0
+        # How many times the "run" function has been called.
+        self.run_number: int = 0
+
+    def run(self, *args, **kwargs):
+        logging.info(args, kwargs)
+        self.raise_exception()
+
+        if 'sudo coredumpctl --no-pager' in args[0]:
+            dummy_output = "No coredumps found"
+        else:
+            dummy_output = args[0]
+        return DummyOutput(dummy_output)
+
+    def raise_exception(self):
+        if self.test_exception:
+            if self.exceptions_amount:
+                self.exceptions_amount -= 1
+                self.run_number += 1
+                raise self.test_exception  # pylint: disable=raising-bad-type
 
 
 class LocalNode:
