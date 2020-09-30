8beafd79fa2a7dab389f1cf4e314e6810e59520f
Kamil Braun <kbraun@scylladb.com>
2020-09-30 17:44:06 +0200

    CategoricalMonkey unit test

--- unit_tests/test_nemesis.py
+++ unit_tests/test_nemesis.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 import sdcm.utils.cloud_monitor  # pylint: disable=unused-import # import only to avoid cyclic dependency
-from sdcm.nemesis import Nemesis, ChaosMonkey
+from sdcm.nemesis import Nemesis, ChaosMonkey, CategoricalMonkey
 from sdcm.cluster_k8s.minikube import MinikubeScyllaPodCluster
 from sdcm.cluster_k8s.gke import GkeScyllaPodCluster
 from sdcm.cluster_gce import ScyllaGCECluster
@@ -27,7 +27,6 @@ def test_list_nemesis_of_added_disrupt_methods():
     nemesis = ChaosMonkey(FakeTester(), None)
     assert 'disrupt_add_remove_dc' in nemesis.get_list_of_methods_by_flags(disruptive=False)
     assert nemesis.call_random_disrupt_method(disrupt_methods=['disrupt_add_remove_dc']) is None
-
 
 def test_is_it_on_kubernetes():
     class FakeMinikubeScyllaPodCluster(MinikubeScyllaPodCluster):
@@ -64,3 +63,28 @@ def test_is_it_on_kubernetes():
     assert not Nemesis(FakeTester(FakeScyllaGCECluster()), None)._is_it_on_kubernetes()
     assert not Nemesis(FakeTester(FakeScyllaAWSCluster()), None)._is_it_on_kubernetes()
     assert not Nemesis(FakeTester(FakeScyllaDockerCluster()), None)._is_it_on_kubernetes()
+
+# pylint: disable=protected-access
+def test_categorical_monkey():
+    runs = []
+    def disrupt_m1(_):
+        runs.append(1)
+    def disrupt_m2(_):
+        runs.append(2)
+    setattr(CategoricalMonkey, 'disrupt_m1', disrupt_m1)
+    setattr(CategoricalMonkey, 'disrupt_m2', disrupt_m2)
+
+    tester = FakeTester()
+
+    nemesis = CategoricalMonkey(tester, None, {'m1': 1}, 0)
+    nemesis._random_disrupt()
+
+    nemesis = CategoricalMonkey(tester, None, {'m2': 1}, 0)
+    nemesis._random_disrupt()
+
+    assert runs == [1, 2]
+
+    nemesis = CategoricalMonkey(tester, None, {'m1': 1, 'm2': 1}, 0)
+    nemesis._random_disrupt()
+
+    assert runs in ([1, 2, 1], [1, 2, 2])
