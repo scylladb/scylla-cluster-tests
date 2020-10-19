ee0d27f2f9f9b53f4eefaa0952fd3a96f1a1d657
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2020-10-19 13:18:52 +0700

    fix(nemesis): does not recognize gke as kubernetes

--- unit_tests/test_nemesis.py
+++ unit_tests/test_nemesis.py
@@ -1,6 +1,11 @@
 from collections import namedtuple
 import sdcm.utils.cloud_monitor  # pylint: disable=unused-import # import only to avoid cyclic dependency
 from sdcm.nemesis import Nemesis, ChaosMonkey
+from sdcm.cluster_k8s.minikube import MinikubeScyllaPodCluster
+from sdcm.cluster_k8s.gke import GkeScyllaPodCluster
+from sdcm.cluster_gce import ScyllaGCECluster
+from sdcm.cluster_aws import ScyllaAWSCluster
+from sdcm.cluster_docker import ScyllaDockerCluster
 
 
 PARAMS = dict(nemesis_interval=1, nemesis_filter_seeds=False)
@@ -22,3 +27,40 @@ def test_list_nemesis_of_added_disrupt_methods():
     nemesis = ChaosMonkey(FakeTester(), None)
     assert 'disrupt_add_remove_dc' in nemesis.get_list_of_methods_by_flags(disruptive=False)
     assert nemesis.call_random_disrupt_method(disrupt_methods=['disrupt_add_remove_dc']) is None
+
+
+def test_is_it_on_kubernetes():
+    class FakeMinikubeScyllaPodCluster(MinikubeScyllaPodCluster):
+        def __init__(self, params: dict = None):
+            self.params = params
+
+    class FakeGkeScyllaPodCluster(GkeScyllaPodCluster):
+        def __init__(self, params: dict = None):
+            self.params = params
+
+    class FakeScyllaGCECluster(ScyllaGCECluster):
+        def __init__(self, params: dict = None):
+            self.params = params
+
+    class FakeScyllaAWSCluster(ScyllaAWSCluster):
+        def __init__(self, params: dict = None):
+            self.params = params
+
+    class FakeScyllaDockerCluster(ScyllaDockerCluster):
+        def __init__(self, params: dict = None):
+            self.params = params
+
+    class FakeTester:
+        def __init__(self, db_cluster):
+            self.params = {'nemesis_interval': 10, 'nemesis_filter_seeds': 1}
+            db_cluster.params = self.params
+            self.db_cluster = db_cluster
+            self.loaders = None
+            self.monitors = None
+
+    assert Nemesis(FakeTester(FakeMinikubeScyllaPodCluster()), None)._is_it_on_kubernetes()
+    assert Nemesis(FakeTester(FakeGkeScyllaPodCluster()), None)._is_it_on_kubernetes()
+
+    assert not Nemesis(FakeTester(FakeScyllaGCECluster()), None)._is_it_on_kubernetes()
+    assert not Nemesis(FakeTester(FakeScyllaAWSCluster()), None)._is_it_on_kubernetes()
+    assert not Nemesis(FakeTester(FakeScyllaDockerCluster()), None)._is_it_on_kubernetes()
