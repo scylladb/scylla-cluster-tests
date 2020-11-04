47a0502f54b985198f38b4d80e9ccf01a640f005
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2020-11-05 00:37:17 +0700

    fix(k8s-gke): wait for default namespace only

--- sdcm/cluster_k8s/gke.py
+++ sdcm/cluster_k8s/gke.py
@@ -142,7 +142,7 @@ class GkeCluster(KubernetesCluster, cluster.BaseCluster):
     def wait_for_init(self):
         LOGGER.info("--- List of nodes in GKE cluster `%s': ---\n%s\n", self.name, self.kubectl("get nodes").stdout)
         LOGGER.info("--- List of pods in GKE cluster `%s': ---\n%s\n", self.name, self.kubectl("get pods -A").stdout)
-        self.kubectl("wait --timeout=10m -A --all --for=condition=Ready pod")
+        self.kubectl("wait --timeout=10m --all --for=condition=Ready pod")
 
     def destroy(self):
         pass
