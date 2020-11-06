c36ad820532c24c93e611824e29ca375e50d8ea4
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2020-11-06 19:16:12 +0000

    improvement(sct_events): rework monitors events

--- sdcm/sct_events/monitors.py
+++ sdcm/sct_events/monitors.py
@@ -11,91 +11,57 @@
 #
 # Copyright (c) 2020 ScyllaDB
 
-import re
-import json
+from typing import Type
 
-from sdcm.sct_events.base import SctEvent, Severity
+from sdcm.sct_events import Severity
+from sdcm.sct_events.base import SctEvent, SctEventProtocol
 
 
-class PrometheusAlertManagerEvent(SctEvent):  # pylint: disable=too-many-instance-attributes
-    _from_str_regexp = re.compile(
-        "[^:]+: alert_name=(?P<alert_name>[^ ]+) type=(?P<type>[^ ]+) start=(?P<start>[^ ]+) "
-        f"end=(?P<end>[^ ]+) description=(?P<description>[^ ]+) updated=(?P<updated>[^ ]+) state=(?P<state>[^ ]+) "
-        f"fingerprint=(?P<fingerprint>[^ ]+) labels=(?P<labels>[^ ]+)")
+class PrometheusAlertManagerEvent(SctEvent, abstract=True):
+    _eq_attrs = (
+        "type",
+        "starts_at",
+        "ends_at",
+        "updated_at",
+        "fingerprint",
+        "labels",
+        "description",
+        "state",
+        "alert_name",
+    )
 
-    def __init__(self,  # pylint: disable=too-many-arguments
-                 raw_alert=None, event_str=None, sct_event_str=None, event_type: str = None, severity=Severity.WARNING):
-        super().__init__()
-        self.severity = severity
-        self.type = event_type
-        if raw_alert:
-            self._load_from_raw_alert(**raw_alert)
-        elif event_str:
-            self._load_from_event_str(event_str)
-        elif sct_event_str:
-            self._load_from_sctevent_str(sct_event_str)
+    start: Type[SctEventProtocol]
+    end: Type[SctEventProtocol]
 
-    def __str__(self):
-        return f"{super().__str__()}: alert_name={self.alert_name} type={self.type} start={self.start} "\
-               f"end={self.end} description={self.description} updated={self.updated} state={self.state} "\
-               f"fingerprint={self.fingerprint} labels={self.labels}"
+    def __init__(self, raw_alert: dict, severity: Severity = Severity.WARNING):
+        super().__init__(severity=severity)
 
-    def _load_from_sctevent_str(self, data: str):
-        result = self._from_str_regexp.match(data)
-        if not result:
-            return False
-        tmp = result.groupdict()
-        if not tmp:
-            return False
-        tmp['labels'] = json.loads(tmp['labels'])
-        for name, value in tmp:
-            setattr(self, name, value)
-        return True
+        self.annotations = raw_alert.get("annotations") or {}
+        self.starts_at = raw_alert.get("startsAt")
+        self.ends_at = raw_alert.get("endsAt")
+        self.updated_at = raw_alert.get("updatedAt")
+        self.fingerprint = raw_alert.get("fingerprint")
+        self.status = raw_alert.get("status") or {}
+        self.labels = raw_alert.get("labels") or {}
 
-    def _load_from_event_str(self, data: str):
+        self.description = self.annotations.get("description", self.annotations.get("summary", ""))
+        self.state = self.status.get("state", "")
+        self.alert_name = self.labels.get("alertname", "")
+
         try:
-            tmp = json.loads(data)
-        except Exception:  # pylint: disable=broad-except
-            return None
-        for name, value in tmp.items():
-            if name not in ['annotations', 'description', 'start', 'end', 'updated', 'fingerprint', 'status', 'labels',
-                            'state', 'alert_name', 'severity', 'type', 'timestamp', 'severity']:
-                return False
-            setattr(self, name, value)
-        if isinstance(self.severity, str):
-            self.severity = getattr(Severity, self.severity)
-        return True
+            self.severity = Severity[self.labels["sct_severity"]]
+        except KeyError:
+            pass
 
-    def _load_from_raw_alert(self,  # pylint: disable=too-many-arguments,invalid-name,unused-argument
-                             annotations: dict = None, startsAt=None, endsAt=None, updatedAt=None, fingerprint=None,
-                             status=None, labels=None, **kwargs):
-        self.annotations = annotations
-        if self.annotations:
-            self.description = self.annotations.get('description', self.annotations.get('summary', ''))
-        else:
-            self.description = ''
-        self.start = startsAt
-        self.end = endsAt
-        self.updated = updatedAt
-        self.fingerprint = fingerprint
-        self.status = status
-        self.labels = labels
-        if self.status:
-            self.state = self.status.get('state', '')
-        else:
-            self.state = ''
-        if self.labels:
-            self.alert_name = self.labels.get('alertname', '')
-        else:
-            self.alert_name = ''
-        sct_severity = self.labels.get('sct_severity')
-        if sct_severity:
-            self.severity = Severity.__dict__.get(sct_severity, Severity.WARNING)
+    @property
+    def msgfmt(self) -> str:
+        return super().msgfmt + ": " + "alert_name={0.alert_name} type={0.type} start={0.starts_at} " \
+                                       "end={0.ends_at} description={0.description} updated={0.updated_at} " \
+                                       "state={0.state} fingerprint={0.fingerprint} labels={0.labels}"
 
     def __eq__(self, other):
-        for name in ['alert_name', 'type', 'start', 'end', 'description', 'updated', 'state', 'fingerprint', 'labels']:
-            other_value = getattr(other, name, None)
-            value = getattr(self, name, None)
-            if value != other_value:
-                return False
-        return True
+        return all(getattr(self, name, None) == getattr(other, name, None) for name in self._eq_attrs)
+
+
+PrometheusAlertManagerEvent.add_subevent_type("start")
+PrometheusAlertManagerEvent.add_subevent_type("end")
---
+++ unit_tests/test_sct_events_monitors.py
@@ -0,0 +1,62 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright (c) 2020 ScyllaDB
+
+import pickle
+import unittest
+
+from sdcm.sct_events import Severity
+from sdcm.sct_events.monitors import PrometheusAlertManagerEvent
+
+
+RAW_ALERT = dict(
+    annotations=dict(
+        description="[10.0.201.178] has been down for more than 30 seconds.",
+        summary="Instance [10.0.201.178] down",
+    ),
+    endsAt="2019-12-26T06:21:09.591Z",
+    startsAt="2019-12-24T17:00:09.591Z",
+    updatedAt="2019-12-26T06:18:09.593Z",
+    labels=dict(
+        alertname="InstanceDown",
+        instance="[10.0.201.178]",
+        job="scylla",
+        monitor="scylla-monitor",
+        severity="2",
+    ),
+)
+
+
+class TestPrometheusAlertManagerEvent(unittest.TestCase):
+    def test_msgfmt(self):
+        event = PrometheusAlertManagerEvent.start(raw_alert=RAW_ALERT)
+        self.assertEqual(
+            str(event),
+            "(PrometheusAlertManagerEvent Severity.WARNING): alert_name=InstanceDown type=start"
+            " start=2019-12-24T17:00:09.591Z end=2019-12-26T06:21:09.591Z"
+            " description=[10.0.201.178] has been down for more than 30 seconds. updated=2019-12-26T06:18:09.593Z"
+            " state= fingerprint=None labels={'alertname': 'InstanceDown', 'instance': '[10.0.201.178]',"
+            " 'job': 'scylla', 'monitor': 'scylla-monitor', 'severity': '2'}"
+        )
+        self.assertEqual(event, pickle.loads(pickle.dumps(event)))
+
+    def test_sct_severity(self):
+        event = PrometheusAlertManagerEvent.end(raw_alert=dict(labels=dict(sct_severity="CRITICAL")))
+        self.assertEqual(event.severity, Severity.CRITICAL)
+        self.assertTrue(str(event).startswith("(PrometheusAlertManagerEvent Severity.CRITICAL)"))
+        self.assertEqual(event, pickle.loads(pickle.dumps(event)))
+
+    def test_sct_severity_wrong(self):
+        event = PrometheusAlertManagerEvent.end(raw_alert=dict(labels=dict(sct_severity="WRONG")))
+        self.assertEqual(event.severity, Severity.WARNING)
+        self.assertTrue(str(event).startswith("(PrometheusAlertManagerEvent Severity.WARNING)"))
+        self.assertEqual(event, pickle.loads(pickle.dumps(event)))
