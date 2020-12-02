7480debb92a9161526843ad5904e801a4a859a8b
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2020-12-02 11:07:54 +0000

    improvement(hydra): sync SSH keys with keystore

--- get-qa-ssh-keys.sh
+++ get-qa-ssh-keys.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 set -e
+
+SSH_KEYS_BUCKET=scylla-qa-keystore
 SSH_KEYS_DIR=~/.ssh
-QA_PRIVATE_KEY_NAMES="scylla-qa-ec2 scylla-test support"
+
 echo "Obtaining QA SSH keys..."
-for pkey_name in ${QA_PRIVATE_KEY_NAMES}
-do
-    QA_PRIVATE_KEY_PATH=${SSH_KEYS_DIR}/${pkey_name}
-    if [ -e ${QA_PRIVATE_KEY_PATH} ]; then
-        echo "QA private key '${QA_PRIVATE_KEY_PATH}' exists. Nothing to update."
-    else
-        echo "QA private key '${QA_PRIVATE_KEY_PATH}' doesn't exist. Retrieving..."
-        mkdir -p ${SSH_KEYS_DIR}
-        aws s3 cp s3://scylla-qa-keystore/${pkey_name} ${QA_PRIVATE_KEY_PATH} || (echo "Please refer to https://github.com/scylladb/scylla-cluster-tests#setting-up-sct-environment to configure your aws-cli" && exit 1)
-        chmod 600 ${QA_PRIVATE_KEY_PATH}
-        echo "Done."
-    fi
-done
+umask 077
+aws s3 sync s3://${SSH_KEYS_BUCKET} ${SSH_KEYS_DIR} --exact-timestamps --exclude '*' \
+  --include scylla-qa-ec2 \
+  --include scylla-test \
+  --include support \
+|| (echo "Please refer to https://github.com/scylladb/scylla-cluster-tests#setting-up-sct-environment to configure your aws-cli" && exit 1)
 echo "QA SSH keys obtained."
