fc649f77f7689d8799b7fac85fc474b9dfdb3a54
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2021-01-08 19:57:57 +0700

    fix(perfRegressionParallelPipeline): send no email from pipeline

--- vars/perfRegressionParallelPipeline.groovy
+++ vars/perfRegressionParallelPipeline.groovy
@@ -177,28 +177,6 @@ def call(Map pipelineParams) {
                                                 }
                                             }
                                         }
-                                        stage("Send email with result for ${sub_test}") {
-                                            catchError(stageResult: 'FAILURE') {
-                                                script {
-                                                    wrap([$class: 'BuildUser']) {
-                                                        dir('scylla-cluster-tests') {
-                                                            def email_recipients = groovy.json.JsonOutput.toJson(params.email_recipients)
-
-                                                            sh """
-                                                            #!/bin/bash
-
-                                                            set -xe
-                                                            env
-
-                                                            echo "Start send email ..."
-                                                            ./docker/env/hydra.sh send-email --logdir "`pwd`" --email-recipients "${email_recipients}"
-                                                            echo "Email sent"
-                                                            """
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
                                     }
                                 }
                             }
