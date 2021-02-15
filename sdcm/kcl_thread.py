2e8fe33565c8306f2567aa34120306dcb6122e8b
Israel Fruchter <fruch@scylladb.com>
2021-02-15 15:45:14 +0200

    feature(hydra_kcl): Adding time based checkpoints, and retries
    Update of the hydra_kcl trying to test if it's the root cause of
    scylladb/scylla#8012
    
    Ref: https://github.com/scylladb/scylla/issues/8012
    Ref: https://github.com/fruch/hydra-kcl/pull/4

--- sdcm/kcl_thread.py
+++ sdcm/kcl_thread.py
@@ -45,7 +45,7 @@ class KclStressThread(DockerBasedStressThread):  # pylint: disable=too-many-inst
         return stress_cmd
 
     def _run_stress(self, loader, loader_idx, cpu_idx):
-        docker = RemoteDocker(loader, "scylladb/hydra-loaders:kcl-jdk8-20201229",
+        docker = RemoteDocker(loader, "scylladb/hydra-loaders:kcl-jdk8-20210215",
                               extra_docker_opts=f'--label shell_marker={self.shell_marker}')
         stress_cmd = self.build_stress_cmd()
 
