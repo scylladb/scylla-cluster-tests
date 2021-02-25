0182c35f5f08d6c8c404583e53fd4fd29d7cc547
Aleksandr Bykov <alex.bykov@scylladb.com>
2021-02-25 15:13:14 +0700

    feature(perf_query): Use wide queries to get results from ES
    Backport pr: 2e717baa feature(es_queries): Filter perf
    result by dashboard query
    
    use queries link in es dashboards to filter performance results

--- sdcm/tester.py
+++ sdcm/tester.py
@@ -1572,7 +1572,7 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
                                                       email_recipients=self.params.get('email_recipients', default=None))
         is_gce = bool(self.params.get('cluster_backend') == 'gce')
         try:
-            results_analyzer.check_regression(self._test_id, is_gce)
+            results_analyzer.check_regression(self._test_id, is_gce, use_wide_query=True)
         except Exception as ex:  # pylint: disable=broad-except
             self.log.exception('Failed to check regression: %s', ex)
 
