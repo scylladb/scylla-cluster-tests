c7a46fbff3b493c3c3f02df61889bd225c079989
julia.yakovlev <julia.yakovlev@scylladb.com>
2021-02-28 09:57:14 +0200

    fix(restart_then_repair): filter out 'Can't find a column family with UUID' backtrace
    In the nemesis RestartThenRepairNode the DATABASE_ERROR 'Can't find a column
    family with UUID' is filtered out, but not backtrace of this error. So the
    test fails on this error. Added filter on DatabaseLogEvent.BACKTRACE

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -414,7 +414,9 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
         # Task https://trello.com/c/llRuLIOJ/2110-add-dbeventfilter-for-nosuchcolumnfamily-error
         # If this error happens during the first boot with the missing disk this issue is expected and it's not an issue
         with DbEventsFilter(db_event=DatabaseLogEvent.DATABASE_ERROR,
-                            line="Can't find a column family with UUID", node=self.target_node):
+                            line="Can't find a column family with UUID", node=self.target_node), \
+            DbEventsFilter(db_event=DatabaseLogEvent.BACKTRACE,
+                           line="Can't find a column family with UUID", node=self.target_node):
             self.target_node.restart()
 
         self.log.info('Waiting scylla services to start after node restart')
