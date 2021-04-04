55bcea0a2d03532026724a20476bb9e2bc27c0b0
Israel Fruchter <fruch@scylladb.com>
2021-04-04 18:43:17 +0300

    fix(getCloudProviderFromBackend): not fail with there no backend defined
    since on the first time some of the jobs are trigger, it doesn't have a backend define
    and this new code doesn't let the whole pipeline to be loaded.

--- vars/getCloudProviderFromBackend.groovy
+++ vars/getCloudProviderFromBackend.groovy
@@ -5,6 +5,9 @@ def call(String backend) {
         'k8s-gke': 'gce',
         'k8s-gce-minikube': 'gce',
         ]
+    if (!backend) {
+        return backend
+    }
     def cloud_provider = backend.trim().toLowerCase()
     return backend_to_provider.get(cloud_provider, cloud_provider)
     }
