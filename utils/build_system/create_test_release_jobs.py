2a6e9c8ac546d6d745b4fb17d7139693d0a7b86c
Aleksandr Bykov <alex.bykov@scylladb.com>
2021-05-19 14:50:22 +0700

    ci(jenkins-pipeline): Create test jobs
    New job allows to create sct test jobs
    on jenkins for scylla version and run first build for job
    to read job parameters for next automatic runs.
    Test jobs are created and first build start one by one
    to avoid overload jenkins query and not to allocate all
    jenkins worker.
    
    Main job parameter 'branch' set the main directory of all creating jobs.
    for example it branch = "branch-4.6" sct test jobs will be created in
    branch-4.6
    
    boolean flag is_enterprise used to mark scylla version as enterprise
    and create all sct enterprise jobs

--- utils/build_system/create_test_release_jobs.py
+++ utils/build_system/create_test_release_jobs.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+
 import jenkins  # pylint: disable=import-error
+
+from sdcm.wait import wait_for
 
 DIR_TEMPLATE = open(Path(__file__).parent / 'folder-template.xml').read()
 JOB_TEMPLATE = open(Path(__file__).parent / 'template.xml').read()
@@ -34,5 +37,31 @@ class JenkinsPipelines:
                 group_name = "/" + group_name
             self.jenkins.create_job(
                 f'{self.base_job_dir}{group_name}/{base_name}{job_name_suffix}', xml_data)
+            self.build_job_first_time(f'{self.base_job_dir}{group_name}/{base_name}{job_name_suffix}')
         except jenkins.JenkinsException as ex:
             print(ex)
+
+    def build_job_first_time(self, name):
+        """start job first time
+
+        Need to start job first time
+        so jenkins will read all job parameters
+        Job started one by one, to avoid situation
+        when all jenkins resources will be allocated
+        """
+        print(f"Start first build {name}")
+        job_id = self.jenkins.build_job(name)
+
+        # wait while worker will be found
+        def check_job_is_started(job_id):
+            return self.jenkins.get_queue_item(job_id).get("executable")
+        wait_for(check_job_is_started, step=5, text="Job is starting", timeout=60, throw_exc=True, job_id=job_id)
+
+        # wait while job will be executed
+        def check_job_is_finished(job_name):
+            return not self.jenkins.get_build_info(name, 1).get("building")
+
+        wait_for(check_job_is_finished, step=5, text="Check job is finished",
+                 timeout=120, throw_exc=True, job_name=name)
+
+        print("First build finished")
