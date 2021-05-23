092fa29a1a5751911047d82b1e51f1150b0361e5
Fabio Gelcer <fabio@scylladb.com>
2021-05-23 11:26:11 +0300

    improvement(perf email report): add amount of reactor stall events
    to be able to analyze better the performance emails,
    this commit will be getting the summary of DEBUG severity
    events (right now only reactor stalls) and will add
    to the body of the email report.
    
    (cherry picked from commit 9ba98dfa1ae10e76409d6cb4967f96068d2afa34)

--- sdcm/tester.py
+++ sdcm/tester.py
@@ -2206,7 +2206,7 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
                                                                       events=get_events_grouped_by_category(_registry=self.events_processes_registry))
         if self.db_cluster.latency_results and self.create_stats:
             self.db_cluster.latency_results = calculate_latency(self.db_cluster.latency_results)
-            self.log.debug(f'collected latency are: {self.db_cluster.latency_results}')
+            self.log.debug(f'collected latency values are: {self.db_cluster.latency_results}')
             self.update({"latency_during_ops": self.db_cluster.latency_results})
 
             self.update_test_details()
