2c0d85a76dfa739e74924cd5a18f6c33593ff2c4
julia.yakovlev <julia.yakovlev@scylladb.com>
2021-06-07 21:00:48 +0300

    fix(sct configuration): ami for wrong version may return
    ami for wrong version may return.
    Example:
    if we ask version '4.0' it returns ami for 4.4.0.
    
    It happens because of we search if the ami name include the version number.
    Version name '4.4.0' includes '4.0'
    
    ami name format example: 'ScyllaDB 4.4.0'
    
    rolling-upgrade-ami test in branch-2020.1 run on '4.4.0' when base_version is '4.0'
    
    (cherry picked from commit 7f97d75f1c841410a16cf1da6b7ee22dc28d0b4a)

--- sdcm/sct_config.py
+++ sdcm/sct_config.py
@@ -1105,7 +1105,8 @@ class SCTConfiguration(dict):
 
                     amis = get_scylla_ami_versions(region)
                     for ami in amis:
-                        if scylla_version in ami['Name']:
+                        # ami['Name'] format example: ScyllaDB 4.4.0
+                        if f" {scylla_version}" in ami['Name']:
                             ami_list.append(ami['ImageId'])
                             break
                     else:
