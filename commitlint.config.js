be3260b80dd094cc96929c8e2bbfe4f3c5a8f9b8
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2021-06-10 16:50:06 +0300

    improvement(commitlint): restrict header to be at max 72 symbols long
    Reason:
    
        The number 72 comes from the fact that 80 characters is the widely
        accepted industry standard for readable character length of one
        line, git automatically adds a padding of 4 characters to the left
        of the commit message body, and to keep everything centered
        and looking nice, 4 characters should be padded to the right.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     rules: {
+        // Header
+        "header-max-length": [2, "always", 72],
         // Subject
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
