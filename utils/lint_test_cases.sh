83c8ff621e1948c8da1f26b12c0adfaf7f548068
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2021-06-14 14:27:48 +0300

    fix(lint_test_cases.sh): propup scylla version to 4.4.1

--- utils/lint_test_cases.sh
+++ utils/lint_test_cases.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-SCT_SCYLLA_VERSION=4.2.0 ./sct.py lint-yamls -i '.yaml' -e 'multi-dc.yaml,multiDC,multidc,multiple-dc,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,jepsen'
+SCT_SCYLLA_VERSION=4.4.1 ./sct.py lint-yamls -i '.yaml' -e 'multi-dc.yaml,multiDC,multidc,multiple-dc,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,jepsen'
 
-SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2019.1.repo' ./sct.py lint-yamls -b gce -i 'multi-dc.yaml,multiDC,multidc,multiple-dc,rolling,artifacts,private-repo,gce,jepsen' -e 'docker'
+SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'multi-dc.yaml,multiDC,multidc,multiple-dc,rolling,artifacts,private-repo,gce,jepsen' -e 'docker'
 
-SCT_SCYLLA_VERSION=4.2.0 ./sct.py lint-yamls -b k8s-gce-minikube -i 'scylla-operator.*\.yaml'
+SCT_SCYLLA_VERSION=4.4.1 ./sct.py lint-yamls -b k8s-gce-minikube -i 'scylla-operator.*\.yaml'
