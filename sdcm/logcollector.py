f0f53823227e25c052f0ee3f19cfed9ff31a408c
Aleksandr Bykov <alex.bykov@scylladb.com>
2021-07-14 19:00:11 +0700

    fix(logcollector): remove pylint errors
    Fix pylint warning messages for logcollector
    
    (cherry picked from commit d6b4fb5cb8ccec613bbeffc301d1544b4b6c2d47)

--- sdcm/logcollector.py
+++ sdcm/logcollector.py
@@ -763,7 +763,8 @@ class ScyllaLogCollector(LogCollector):
         cluster_log_type {str} -- cluster type name
     """
     log_entities = [FileLog(name='system.log',
-                            command="sudo journalctl --no-tail --no-pager -u scylla-ami-setup.service -u scylla-image-setup.service -u scylla-io-setup.service -u scylla-server.service -u scylla-jmx.service", search_locally=True),
+                            command="sudo journalctl --no-tail --no-pager -u scylla-ami-setup.service -u scylla-image-setup.service "
+                                    "-u scylla-io-setup.service -u scylla-server.service -u scylla-jmx.service", search_locally=True),
                     CommandLog(name='cpu_info',
                                command='cat /proc/cpuinfo'),
                     CommandLog(name='mem_info',
