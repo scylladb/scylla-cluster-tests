b61fc64c5e4e7f9542fdedaa4bdaf29785a0e4f9
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2021-08-10 17:29:56 +0300

    fix(tester.py): move out alive proc/thread logic
    Move show_alive_threads and show_alive_processes out from tester.py

--- sdcm/tester.py
+++ sdcm/tester.py
@@ -25,11 +25,8 @@ from typing import NamedTuple, Optional, Union, List, Dict, Any, Type
 from uuid import uuid4
 from functools import wraps, cached_property
 import threading
-import multiprocessing
-import inspect
 import signal
 import sys
-import traceback
 
 from invoke.exceptions import UnexpectedExit, Failure
 
@@ -70,6 +67,8 @@ from sdcm.sct_events.file_logger import get_events_grouped_by_category, get_logg
 from sdcm.sct_events.events_analyzer import stop_events_analyzer
 from sdcm.stress_thread import CassandraStressThread
 from sdcm.gemini_thread import GeminiStressThread
+from sdcm.utils.threads_and_processes_alive import gather_live_processes_and_dump_to_file, \
+    gather_live_threads_and_dump_to_file
 from sdcm.ycsb_thread import YcsbStressThread
 from sdcm.ndbench_thread import NdBenchStressThread
 from sdcm.kcl_thread import KclStressThread, CompareTablesSizesThread
@@ -2185,72 +2184,11 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
 
     @silence()
     def show_alive_threads(self):
-        if not threading.active_count():
-            return False
-        source_modules = []
-        result = False
-        with open(self.left_processes_log, 'a') as log_file:
-            for thread in threading.enumerate():
-                if thread is threading.current_thread():
-                    continue
-                result = True
-                source = '<no code available>'
-                module = 'Unknown'
-                if thread.__class__ is threading.Thread:
-                    if thread.run.__func__ is not threading.Thread.run:
-                        module = thread.run.__module__
-                        source = inspect.getsource(thread.run)
-                    elif getattr(thread, '_target', None):
-                        module = thread._target.__module__  # pylint: disable=protected-access
-                        source = inspect.getsource(thread._target)  # pylint: disable=protected-access
-                else:
-                    module = thread.__module__
-                    source = inspect.getsource(thread.__class__)
-                if module not in source_modules:
-                    source_modules.append(module)
-                log_file.write(f"========= Thread {thread.name} from {module} =========\n")
-                log_file.write(f"========= SOURCE =========\n{source}\n")
-                log_file.write(f"========= STACK TRACE =========\n{self._get_stacktrace(thread)}\n")
-                log_file.write(f"========= END OF Thread {thread.name} from {module} =========\n")
-        if result:
-            self.log.error("There are some threads left alive from following modules: %s",
-                           ",".join(source_modules))
-        return result
-
-    def _get_stacktrace(self, thread):  # pylint: disable=no-self-use
-        frame = sys._current_frames().get(thread.ident, None)  # pylint: disable=protected-access
-        output = []
-        for filename, lineno, name, line in traceback.extract_stack(frame):
-            output.append('File: "%s", line %d, in %s' % (filename,
-                                                          lineno, name))
-            if line:
-                output.append("  %s" % (line.strip()))
-        return '\n'.join(output)
+        return gather_live_threads_and_dump_to_file(self.left_processes_log)
 
     @silence()
     def show_alive_processes(self):
-        if not multiprocessing.active_children():
-            return False
-        source_modules = []
-        with open(self.left_processes_log, 'a') as log_file:
-            for proc in multiprocessing.active_children():
-                source = '<no code available>'
-                module = 'Unknown'
-                if proc.__class__ is multiprocessing.Process:
-                    if proc.run.__func__ != multiprocessing.Process.run:
-                        module = proc.run.__module__
-                        source = inspect.getsource(proc.run)
-                else:
-                    module = proc.__module__
-                    source = inspect.getsource(proc.__class__)
-                if module not in source_modules:
-                    source_modules.append(module)
-                log_file.write(f"========= Process {proc.name} from {module} =========\n")
-                log_file.write(f"========= SOURCE =========\n{source}\n")
-                log_file.write(f"========= END OF Process {proc.name} from {module}  =========\n")
-        self.log.error("There are some processes left alive from the following modules %s",
-                       ",".join(source_modules))
-        return True
+        return gather_live_processes_and_dump_to_file(self.left_processes_log)
 
     @silence()
     def clean_resources(self):
