ec258c3c91feff8decc5bb1505dd27cfb5a27be6
MaciekCisowski <maciej.cisowski@scylladb.com>
2021-08-12 15:42:39 +0200

    fix(nodetool_event): circular ref on json dumps

--- sdcm/sct_events/nodetool.py
+++ sdcm/sct_events/nodetool.py
@@ -10,9 +10,33 @@
 # See LICENSE for more details.
 #
 # Copyright (c) 2021 ScyllaDB
+from dataclasses import dataclass, asdict
+from json import JSONEncoder
+from typing import Any, Type
 
 from sdcm.sct_events import Severity
 from sdcm.sct_events.base import ContinuousEvent
+
+
+@dataclass
+class NodetoolCommand:
+    cmd: str
+    options: str
+
+    def __post_init__(self):
+        cmd_split = self.cmd.split()
+        if len(cmd_split) > 1:
+            options_to_list = [self.options] if self.options else []
+            self.options = ' '.join(cmd_split[1:] + options_to_list)
+            self.cmd = cmd_split[0]
+
+
+class NodetoolEventEncoder(JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, NodetoolCommand):
+            return asdict(o)
+        else:
+            return super().default(o)
 
 
 # pylint: disable=too-many-instance-attributes
@@ -25,27 +49,22 @@ class NodetoolEvent(ContinuousEvent):
                  publish_event=True):
         super().__init__(severity=severity, publish_event=publish_event)
 
-        # handle the case if command is like "snapshot -kc keyspace1"
-        cmd_splitted = nodetool_command.split()
-        if len(cmd_splitted) > 1:
-            options_to_list = [options] if options else []
-            options = ' '.join(cmd_splitted[1:] + options_to_list)
-
-        if cmd_splitted:
-            self.nodetool_command = cmd_splitted[0]
-        self.options = options
+        self.nodetool_command = NodetoolCommand(cmd=nodetool_command, options=options)
         self.node = str(node)
         self.full_traceback = None
 
     @property
     def msgfmt(self) -> str:
-        fmt = super().msgfmt + ": nodetool_command={0.nodetool_command}"
+        fmt = super().msgfmt + ": nodetool_command={0.nodetool_command.cmd}"
         if self.node:
             fmt += " node={0.node}"
-        if self.options:
-            fmt += " options={0.options}"
+        if self.nodetool_command.options:
+            fmt += " options={0.nodetool_command.options}"
         if self.errors:
             fmt += " errors={0.errors}"
         if self.full_traceback:
             fmt += "\n{0.full_traceback}"
         return fmt
+
+    def to_json(self, encoder: Type[JSONEncoder] = NodetoolEventEncoder) -> str:
+        return super().to_json(encoder=encoder)
