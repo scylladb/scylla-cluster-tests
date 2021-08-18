3d1d5df32e14de4e854d7ca2e015ef647d534f7c
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2021-08-18 16:20:06 +0300

    fix(rollingUpgradePipeline): remove cleanSCTRunners step
    Remove it for following reasons:
    - This CI job doesn't create SCT runner, so such step becomes redundant.
    - Jenkins fails with following error:
    
        java.lang.NoSuchMethodError: No such DSL method 'steps' found among
            steps [addEmbeddableBadgeConfiguration, ...]
    
      When it tries to run 'cleanSCTRunners' step.

--- vars/rollingUpgradePipeline.groovy
+++ vars/rollingUpgradePipeline.groovy
@@ -218,19 +218,6 @@ def call(Map pipelineParams) {
                                                 }
                                             }
                                         }
-                                        stage('Clean SCT Runners') {
-                                            steps {
-                                                catchError(stageResult: 'FAILURE') {
-                                                    script {
-                                                        wrap([$class: 'BuildUser']) {
-                                                            dir('scylla-cluster-tests') {
-                                                                cleanSctRunners(params, currentBuild)
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
                                     }
                                 }
                             }
