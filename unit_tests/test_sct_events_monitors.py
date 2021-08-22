cdb658dfc7fc1a510ee1cac68694a022b79ace1c
julia.yakovlev <julia.yakovlev@scylladb.com>
2021-08-22 13:05:17 +0300

    fix(unit test): events_monitors according to changes of Prometheus event

--- unit_tests/test_sct_events_monitors.py
+++ unit_tests/test_sct_events_monitors.py
@@ -38,27 +38,33 @@ RAW_ALERT = dict(
 
 class TestPrometheusAlertManagerEvent(unittest.TestCase):
     def test_msgfmt(self):
-        event = PrometheusAlertManagerEvent.start(raw_alert=RAW_ALERT)
+        event = PrometheusAlertManagerEvent(raw_alert=RAW_ALERT)
         event.event_id = "536eaf22-3d8f-418a-9381-fe0bcdce7ad9"
+        event.publish_event = False
+        event.begin_event()
         self.assertEqual(
             str(event),
-            "(PrometheusAlertManagerEvent Severity.WARNING) period_type=not-set "
-            "event_id=536eaf22-3d8f-418a-9381-fe0bcdce7ad9: alert_name=InstanceDown type=start"
-            " start=2019-12-24T17:00:09.591Z end=2019-12-26T06:21:09.591Z"
-            " description=[10.0.201.178] has been down for more than 30 seconds. updated=2019-12-26T06:18:09.593Z"
-            " state= fingerprint=None labels={'alertname': 'InstanceDown', 'instance': '[10.0.201.178]',"
-            " 'job': 'scylla', 'monitor': 'scylla-monitor', 'severity': '2'}"
+            "(PrometheusAlertManagerEvent Severity.NORMAL) period_type=begin "
+            "event_id=536eaf22-3d8f-418a-9381-fe0bcdce7ad9: alert_name=InstanceDown node=[10.0.201.178] "
+            "start=2019-12-24T17:00:09.591Z end=2019-12-26T06:21:09.591Z "
+            "description=[10.0.201.178] has been down for more than 30 seconds. updated=2019-12-26T06:18:09.593Z "
+            "state= fingerprint=None labels={'alertname': 'InstanceDown', 'instance': '[10.0.201.178]', 'job': 'scylla'"
+            ", 'monitor': 'scylla-monitor', 'severity': '2'}"
         )
         self.assertEqual(event, pickle.loads(pickle.dumps(event)))
 
     def test_sct_severity(self):
-        event = PrometheusAlertManagerEvent.end(raw_alert=dict(labels=dict(sct_severity="CRITICAL")))
+        event = PrometheusAlertManagerEvent(raw_alert=dict(labels=dict(sct_severity="CRITICAL")))
+        event.publish_event = False
+        event.end_event()
         self.assertEqual(event.severity, Severity.CRITICAL)
         self.assertTrue(str(event).startswith("(PrometheusAlertManagerEvent Severity.CRITICAL)"))
         self.assertEqual(event, pickle.loads(pickle.dumps(event)))
 
     def test_sct_severity_wrong(self):
-        event = PrometheusAlertManagerEvent.end(raw_alert=dict(labels=dict(sct_severity="WRONG")))
+        event = PrometheusAlertManagerEvent(raw_alert=dict(labels=dict(sct_severity="WRONG")))
+        event.publish_event = False
+        event.end_event()
         self.assertEqual(event.severity, Severity.WARNING)
         self.assertTrue(str(event).startswith("(PrometheusAlertManagerEvent Severity.WARNING)"))
         self.assertEqual(event, pickle.loads(pickle.dumps(event)))
