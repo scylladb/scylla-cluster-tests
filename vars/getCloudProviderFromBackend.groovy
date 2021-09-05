f032d508f9801c8f5c59c1c6d01516359d2d518c
Yaron Gilor <yarongilor@scylladb.com>
2021-09-05 22:30:17 +0300

    fix(siren backends): Adjust siren backens for sct cloud-provider option
    	'aws-siren' and 'gce-siren' values were added as new backends.
    	Thus it should be adjusted when cloud-provider value is requested.

--- vars/getCloudProviderFromBackend.groovy
+++ vars/getCloudProviderFromBackend.groovy
@@ -6,6 +6,8 @@ def call(String backend) {
         'k8s-local-kind-aws': 'aws',
         'k8s-local-kind-gce': 'gce',
         'k8s-gce-minikube': 'gce',
+        'aws-siren': 'aws',
+        'gce-siren': 'gce',
         ]
     if (!backend) {
         return backend
