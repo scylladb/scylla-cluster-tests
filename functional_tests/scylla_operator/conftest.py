2eb6260fd3f323bf7ed2d2f7c6c45ff82d4a316a
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2021-10-01 12:29:08 +0300

    fix(k8s-functional): refresh Scylla IPs after pod replacement
    Two K8S functional tests that replace a Scylla pod don't refresh
    IP addresses in appropriate node objects.
    And if we try to check cluster status with 'nodetool status' command
    after running any of those tests we fail, because IPs do not match
    anymore.
    So, fix it by removing IP cache in those 2 tests.
    Also, check cluster members states after each of the tests to make
    sure that each next test gets workable cluster.

--- functional_tests/scylla_operator/conftest.py
+++ functional_tests/scylla_operator/conftest.py
@@ -156,9 +156,11 @@ def _bring_cluster_back_to_original_state(
 
         if restart:
             db_cluster.restart_scylla()
+        db_cluster.wait_for_nodes_up_and_normal(
+            nodes=db_cluster.nodes, verification_node=db_cluster.nodes[0])
     except Exception as exc:  # pylint: disable=broad-except
         tester.healthy_flag = False
-        pytest.fail("Failed to bring cluster nodes back to original number due to :\n" +
+        pytest.fail("Failed to bring cluster nodes back to original state due to :\n" +
                     "".join(traceback.format_exception(type(exc), exc, exc.__traceback__)))
 
 
