7bafa488d960504f4a38495b8706e0d61d681985
Alexey Kartashov <alexey.kartashov@scylladb.com>
2021-10-13 13:31:08 +0300

    fix(jenkins): Increase "test duration" stage timeout by 1 minute
    Due to running install-argus.sh, the pipeline now takes a bit longer to
    complete, causing it to fail

--- vars/longevityPipeline.groovy
+++ vars/longevityPipeline.groovy
@@ -159,7 +159,7 @@ def call(Map pipelineParams) {
             stage('Get test duration') {
                 steps {
                     catchError(stageResult: 'FAILURE') {
-                        timeout(time: 1, unit: 'MINUTES') {
+                        timeout(time: 2, unit: 'MINUTES') {
                             script {
                                 wrap([$class: 'BuildUser']) {
                                     dir('scylla-cluster-tests') {
