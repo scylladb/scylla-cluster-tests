d9ab1766ba83d0300ee0b54b1229e3029025130b
Lukasz Sojka <lukasz.sojka@scylladb.com>
2021-10-28 12:23:23 +0200

    fix(common.py): convert version keys to strings

---
+++ unit_tests/test_sdcm_mgmt_common.py
@@ -0,0 +1,15 @@
+from sdcm.mgmt.common import get_manager_scylla_backend, get_manager_repo_from_defaults
+from sdcm.utils.distro import Distro
+
+
+class TestManagerVersions:
+
+    def test_get_manager_scylla_backend_returns_repo_address(self):  # pylint: disable=no-self-use
+        url = get_manager_scylla_backend("2021", Distro.UBUNTU20)
+
+        assert url == 'http://downloads.scylladb.com/deb/ubuntu/scylla-2021.1.list'
+
+    def test_get_manager_repo_from_defaults_returns_repo_address(self):  # pylint: disable=no-self-use
+        url = get_manager_repo_from_defaults("2.5", Distro.UBUNTU20)
+
+        assert url == 'http://downloads.scylladb.com/deb/ubuntu/scylladb-manager-2.5-focal.list'
