0869d8dcd8c573000fb221e0fefd739ece391a01
Alexey Kartashov <alexey.kartashov@scylladb.com>
2021-11-01 17:12:27 +0300

    fix(install_argus.sh): Take ownership of package directory if it exists

--- install_argus.sh
+++ install_argus.sh
@@ -3,7 +3,11 @@ set -eu
 LOGFILE="/tmp/argus-install.log"
 echo "Installing Argus pip package..."
 echo "[TODO] Remove this once Argus stabilizes"
-sudo /usr/local/bin/pip3 install --force -e git+https://github.com/bentsi/argus#egg=argus >"$LOGFILE" 2>&1 && echo "Package installed." || (
+sudo /usr/local/bin/pip3 install --force -e git+https://github.com/bentsi/argus#egg=argus >"$LOGFILE" 2>&1 && (
+  echo "Package installed."
+  if [ -d "./src" ]; then sudo chown "$(whoami):$(whoami)" -R "./src"; fi
+  exit 0
+) || (
   env
   echo "Argus failed to install!"
   cat "$LOGFILE"
