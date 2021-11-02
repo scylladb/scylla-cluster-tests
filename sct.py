10d86e068ca3583fc520a23cbe171701f43fa4d4
Alexey Kartashov <alexey.kartashov@scylladb.com>
2021-11-02 14:31:13 +0300

    fix(argus_test_run): Use argus v0.3.3 features
    Removed database initialization from argus_test_run to prevent bugs
    related to lingering threads and to abstract away database connection.

--- sct.py
+++ sct.py
@@ -34,7 +34,8 @@ import click
 import click_completion
 from prettytable import PrettyTable
 
-from sdcm.argus_test_run import ArgusTestRun
+from sdcm.argus_test_run import ArgusTestRun, ArgusConfig
+from sdcm.keystore import KeyStore
 from sdcm.remote import LOCALRUNNER
 from sdcm.results_analyze import PerformanceResultsAnalyzer, BaseResultsAnalyzer
 from sdcm.sct_config import SCTConfiguration
@@ -986,7 +987,8 @@ def collect_logs(test_id=None, logdir=None, backend=None, config_file=None):
 
 def store_logs_in_argus(test_id: UUID, logs: dict[str, list[list[str] | str]]):
     try:
-        test_run = ArgusTestRun.get(test_id=test_id)
+        test_run = ArgusTestRun.get(test_id=test_id, config=ArgusConfig(**KeyStore().get_argusdb_credentials(),
+                                                                        keyspace_name="argus"))
         for cluster_type, s3_links in logs.items():
             for link in s3_links:
                 test_run.run_info.logs.add_log(cluster_type, link)
