2b9e798128cb2bcc010e242df8bc92a0ca6ba174
Shlomi Balalis <Balalis@scylladb.com>
2021-11-04 11:03:31 +0200

    fix(status): TO SQUASSHHHHHHHHHHHHHHHHHHHH

--- sdcm/mgmt/cli.py
+++ sdcm/mgmt/cli.py
@@ -156,7 +156,8 @@ class ManagerTask:
         # The manager will sometimes retry a task a few times if it's defined this way, and so in the case of
         # a failure in the task the manager can present the task's status as 'ERROR (#/4)'
         tmp = str_status.split()
-        if ' '.join(tmp) == 'ERROR (4/4)':
+        # We don't examine the whole string, since sometimes error messages can appear after the status
+        if ' '.join(tmp[0:2]) == 'ERROR (4/4)':
             return TaskStatus.ERROR_FINAL
         return TaskStatus.from_str(tmp[0])
 
