b168cb562c4c22cc67ef8d48396657b5f7216d48
julia.yakovlev <julia.yakovlev@scylladb.com>
2021-11-07 10:58:27 +0200

    improvement(email): add link to the job that recreate a monitor
    This commit will improve our issues and do a life easier to developers.
    Add to the issue link to the job that restore the monitor with it's parameters.

--- sdcm/tester.py
+++ sdcm/tester.py
@@ -2957,6 +2957,8 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
             scylla_instance_type = "N/A"
 
         job_name = os.environ.get('JOB_NAME').split("/")[-1] if os.environ.get('JOB_NAME') else config_file_name
+        restore_monitor_job_base_link = \
+            "https://jenkins.scylladb.com/view/QA/job/QA-tools/job/hydra-show-monitor/parambuild/?"
 
         return {"backend": backend,
                 "build_id": os.environ.get('BUILD_NUMBER', ''),
@@ -2980,7 +2982,8 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
                 "test_name": self.id(),
                 "test_status": test_status,
                 "username": get_username(),
-                "shard_awareness_driver": self.is_shard_awareness_driver}
+                "shard_awareness_driver": self.is_shard_awareness_driver,
+                "restore_monitor_job_base_link": restore_monitor_job_base_link}
 
     @silence()
     def tag_ami_with_result(self):
