ff2d65c46bd4ef74e4d0927f755ddf0677a1180f
Lukasz Sojka <lukasz.sojka@scylladb.com>
2021-11-17 11:59:08 +0100

    test(nemesis): add remove dc nemesis fixes

--- sdcm/utils/replication_strategy_utils.py
+++ sdcm/utils/replication_strategy_utils.py
@@ -1,16 +1,49 @@
+import ast
 import re
 
 from contextlib import ContextDecorator
-from dataclasses import dataclass
-from typing import List, Callable
+from typing import Callable, Dict
 
 from sdcm.cluster import BaseNode
 
 
-@dataclass
-class KeyspaceReplicationStrategy:
-    keyspace: str
-    strategy: str
+class ReplicationStrategy:  # pylint: disable=too-few-public-methods
+
+    @classmethod
+    def from_string(cls, replication_string):
+        replication_value = re.search(r".*replication[\s]*=[\s]*(\{.*\})", replication_string, flags=re.IGNORECASE)
+        strategy_params = ast.literal_eval(replication_value[1])
+        strategy_class = strategy_params.pop("class")
+        for class_ in replication_strategies:
+            if strategy_class == class_.class_:
+                return class_(**strategy_params)
+        raise ValueError(f"Couldn't find such replication strategy: {replication_value}")
+
+
+class SimpleReplicationStrategy(ReplicationStrategy):
+
+    class_: str = 'SimpleStrategy'
+
+    def __init__(self, replication_factor: int):
+        self.replication_factor = replication_factor
+
+    def __str__(self):
+        return f"{{'class': '{self.class_}', 'replication_factor': {self.replication_factor}}}"
+
+
+class NetworkTopologyReplicationStrategy(ReplicationStrategy):
+
+    class_: str = 'NetworkTopologyStrategy'
+
+    def __init__(self, **replication_factors: int):
+        self.replication_factors = replication_factors
+
+    def __str__(self):
+        factors = ', '.join([f"'{key}': {value}" for key, value in self.replication_factors.items()])
+        return f"{{'class': '{self.class_}', {factors}}}"
+
+
+replication_strategies = [SimpleReplicationStrategy, NetworkTopologyReplicationStrategy]
 
 
 class temporary_replication_strategy_setter(ContextDecorator):  # pylint: disable=invalid-name
@@ -19,24 +52,23 @@ class temporary_replication_strategy_setter(ContextDecorator):  # pylint: disabl
 
     def __init__(self, node: BaseNode) -> None:
         self.node = node
-        self.preserved: List[KeyspaceReplicationStrategy] = []
+        self.preserved: Dict[str, ReplicationStrategy] = {}
 
-    def __enter__(self) -> Callable[[List[KeyspaceReplicationStrategy]], None]:
+    def __enter__(self) -> Callable[..., None]:
         return self
 
     def __exit__(self, *exc) -> bool:
-        self(self.preserved)
+        self(**self.preserved)
         return False
 
-    def _preserve_replication_strategy(self, keyspace: str):
-        if keyspace in [rep.keyspace for rep in self.preserved]:
+    def _preserve_replication_strategy(self, keyspace: str) -> None:
+        if keyspace in self.preserved:
             return  # already preserved
         create_ks_statement = self.node.run_cqlsh(f"describe {keyspace}").stdout.splitlines()[1]
-        replication_strategy = re.search(r".*replication = (\{.*\})", create_ks_statement).group(1)
-        self.preserved.append(KeyspaceReplicationStrategy(keyspace, replication_strategy))
+        self.preserved[keyspace] = (ReplicationStrategy.from_string(create_ks_statement))
 
-    def __call__(self, replication_strategies: List[KeyspaceReplicationStrategy]) -> None:
-        for replication_strategy in replication_strategies:
-            self._preserve_replication_strategy(replication_strategy.keyspace)
-            cql = f"ALTER KEYSPACE {replication_strategy.keyspace} WITH replication = {replication_strategy.strategy}"
+    def __call__(self, **keyspaces: ReplicationStrategy) -> None:
+        for keyspace, strategy in keyspaces.items():
+            self._preserve_replication_strategy(keyspace)
+            cql = f"ALTER KEYSPACE {keyspace} WITH replication = {strategy}"
             self.node.run_cqlsh(cql)
