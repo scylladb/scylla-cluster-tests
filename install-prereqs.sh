5fe14349c0ea84ef7c1922d538f326ae83c3474e
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2021-12-01 11:21:59 +0300

    fix(install-prereqs): add ubuntu support

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -1,47 +1,57 @@
 #!/usr/bin/env bash
-yum install -y epel-release
-yum -y update
-yum install -y wget unzip
 
-# Python dependencies
-yum install -y python-devel python-pip
-pip install --upgrade pip
+if yum --help; then
+  yum install -y epel-release
+  yum -y update
+  yum install -y wget unzip
 
-# Needed for PhantomJS
-yum install -y freetype-devel libpng-devel bzip2 bitmap-fonts fontconfig
+  # Python dependencies
+  yum install -y python-devel python-pip
+  pip install --upgrade pip
 
-# Needed for Cassandra Python driver
-yum install -y gcc
+  # Needed for PhantomJS
+  yum install -y freetype-devel libpng-devel bzip2 bitmap-fonts fontconfig
 
-# Install OpenSSH client - needed to ssh to DB servers/ Loaders/ monitors
-yum install -y openssh-clients
+  # Needed for Cassandra Python driver
+  yum install -y gcc
 
-# Install Git - needed to get current SCT branch
-yum install -y git
+  # Install OpenSSH client - needed to ssh to DB servers/ Loaders/ monitors
+  yum install -y openssh-clients
 
-# Install OpenSSL - needed to update encryption key
-yum install -y openssl
-yum install -y iproute
-yum install -y which
+  # Install Git - needed to get current SCT branch
+  yum install -y git
 
-# Install Docker
-yum install -y sudo  # needed for Docker container build
+  # Install OpenSSL - needed to update encryption key
+  yum install -y openssl
+  yum install -y iproute
+  yum install -y which
+
+  # Install Docker
+  yum install -y sudo  # needed for Docker container build
+
+  # Install gettext - needed for k8s-* backends
+  yum install -y gettext
+
+elif apt --help; then
+  apt-get install -y --no-install-recommends build-essential cmake libssl-dev zlib1g-dev libffi-dev
+
+else
+  echo "Unsupported distro"
+  exit 1
+fi
+
+# Install kubectl needed for k8s-* backends
+
+curl -o /tmp/kubectl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install docker
+
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 groupadd docker || true
 usermod -aG docker $USER || true
 
-# Install kubectl and gettext - needed for k8s-* backends
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-yum install -y kubectl gettext
 
 # Make sdcm available in python path
 if [ "$1" == "docker" ]; then
