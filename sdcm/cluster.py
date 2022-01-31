1bf5e983f9345eaf814a0f7abd43a0b46ab97068
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2022-01-31 17:55:32 +0200

    fix(cluster.py): make K8S work again
    Recently was added common VMs-specific code into the
    'sdcm/cluster.py' module -> 'cql_ip_address' property.
    And this property doesn't work for K8S for 2 reasons:
    - On K8S we never have 'broadcast_rpc_address' be setup.
    - On K8S we do not use ScyllaYaml class, because it's work
      is done by Operator itself.
    
    So, detect that we are on the K8S in this property and return expected
    value for it.

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -846,6 +846,8 @@ class BaseNode(AutoSshContainerMixin, WebDriverContainerMixin):  # pylint: disab
 
     @property
     def cql_ip_address(self):
+        if self.is_kubernetes():
+            return self.ip_address
         with self.remote_scylla_yaml() as scylla_yaml:
             return scylla_yaml.broadcast_rpc_address if scylla_yaml.broadcast_rpc_address else self.ip_address
 
