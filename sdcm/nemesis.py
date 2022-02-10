2302dccf4f1207d0ba2cfebfc3fb30ff444b4073
Fabio Gelcer <fabio@scylladb.com>
2022-02-10 22:50:10 +0200

    fix(nemesis.py): disrupt_remove_node_then_add_node description
    the nemesis description is missing an important
    step, that was misleading when quickly reading it
    (instead of the code).

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -2438,10 +2438,11 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
         """
         https://docs.scylladb.com/operating-scylla/procedures/cluster-management/remove_node/
 
-        1.Terminate node
-        2.Run full repair
-        3.Nodetool removenode
-        4.Add new node
+        1. Terminate node
+        2. Run full repair
+        3. Nodetool removenode
+        4. Add new node
+        5. Run nodetool cleanup (on each node) for each keyspace
         """
         if self.cluster.params.get("db_type") == 'cloud_scylla':
             raise UnsupportedNemesis("Skipping this nemesis due this job run from Siren cloud with 2019 version!")
