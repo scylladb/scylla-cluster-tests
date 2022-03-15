895fa521457251c04f6fe7c11d69a2bfef98888c
Israel Fruchter <fruch@scylladb.com>
2022-03-15 16:32:44 +0200

    fix(test-case-lint): ignore the RBO partial test yamls
    the linting expect every signal yaml in test-cases folder
    to be complate, the new configuration inside `repair-based-operations`
    folder breaks that, for now we'll ignore them insted of moving them
    into configuration folder

--- utils/lint_test_cases.sh
+++ utils/lint_test_cases.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 OUT=0
-SCT_SCYLLA_VERSION=4.4.1 ./sct.py lint-yamls -i '.yaml' -e 'multi-dc,multiDC,multidc,multiple-dc,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,jepsen'
+SCT_SCYLLA_VERSION=4.4.1 ./sct.py lint-yamls -i '.yaml' -e 'multi-dc,multiDC,multidc,multiple-dc,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,jepsen,repair-based-operations'
 OUT=$(($OUT + $?))
 
 SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'multi-dc,multiDC,multidc,multiple-dc,rolling,artifacts,private-repo,gce,jepsen' -e 'docker'
