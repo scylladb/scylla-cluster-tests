b8f18ef76d43c20649389f8dda7761612972f6b9
MaciekCisowski <maciej.cisowski@scylladb.com>
2022-04-12 21:16:19 +0200

    fix(rolling_upgrade): improve error handling and logging
    (cherry picked from commit 1a5756052206ed24f7b1331ecca3db2d0e22e12a)

--- vars/supportedUpgradeFromVersions.groovy
+++ vars/supportedUpgradeFromVersions.groovy
@@ -5,11 +5,12 @@ def call(List base_versions_list, String linux_distro, String new_scylla_repo) {
         def result = sh (returnStdout: true,
                          script: """ ./docker/env/hydra.sh get-scylla-base-versions --only-print-versions true \
                                      --linux-distro ${linux_distro} --scylla-repo ${new_scylla_repo} """)
-        println(result)
+        printf('Docker get-scylla-base-versions output:\n%s', result)
         def last_line = result.split('\n')[-1]
-        if (last_line.contains('Base Versions: ')) {
+        if (last_line.matches("Base\\sVersions:\\s*[\\d\\w\\W]*")) {
             return last_line.replaceAll('Base Versions: ', '').split(' ')
         } else {
+        println("Did not find a valid base versions string!")
             throw new Exception("Didn't get valid base versions automatically!")
         }
 
