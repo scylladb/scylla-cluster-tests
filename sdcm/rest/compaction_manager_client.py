cb93c5bde2fb3034c2143a202db285aa9536e63b
Lukasz Sojka <lukasz.sojka@scylladb.com>
2022-04-15 10:39:28 +0200

    test(reshape): reshape on boot fixes
    (cherry picked from commit 49f587f9438801523500dc03d8de8eca9fbe53fb)

---
+++ sdcm/rest/compaction_manager_client.py
@@ -0,0 +1,35 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright (c) 2022 ScyllaDB
+
+import logging
+from typing import Literal
+
+from fabric.runners import Result
+
+from sdcm.cluster import BaseNode
+from sdcm.rest.remote_curl_client import RemoteCurlClient
+
+LOGGER = logging.getLogger(__name__)
+
+
+class CompactionManagerClient(RemoteCurlClient):
+    def __init__(self, node: BaseNode):
+        super().__init__(host="localhost:10000", endpoint="compaction_manager", node=node)
+
+    def stop_compaction(self, compaction_type: Literal["reshape"]) -> Result:
+        """Stops compaction using Scylla Rest API.
+        """
+        LOGGER.debug("Stopping reshape compaction via Scylla REST API")
+        params = {"type": compaction_type.upper()}
+
+        return self.run_remoter_curl(method="POST", path="stop_compaction", params=params, timeout=360)
--- sdcm/rest/remote_curl_client.py
+++ sdcm/rest/remote_curl_client.py
@@ -1,7 +1,24 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright (c) 2022 ScyllaDB
+
 from typing import Literal
 
 from sdcm.cluster import BaseNode
 from sdcm.rest.rest_client import RestClient
+
+
+class ScyllaApiException(Exception):
+    pass
 
 
 class RemoteCurlClient(RestClient):
@@ -12,5 +29,10 @@ class RemoteCurlClient(RestClient):
 
     def run_remoter_curl(self, method: Literal["GET", "POST"], path: str, params: dict[str, str], timeout: int = 120):
         prepared_request = self._prepare_request(method=method, path=path, params=params)
-
-        return self._remoter.run(f'curl -v -X {prepared_request.method} "{prepared_request.url}"', timeout=timeout)
+        result = self._remoter.run(f'curl -v -X {prepared_request.method} "{prepared_request.url}"', timeout=timeout)
+        if result.failed:
+            raise ScyllaApiException(f"Scylla Rest Api request failed. Method: {prepared_request.method}, "
+                                     f"url: {prepared_request.url}, "
+                                     f"stdout: {result.stdout}, "
+                                     f"stderr: {result.stderr}")
+        return result
