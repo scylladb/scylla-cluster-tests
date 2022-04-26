152e6570d4213bac00f245412c906dda467eb714
Shlomi Balalis <Balalis@scylladb.com>
2022-04-26 17:04:22 +0300

    fix(manager upgrade): no longer only backup up system keyspaces
    As part of the upgrade test, we create a backup (of the system keyspaces) and
    stop it mid-run, with the intention of continuing it in a later stage of the test.
    The problem is that backup only the system keyspaces is causing the task to end
    way too fast, so we don't have a chance to stop it.
    As such, I added the ability in the _generate_load function to create a new, larger
    keyspace and changed the task to backup only it so it will last longer.

--- mgmt_upgrade_test.py
+++ mgmt_upgrade_test.py
@@ -106,11 +106,12 @@ class ManagerUpgradeTest(BackupFunctionsMixIn, ClusterTester):
                 f"Unknown failure in task {rerunning_backup_task.id}"
 
         with self.subTest("Creating a backup task and stopping it"):
+            self.generate_load_and_wait_for_results(keyspace_name_to_replace="keyspace2")
             legacy_args = "--force" if manager_tool.sctool.client_version.startswith("2.1") else None
             pausable_backup_task = mgr_cluster.create_backup_task(
                 interval="1d",
                 location_list=self.locations,
-                keyspace_list=["system_*"],
+                keyspace_list=["keyspace2"],
                 legacy_args=legacy_args,
             )
             pausable_backup_task.wait_for_status(list_status=[TaskStatus.RUNNING], timeout=180, step=2)
