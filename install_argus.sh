ec4116c656bd55d8bfb4fcb9aa8d8b52c09b724f
Fabio Gelcer <fabio@scylladb.com>
2022-05-04 14:44:07 +0300

    fix(argus): workaround arguns installation in an old branch
    installation of argus is failing on old branches,
    because changes done in Argus, making the early
    stages of the test to fail, so the tests are not
    even starting.

--- install_argus.sh
+++ install_argus.sh
@@ -3,8 +3,9 @@ set -eu
 LOGFILE="/tmp/argus-install.log"
 echo "Installing Argus pip package..."
 echo "[TODO] Remove this once Argus stabilizes"
-sudo /usr/local/bin/pip3 install --force -e git+https://github.com/bentsi/argus#egg=argus >"$LOGFILE" 2>&1 && (
+sudo /usr/local/bin/pip3 install --force https://github.com/scylladb/argus/archive/refs/tags/v0.5.0.zip >"$LOGFILE" 2>&1 && (
   echo "Package installed."
+  cat "$LOGFILE"
   if [ -d "./src" ]; then sudo chown "$(whoami):$(whoami)" -R "./src"; fi
   exit 0
 ) || (
