ba3b3e16cde6b458e219fdb19c169ecbacfe4b7c
Fabio Gelcer <fabio@scylladb.com>
2022-05-30 16:13:14 +0300

    fix(perfRegression): `get_test_duration` timeout increase
    we seen very often, lately, the function that
    calculates the pipeline timeout (called
    `get_test_duration`) timing out itself,
    making the whole run to fail, as next
    stages that require a timeout value will
    get `None` and will fail.

--- vars/perfRegressionParallelPipeline.groovy
+++ vars/perfRegressionParallelPipeline.groovy
@@ -101,7 +101,7 @@ def call(Map pipelineParams) {
                 }
                 steps {
                     catchError(stageResult: 'FAILURE') {
-                        timeout(time: 1, unit: 'MINUTES') {
+                        timeout(time: 5, unit: 'MINUTES') {
                             script {
                                 wrap([$class: 'BuildUser']) {
                                     dir('scylla-cluster-tests') {
