115ac365cc804800eafadbf3bfb9531abec8623e
Israel Fruchter <fruch@scylladb.com>
2022-06-01 13:33:21 +0300

    fix(sct_config): user get readable messge if versio/repo missing
    The message would be base on the backend it's using stating
    the expected argument to pass to select the scylla version under
    test
    
    ```
    E       AssertionError: scylla version/repos wasn't configured correctly
    E       configure one of the following: ['azure_image_db']
    E       or one of those environment variables: ['SCT_AZURE_IMAGE_DB']
    ```
    
    Fixes: #4838

--- utils/lint_test_cases.sh
+++ utils/lint_test_cases.sh
@@ -4,7 +4,7 @@ OUT=0
 SCT_SCYLLA_VERSION=4.4.1 ./sct.py lint-yamls -i '.yaml' -e 'multi-dc,multiDC,multidc,multiple-dc,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,jepsen,repair-based-operations'
 OUT=$(($OUT + $?))
 
-SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'multi-dc,multiDC,multidc,multiple-dc,rolling,artifacts,private-repo,gce,jepsen' -e 'docker'
+SCT_GCE_IMAGE_DB=image SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'multi-dc,multiDC,multidc,multiple-dc,rolling,artifacts,private-repo,gce,jepsen' -e 'docker,azure'
 OUT=$(($OUT + $?))
 
 SCT_SCYLLA_VERSION=4.4.1 ./sct.py lint-yamls -b k8s-local-kind -i 'scylla-operator.*\.yaml'
