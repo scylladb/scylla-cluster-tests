62724427502295044f16141955393570fc7b93fb
Israel Fruchter <fruch@scylladb.com>
2022-06-21 13:57:30 +0300

    fix(longevityPipeline): add support for passing down `docker_image`
    Since in k8s longevity we want to be able to control the
    source dockerhub project source to pull the scylla image from

--- vars/runSctTest.groovy
+++ vars/runSctTest.groovy
@@ -69,6 +69,10 @@ def call(Map params, String region, functional_test = false, Map pipelineParams 
         export SCT_K8S_LOG_API_CALLS=${pipelineParams.k8s_log_api_calls}
     fi
 
+    if [[ -n "${params.docker_image ? params.docker_image : ''}" ]] ; then
+        export SCT_DOCKER_IMAGE=${params.docker_image}
+    fi
+
     if [[ -n "${params.scylla_mgmt_agent_version ? params.scylla_mgmt_agent_version : ''}" ]] ; then
         export SCT_SCYLLA_MGMT_AGENT_VERSION=${params.scylla_mgmt_agent_version}
     fi
