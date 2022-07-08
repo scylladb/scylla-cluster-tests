ba28cc29e4c0441d677a348361768ed68183ae75
Lukasz Sojka <lukasz.sojka@scylladb.com>
2022-07-08 12:47:06 +0200

    feature(gce-log-monitor): error events for GCE hostError
    Recently we are facing issues with GCE. Sometimes GCE raises 'hostError'
    which means machine that hosts instance failed. In result GCE tries to
    restart instance. It often happens when some other node gets restarted.
    In that case, usually we get c-s errors about missing quorum.
    Also sometimes we miss SpotTerminationEvent in GCE.
    
    This feature is using GCE logging mechanism to fetch erroneous log
    events and publish them as SCT events. For this, spot verification
    thread was used (and replaced former logic that used metadata api).

---
+++ sdcm/sct_events/gce_events.py
@@ -0,0 +1,23 @@
+from typing import Dict
+
+from dateutil import parser
+
+from sdcm.sct_events import Severity
+from sdcm.sct_events.base import SctEvent, EventPeriod
+
+
+class GceInstanceEvent(SctEvent):
+
+    def __init__(self,
+                 gce_log_entry: Dict,
+                 severity=Severity.ERROR):
+        self.date = str(parser.parse(gce_log_entry["timestamp"]).astimezone())
+        self.node = gce_log_entry["protoPayload"]["resourceName"].split("/")[-1]
+        self.method = gce_log_entry["protoPayload"]["methodName"]
+        self.message = gce_log_entry["protoPayload"]["status"]["message"]
+        self.period_type = EventPeriod.INFORMATIONAL.value
+        super().__init__(severity=severity)
+
+    @property
+    def msgfmt(self):
+        return super().msgfmt + ": {0.method} on node {0.node} at {0.date}: {0.message}"
