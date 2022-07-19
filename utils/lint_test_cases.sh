b74a25d7045a528d9df6812b30817bafbcee08ef
Shlomi Balalis <Balalis@scylladb.com>
2022-07-19 10:50:53 +0300

    fix(lint_test_cases.sh): linting multi region yamls separately
    Since now gce_datacenter is a multi region params, the previous linting of the yaml
    files failed, so I altered it to take the datacenter count into account

--- utils/lint_test_cases.sh
+++ utils/lint_test_cases.sh
@@ -4,7 +4,15 @@ OUT=0
 SCT_SCYLLA_VERSION=4.4.1 ./sct.py lint-yamls -i '.yaml' -e 'multi-dc,multiDC,multidc,multiple-dc,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,jepsen,repair-based-operations'
 OUT=$(($OUT + $?))
 
-SCT_GCE_IMAGE_DB=image SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'multi-dc,multiDC,multidc,multiple-dc,rolling,artifacts,private-repo,gce,jepsen' -e 'docker,azure'
+SCT_GCE_IMAGE_DB=image SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'rolling,artifacts,private-repo,gce,jepsen' -e 'multi-dc,docker,azure'
+OUT=$(($OUT + $?))
+
+echo "multi dc yamls with 2 regions"
+SCT_GCE_IMAGE_DB=image SCT_GCE_DATACENTER="us-east1 us-west1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'multi-dc-rolling,3h-multidc,multidc-4h,topology-changes,snitch-multi-dc,manager-regression-multiDC' -e 'docker,azure,shutdown'
+OUT=$(($OUT + $?))
+
+echo "multi dc yamls with 3 regions"
+SCT_GCE_IMAGE_DB=image SCT_GCE_DATACENTER="us-east1 us-west1 eu-north1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i '24h-multidc,large-cluster,counters-multidc' -e 'docker,azure,shutdown'
 OUT=$(($OUT + $?))
 
 SCT_SCYLLA_VERSION=4.4.1 ./sct.py lint-yamls -b k8s-local-kind -i 'scylla-operator.*\.yaml'
