7d9fbadde7b6a3087117585b15b683ff2a4b6bb3
Aleksandr Bykov <alex.bykov@scylladb.com>
2022-09-05 17:04:56 +0700

    fix(SCTDashboard): increase screenshot size
    After new panels were added to sct dashboard,
    the old size of screenshot is not enough,
    because several first panels are scroll out
    from view.
    
    fix issue #5224
    
    (cherry picked from commit c63e91e6513ca161e3b32dcd0436de3a3a3d1d9b)

--- sdcm/monitorstack/ui.py
+++ sdcm/monitorstack/ui.py
@@ -166,7 +166,7 @@ class ServerMetricsNemesisDashboard(Dashboard):
     name = f'{test_name}scylla-per-server-metrics-nemesis'
     title = 'Scylla Per Server Metrics Nemesis'
     path = 'dashboard/db/{dashboard_name}-{version}'
-    resolution = '1920px*8000px'
+    resolution = '1920px*15000px'
     panels = [Panel("Total Requests"),
               Panel("Load per Instance"),
               Panel("Requests Served per Instance"),
