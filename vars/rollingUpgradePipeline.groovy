f44023b242a154fab61558203f5e197589015dbc
MaciekCisowski <maciej.cisowski@scylladb.com>
2022-10-05 12:20:18 +0200

    refactor(rolling_upgrade): add catchError for getting base versions
    This commit adds the catchError method for the rollingUpgradePipeline
    script, getting base versions step. This should imrove debugging any
    potential errors in that stage of the pipeline.

--- vars/rollingUpgradePipeline.groovy
+++ vars/rollingUpgradePipeline.groovy
@@ -94,22 +94,24 @@ def call(Map pipelineParams) {
                     }
                 }
                 steps {
-                    timeout(time: 10, unit: 'MINUTES') {
-                        script {
-                            wrap([$class: 'BuildUser']) {
-                                dir('scylla-cluster-tests') {
-                                    checkout scm
-                                    ArrayList base_versions_list = params.base_versions.contains('.') ? params.base_versions.split('\\,') : []
-                                    supportedVersions = supportedUpgradeFromVersions(
-                                        base_versions_list,
-                                        pipelineParams.linux_distro,
-                                        params.new_scylla_repo
-                                    )
-                                    (testDuration,
-                                     testRunTimeout,
-                                     runnerTimeout,
-                                     collectLogsTimeout,
-                                     resourceCleanupTimeout) = getJobTimeouts(params, builder.region)
+                    catchError(stageResult: "FAILURE") {
+                        timeout(time: 10, unit: 'MINUTES') {
+                            script {
+                                wrap([$class: 'BuildUser']) {
+                                    dir('scylla-cluster-tests') {
+                                        checkout scm
+                                        ArrayList base_versions_list = params.base_versions.contains('.') ? params.base_versions.split('\\,') : []
+                                        supportedVersions = supportedUpgradeFromVersions(
+                                            base_versions_list,
+                                            pipelineParams.linux_distro,
+                                            params.new_scylla_repo
+                                        )
+                                        (testDuration,
+                                         testRunTimeout,
+                                         runnerTimeout,
+                                         collectLogsTimeout,
+                                         resourceCleanupTimeout) = getJobTimeouts(params, builder.region)
+                                    }
                                 }
                             }
                         }
