fadd17950e1fa9d57e1379c3a203139a66bce4db
MaciekCisowski <maciej.cisowski@scylladb.com>
2022-10-12 19:22:24 +0200

    fix(nemesis): wait for db_up in start_and_interrupt_rebuild_streaming()
    fixes: https://github.com/scylladb/scylla-cluster-tests/issues/5285
    
    The method start_and_interrupt_rebuild_streaming() is used to
    hard reboot a node during the rebuild operation. After the reboot
    we attempt to rebuild. We waited on ssh_up on the target machine,
    but that can be true before Scylla finishes it's own restart. This
    commit introduces a wait for db_up to avoid asking the db for a
    rebuild before it's up.

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -3161,6 +3161,7 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
             delay=1
         )
         ParallelObject(objects=[trigger, watcher], timeout=timeout + 60).call_objects()
+        self.target_node.wait_db_up(timeout=300)
         self.target_node.run_nodetool("rebuild")
 
     def disrupt_decommission_streaming_err(self):
