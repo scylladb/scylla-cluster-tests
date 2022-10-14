c39a91d47d1ab2e984f420f6cabbe80c4df5fe5b
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2022-10-14 13:39:16 +0300

    refactor(k8s-multitenancy): reuse common code for perf and longevity
    List of changes:
    - Use common code for the 'perf' and 'longevity' modules
    - Handle 'stop_resources' method per-cluster
    - Change the way of operating the multitenant config options
    - Add support for the nemesis-related config options

--- longevity_operator_multi_tenant_test.py
+++ longevity_operator_multi_tenant_test.py
@@ -13,96 +13,25 @@
 #
 # Copyright (c) 2022 ScyllaDB
 
-import copy
-import logging
-import time
-
 from longevity_test import LongevityTest
-
 from sdcm.utils.common import ParallelObject
+from sdcm.utils.operator.multitenant_common import MultiTenantTestMixin
 
 
-from sdcm.utils.operator.multitenant_common import set_stress_command_to_tenant
-
-
-class MultiTenantBase(LongevityTest):
-    _testMethodName = "runTest"
-
-
-# pylint: disable=too-many-instance-attributes
-class ScyllaClusterStats(MultiTenantBase):
-    # pylint: disable=too-many-arguments,super-init-not-called
-    def __init__(self, db_cluster, loaders, monitors, prometheus_db, params, test_config, cluster_index):
-        self.db_cluster = db_cluster
-        self.loaders = loaders
-        self.monitors = monitors
-        self.prometheus_db = prometheus_db
-        self.params = copy.deepcopy(params)
-        self.test_config = test_config
-        self._duration = self.params.get(key='test_duration')
-        self.log = logging.getLogger(self.__class__.__name__)
-        self.create_stats = self.params.get(key='store_perf_results')
-        self.status = "RUNNING"
-        self.cluster_index = str(cluster_index)
-        self._test_id = self.test_config.test_id() + f"--{cluster_index}"
-        self._test_index = self.get_str_index()
-        self.start_time = time.time()
-        self._duration = self.params.get(key='test_duration')
-        self.timeout_thread = self._init_test_timeout_thread()
-        self.test_config.reuse_cluster(False)
-
-    def get_str_index(self):
-        return f"k8s-longevity-{self.db_cluster.k8s_cluster.tenants_number}-tenants"
-
-    def id(self):  # pylint: disable=invalid-name
-        return self.test_config.test_id() + f"-{self._test_index}"
-
-    def __str__(self) -> str:
-        return self._test_index + f"--{self.cluster_index}"
-
-    def __repr__(self) -> str:
-        return self.__str__()
-
-
-class LongevityOperatorMulitiTenantTest(MultiTenantBase):
-    scylla_clusters_stats = []
-
-    def setUp(self):
-        super().setUp()
-        for i, db_cluster in enumerate(self.db_clusters_multitenant):  # pylint: disable=no-member
-            self.scylla_clusters_stats.append(ScyllaClusterStats(
-                db_cluster=db_cluster,
-                loaders=self.loaders_multitenant[i],  # pylint: disable=no-member
-                monitors=self.monitors_multitenant[i],  # pylint: disable=no-member
-                prometheus_db=self.prometheus_db_multitenant[i],  # pylint: disable=no-member
-                params=self.params,
-                test_config=self.test_config,
-                cluster_index=i + 1,
-            ))
-
-            current_stress_cmds = set_stress_command_to_tenant(params=self.params, tenant_number=i)
-            for stress_cmd_param, stress_cmds in current_stress_cmds.items():
-                self.scylla_clusters_stats[i].params[stress_cmd_param] = stress_cmds
-
-            self.log.debug("stress_cmd for cluster %s: %s", self.db_cluster.name,
-                           self.scylla_clusters_stats[i].params["stress_cmd"])
-            self.log.debug("stress_read_cmd for cluster %s: %s", self.db_cluster.name,
-                           self.scylla_clusters_stats[i].params["stress_read_cmd"])
-            self.log.debug("prepare_write_cmd for cluster %s: %s", self.db_cluster.name,
-                           self.scylla_clusters_stats[i].params["prepare_write_cmd"])
+class LongevityOperatorMultiTenantTest(MultiTenantTestMixin, LongevityTest):
 
     def test_custom_time(self):
-        def _run_test_on_one_tenant(scylla_cluster_stats):
-            self.log.info("Longevity test for cluster %s with parameters: %s", scylla_cluster_stats.db_cluster,
-                          scylla_cluster_stats.params)
-            scylla_cluster_stats.test_custom_time()
+        def _run_test_on_one_tenant(tenant):
+            self.log.info("Longevity test for cluster %s with parameters: %s",
+                          tenant.db_cluster, tenant.params)
+            tenant.test_custom_time()
 
         self.log.info("Starting tests worker threads")
 
         self.log.info("Clusters count: %s", self.k8s_cluster.tenants_number)
         object_set = ParallelObject(
             timeout=int(self.test_duration) * 60,
-            objects=[[scs] for scs in self.scylla_clusters_stats],
+            objects=[[tenant] for tenant in self.tenants],
             num_workers=self.k8s_cluster.tenants_number
         )
         object_set.run(func=_run_test_on_one_tenant, unpack_objects=True, ignore_exceptions=False)
--- performance_regression_operator_multi_tenant_test.py
+++ performance_regression_operator_multi_tenant_test.py
@@ -13,52 +13,13 @@
 #
 # Copyright (c) 2022 ScyllaDB
 
-import copy
-import logging
-
 from performance_regression_test import PerformanceRegressionTest
 from sdcm.utils.common import ParallelObject
-
-
-# pylint: disable=super-init-not-called,consider-using-enumerate
-# pylint: disable=too-many-instance-attributes,too-many-arguments
-class ScyllaClusterStats(PerformanceRegressionTest):
-    def __init__(self, db_cluster, loaders, monitors, prometheus_db, params, test_config,
-                 cluster_index):
-        self.db_cluster = db_cluster
-        self.loaders = loaders
-        self.monitors = monitors
-        self.prometheus_db = prometheus_db
-        self.params = copy.deepcopy(params)
-        self.log = logging.getLogger(self.__class__.__name__)
-        self._es_doc_type = "test_stats"
-        self._stats = self._init_stats()
-        self.test_config = test_config
-        self._duration = self.params.get(key='test_duration')
-        self.create_stats = self.params.get(key='store_perf_results')
-        self.status = "RUNNING"
-        self.cluster_index = str(cluster_index)
-        self._test_id = self.test_config.test_id() + f"--{cluster_index}"
-        self._test_index = self.get_str_index()
-        self.create_test_stats()
-        self.start_time = self.get_test_start_time()
-
-    def get_str_index(self):
-        return f"k8s-perf-{self.db_cluster.k8s_cluster.tenants_number}-tenants"
-
-    def id(self):  # pylint: disable=invalid-name
-        return self._test_index
-
-    def __str__(self) -> str:
-        return self._test_index + f"--{self.cluster_index}"
-
-    def __repr__(self) -> str:
-        return self.__str__()
+from sdcm.utils.operator.multitenant_common import MultiTenantTestMixin
 
 
 # pylint: disable=too-many-public-methods
-class PerformanceRegressionOperatorMultiTenantTest(PerformanceRegressionTest):
-    scylla_clusters_stats = []
+class PerformanceRegressionOperatorMultiTenantTest(MultiTenantTestMixin, PerformanceRegressionTest):
     load_iteration_timeout_sec = 7200
 
     def create_test_stats(self, *args, **kwargs):  # pylint: disable=unused-argument
@@ -70,51 +31,28 @@ class PerformanceRegressionOperatorMultiTenantTest(PerformanceRegressionTest):
     def update_test_with_errors(self):
         self.log.info("update_test_with_errors: Suppress writing errors to ES")
 
-    def setUp(self):
-        super().setUp()
-        for i in range(len(self.db_clusters_multitenant)):  # pylint: disable=no-member
-            self.scylla_clusters_stats.append(ScyllaClusterStats(
-                db_cluster=self.db_clusters_multitenant[i],  # pylint: disable=no-member
-                loaders=self.loaders_multitenant[i],  # pylint: disable=no-member
-                monitors=self.monitors_multitenant[i],  # pylint: disable=no-member
-                prometheus_db=self.prometheus_db_multitenant[i],  # pylint: disable=no-member
-                params=self.params,
-                test_config=self.test_config,
-                cluster_index=i + 1,
-            ))
-            for stress_cmd_param in self.params.stress_cmd_params:
-                current_stress_cmd = self.params.get(stress_cmd_param)
-                if not isinstance(current_stress_cmd, list):
-                    continue
-                # NOTE: 'prepare_write_cmd' is allowed to be list of strs
-                if stress_cmd_param == 'prepare_write_cmd':
-                    if not all((isinstance(current_stress_cmd_element, list)
-                                for current_stress_cmd_element in current_stress_cmd)):
-                        continue
-                self.scylla_clusters_stats[i].params[stress_cmd_param] = current_stress_cmd[i]
-
     def run_fstrim_on_all_db_nodes(self):
-        for db_cluster in self.db_clusters_multitenant:  # pylint: disable=no-member
-            self.log.info("Running fstrim command on the '%s' DB cluster", db_cluster.name)
-            db_cluster.fstrim_scylla_disks_on_nodes()
+        for tenant in self.tenants:
+            self.log.info("Running fstrim command on the '%s' DB cluster", tenant.db_cluster.name)
+            tenant.db_cluster.fstrim_scylla_disks_on_nodes()
 
     def wait_no_compactions_running(self, *args, **kwargs):
-        for scylla_cluster_stats in self.scylla_clusters_stats:
+        for tenant in self.tenants:
             self.log.info("Waiting for compactions to finish on the '%s' DB cluster",
-                          scylla_cluster_stats.db_cluster.name)
-            scylla_cluster_stats.wait_no_compactions_running(*args, **kwargs)
+                          tenant.db_cluster.name)
+            tenant.wait_no_compactions_running(*args, **kwargs)
 
     def preload_data(self, compaction_strategy=None):
-        def _preload_data(scylla_cluster_stats):
-            prepare_write_cmd = scylla_cluster_stats.params.get('prepare_write_cmd')
-            db_cluster_name = scylla_cluster_stats.db_cluster.name
+        def _preload_data(tenant):
+            prepare_write_cmd = tenant.params.get('prepare_write_cmd')
+            db_cluster_name = tenant.db_cluster.name
             if not prepare_write_cmd:
                 self.log.warning(
                     "No prepare command defined in YAML for the '%s' cluster",
                     db_cluster_name)
                 return
             self.log.info("Running preload command for the '%s' cluster", db_cluster_name)
-            scylla_cluster_stats.create_test_stats(
+            tenant.create_test_stats(
                 sub_type='write-prepare', doc_id_with_timestamp=True)
             stress_queue, params = [], {
                 'prefix': 'preload-',
@@ -129,65 +67,65 @@ class PerformanceRegressionOperatorMultiTenantTest(PerformanceRegressionTest):
                 params.update(dict(stats_aggregate_cmds=False))
                 self.log.debug("'%s' DB cluster: RUNNING stress cmd: %s",
                                db_cluster_name, stress_cmd)
-                stress_queue.append(scylla_cluster_stats.run_stress_thread(**params))
+                stress_queue.append(tenant.run_stress_thread(**params))
             for stress in stress_queue:
-                scylla_cluster_stats.get_stress_results(queue=stress, store_results=False)
+                tenant.get_stress_results(queue=stress, store_results=False)
 
-            scylla_cluster_stats.update_test_details()
+            tenant.update_test_details()
 
         self.log.info("Running preload operation in parallel on all the DB clusters")
         object_set = ParallelObject(
             timeout=self.load_iteration_timeout_sec,
-            objects=[[scs] for scs in self.scylla_clusters_stats],
-            num_workers=len(self.scylla_clusters_stats),
+            objects=[[tenant] for tenant in self.tenants],
+            num_workers=len(self.tenants),
         )
         object_set.run(func=_preload_data, unpack_objects=True, ignore_exceptions=False)
 
     def run_read_workload(self, nemesis=False):
-        def _run_read_workload(scylla_cluster_stats, nemesis):
-            scylla_cluster_stats.run_read_workload(nemesis=nemesis)
+        def _run_read_workload(tenant, nemesis):
+            tenant.run_read_workload(nemesis=nemesis)
 
         self.log.info("Running 'read' workload operation in parallel")
         object_set = ParallelObject(
             timeout=self.load_iteration_timeout_sec,
-            objects=[[scs, nemesis] for scs in self.scylla_clusters_stats],
-            num_workers=len(self.scylla_clusters_stats),
+            objects=[[tenant, nemesis] for tenant in self.tenants],
+            num_workers=len(self.tenants),
         )
         object_set.run(func=_run_read_workload, unpack_objects=True, ignore_exceptions=False)
 
     def run_write_workload(self, nemesis=False):
-        def _run_write_workload(scylla_cluster_stats, nemesis):
-            scylla_cluster_stats.run_write_workload(nemesis=nemesis)
+        def _run_write_workload(tenant, nemesis):
+            tenant.run_write_workload(nemesis=nemesis)
 
         self.log.info("Running 'write' workload operation in parallel")
         object_set = ParallelObject(
             timeout=self.load_iteration_timeout_sec,
-            objects=[[scs, nemesis] for scs in self.scylla_clusters_stats],
-            num_workers=len(self.scylla_clusters_stats),
+            objects=[[tenant, nemesis] for tenant in self.tenants],
+            num_workers=len(self.tenants),
         )
         object_set.run(func=_run_write_workload, unpack_objects=True, ignore_exceptions=False)
 
     def run_mixed_workload(self, nemesis: bool = False):
-        def _run_mixed_workload(scylla_cluster_stats, nemesis):
-            scylla_cluster_stats.run_mixed_workload(nemesis=nemesis)
+        def _run_mixed_workload(tenant, nemesis):
+            tenant.run_mixed_workload(nemesis=nemesis)
 
         self.log.info("Running 'mixed' workload operation in parallel")
         object_set = ParallelObject(
             timeout=self.load_iteration_timeout_sec,
-            objects=[[scs, nemesis] for scs in self.scylla_clusters_stats],
-            num_workers=len(self.scylla_clusters_stats),
+            objects=[[tenant, nemesis] for tenant in self.tenants],
+            num_workers=len(self.tenants),
         )
         object_set.run(func=_run_mixed_workload, unpack_objects=True, ignore_exceptions=False)
 
     def run_workload(self, stress_cmd, nemesis=False, sub_type=None):
-        def _run_workload(scylla_cluster_stats, stress_cmd, nemesis):
-            scylla_cluster_stats.run_workload(stress_cmd=stress_cmd, nemesis=nemesis)
+        def _run_workload(tenant, stress_cmd, nemesis):
+            tenant.run_workload(stress_cmd=stress_cmd, nemesis=nemesis)
 
         self.log.info("Running workload in parallel with following command:\n%s", stress_cmd)
         object_set = ParallelObject(
             timeout=self.load_iteration_timeout_sec,
-            objects=[[scs, stress_cmd, nemesis] for scs in self.scylla_clusters_stats],
-            num_workers=len(self.scylla_clusters_stats),
+            objects=[[tenant, stress_cmd, nemesis] for tenant in self.tenants],
+            num_workers=len(self.tenants),
         )
         object_set.run(func=_run_workload, unpack_objects=True, ignore_exceptions=False)
 
