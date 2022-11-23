366ae83f44f42f648404a9598a7e28364438aa36
MaciekCisowski <maciej.cisowski@scylladb.com>
2022-11-23 15:51:34 +0100

    fix(rolling_upgrade): add azure region name param to getJenkinsLabels
    This commit adds the azure_region_name param to the rolling upgrade
    pipeline. Without this param the pipeline would use any available
    builder in Jenkins, which sometimes ends up with us running on cloud
    team's builders, which are configured differently from ours. This in
    turn leads to failures of our hydra commands. After the commit we
    should only run on QA builders.

--- vars/rollingUpgradePipeline.groovy
+++ vars/rollingUpgradePipeline.groovy
@@ -4,7 +4,7 @@ List supportedVersions = []
 (testDuration, testRunTimeout, runnerTimeout, collectLogsTimeout, resourceCleanupTimeout) = [0,0,0,0,0]
 
 def call(Map pipelineParams) {
-    def builder = getJenkinsLabels(params.backend, params.region, params.gce_datacenter)
+    def builder = getJenkinsLabels(params.backend, params.region, params.gce_datacenter, params.azure_region_name)
 
     pipeline {
         agent none
