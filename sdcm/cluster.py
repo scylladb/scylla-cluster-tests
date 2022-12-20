scylladb/scylla-cluster-tests

commit c5591e26de2c596af35326c3821620017cdc05ed

fix(cluster_docker): install java-8 as workaround
Install java-8 to run c-s command with hdr logs.
With latest java version c-s failed to run with HDR

sdcm/cluster.py
@@ -4659,6 +4659,16 @@ class BaseLoaderSet():
         if self.params.get('client_encrypt'):
             node.config_client_encrypt()
 
+        # Install java-8 for workaround hdrhistogram
+        if node.is_rhel_like():
+            node.remoter.sudo('yum install -y java-1.8.0-openjdk-devel', verbose=True, ignore_status=True)
+            node.remoter.sudo("ln -sf /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.*/jre/bin/java* /etc/alternatives/java",
+                              verbose=True, ignore_status=True)
+        else:
+            node.remoter.sudo('apt install -y openjdk-8-jre', verbose=True, ignore_status=True)
+            node.remoter.sudo("ln -sf /usr/lib/jvm/java-1.8.0-openjdk-*/bin/java* /etc/alternatives/java",
+                              verbose=True, ignore_status=True)
+
         result = node.remoter.run('test -e ~/PREPARED-LOADER', ignore_status=True)
         node.remoter.sudo("bash -cxe \"echo '*\t\thard\tcore\t\tunlimited\n*\t\tsoft\tcore\t\tunlimited' "
                           ">> /etc/security/limits.d/20-coredump.conf\"")
@@ -4697,6 +4707,28 @@ class BaseLoaderSet():
                 apt-get update
                 apt-get install -y openjdk-11-jre openjdk-11-jre-headless
             """))
+
+        scylla_repo_loader = self.params.get('scylla_repo_loader')
+        if not scylla_repo_loader:
+            scylla_repo_loader = self.params.get('scylla_repo')
+        node.download_scylla_repo(scylla_repo_loader)
+        if node.is_rhel_like():
+            node.remoter.run('sudo yum install -y {}-tools'.format(node.scylla_pkg()))
+            node.remoter.sudo('yum install -y java-1.8.0-openjdk-devel', verbose=True, ignore_status=True)
+            node.remote.sudo("ln -sf /usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java* /etc/alternatives/java",
+                             verbose=True, ignore_status=True)
+        else:
+            node.remoter.run('sudo apt-get update')
+            node.remoter.run('sudo apt-get install -y '
+                             ' {}-tools '.format(node.scylla_pkg()))
+            node.remoter.sudo('apt instally -y openjdk-8-jre', verbose=True, ignore_status=True)
+            node.remoter.sudo("ln -sf /usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/java* /etc/alternatives/java",
+                              verbose=True, ignore_status=True)
+
+        if db_node_address is not None:
+            node.remoter.run("echo 'export DB_ADDRESS=%s' >> $HOME/.bashrc" % db_node_address)
+
+        node.wait_cs_installed(verbose=verbose)
 
         scylla_repo_loader = self.params.get('scylla_repo_loader')
         if not scylla_repo_loader:

sdcm/cluster_docker.py
@@ -351,7 +351,12 @@ class LoaderSetDocker(cluster.BaseLoaderSet, DockerCluster):
                                n_nodes=n_nodes,
                                params=params)
 
-    def node_setup(self, node, verbose=False, db_node_address=None, **kwargs):
+    def node_setup(self, node: DockerNode, verbose=False, db_node_address=None, **kwargs):
+        node.wait_ssh_up(verbose=verbose)
+        node.remoter.sudo("apt update", verbose=True, ignore_status=True)
+        node.remoter.sudo("apt install -y openjdk-8-jre", verbose=True, ignore_status=True)
+        node.remoter.sudo("ln -sf /usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java* /etc/alternatives/java",
+                          verbose=True, ignore_status=True)
 
         if self.params.get('client_encrypt'):
             node.config_client_encrypt()

commit 8aff95e7a8b4c64fcdee796af3f7604c45025cb2

fix(loader): typo in java-8 installation
c5591e26de2c596af35326c3821620017cdc05ed introduced new
code that was failing like:

```
File ".../sdcm/cluster.py", line 4718, in node_setup
    node.remote.sudo("ln -sf /usr/lib/jvm/java-1.8.0-openjdk-
                      amd64/jre/bin/java* /etc/alternatives/java",
AttributeError: 'GCENode' object has no attribute 'remote'
```

sdcm/cluster.py
@@ -4715,8 +4715,8 @@ class BaseLoaderSet():
         if node.is_rhel_like():
             node.remoter.run('sudo yum install -y {}-tools'.format(node.scylla_pkg()))
             node.remoter.sudo('yum install -y java-1.8.0-openjdk-devel', verbose=True, ignore_status=True)
-            node.remote.sudo("ln -sf /usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java* /etc/alternatives/java",
-                             verbose=True, ignore_status=True)
+            node.remoter.sudo("ln -sf /usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java* /etc/alternatives/java",
+                              verbose=True, ignore_status=True)
         else:
             node.remoter.run('sudo apt-get update')
             node.remoter.run('sudo apt-get install -y '