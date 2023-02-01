9b3cf337067f3819b8b8f51e5437faf728915e5c
Aleksandr Bykov <alex.bykov@scylladb.com>
2023-02-01 15:37:43 +0700

    feature(sct-create-es-index): create es index with mappings
    periodicaly it needs to create es index with specified mapping field type.
    
    Added new sct subcomand create-es-index with appropriate parameters
    which allow to create index with doc_type and specified mappings

---
+++ sdcm/utils/es_index.py
@@ -0,0 +1,29 @@
+import logging
+import json
+import sys
+import os
+
+from typing import Any
+from pathlib import Path
+
+from sdcm.es import ES
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def create_index(index_name: str, mappings: dict, **kwargs):
+    if not mappings:
+        raise Exception(f"Mappings configuration is empty {mappings}")
+    es_client = ES()
+    doc_type = kwargs.get("doc_type")
+    es_client.indices.create(index=index_name)
+    es_client.indices.put_mapping(index=index_name, doc_type=doc_type, body=mappings,  # pylint: disable=unexpected-keyword-arg
+                                  include_type_name=True)
+
+
+def get_mapping(mapping_filepath: str) -> Any:
+    file_content = Path(mapping_filepath).read_text(encoding="utf-8")
+    mapping = json.loads(file_content)
+    return mapping
