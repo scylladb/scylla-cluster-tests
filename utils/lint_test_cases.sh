6e784ffd70a8897e55e09230e153011283ad606d
Israel Fruchter <fruch@scylladb.com>
2023-02-09 13:57:41 +0200

    fix(lint_test_cases): stop using old scylla versions
    since those version doesn't exists anymore, we should stop using them

--- utils/lint_test_cases.sh
+++ utils/lint_test_cases.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 OUT=0
-SCT_SCYLLA_VERSION=4.4.1 ./sct.py lint-yamls -i '.yaml' -e 'azure,multi-dc,multiDC,multidc,multiple-dc,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,jepsen,repair-based-operations'
+SCT_AMI_ID_DB_SCYLLA=ami-1234 ./sct.py lint-yamls -i '.yaml' -e 'azure,multi-dc,multiDC,multidc,multiple-dc,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,jepsen,repair-based-operations,add-new-dc'
 OUT=$(($OUT + $?))
 
-SCT_SCYLLA_VERSION=5.0.1 SCT_AZURE_REGION_NAME="eastus" ./sct.py lint-yamls --backend azure -i azure
+SCT_AZURE_IMAGE_DB=image SCT_AZURE_REGION_NAME="eastus" ./sct.py lint-yamls --backend azure -i azure
 OUT=$(($OUT + $?))
 
 SCT_GCE_IMAGE_DB=image SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'rolling,artifacts,private-repo,gce,jepsen' -e 'multi-dc,multiDC,docker,azure'
@@ -18,7 +18,7 @@ echo "multi dc yamls with 3 regions"
 SCT_GCE_IMAGE_DB=image SCT_GCE_DATACENTER="us-east1 us-west1 eu-north1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i '24h-multidc,large-cluster,counters-multidc,cdc-8h-multi-dc' -e 'docker,azure,shutdown'
 OUT=$(($OUT + $?))
 
-SCT_SCYLLA_VERSION=4.4.1 ./sct.py lint-yamls -b k8s-local-kind -i 'scylla-operator.*\.yaml'
+SCT_AMI_ID_DB_SCYLLA=ami-1234 ./sct.py lint-yamls -b k8s-local-kind -i 'scylla-operator.*\.yaml'
 OUT=$(($OUT + $?))
 
 exit $OUT
