c3d6864b8bf330516f8be69fc55649867f9fde0f
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2023-02-17 18:23:45 +0200

    Revert "fix(pipeline): set empty test_name when not set"
    This reverts commit c711fb828ed5bcf9a53c8f6f65322568f04adfc4.

--- vars/createSctRunner.groovy
+++ vars/createSctRunner.groovy
@@ -3,7 +3,7 @@
 def call(Map params, Integer test_duration, String region) {
     def cloud_provider = getCloudProviderFromBackend(params.backend)
     def test_config = groovy.json.JsonOutput.toJson(params.test_config)
-    def test_name = groovy.json.JsonOutput.toJson(params.get('test_name', ''))
+    def test_name = groovy.json.JsonOutput.toJson(params.test_name)
 
     // NOTE: EKS jobs have 'availability_zone' be defined as 'a,b'
     //       So, just pick up the first one for the SCT runner in such a case.
