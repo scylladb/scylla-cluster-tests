c2ce43365bf2b69b749e195452d4b0cd38359221
Alexey Kartashov <alexey.kartashov@scylladb.com>
2023-03-12 22:59:10 +0300

    fix(argus.py): Handle cases where we can't use argus
    This fix handles scenarios where get_argus_client is either provided
    nothing or a non-uuid value to the client. Return MagicMock in such a
    scenario to replicate previous behaviour

--- sct.py
+++ sct.py
@@ -36,6 +36,7 @@ import click
 import click_completion
 import yaml
 from prettytable import PrettyTable
+from argus.client.sct.types import LogLink
 
 import sct_ssh
 from sdcm.localhost import LocalHost
@@ -1091,7 +1092,6 @@ def collect_logs(test_id=None, logdir=None, backend=None, config_file=None):
 def store_logs_in_argus(test_id: UUID, logs: dict[str, list[list[str] | str]]):
     # pylint: disable=import-outside-toplevel
     try:
-        from argus.client.sct.types import LogLink
         argus_client = get_argus_client(run_id=test_id)
         log_links = []
         for _, s3_links in logs.items():
