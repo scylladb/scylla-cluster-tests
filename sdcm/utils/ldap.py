e4e199bbb499bdaf24661982ccfdb0d6cf792c58
yarongilor <yarongilor@scylla.com>
2023-03-16 18:47:43 +0200

    test(Ldap): Add/remove Ldap role permissions during stress
    	Test adding a new user with Ldap permissions,
    	and run some load for it.
    	Then remove permissions and wait for its load to end.

--- sdcm/utils/ldap.py
+++ sdcm/utils/ldap.py
@@ -14,8 +14,11 @@
 from time import sleep
 from enum import Enum
 
+from cassandra import Unauthorized
 from ldap3 import Server, Connection, ALL, ALL_ATTRIBUTES
 from ldap3.core.exceptions import LDAPSocketOpenError
+
+from sdcm.keystore import KeyStore
 from sdcm.utils.decorators import retrying
 
 
@@ -91,3 +94,182 @@ class LdapContainerMixin:  # pylint: disable=too-few-public-methods
     @staticmethod
     def modify_ldap_entry(*args, **kwargs):
         return LdapContainerMixin.ldap_conn.modify(*args, **kwargs)
+
+    @staticmethod
+    def delete_ldap_entry(*args, **kwargs):
+        return LdapContainerMixin.ldap_conn.delete(*args, **kwargs)
+
+
+class LdapUtilsMixin:
+    """This mixin can be added to any class that inherits the 'ClusterTester' one and configures Ldap container"""
+
+    @property
+    def ldap_server_ip(self) -> str:
+        if self.params.get('ldap_server_type') == LdapServerType.MS_AD:
+            ldap_ms_ad_credentials = KeyStore().get_ldap_ms_ad_credentials()
+            return ldap_ms_ad_credentials["server_address"]
+        ldap_server_ip = '127.0.0.1' if self.test_config.IP_SSH_CONNECTIONS == 'public' \
+            else self.test_config.LDAP_ADDRESS[0]
+        return ldap_server_ip
+
+    @property
+    def ldap_port(self) -> str:
+        if self.params.get('ldap_server_type') == LdapServerType.MS_AD:
+            return LDAP_PORT
+        ldap_port = LDAP_SSH_TUNNEL_LOCAL_PORT if self.test_config.IP_SSH_CONNECTIONS == 'public' \
+            else self.test_config.LDAP_ADDRESS[1]
+        return ldap_port
+
+    def _add_ldap_entry(self, ldap_entry: list):
+        self.log.debug("Adding an Ldap entry of: %s", ldap_entry)
+        username = f'cn=admin,{LDAP_BASE_OBJECT}'
+        self.localhost.add_ldap_entry(ip=self.ldap_server_ip, ldap_port=self.ldap_port,
+                                      user=username, password=LDAP_PASSWORD, ldap_entry=ldap_entry)
+
+    def create_role_in_ldap(self, ldap_role_name: str, unique_members: list):
+        unique_members_list = [f'uid={user},ou=Person,{LDAP_BASE_OBJECT}' for user in unique_members]
+        role_entry = [
+            f'cn={ldap_role_name},{LDAP_BASE_OBJECT}',
+            ['groupOfUniqueNames', 'simpleSecurityObject', 'top'],
+            {
+                'uniqueMember': unique_members_list,
+                'userPassword': LDAP_PASSWORD
+            }
+        ]
+        self._add_ldap_entry(ldap_entry=role_entry)
+
+    def search_ldap_role(self, ldap_role_name: str, raise_error: bool = True) -> str:
+        ldap_entry = f'(cn={ldap_role_name})'
+        self.log.debug("Searching for Ldap entry: %s, %s", LDAP_BASE_OBJECT, ldap_entry)
+        res = self.localhost.search_ldap_entry(LDAP_BASE_OBJECT, ldap_entry)
+        self.log.debug("Ldap entry Search result: %s", res)
+        if not res and raise_error:
+            raise Exception(f'Failed to find {ldap_role_name} in Ldap.')
+        distinguished_name = str(res).split()[1]
+        return distinguished_name
+
+    def search_ldap_user(self, ldap_user_name: str, raise_error: bool = True) -> str:
+        ldap_entry = f'(uid={ldap_user_name})'
+        self.log.debug("Searching for Ldap user: %s, %s", LDAP_BASE_OBJECT, ldap_entry)
+        res = self.localhost.search_ldap_entry(search_base=LDAP_BASE_OBJECT, search_filter=ldap_entry)
+        self.log.debug("Ldap user search result: %s", res)
+        if not res and raise_error:
+            raise Exception(f'Failed to find {ldap_user_name} in Ldap.')
+        distinguished_name = str(res).split()[1]
+        return distinguished_name
+
+    def modify_ldap_role_delete_member(self, ldap_role_name: str, member_name: str, raise_error: bool = True):
+        distinguished_name = self.search_ldap_role(ldap_role_name=ldap_role_name, raise_error=raise_error)
+        self.log.debug("Deleting member %s from Ldap entry: %s", member_name, distinguished_name)
+        unique_member_update = {'uniqueMember': [
+            ('MODIFY_DELETE', [f'uid={member_name},ou=Person,{LDAP_BASE_OBJECT}'])]}
+        res = self.localhost.modify_ldap_entry(distinguished_name, unique_member_update)
+        if not res and raise_error:
+            raise Exception(f'Failed to delete entry {distinguished_name} from Ldap role {ldap_role_name}')
+
+    def delete_ldap_role(self, ldap_role_name: str, raise_error: bool = True):
+        distinguished_name = self.search_ldap_role(ldap_role_name=ldap_role_name, raise_error=raise_error)
+        self.log.debug("Deleting Ldap entry: %s", distinguished_name)
+        res = self.localhost.delete_ldap_entry(distinguished_name)
+        if not res and raise_error:
+            raise Exception(f'Failed to delete entry {distinguished_name} from Ldap.')
+
+    def delete_ldap_user(self, ldap_user_name: str, raise_error: bool = True):
+        distinguished_name = self.search_ldap_user(ldap_user_name=ldap_user_name, raise_error=raise_error)
+        self.log.debug("Deleting Ldap user entry: %s", distinguished_name)
+        res = self.localhost.delete_ldap_entry(distinguished_name)
+        if not res and raise_error:
+            raise Exception(f'Failed to delete entry {distinguished_name} from Ldap.')
+
+    @retrying(n=10, sleep_time=30, message='Waiting for no user permissions verification', allowed_exceptions=(ValueError,))
+    def wait_verify_user_no_permissions(self, username: str):
+        """
+        Checks for unauthorized user permission following roles update.
+        Creating a keyspace by the user and verify it fails as expected.
+        """
+        self.log.debug("Verifying user %s roles permission change in Scylla DB (following an LDAP Role update) ", username)
+
+        try:
+            with self.db_cluster.cql_connection_patient(node=self.db_cluster.nodes[0], user=username,
+                                                        password=LDAP_PASSWORD) as session:
+                session.execute(
+                    """ CREATE KEYSPACE IF NOT EXISTS test_no_permission WITH replication = {'class': 'SimpleStrategy',
+                    'replication_factor': 1} """)
+                session.execute(""" DROP KEYSPACE IF EXISTS test_no_permission """)
+            raise ValueError('DID NOT RAISE')
+        except Unauthorized:
+            return
+
+    @retrying(n=10, sleep_time=30, message='Waiting for user permissions update', allowed_exceptions=Unauthorized)
+    def wait_verify_user_permissions(self, username: str, keyspace: str = 'customer_ldap'):
+        """
+        Checks for authorized user permission following roles update.
+        Creating a keyspace by the user and verify it doesn't fail.
+        """
+        self.log.debug("Verifying user %s roles permission change in Scylla DB (following an LDAP Role update) ", username)
+
+        with self.db_cluster.cql_connection_patient(node=self.db_cluster.nodes[0], user=username,
+                                                    password=LDAP_PASSWORD) as session:
+            session.execute(
+                """ CREATE KEYSPACE IF NOT EXISTS test_permission_ks WITH replication = {'class': 'SimpleStrategy',
+                'replication_factor': 1} """)
+            session.execute(""" DROP KEYSPACE IF EXISTS test_permission_ks """)
+
+            session.execute(
+                f""" CREATE KEYSPACE IF NOT EXISTS {keyspace} WITH replication = {{'class': 'SimpleStrategy',
+                'replication_factor': 3}} """)
+            session.execute(
+                f""" CREATE TABLE IF NOT EXISTS {keyspace}.info (key varchar, c varchar, v varchar, PRIMARY KEY(key, c)) """)
+            session.execute('INSERT INTO customer_ldap.info (key, c, v) VALUES (\'key1\', \'c1\', \'v1\')')
+            session.execute("SELECT * from customer_ldap.info LIMIT 1")
+
+    @retrying(n=10, sleep_time=30, message='Waiting for user permissions update', allowed_exceptions=(AssertionError,))
+    def wait_for_user_roles_update(self, username: str, are_roles_expected: bool):
+        """
+        Checks for updated output of user roles.
+        Example command: LIST ROLES OF new_user;
+        Example output:
+        LIST ROLES OF new_user;
+        ╭─────────┬─────────┬──────────────────────┬────────────╮
+        │role     │ super   │ login                │ options    │
+        ├─────────┼─────────┼──────────────────────┼────────────┤
+        │customer │ False   │ False                │ {}         │
+        ├─────────┼─────────┼──────────────────────┼────────────┤
+        │trainer  │ False   │ False                │ {}         │
+        ├─────────┼─────────┼──────────────────────┼────────────┤
+        │new_user │ False   │ True                 │ {}         │
+        ╰─────────┴─────────┴──────────────────────┴────────────╯
+        """
+        self.log.debug("Waiting user %s roles change in Scylla DB (following an LDAP Role update) ", username)
+        with self.db_cluster.cql_connection_patient(node=self.db_cluster.nodes[0]) as session:
+            query = f"LIST ROLES OF '{username}';"
+            result = session.execute(query)
+            output = result.all()
+            self.log.debug("LIST ROLES OF %s: %s", username, output)
+            if are_roles_expected:
+                assert len(output) > 1
+            else:
+                assert len(output) == 1
+
+    def add_user_in_ldap(self, username: str):
+        user_entry = [
+            f'uid={username},ou=Person,{LDAP_BASE_OBJECT}',
+            ['uidObject', 'organizationalPerson', 'top'],
+            {
+                'userPassword': LDAP_PASSWORD,
+                'sn': 'PersonSn',
+                'cn': 'PersonCn'
+            }
+        ]
+        self._add_ldap_entry(ldap_entry=user_entry)
+
+    def create_role_in_scylla(self, node, role_name: str, is_superuser: bool = True,
+                              is_login: bool = True):
+        self.log.debug("Configuring a Role %s in Scylla DB", role_name)
+        create_role_cmd = f'CREATE ROLE IF NOT EXISTS \'{role_name}\''
+        superuser_argument = ' WITH SUPERUSER=true' if is_superuser else ' WITH SUPERUSER=false'
+        login_argument = ' AND login=true' if is_login else ' AND login=false'
+        create_role_cmd += superuser_argument + login_argument
+        if not self.params.get('prepare_saslauthd'):
+            create_role_cmd += f' AND password=\'{LDAP_PASSWORD}\''
+        node.run_cqlsh(create_role_cmd)
