76f7416bc667c2c5ac4bb2c4b56673ce600bc26d
yarongilor <yarongilor@scylla.com>
2023-03-27 18:13:56 +0300

    fix(Ldap connection): Lower severity of connection-reset-by-peer event
    	This event can happens when a node gets down.
    	Refs: https://github.com/scylladb/scylla-enterprise/issues/2710

--- unit_tests/test_sct_events_database.py
+++ unit_tests/test_sct_events_database.py
@@ -26,6 +26,7 @@ class TestDatabaseLogEvent(unittest.TestCase):
         self.assertTrue(issubclass(DatabaseLogEvent.UNKNOWN_VERB, DatabaseLogEvent))
         self.assertTrue(issubclass(DatabaseLogEvent.CLIENT_DISCONNECT, DatabaseLogEvent))
         self.assertTrue(issubclass(DatabaseLogEvent.SEMAPHORE_TIME_OUT, DatabaseLogEvent))
+        self.assertTrue(issubclass(DatabaseLogEvent.LDAP_CONNECTION_RESET, DatabaseLogEvent))
         self.assertTrue(issubclass(DatabaseLogEvent.SYSTEM_PAXOS_TIMEOUT, DatabaseLogEvent))
         self.assertTrue(issubclass(DatabaseLogEvent.SERVICE_LEVEL_CONTROLLER, DatabaseLogEvent))
         self.assertTrue(issubclass(DatabaseLogEvent.RESTARTED_DUE_TO_TIME_OUT, DatabaseLogEvent))
