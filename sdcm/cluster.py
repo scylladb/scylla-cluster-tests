8badb5c4a3648c9595234e4be8128e09f5908af1
Israel Fruchter <fruch@scylladb.com>
2023-04-23 22:59:43 +0300

    fix(azure-artifact): support public address in `cql_ip_address`
    since 506c0bd353907a1b883e87355593c570c1bed84c azure artifacts
    are working with public address, but some of SCT around connecting
    to CQL isn't supporting correctly the public option.
    
    (cherry picked from commit 0db057d9c05722fa6cc85f07419d42d99cbc00a9)

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -832,6 +832,8 @@ class BaseNode(AutoSshContainerMixin, WebDriverContainerMixin):  # pylint: disab
 
     @cached_property
     def cql_ip_address(self):
+        if self.test_config.IP_SSH_CONNECTIONS == 'public':
+            return self.ip_address
         with self.remote_scylla_yaml() as scylla_yaml:
             return scylla_yaml.broadcast_rpc_address if scylla_yaml.broadcast_rpc_address else self.ip_address
 
