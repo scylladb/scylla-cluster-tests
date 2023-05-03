scylladb/scylla-cluster-tests

commit f28359d61b78d7ffdf7ebd57393aa81f94fa1430

fix(health_events): add retry and did not raise exception
Nemesis thread in the https://jenkins.scylladb.com/job/enterprise-2023.1/job/longevity/job/
longevity_mv_synchronous_updates-12h/3/ test failed due to 'get_group0_members' timeout.

Add retry decorator and did not raise exception on 'get_group0_members' and
'get_token_ring_members' during node health check.

sdcm/cluster.py
@@ -3020,6 +3020,7 @@ class BaseNode(AutoSshContainerMixin, WebDriverContainerMixin):  # pylint: disab
             return
         self.remoter.run(f"sudo kill -s SIGHUP {pid}")
 
+    @retrying(n=5, sleep_time=5, raise_on_exceeded=False)
     def get_token_ring_members(self) -> list[dict[str, str]]:
         token_ring_members = []
         self.log.debug("Get token ring members")
@@ -3039,6 +3040,7 @@ class BaseNode(AutoSshContainerMixin, WebDriverContainerMixin):  # pylint: disab
         self.log.debug("Token ring members %s", token_ring_members)
         return token_ring_members
 
+    @retrying(n=5, sleep_time=5, raise_on_exceeded=False)
     def get_group0_members(self) -> list[dict[str, str]]:
         self.log.debug("Get group0 members")
         group0_members = []

commit 60ce119f7f06f624944513475617bae666313ed4

fix(get_group0_members): re-raise the exception to make retry work
The retry for this method was introduced in the PR
https://github.com/scylladb/scylla-cluster-tests/pull/6039
Since most of the code of `get_group0_members` is placed in `try` block
any possible exception is concealed inside `except` block and does not
go outside.
So the `retrying` decorator never has a reason to start retrying.
Added `raise` for the exception to trigger the retry for the method.

sdcm/cluster.py
@@ -3064,6 +3064,8 @@ class BaseNode(AutoSshContainerMixin, WebDriverContainerMixin):  # pylint: disab
             self.log.error(err_msg)
             InfoEvent(message=err_msg, severity=Severity.ERROR).publish()
 
+            raise
+
         self.log.debug("Group0 members: %s", group0_members)
         return group0_members
 