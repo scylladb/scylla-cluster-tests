53b9cd7ce6fe59d9dbad1047608edfc1fa5d045b
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2023-05-08 15:22:23 +0300

    Revert "improvement(large partitions): Set random clustering order for s-b table"
    This reverts commit 21ff073b98ba035c64b21ff79b97cf520fec419e.

--- test_lib/scylla_bench_tools.py
+++ test_lib/scylla_bench_tools.py
@@ -1,29 +1,27 @@
 import logging
-import random
 
 LOGGER = logging.getLogger(__name__)
 
 
-def create_scylla_bench_table_query(compaction_strategy=None, seed: int = None):
+def create_scylla_bench_table_query(compaction_strategy=None):
     """
 
     :return: cql create table query for scylla-bench
     """
-    seed = seed or random.randint(0, 1000)
-    clustering_order = random.Random(seed).choice(['ASC', 'DESC'])
-    compaction_strategy_option = "AND compaction = {{'class': '{}'}}".format(
+
+    compaction_strategy_option = "AND compaction = {{'class': '{}'}};".format(
         compaction_strategy) if compaction_strategy else ""
-    scylla_bench_table_query = f"""
+    scylla_bench_table_query = """
                     CREATE TABLE IF NOT EXISTS scylla_bench.test (
                     pk bigint,
                     ck bigint,
                     v blob,
                     PRIMARY KEY (pk, ck)
-                ) WITH CLUSTERING ORDER BY (ck {clustering_order})
+                ) WITH CLUSTERING ORDER BY (ck ASC)
                     AND bloom_filter_fp_chance = 0.01
-                    AND caching = {{'keys': 'ALL', 'rows_per_partition': 'ALL'}}
+                    AND caching = {'keys': 'ALL', 'rows_per_partition': 'ALL'}
                     AND comment = ''
-                    AND compression = {{}}
+                    AND compression = {}
 
                     AND crc_check_chance = 1.0
                     AND dclocal_read_repair_chance = 0.0
@@ -34,7 +32,5 @@ def create_scylla_bench_table_query(compaction_strategy=None, seed: int = None):
                     AND min_index_interval = 128
                     AND read_repair_chance = 0.0
                     AND speculative_retry = 'NONE'
-                    {compaction_strategy_option};
-                    """
-    LOGGER.debug("Generated a create-table query with a seed of [%s]: %s", seed, scylla_bench_table_query)
+                    """ + compaction_strategy_option
     return scylla_bench_table_query
