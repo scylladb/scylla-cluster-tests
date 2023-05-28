d509bf9d184430c1b3c5d358d8faf5b748ea92e0
Shlomi Balalis <Balalis@scylladb.com>
2023-05-28 19:44:55 +0300

    feature(artifact_test): testing the output of perftune.py
    perftune.py is a script within seastar (which is a part of scylla_setup)
    that configures several system parameters, like the amount of CPU cores that
    are designated to handle IRQ, and which are not.
    
    I have recorded the expected results as of master 20230717.567b4536892f
    in defaults/perftune_results.json, and the test will compare them to
    the output of the script.
    
    Ref: https://github.com/scylladb/scylla-enterprise/issues/2909
    (cherry picked from commit a95477090283726d595b27021d9662669c748ea4)

--- artifacts_test.py
+++ artifacts_test.py
@@ -27,6 +27,7 @@ from sdcm.utils.adaptive_timeouts import NodeLoadInfoServices
 from sdcm.utils.housekeeping import HousekeepingDB
 from sdcm.utils.common import get_latest_scylla_release, ScyllaProduct
 from sdcm.utils.decorators import retrying
+from sdcm.utils.perftune_validator import PerftuneOutputChecker
 
 STRESS_CMD: str = "/usr/bin/cassandra-stress"
 
@@ -432,6 +433,11 @@ class ArtifactsTest(ClusterTester):  # pylint: disable=too-many-public-methods
                                                             expected_status_code=expected_housekeeping_status_code,
                                                             new_row_expected=True,
                                                             backend=backend)
+
+        if backend != 'docker':
+            with self.subTest("Check the output of perftune.py"):
+                perftune_checker = PerftuneOutputChecker(self.node)
+                perftune_checker.compare_perftune_results()
 
         if backend == 'docker':
             with self.subTest("Check docker latest tags"):
