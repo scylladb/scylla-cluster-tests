1e74476885dbfb88a79d853bb6e9c1d1b2293445
Artsiom Mishuta <artsiom.mishuta@scylladb.com>
2023-05-31 17:39:05 +0200

    fix(Argus): Fis for Create Argus Test Run step
    fix ussue https://github.com/scylladb/scylla-cluster-tests/issues/6205
    ARM artifacts are failing on Create Argus Test Run stage

--- vars/createArgusTestRun.groovy
+++ vars/createArgusTestRun.groovy
@@ -2,14 +2,13 @@
 
 def call(Map params) {
     def test_config = groovy.json.JsonOutput.toJson(params.test_config)
-
     sh """#!/bin/bash
         set -xe
 
         echo "Creating Argus test run ..."
 
         export SCT_CLUSTER_BACKEND="${params.backend}"
-        export SCT_CONFIG_FILES="${test_config}"
+        export SCT_CONFIG_FILES=${test_config}
 
         ./docker/env/hydra.sh create-argus-test-run
 
