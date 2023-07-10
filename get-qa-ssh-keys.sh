e1e85b4508b227a6076920152b8d066ce382d4cb
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2023-07-10 11:41:10 +0000

    fix(cloud): remove defaults for cloud_credentials_path
    - Don't sync `support` SSH key
    - Remove default value for `cloud_credentials_path`: a test should provide this value

--- get-qa-ssh-keys.sh
+++ get-qa-ssh-keys.sh
@@ -9,7 +9,6 @@ umask 077
 aws s3 sync s3://${SSH_KEYS_BUCKET} ${SSH_KEYS_DIR} --exact-timestamps --exclude '*' \
   --include scylla-qa-ec2 \
   --include scylla-test \
-  --include support \
   --include scylla_test_id_ed25519 \
 || (echo "Please refer to https://github.com/scylladb/scylla-cluster-tests#setting-up-sct-environment to configure your aws-cli" && exit 1)
 echo "QA SSH keys obtained."
