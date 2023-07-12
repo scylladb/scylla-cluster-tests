ad1603b8fa3dbe8c28f7470af0df128429736b2b
Julia Yakovlev <julia.yakovlev@scylladb.com>
2023-07-12 12:23:59 +0300

    improvement(sla tests): wait for a service level is propagated to nodes
    Wait for scheduler group is created for a new service level on every node before start the load.
    Use 'scylla_scheduler_group_shares' prometheus metric for that.
    Now we wait about 15-30 sec. But if scheduler group was not created, the service level does not
    receive the resources and the nemesis is failed with 'no recources' error. But it wrong.
    Correct error - scheduler group  was not created on the node(s)

--- longevity_sla_test.py
+++ longevity_sla_test.py
@@ -13,7 +13,9 @@
 #
 # Copyright (c) 2022 ScyllaDB
 from longevity_test import LongevityTest
+from sdcm.sla.libs.sla_utils import SlaUtils
 from sdcm.utils import loader_utils
+from sdcm.utils.adaptive_timeouts import adaptive_timeout, Operations
 from test_lib.sla import create_sla_auth
 
 
@@ -32,11 +34,19 @@ class LongevitySlaTest(LongevityTest, loader_utils.LoaderUtilsMixin):
             # Add index (shares position in the self.service_level_shares list) to role and service level names to do
             # it unique and prevent failure when try to create role/SL with same name
             for index, shares in enumerate(self.service_level_shares):
-                self.roles.append(create_sla_auth(session=session, shares=shares, index=index))
+                self.roles.append(create_sla_auth(session=session, shares=shares, index=str(index)))
 
             if self.params.get("run_fullscan"):
                 self.fullscan_role = create_sla_auth(session=session, shares=self.FULLSCAN_SERVICE_LEVEL_SHARES,
-                                                     index=0)
+                                                     index="0")
+
+        # Wait for all SLs are propagated to all nodes
+        for role in self.roles + [self.fullscan_role]:
+            # self.fullscan_role may be None if "run_fullscan" is not defined
+            if role:
+                with adaptive_timeout(Operations.SERVICE_LEVEL_PROPAGATION, node=self.db_cluster.nodes[0], timeout=15,
+                                      service_level_for_test_step="MAIN_SERVICE_LEVEL"):
+                    SlaUtils().wait_for_service_level_propagated(cluster=self.db_cluster, service_level=role.attached_service_level)
 
         self.add_sla_credentials_to_stress_cmds(workload_names=['prepare_write_cmd', 'stress_cmd', 'stress_read_cmd'],
                                                 roles=self.roles, params=self.params,
