scylladb/scylla-cluster-tests

commit a76ed862ad99c58012699546822eab08a94b2ea9

feature(pipeline_test_duration): Generic test_duration for longevity
QA-Proj: https://github.com/scylladb/qa-tasks/issues/854

Set generic test duration from job settings

Introduce 2 new sct parameters: prepare_duration and stress_duration

If stress duration is set in job pipeline,then test duration is
calculated as prepare_duration + stress_duration + spare_time(=10min)

in sct the test_duration also is counted as prepare_duration + stress_duration
if stress duration > 0 or used old parameter test_duration to run test
as before.

vars/getJobTimeouts.groovy
@@ -22,8 +22,20 @@ List<Integer> call(Map params, String region){
     """
     def testData = sh(script: cmd, returnStdout: true).trim()
     println(testData)
-    testData = testData =~ /test_duration: (\d+)/
-    testDuration = testData[0][1].toInteger()
+    if (params.stress_duration == "") {
+        testData = testData =~ /test_duration: (\d+)/
+        testDuration = testData[0][1].toInteger()
+    } else {
+        stressDuration = params.stress_duration.toInteger()
+        try {
+            prepareDuration = params.prepare_stress_duration.toInteger()
+        } catch (e) {
+            testData = testData =~ /prepare_stress_duration: (\d+)/
+            prepareDuration = testData[0][1].toInteger()
+        }
+        Integer stressEndup = 10
+        testDuration = prepareDuration + stressDuration + stressEndup
+    }
     Integer testStartupTimeout = 20
     Integer testTeardownTimeout = 40
     Integer collectLogsTimeout = 90

defaults/test_default.yaml
@@ -1,6 +1,8 @@
 db_type: "scylla"
 
 test_duration: 60
+prepare_stress_duration: 300 # 5 hours
+stress_duration: 0
 
 ip_ssh_connections: 'private'
 

sdcm/sct_config.py
@@ -172,7 +172,17 @@ class SCTConfiguration(dict):
                   Test duration (min). Parameter used to keep instances produced by tests
                   and for jenkins pipeline timeout and TimoutThread.
              """),
-
+        dict(name="prepare_stress_duration", env="SCT_PREPARE_STRESS_DURATION", type=int,
+             help="""
+                  Time in minutes, which is required to run prepare stress commands
+                  defined in prepare_*_cmd for dataset generation, and is used in
+                  test duration calculation
+             """),
+        dict(name="stress_duration", env="SCT_STRESS_DURATION", type=int,
+             help="""
+                  Time in minutes, Time of execution for stress commands from stress_cmd parameters
+                  and is used in test duration calculation
+             """),
         dict(name="n_db_nodes", env="SCT_N_DB_NODES", type=int_or_list,
              help="Number list of database nodes in multiple data centers."),
 
@@ -1781,6 +1791,20 @@ class SCTConfiguration(dict):
             if not (authenticator_password and authenticator_user):
                 raise ValueError("For PasswordAuthenticator authenticator authenticator_user and authenticator_password"
                                  " have to be provided")
+
+        # 13) validate stress and prepare duration:
+        if stress_duration := self.get('stress_duration'):
+            try:
+                self['stress_duration'] = abs(int(stress_duration))
+            except ValueError:
+                raise ValueError(f'Configured stress duration for generic test duratinon have to be \
+                                 positive integer number in minutes. Current value: {stress_duration}') from ValueError
+        if prepare_stress_duration := self.get('prepare_stress_duration'):
+            try:
+                self['prepare_stress_duration'] = abs(int(prepare_stress_duration))
+            except ValueError:
+                raise ValueError(f'Configured stress duration for generic test duratinon have to be \
+                                 positive integer number in minutes. Current value: {prepare_stress_duration}') from ValueError
 
         # 14 Validate run_fullscan parmeters
         if run_fullscan_params := self.get("run_fullscan"):

sdcm/test_config.py
@@ -28,6 +28,7 @@ LOGGER = logging.getLogger(__name__)
 
 class TestConfig(metaclass=Singleton):  # pylint: disable=too-many-public-methods
     TEST_DURATION = 60
+    TEST_WARMUP_TEARDOWN = 60
     RSYSLOG_SSH_TUNNEL_LOCAL_PORT = 5000
     RSYSLOG_IMJOURNAL_RATE_LIMIT_INTERVAL = 600
     RSYSLOG_IMJOURNAL_RATE_LIMIT_BURST = 20000

sdcm/tester.py
@@ -311,7 +311,7 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
         self.log.debug("IP used for SSH connections is '%s'",
                        ip_ssh_connections)
         self.test_config.set_ip_ssh_connections(ip_ssh_connections)
-        self._duration = self.params.get(key='test_duration')
+        self._init_test_duration()
         post_behavior_db_nodes = self.params.get('post_behavior_db_nodes')
         self.log.debug('Post behavior for db nodes %s', post_behavior_db_nodes)
         self.test_config.keep_cluster(node_type='db_nodes', val=post_behavior_db_nodes)
@@ -373,6 +373,18 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
 
         time.sleep(0.5)
         InfoEvent(message=f"TEST_START test_id={self.test_config.test_id()}").publish()
+
+    def _init_test_duration(self):
+        self._stress_duration: int = self.params.get('stress_duration')
+        self._prepare_stress_duration: int = self.params.get('prepare_stress_duration')
+        if self._stress_duration:
+            self._duration = int(self._prepare_stress_duration) + int(self._stress_duration) + \
+                self.test_config.TEST_WARMUP_TEARDOWN
+
+            self.log.info("SCT Test duration: %s; Stress duration: %s; Prepare duration: %s",
+                          self._duration, self._stress_duration, self._prepare_stress_duration)
+        else:
+            self._duration = self.params.get("test_duration")
 
     def init_argus_run(self):
         try:
@@ -1895,6 +1907,9 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
         # stress_cmd = self._cs_add_node_flag(stress_cmd)
         if duration:
             timeout = self.get_duration(duration)
+        elif self._stress_duration and ' duration=' in stress_cmd:
+            timeout = self.get_duration(self._stress_duration)
+            stress_cmd = re.sub(r'\sduration=\d+[mhd]\s', f' duration={self._stress_duration}m ', stress_cmd)
         else:
             timeout = get_timeout_from_stress_cmd(stress_cmd) or self.get_duration(duration)
 
@@ -1929,6 +1944,9 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
 
         if duration:
             timeout = self.get_duration(duration)
+        elif self._stress_duration and '-duration=' in stress_cmd:
+            timeout = self.get_duration(self._stress_duration)
+            stress_cmd = re.sub(r'\s-duration[=\s]+\d+[mhd]+\s*', f' -duration={self._stress_duration}m ', stress_cmd)
         else:
             timeout = get_timeout_from_stress_cmd(stress_cmd) or self.get_duration(duration)
         stop_test_on_failure = False if not self.params.get("stop_test_on_stress_failure") else stop_test_on_failure
@@ -2080,6 +2098,10 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
     def run_gemini(self, cmd, duration=None):
         if duration:
             timeout = self.get_duration(duration)
+        elif self._stress_duration and ' --duration' in cmd:
+            timeout = self.get_duration(self._stress_duration)
+            cmd = re.sub(r'\s--duration\s+\d+[mhd]\s', f' --duration {self._stress_duration}m ', cmd)
+            cmd = re.sub(r'\s--warmup\s+\d+[mhd]\s', f' --warmup {int(self._stress_duration * .2)}m ', cmd)
         else:
             timeout = get_timeout_from_stress_cmd(cmd) or self.get_duration(duration)
         if self.create_stats:

test-cases/longevity/longevity-large-partition-8h.yaml
@@ -15,10 +15,10 @@ prepare_verify_cmd: ["scylla-bench -workload=sequential -mode=read -replication-
                      "scylla-bench -workload=sequential -mode=read -replication-factor=3 -partition-count=25 -clustering-row-count=5555 -partition-offset=76 -clustering-row-size=uniform:1024..2048 -concurrency=100 -connection-count=100 -consistency-level=quorum -rows-per-request=10 -timeout=30s -iterations 1 -validate-data"
                     ]
 
-stress_cmd: ["scylla-bench -workload=sequential -mode=write -replication-factor=3 -partition-count=25 -clustering-row-count=10000 -partition-offset=401 -clustering-row-size=uniform:10..1024     -concurrency=10  -connection-count=10  -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 10",
-             "scylla-bench -workload=sequential -mode=write -replication-factor=3 -partition-count=25 -clustering-row-count=100   -partition-offset=426 -clustering-row-size=uniform:2048..5120   -concurrency=10  -connection-count=10  -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 10",
-             "scylla-bench -workload=sequential -mode=write -replication-factor=3 -partition-count=25 -clustering-row-count=10    -partition-offset=451 -clustering-row-size=uniform:5120..8192   -concurrency=10  -connection-count=10  -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 10",
-             "scylla-bench -workload=sequential -mode=write -replication-factor=3 -partition-count=25 -clustering-row-count=1     -partition-offset=476 -clustering-row-size=uniform:8192..10240  -concurrency=10  -connection-count=10  -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 10",
+stress_cmd: ["scylla-bench -workload=sequential -mode=write -replication-factor=3 -partition-count=25 -clustering-row-count=10000 -partition-offset=401 -clustering-row-size=uniform:10..1024     -concurrency=10  -connection-count=10  -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 0 -duration=540m",
+             "scylla-bench -workload=sequential -mode=write -replication-factor=3 -partition-count=25 -clustering-row-count=100   -partition-offset=426 -clustering-row-size=uniform:2048..5120   -concurrency=10  -connection-count=10  -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 0 -duration=540m",
+             "scylla-bench -workload=sequential -mode=write -replication-factor=3 -partition-count=25 -clustering-row-count=10    -partition-offset=451 -clustering-row-size=uniform:5120..8192   -concurrency=10  -connection-count=10  -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 0 -duration=540m",
+             "scylla-bench -workload=sequential -mode=write -replication-factor=3 -partition-count=25 -clustering-row-count=1     -partition-offset=476 -clustering-row-size=uniform:8192..10240  -concurrency=10  -connection-count=10  -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 0 -duration=540m",
              "scylla-bench -workload=sequential -mode=read  -replication-factor=3 -partition-count=25 -clustering-row-count=5555                        -clustering-row-size=uniform:1024..2048   -concurrency=100 -connection-count=100 -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 0 -duration=540m -validate-data",
              "scylla-bench -workload=sequential -mode=read  -replication-factor=3 -partition-count=25 -clustering-row-count=5555 -partition-offset=26   -clustering-row-size=uniform:1024..2048   -concurrency=100 -connection-count=100 -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 0 -duration=540m -validate-data",
              "scylla-bench -workload=sequential -mode=read  -replication-factor=3 -partition-count=25 -clustering-row-count=5555 -partition-offset=51   -clustering-row-size=uniform:1024..2048   -concurrency=100 -connection-count=100 -consistency-level=quorum -rows-per-request=10 -timeout=30s -retry-number=30 -retry-interval=80ms,1s -iterations 0 -duration=540m -validate-data",

vars/longevityPipeline.groovy
@@ -38,6 +38,15 @@ def call(Map pipelineParams) {
                description: 'Availability zone',
                name: 'availability_zone')
 
+            string(defaultValue: "",
+               description: 'Duration in minutes for stress commands(gemini, c-s, s-b)',
+               name: 'stress_duration')
+
+            string(defaultValue: "",
+               description: ('Time duration in minutes for preparing dataset with commands prepare_*_cmd, if empty value, default value is 5h = 300 minutes.' +
+                             'Prepare commands could finish earlier and have not to run full prepare_stress_duration time'),
+               name: 'prepare_stress_duration')
+
             string(defaultValue: '', description: '', name: 'scylla_ami_id')
             string(defaultValue: '', description: '', name: 'gce_image_db')
             string(defaultValue: '', description: '', name: 'azure_image_db')

vars/runSctTest.groovy
@@ -27,6 +27,14 @@ def call(Map params, String region, functional_test = false, Map pipelineParams 
     export SCT_CONFIG_FILES=${test_config}
     export SCT_COLLECT_LOGS=false
 
+    if [[ -n "${params.stress_duration ? params.stress_duration : ''}" ]] ; then
+        export SCT_STRESS_DURATION=${params.stress_duration}
+    fi
+
+    if [[ -n "${params.prepare_stress_duration ? params.prepare_stress_duration : ''}" ]] ; then
+        export SCT_PREPARE_STRESS_DURATION=${params.prepare_stress_duration}
+    fi
+
     if [[ -n "${params.region ? params.region : ''}" ]] ; then
         export SCT_REGION_NAME=${current_region}
     fi

commit 39e1c8b451b1a0998ef8a8d375642da6e367a407

fix(pipelines): handle stress_duration correctly
a76ed862ad99c58012699546822eab08a94b2ea9 introduced a new
parameter to the longevity pipeline, but it's was breaking
all other pipelines, like:

```
java.lang.NullPointerException: Cannot invoke method toInteger() on null object
```

vars/getJobTimeouts.groovy
@@ -22,7 +22,7 @@ List<Integer> call(Map params, String region){
     """
     def testData = sh(script: cmd, returnStdout: true).trim()
     println(testData)
-    if (params.stress_duration == "") {
+    if (params.stress_duration == "" || params.stress_duration == null) {
         testData = testData =~ /test_duration: (\d+)/
         testDuration = testData[0][1].toInteger()
     } else {