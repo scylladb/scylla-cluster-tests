c7796d9e2b5fc48ea874d1fce34d84a95d731d77
Shlomi Balalis <Balalis@scylladb.com>
2023-07-31 16:37:53 +0300

    fix(run_cassandra_stress): use transport param if client_encrypt is true
    (cherry picked from commit cc9dfb0de8370d566e97a8e652c35b085531577d)

--- artifacts_test.py
+++ artifacts_test.py
@@ -115,8 +115,10 @@ class ArtifactsTest(ClusterTester):  # pylint: disable=too-many-public-methods
                         f"Cluster name: {self.db_cluster.name}")
 
     def run_cassandra_stress(self, args: str):
-        result = self.node.remoter.run(
-            f"{self.node.add_install_prefix(STRESS_CMD)} {args} -node {self.node.ip_address}")
+        stress_cmd = f"{self.node.add_install_prefix(STRESS_CMD)} {args} -node {self.node.ip_address}"
+        if self.params.get('client_encrypt'):
+            stress_cmd += " -transport 'truststore=/etc/scylla/ssl_conf/client/cacerts.jks truststore-password=cassandra'"
+        result = self.node.remoter.run(stress_cmd)
         assert "java.io.IOException" not in result.stdout
         assert "java.io.IOException" not in result.stderr
 
