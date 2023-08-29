b295d74a58ef552a7af6a6520468a548c24453a9
Israel Fruchter <fruch@scylladb.com>
2023-08-29 20:16:17 +0300

    fix(azure-vm): disable azure agents
    since those agents are enabling auditd, which in turn spams our logs
    with information we don't need
    
    Fixes: https://github.com/scylladb/scylla-enterprise-machine-image/issues/63

--- unit_tests/provisioner/fake_azure_service.py
+++ unit_tests/provisioner/fake_azure_service.py
@@ -520,7 +520,7 @@ class FakeVirtualMachines:
                         }
                     },
                     "secrets": [],
-                    "allowExtensionOperations": True,
+                    "allowExtensionOperations": False,
                     "requireGuestProvisionSignal": True
                 },
                 "networkProfile": {
