afa4a1bdc5630f68186ce4593c4a80d0b9886a0f
Alexey Kartashov <alexey.kartashov@scylladb.com>
2023-08-31 02:33:14 +0200

    fix(nemesis.py): Use apt to install stress-ng on ubuntu
    Since stress-ng snap was taken down by Canonical for some reason this
    commit removes the snapd installation and replaces stress-ng install to
    main ubuntu repositories.
    
    Closes #6555
    
    (cherry picked from commit 2bdb3a21aef2bd6d210308bf893f5f7813fc3d92)

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -3888,9 +3888,7 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
             self.target_node.install_epel()
             self.target_node.remoter.sudo('yum install -y stress-ng')
         elif self.target_node.distro.is_ubuntu:
-            # Install stress on Ubuntu: https://snapcraft.io/install/stress-ng/ubuntu
-            self.target_node.remoter.sudo('apt -y install snapd')
-            self.target_node.remoter.sudo('snap install stress-ng')
+            self.target_node.remoter.sudo('apt-get -y install stress-ng')
         else:
             raise UnsupportedNemesis(f"{self.target_node.distro} OS not supported!")
 
