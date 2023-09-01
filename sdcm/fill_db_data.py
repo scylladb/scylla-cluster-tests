61638645d6b208c8e90101655c2fee07cc4117e6
Kefu Chai <kefu.chai@scylladb.com>
2023-09-01 14:35:56 +0800

    feature(unit_test): drop unnecessary experimental_features
    in a previous change, all experimental features which were previous
    enabled by '--experimental' were re-enabled. this brings the test
    to the state before scylla dropped the 'experimental' option. but
    quite a few tests either does not exercise any of the experimental
    or just need a certain one of them. so, in this change, we either
    drop the full set of default-enabled experimental features or
    just enable the one to be tested.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>
    (cherry picked from commit 6c353ad0c75fe62c4c1750cd68461ded7b9ac433)

--- sdcm/fill_db_data.py
+++ sdcm/fill_db_data.py
@@ -432,7 +432,6 @@ class FillDatabaseData(ClusterTester):
                 [[-4]]
             ],
             'min_version': '1.7',
-            'skip_condition': "'experimental_features' in self.params",
             'max_version': '',
             'skip': ''},
         {
