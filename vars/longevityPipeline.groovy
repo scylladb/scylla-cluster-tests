df67be4b354dfa01ce7b534f2d25181cd0ac0336
Lukasz Sojka <lukasz.sojka@scylladb.com>
2023-10-23 13:28:26 +0200

    test(custom): new custom schema test with gemini
    New tests with Gemini with custom schema.

--- vars/longevityPipeline.groovy
+++ vars/longevityPipeline.groovy
@@ -174,7 +174,7 @@ def call(Map pipelineParams) {
                             dir("scylla-qa-internal") {
                                 git(url: 'git@github.com:scylladb/scylla-qa-internal.git',
                                     credentialsId:'b8a774da-0e46-4c91-9f74-09caebaea261',
-                                    branch: 'master')
+                                    branch: 'custom_profile')
                             }
                         }
                     }
