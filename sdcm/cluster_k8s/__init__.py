d1974a1c6cedd77553cd18068b9202e11c998416
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2023-10-23 13:28:51 +0300

    improvement(k8s): use '0.2.0-rc.0' for k8s-local-volume-provisioner
    This release has the bugfix for the following bug:
    
    https://github.com/scylladb/k8s-local-volume-provisioner/issues/21

--- sdcm/cluster_k8s/__init__.py
+++ sdcm/cluster_k8s/__init__.py
@@ -124,7 +124,7 @@ MINIO_NAMESPACE = "minio"
 SCYLLA_CONFIG_NAME = "scylla-config"
 SCYLLA_AGENT_CONFIG_NAME = "scylla-agent-config"
 
-K8S_LOCAL_VOLUME_PROVISIONER_VERSION = "0.1.0-rc.0"  # without 'v' prefix
+K8S_LOCAL_VOLUME_PROVISIONER_VERSION = "0.2.0-rc.0"  # without 'v' prefix
 # NOTE: these values are taken from the default values of the "scylla-manager" helm chart.
 #       Needs to be defined separately to be able to reuse for image caching running on local K8S
 SCYLLA_VERSION_IN_SCYLLA_MANAGER = "5.2.6"
