6dcc81ec60ae95103b9de9c06bc83b3727fd3aed
Alexey Kartashov <alexey.kartashov@scylladb.com>
2023-11-13 16:53:30 +0100

    refactor(NemesisJobGenerator): Add configuration options for nemeses
    This change changes how Nemesis pipelines are generated. Previously,
    each pipeline would have its own yaml config file with its params. This
    made it impossible to both use the generator and have additional
    configuration parameters required for some of the nemeses. This is now
    fixed - The additional configuration options can now be defined in
    nemesis_jobs_configs.py module, which contains two mappings - One for a
    type of configuration required and one for mapping specific nemesis to a
    specific type. Additionally, the base config is now generated only once
    for each job type (However it is still global - adding a new base config
        will change all nemesis jobs to use this base config)
    If the generator encounters an existing nemesis config that differs from
    what it generated it will abort the generation and print the difference
    (to be added into the mapping file).
    
    Fixes #6698

--- sdcm/utils/nemesis.py
+++ sdcm/utils/nemesis.py
@@ -1,9 +1,12 @@
 import logging
 from functools import cached_property
+from difflib import unified_diff
 from pathlib import Path
 
 import yaml
 from jinja2 import Template
+
+import sdcm.utils.nemesis_jobs_configs as config
 
 
 DEFAULT_JOB_NAME = "longevity-5gb-1h"
@@ -26,7 +29,8 @@ class NemesisJobGenerator:
         self.nemesis_class_list = self.load_nemesis_class_list()
         self.base_job = base_job if base_job else DEFAULT_JOB_NAME
         self.backend = backend if backend else DEFAULT_BACKEND
-        self.config_template_name = f"template-{self.base_job}.yaml.j2"
+        self.config_template_name = f"template-{self.base_job}-base.yaml.j2"
+        self.nemesis_config_template = "template-nemesis-config.yaml.j2"
         self.job_template_name = f"template-{self.base_job}.jenkinsfile.j2"
 
     @property
@@ -35,7 +39,7 @@ class NemesisJobGenerator:
 
     @property
     def nemesis_test_config_dir(self):
-        return self.base_dir / "test-cases" / "nemesis"
+        return self.base_dir / "configurations" / "nemesis"
 
     @property
     def nemesis_class_list_path(self) -> Path:
@@ -46,13 +50,18 @@ class NemesisJobGenerator:
         return self.base_dir / "jenkins-pipelines" / "nemesis"
 
     @cached_property
-    def nemesis_config_file_template(self) -> str:
+    def nemesis_base_config_file_template(self) -> str:
         with (self.template_path / TEST_CASE_TEMPLATE_DIR / self.config_template_name).open() as file:
             return file.read()
 
     @cached_property
     def nemesis_job_template(self) -> str:
         with (self.template_path / JOB_PIPELINE_TEMPLATE_DIR / self.job_template_name).open() as file:
+            return file.read()
+
+    @cached_property
+    def nemesis_config_template_content(self) -> str:
+        with (self.template_path / TEST_CASE_TEMPLATE_DIR / self.nemesis_config_template).open() as file:
             return file.read()
 
     def load_nemesis_class_list(self) -> list[str]:
@@ -66,23 +75,55 @@ class NemesisJobGenerator:
 
     def create_test_cases_from_template(self):
         for cls in self.nemesis_class_list:
-            new_config_name = f"{self.base_job}-{cls}.yaml"
-            nemesis_config_body = Template(self.nemesis_config_file_template).render(
+            new_config_name = f"{cls}.yaml"
+            nemesis_config_body = Template(self.nemesis_config_template_content).render(
                 {"nemesis_class": cls})
-            with (self.nemesis_test_config_dir / new_config_name).open(mode="w") as file:
-                file.write(nemesis_config_body + '\n')
+            target_config = self.nemesis_test_config_dir / new_config_name
+            if target_config.exists():
+                handle = target_config.open("r+")
+                content = handle.read()
+                if len(content) != len(nemesis_config_body):
+                    LOGGER.warning("Potential altered config %s.yaml, checking for conflicts", cls)
+                    new_config = yaml.safe_load(nemesis_config_body)
+                    old_config = yaml.safe_load(content)
+                    new_keys = set(new_config.keys())
+                    old_keys = set(old_config.keys())
+                    if len((key_diff := new_keys.symmetric_difference(old_keys))) != 0:
+                        diff = unified_diff(
+                            content.splitlines(keepends=True),
+                            nemesis_config_body.splitlines(keepends=True),
+                            fromfile=f"{cls}-old.yaml",
+                            tofile=f"{cls}-new.yaml"
+                        )
+                        LOGGER.error("Difference detected in %s.yaml, Diff:\n%s\n\nExtra keys: %s",
+                                     cls, "\n".join(diff), ", ".join(key_diff))
+                        continue
+            else:
+                handle = target_config.open("w")
+            handle.seek(0)
+            handle.truncate()
+            handle.write(nemesis_config_body)
+            handle.write("\n")
+            handle.close()
             LOGGER.info("Created test config file: %s", new_config_name)
 
     def create_job_files_from_template(self) -> list[Path]:
         pipeline_files = []
         for cls in self.nemesis_class_list:
-            config_name = f"{self.base_job}-{cls}.yaml"
+            additional_configs = config.NEMESIS_REQUIRED_ADDITIONAL_CONFIGS.get(cls, [])
+            additional_params = config.NEMESIS_ADDITIONAL_PIPELINE_PARAMS.get(cls, {})
+            config_name = [
+                str(self.nemesis_test_config_dir / f"{self.base_job}-nemesis.yaml"),
+                str(self.nemesis_test_config_dir / f"{cls}.yaml"),
+                *additional_configs,
+            ]
             job_file_name = f"{self.base_job}-{cls}-{self.backend}.jenkinsfile"
             nemesis_job_groovy_source = Template(self.nemesis_job_template).render(
                 {
                     "nemesis_longevity_config": config_name,
                     "backend": self.backend,
-                    "region": self.BACKEND_TO_REGION.get(self.backend, "eu-west-1")
+                    "region": self.BACKEND_TO_REGION.get(self.backend, "eu-west-1"),
+                    "additional_params": additional_params,
                 })
             job_path = self.base_nemesis_job_dir / job_file_name
             with job_path.open("w") as file:
@@ -92,6 +133,14 @@ class NemesisJobGenerator:
             pipeline_files.append(job_path)
 
         return pipeline_files
+
+    def render_base_job_config(self, params: dict | None = None):
+        params = params if params else {}
+        base_config_body = Template(self.nemesis_base_config_file_template).render(params)
+        with (self.nemesis_test_config_dir / f"{self.base_job}-nemesis.yaml").open(mode="w") as file:
+            file.write(base_config_body + '\n')
+
+        LOGGER.info("Rendered base config:\n\n%s\n\n", self.nemesis_base_config_file_template)
 
     def verify_env(self):
         assert self.template_path.exists(), \
