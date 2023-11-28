f7bc03233dd1fdcc6d0468c4b02434171d295546
Alexey Kartashov <alexey.kartashov@scylladb.com>
2023-11-28 13:23:09 +0100

    feature(argus): Track origin url and branch name for argus
    This change adds new get_git_status_info function, which will output
    git branch and upstream url information for argus. For compatibility and
    to account for corner cases, previously used get_git_commit_id function
    will still be used in face get_git_status_info fails to collect data.
    
    Fixes scylladb/argus#302

--- sdcm/utils/git.py
+++ sdcm/utils/git.py
@@ -3,8 +3,13 @@ Simple git wrappers that provide useful information about current repository
 """
 import subprocess
 import logging
+from typing import TypedDict
 
 LOGGER = logging.getLogger(__name__)
+
+
+GitStatus = TypedDict('GitStatus', {'branch.oid': str, 'branch.head': str,
+                      'branch.upstream': str | None, 'upstream.url': str | None})
 
 
 def get_git_commit_id() -> str:
@@ -23,6 +28,65 @@ def get_git_current_branch() -> str:
         LOGGER.warning("Error running git command", exc_info=True)
         return "#ERROR"
     return proc.stdout.decode(encoding="utf-8").strip()
+
+
+def get_git_status_info() -> GitStatus:
+    # pylint: disable=too-many-locals
+    try:
+        #    Example output:
+        #    # branch.oid 0748e3a512b7e698bc4b2e3e8ec8d46d0b3244ae
+        #    # branch.head add-argus-branch-tracking
+        #    # branch.upstream origin/add-argus-branch-tracking
+        #    # branch.ab +0 -0
+        status_proc = subprocess.run(args=["git", "status", "-b", "--porcelain=v2",
+                                     ".nothing"], check=True, capture_output=True)
+        output = status_proc.stdout.decode(encoding="utf-8").strip()
+        git_status: GitStatus = {}
+        for line in (line for line in output.split("\n") if line.startswith("#")):
+            key, value = line[2:].split(" ", maxsplit=1)
+            git_status[key] = value
+
+        if "(detached)" in git_status["branch.head"]:
+            # Detached HEAD, we need to find the actual heads and remotes
+            # We will potentially lose accuracy here as there can be more than one ref
+            # pointing to an object id.
+
+            # Example output:
+            # e21d30bee0cfcfe546b457671b839b87f0a6da0e refs/remotes/origin/branch-2020.1
+            # fafa2877f879ba840cc199001ad23807039c8fc6 refs/remotes/origin/branch-2021.1
+            # a7bd7066f25c45365d81cec7fd2bce39e318c7ee refs/remotes/origin/branch-2022.1
+            # bd87ca2b6462ba2e78fba2cc45495c0d1cdaf286 refs/remotes/origin/branch-4.2
+            proc = subprocess.run(args=["git", "show-ref"], check=True, capture_output=True)
+            output = proc.stdout.decode(encoding="utf-8").strip()
+            target_oid = git_status["branch.oid"]
+            head_found = False
+            remote_found = False
+            for _, ref in (line.split(" ", maxsplit=1) for line in output.splitlines() if target_oid in line):
+                _, ref_type, ref_name = ref.split("/", maxsplit=2)
+                match ref_type:
+                    case "heads":
+                        if head_found:
+                            continue
+                        git_status["branch.head"] = ref_name
+                        head_found = True
+                    case "remotes":
+                        if remote_found:
+                            continue
+                        git_status["branch.upstream"] = ref_name
+                        remote_found = True
+
+        remote, _ = git_status["branch.upstream"].split("/", maxsplit=1)
+        #    Example output:
+        #    gh:k0machi/scylla-cluster-tests
+        origin_proc = subprocess.run(
+            args=["git", "config", "--get", f"remote.{remote}.url"], check=True, capture_output=True)
+        output = origin_proc.stdout.decode(encoding="utf-8").strip()
+        git_status["upstream.url"] = output.strip()
+
+    except (subprocess.CalledProcessError, KeyError):
+        LOGGER.warning("Error parsing git information", exc_info=True)
+        return {}
+    return git_status
 
 
 def clone_repo(remoter, repo_url: str, destination_dir_name: str = "", clone_as_root=True):
