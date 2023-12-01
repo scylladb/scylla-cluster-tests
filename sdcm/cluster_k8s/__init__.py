b38f59287b1b244cd5a38cb97aa891568ac04cfc
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2023-12-01 18:24:20 +0200

    improvement(defaults): use latest 5.2.x as default scylla version

--- sdcm/cluster_k8s/__init__.py
+++ sdcm/cluster_k8s/__init__.py
@@ -129,7 +129,7 @@ SCYLLA_AGENT_CONFIG_NAME = "scylla-agent-config"
 K8S_LOCAL_VOLUME_PROVISIONER_VERSION = "0.2.0-rc.0"  # without 'v' prefix
 # NOTE: these values are taken from the default values of the "scylla-manager" helm chart.
 #       Needs to be defined separately to be able to reuse for image caching running on local K8S
-SCYLLA_VERSION_IN_SCYLLA_MANAGER = "5.2.6"
+SCYLLA_VERSION_IN_SCYLLA_MANAGER = "5.2.11"
 SCYLLA_MANAGER_AGENT_VERSION_IN_SCYLLA_MANAGER = "3.1.1"
 
 # NOTE: add custom annotations to a ServiceAccount used by a ScyllaCluster
