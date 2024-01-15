cf25c741acb4b6b18dbdea6b349558beb6706568
Israel Fruchter <fruch@scylladb.com>
2024-01-15 08:59:49 +0200

    fix(nemesis.yml): update the list with re-introduced ones

--- unit_tests/test_nemesis_sisyphus.py
+++ unit_tests/test_nemesis_sisyphus.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 import yaml
 
+from sdcm import sct_abs_path
 from sdcm.nemesis import Nemesis, SisyphusMonkey
 from sdcm.cluster import BaseScyllaCluster
 
@@ -71,16 +72,16 @@ def test_list_all_available_nemesis(generate_file=True):
     disruption_list, disruptions_dict, disruption_classes = sisyphus.get_list_of_disrupt_methods(
         subclasses_list=subclasses, export_properties=True)
 
-    assert len(disruption_list) == 85
+    assert len(disruption_list) == 87
 
     if generate_file:
-        with open('data_dir/nemesis.yml', 'w', encoding="utf-8") as outfile1:
+        with open(sct_abs_path('data_dir/nemesis.yml'), 'w', encoding="utf-8") as outfile1:
             yaml.dump(disruptions_dict, outfile1, default_flow_style=False)
 
-        with open('data_dir/nemesis_classes.yml', 'w', encoding="utf-8") as outfile2:
+        with open(sct_abs_path('data_dir/nemesis_classes.yml'), 'w', encoding="utf-8") as outfile2:
             yaml.dump(disruption_classes, outfile2, default_flow_style=False)
 
-    with open('data_dir/nemesis.yml', 'r', encoding="utf-8") as nemesis_file:
+    with open(sct_abs_path('data_dir/nemesis.yml'), 'r', encoding="utf-8") as nemesis_file:
         static_nemesis_list = yaml.safe_load(nemesis_file)
 
     assert static_nemesis_list == disruptions_dict
