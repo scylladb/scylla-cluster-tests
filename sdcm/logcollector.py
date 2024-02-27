f9d7739af4f06eca95ffd00d75636e135c9948a1
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2024-02-27 19:20:03 +0200

    improvement(custom_d1): use greatly improved latte+rune scripts
    TODO:
    - Increase the instance types for DB and loaders.
    - Update stress commands to produce more load as it is in requirements.

--- sdcm/logcollector.py
+++ sdcm/logcollector.py
@@ -908,7 +908,7 @@ class LoaderLogCollector(LogCollector):
                 search_locally=True),
         FileLog(name="*cs-hdr-*.hdr",
                 search_locally=True),
-        FileLog(name='*latte*.log',
+        FileLog(name='*latte*',
                 search_locally=True),
     ]
 
--- vars/checkoutQaInternal.groovy
+++ vars/checkoutQaInternal.groovy
@@ -2,8 +2,10 @@
 
 def call(Map params = [:]) {
     dir("scylla-qa-internal") {
-        git(url: 'git@github.com:scylladb/scylla-qa-internal.git',
+        // TODO: Use 'scylladb/scylla-qa-internal' repo when following PR gets merged:
+        //       https://github.com/scylladb/scylla-qa-internal/pull/38
+        git(url: 'git@github.com:vponomaryov/scylla-qa-internal.git',
             credentialsId:'b8a774da-0e46-4c91-9f74-09caebaea261',
-            branch: 'master')
+            branch: 'custom-d1-update')
     }
 }
