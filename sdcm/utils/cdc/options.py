d50ecebce927f43b65e3330c2e6ae11498c6ff4d
Aleksandr Bykov <alex.bykov@scylladb.com>
2024-03-06 07:14:24 +0700

    fix(CDC_OPTIONS): Use boolean value for cdc preimage option
    CDC_SETTINGS_NAMES_VALUES['preimage'] uses string values "true" "false"
    instead of bool True False. This cause assertion error if it was changed
    
    ```
    AssertionError: CDC extension settings are differs.
    Current: {'delta': 'full', 'enabled': True, 'preimage': True, 'postimage': True, 'ttl': '1800'}
    expected: {'delta': 'full', 'enabled': True, 'preimage': 'true', 'postimage': True, 'ttl': '1800'}
    ```
    
    (cherry picked from commit 1350655546377bd723e564783a9e8f8030f882d1)

--- sdcm/utils/cdc/options.py
+++ sdcm/utils/cdc/options.py
@@ -11,7 +11,7 @@ CDC_LOGTABLE_SUFFIX = "_scylla_cdc_log"
 CDC_SETTINGS_NAMES_VALUES = {
     "enabled": [True, False],
     "delta": ["full", "keys"],
-    "preimage": ["full", "true", "false"],
+    "preimage": ["full", True, False],
     "postimage": [True, False],
     "ttl": "86400"
 }
