248d620ddfab8613aed7de049e588689ff6344ca
Israel Fruchter <fruch@scylladb.com>
2024-03-07 14:24:49 +0200

    feature(k8s-functional): report test results with junit xml
    we should be sending those results to Argus and to Jenkins
    so it would be easier to debug those runs
    
    Ref: #7252

--- sdcm/logcollector.py
+++ sdcm/logcollector.py
@@ -1014,6 +1014,8 @@ class BaseSCTLogCollector(LogCollector):
         FileLog(name='result_gradual_increase.log'),
         FileLog(name='partition_range_scan_diff_*.log',
                 search_locally=True),
+        FileLog(name='junit.xml',
+                search_locally=True),
     ]
     cluster_log_type = 'sct-runner-events'
     cluster_dir_prefix = 'sct-runner-events'
