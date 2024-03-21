3d864da2ca1e12716654c76e5cf32a438f5b9f4b
Israel Fruchter <fruch@scylladb.com>
2024-03-21 16:10:35 +0200

    fix(test_default_filters): take it out of unittest
    since this test is depened on github API calls, and we are
    hitting the rate-limitting for them a bit too often now.
    
    moving this test to integration test, and removeing
    the event default filter code from the test code,
    so non of the test would be using it.
    
    (cherry picked from commit 04aec0dc354eb6331ab29d87c630eaf1e5d49603)

--- unit_tests/lib/events_utils.py
+++ unit_tests/lib/events_utils.py
@@ -17,13 +17,11 @@ import tempfile
 import unittest.mock
 from contextlib import contextmanager
 
-from sdcm.sct_events.setup import EVENTS_DEVICE_START_DELAY, start_events_device, stop_events_device, enable_default_filters
+from sdcm.sct_events.setup import EVENTS_DEVICE_START_DELAY, start_events_device, stop_events_device
 from sdcm.sct_events.events_device import start_events_main_device, get_events_main_device
 from sdcm.sct_events.file_logger import get_events_logger
 from sdcm.sct_events.events_processes import EventsProcessesRegistry
 from sdcm.sct_events.event_counter import get_events_counter
-from sdcm.sct_config import SCTConfiguration
-from sdcm.utils.context_managers import environment
 
 
 class EventsUtilsMixin:
@@ -45,8 +43,6 @@ class EventsUtilsMixin:
             cls.events_processes_registry_patcher.start()
         if events_device:
             start_events_device(_registry=cls.events_processes_registry)
-            with environment(SCT_CLUSTER_BACKEND='docker'):
-                enable_default_filters(SCTConfiguration())
         elif events_main_device:
             start_events_main_device(_registry=cls.events_processes_registry)
             time.sleep(EVENTS_DEVICE_START_DELAY)
