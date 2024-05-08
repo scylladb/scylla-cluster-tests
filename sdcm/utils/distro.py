85afa34b1b12853b1839d10ee1e353c850608fc2
Israel Fruchter <fruch@scylladb.com>
2024-05-08 17:31:45 +0300

    feature(artifacts-tests): add support for ubuntu 24.04
    as part of an effort to start supporting using ubuntu 24.04
    as the basis for scylla images, we are introducing a new artifact
    tests to cover it
    
    Closes: scylladb/scylla-cluster-tests#7410
    (cherry picked from commit 1d818d2e1ecf24c022f310bc235feb8cb945760f)

--- sdcm/utils/distro.py
+++ sdcm/utils/distro.py
@@ -38,7 +38,7 @@ KNOWN_OS = (
     ("AMAZON", "amzn", ["2"], DistroBase.RHEL),
     ("ROCKY", "rocky", ["8", "9"], DistroBase.RHEL),
     ("DEBIAN", "debian", ["10", "11"], DistroBase.DEBIAN),
-    ("UBUNTU", "ubuntu", ["20.04", "21.04", "21.10", "22.04"], DistroBase.DEBIAN),
+    ("UBUNTU", "ubuntu", ["20.04", "21.04", "21.10", "22.04", "24.04"], DistroBase.DEBIAN),
     ("SLES", "sles", ["15"], DistroBase.UNKNOWN),
     ("FEDORA", "fedora", ["34", "35", "36"], DistroBase.RHEL),
     ("MINT", "linuxmint", ["20", "21"], DistroBase.DEBIAN),
