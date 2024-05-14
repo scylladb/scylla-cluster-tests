03155c1b49d58e4b7956654199746df88c99230f
Liapkovich <mikita.liapkovich@scylladb.com>
2024-05-14 17:27:05 +0200

    fix(manager): run healthcheck_change_max_timeout test only for multiDC
    The test is valid only for multiDC configuration. Otherwise, it should
    be skipped.

--- mgmt_cli_test.py
+++ mgmt_cli_test.py
@@ -349,9 +349,8 @@ class MgmtCliTest(BackupFunctionsMixIn, ClusterTester):
             self.test_mgmt_cluster_crud()
         with self.subTest('Mgmt cluster Health Check'):
             self.test_mgmt_cluster_healthcheck()
-        # test_healthcheck_change_max_timeout requires a multi dc run. And since ipv6 cannot run in multi dc, this test
-        # function will be skipped for ipv6 runs.
-        if self.db_cluster.nodes[0].test_config.IP_SSH_CONNECTIONS != "ipv6":
+        # test_healthcheck_change_max_timeout requires a multi dc run
+        if self.db_cluster.nodes[0].test_config.MULTI_REGION:
             with self.subTest('Basic test healthcheck change max timeout'):
                 self.test_healthcheck_change_max_timeout()
         with self.subTest('Basic test suspend and resume'):
