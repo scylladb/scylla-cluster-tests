cf22f16626ba377b72ca494eb0f8988c83208b09
Israel Fruchter <fruch@scylladb.com>
2024-05-21 09:16:12 +0300

    feature(aws): support ami as ssm links
    with this option we can use latest of specific types of images
    and don't need to keep it updated manually all the time
    
    only some of the major distro are supported, but those are exactly
    the ones we need
    
    this is similar to the `family` type that we use in GCE
    
    (cherry picked from commit 9284e91b6f4a0799510fd9d017917cbf40665c1d)

--- unit_tests/test_config.py
+++ unit_tests/test_config.py
@@ -800,6 +800,16 @@ class ConfigurationTests(unittest.TestCase):  # pylint: disable=too-many-public-
         self.assertEqual(conf.get('n_db_nodes'), 3)
         self.assertEqual(conf.get('availability_zone'), 'b,c')
 
+    @pytest.mark.integration
+    def test_32_resolve_aws_ssm_links(self):
+        os.environ['SCT_CLUSTER_BACKEND'] = 'aws'
+        os.environ['SCT_AMI_ID_DB_SCYLLA'] = 'resolve:ssm:/aws/service/debian/release/11/latest/amd64'
+
+        conf = sct_config.SCTConfiguration()
+        conf.verify_configuration()
+
+        assert conf["ami_id_db_scylla"].startswith('ami-')
+
 
 if __name__ == "__main__":
     unittest.main()
