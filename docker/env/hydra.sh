f76d61bb88a1f301abffd525249f1c97f2a1407a
Israel Fruchter <fruch@scylladb.com>
2024-05-23 23:53:22 +0300

    feature(ssh-keys): switch GCE and docker off rsa keys
    since rsa keys aren't supported by default by ssh anymore
    we are switching them out
    
    (cherry picked from commit 0ce1bf5e79acc14378046d0f3594c530b02624f8)
    (cherry picked from commit 3ac166c09269ba982e2fd772ee3fac0ab2fa252d)

--- docker/env/hydra.sh
+++ docker/env/hydra.sh
@@ -309,11 +309,9 @@ if [[ -n "$RUNNER_IP" ]]; then
     trap clean_ssh_agent EXIT
 
     if [ -z "$HYDRA_DRY_RUN" ]; then
-        ssh-add ~/.ssh/scylla_test_id_ed25519
         ssh-add ~/.ssh/scylla-test
         ssh-add ~/.ssh/scylla_test_id_ed25519
     else
-        echo ssh-add ~/.ssh/scylla_test_id_ed25519
         echo ssh-add ~/.ssh/scylla-test
         echo ssh-add ~/.ssh/scylla_test_id_ed25519
     fi
