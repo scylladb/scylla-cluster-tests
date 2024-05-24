c76165ecf11dbe1453a41777d92467290a245a19
Liapkovich <mikita.liapkovich@scylladb.com>
2024-05-24 13:22:05 +0200

    feature(manager): extend email message with new parameters
    Support of backup/restore operations metrics were added into email
    notification coming from Manager tests. This change will be quite
    beneficial for restore test with large datasets to compare the
    operation time and restore approach used (batch-size, parallel).
    
    (cherry picked from commit 51386177fe158d1df2eec843653b9e3f41111884)

--- mgmt_cli_test.py
+++ mgmt_cli_test.py
@@ -452,10 +452,26 @@ class MgmtCliTest(BackupFunctionsMixIn, ClusterTester):
         self.log.info("Prepare data for email")
 
         email_data = self._get_common_email_data()
-        email_data.update({"manager_server_repo": self.params.get("scylla_mgmt_address"),
-                           "manager_agent_repo": self.params.get("scylla_mgmt_agent_address") or
-                           self.params.get("scylla_mgmt_address"), })
 
+        restore_parameters = self.params.get("mgmt_restore_params")
+        if restore_parameters:
+            restore_parameters = restore_parameters.dict()
+
+        agent_backup_config = self.params.get("mgmt_agent_backup_config")
+        if agent_backup_config:
+            agent_backup_config = agent_backup_config.dict()
+
+        email_data.update(
+            {
+                "manager_server_repo": self.params.get("scylla_mgmt_address"),
+                "manager_agent_repo": (self.params.get("scylla_mgmt_agent_address") or
+                                       self.params.get("scylla_mgmt_address")),
+                "backup_time": str(self.manager_test_metrics.backup_time),
+                "restore_time": str(self.manager_test_metrics.restore_time),
+                "restore_parameters": restore_parameters,
+                "agent_backup_config": agent_backup_config,
+            }
+        )
         return email_data
 
     def test_backup_and_restore_only_data(self):
