9dfea78f5b33f86805ae88bb4b6859995507a1fd
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2024-06-03 20:37:23 +0300

    fix(pipelines): raise error when resource provisioning fails

--- vars/longevityPipeline.groovy
+++ vars/longevityPipeline.groovy
@@ -227,7 +227,7 @@ def call(Map pipelineParams) {
             }
             stage('Provision Resources') {
                 steps {
-                    catchError() {
+                    catchError(stageResult: 'FAILURE') {
                         script {
                             wrap([$class: 'BuildUser']) {
                                 dir('scylla-cluster-tests') {
