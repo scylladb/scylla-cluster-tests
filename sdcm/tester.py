scylladb/scylla-cluster-tests

commit 7632fad17d8ebb343168f9af7906a8261a5b28c3

improvement(manager): save manager version in Argus
Collect and save in Argus manager version

sdcm/tester.py
@@ -57,6 +57,7 @@ from sdcm.cluster_aws import MonitorSetAWS
 from sdcm.cluster_k8s import mini_k8s, gke, eks
 from sdcm.cluster_k8s.eks import MonitorSetEKS
 from sdcm.cql_stress_cassandra_stress_thread import CqlStressCassandraStressThread
+from sdcm.mgmt import get_scylla_manager_tool
 from sdcm.provision.aws.capacity_reservation import SCTCapacityReservation
 from sdcm.kafka.kafka_cluster import LocalKafkaCluster
 from sdcm.provision.azure.provisioner import AzureProvisioner
@@ -501,6 +502,20 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
             self.test_config.argus_client().submit_packages(packages_to_submit)
         except Exception:  # pylint: disable=broad-except
             self.log.error("Unable to collect package versions for Argus - skipping...", exc_info=True)
+
+    def argus_collect_manager_version(self):
+        if self.params.get('use_mgmt'):
+            manager_tool = get_scylla_manager_tool(manager_node=self.monitors.nodes[0])
+            self.log.info("Saving manager version in Argus...")
+            # sctool.version output:
+            #   [['Client version: 3.2.8-0.20240517.5f324acd2'], ['Server version: 3.2.8-0.20240517.5f324acd2']]
+            client_version, server_version, *_ = (version[0].split(":")[1].strip()
+                                                  for version in manager_tool.sctool.version)
+            manager_packages = [Package(name="scylla-manager-client", date="",
+                                        version=client_version, revision_id="", build_id=""),
+                                Package(name="scylla-manager-server", date="",
+                                        version=server_version, revision_id="", build_id="")]
+            self.test_config.argus_client().submit_packages(manager_packages)
 
     def generate_operator_packages(self) -> list[Package]:
         operator_packages = []
@@ -975,7 +990,8 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
             futures = [
                 executor.submit(_db_post_validation),
                 executor.submit(self.argus_collect_packages),
-                executor.submit(self.argus_get_scylla_version)
+                executor.submit(self.argus_get_scylla_version),
+                executor.submit(self.argus_collect_manager_version),
             ]
 
             for db_cluster in self.db_clusters_multitenant:

commit 914bc2e8c4eed312c4eafcea21a0b54215c2888c

fix(scylla-manager): get version if monitor node exists
This issue was caused by https://github.com/scylladb/scylla-cluster-tests/pull/7538.
The monitor node is not created in artifact test that causes to failure:

File sdcm/tester.py, line 508, in argus_collect_manager_version
  manager_tool = get_scylla_manager_tool(manager_node=self.monitors.nodes[0])
  IndexError: list index out of range

sdcm/tester.py
@@ -504,7 +504,7 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
             self.log.error("Unable to collect package versions for Argus - skipping...", exc_info=True)
 
     def argus_collect_manager_version(self):
-        if self.params.get('use_mgmt'):
+        if self.monitors.nodes and self.params.get('use_mgmt'):
             manager_tool = get_scylla_manager_tool(manager_node=self.monitors.nodes[0])
             self.log.info("Saving manager version in Argus...")
             # sctool.version output:

unit_tests/test_tester.py
@@ -97,6 +97,9 @@ class ClusterTesterForTests(ClusterTester):
     def save_email_data(self):
         pass
 
+    def argus_collect_manager_version(self):
+        pass
+
     def tearDown(self):
         super().tearDown()
         self._validate_results()