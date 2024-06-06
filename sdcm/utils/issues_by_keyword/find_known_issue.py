a0f2febf79947875f937fdce56f99afeab69f7e8
Julia Yakovlev <julia.yakovlev@scylladb.com>
2024-06-06 16:06:13 +0300

    improvement(event): find Scylla reactor_stall issue by known keyword
    This commit present new helpful mechanism that allow to find the known Scylla issue by backtrace
    while decoding and addthis issue to the event.
    New mapping file keeps "keyword-issue" for reactor stalls only for now.
    But it can be expanded for other backtraces (for example, coredumps) .
    Found issue is added to the event new event attribute 'known_issue'.
    Argus may add this issue to the event caption using the new attribute.
    
    (cherry picked from commit de337e2f3f00bf0fbf41926810004d0749a660f3)

---
+++ sdcm/utils/issues_by_keyword/find_known_issue.py
@@ -0,0 +1,37 @@
+import logging
+from collections import defaultdict
+from pathlib import Path
+from typing import NamedTuple
+
+import yaml
+
+from sdcm.utils.metaclasses import Singleton
+
+LOGGER = logging.getLogger(__name__)
+
+
+class MapKeywords(NamedTuple):
+    keyword: str
+    issue: str
+
+
+# pylint: disable=too-few-public-methods
+class FindIssuePerBacktrace(metaclass=Singleton):
+
+    MAPPING_FILE = Path(__file__).parent / "issue_by_keyword.yaml"
+
+    def __init__(self):
+        with open(file=self.MAPPING_FILE, encoding="utf-8", mode="r") as map_file:
+            yaml_details = yaml.safe_load(map_file)
+
+        self.map = defaultdict(list)
+        for backtrace_type, values in yaml_details.items():
+            for one_pair in values:
+                self.map[backtrace_type].append(MapKeywords(keyword=one_pair["keyword"], issue=one_pair["issue"]))
+
+    def find_issue(self, backtrace_type: str, decoded_backtrace: str) -> str | None:
+        LOGGER.debug("Search for an issue for %s backtrace", backtrace_type)
+        for all_keywords in self.map.get(backtrace_type, []):
+            if all_keywords.keyword in decoded_backtrace:
+                return all_keywords.issue
+        return None
