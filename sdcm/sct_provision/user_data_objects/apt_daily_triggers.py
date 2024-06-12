b52a62ddfaeccea8c5253dfb6429446672b7ea2a
Lukasz Sojka <lukasz.sojka@scylladb.com>
2024-06-12 14:00:24 +0200

    improvement(setup): disable apt daily triggers in cloud-init
    Apt daily triggers disabling takes time and is quite late in the
    proces. This prolongs setup due chance that triggers are fired before
    installation of syslog-ng and other packages and makes apt lock to be
    awaited. Also doing it with `remoter` class in multiple commands is not
    most efficient way of doing it.
    
    Moved disabling apt daily triggers to cloud-init step as the first in
    the process.
    
    closes: https://github.com/scylladb/qa-tasks/issues/1695

---
+++ sdcm/sct_provision/user_data_objects/apt_daily_triggers.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from sdcm.provision.common.utils import disable_daily_apt_triggers
+from sdcm.sct_provision.user_data_objects import SctUserDataObject
+
+
+@dataclass
+class DisableAptTriggersUserDataObject(SctUserDataObject):
+
+    @property
+    def is_applicable(self) -> bool:
+        return True
+
+    @property
+    def script_to_run(self) -> str:
+        return disable_daily_apt_triggers()
