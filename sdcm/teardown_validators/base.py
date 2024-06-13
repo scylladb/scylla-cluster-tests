2bb1fc3c05de1f785ff7161cc50696b7a04ae7ea
Dmitriy Kruglov <dmytro.kruglov@scylladb.com>
2024-06-14 00:24:16 +0200

    feature(teardown-validators): nemeses error events validator
    In some cases (e.g. parallel nemeses) we don't want to fail the test based on an error
    event, but rather look for coredumps or critical level events.
    
    The change introduces a teardown validator, which takes into account only pre-defined
    error events to make a decision if the test is failed.
    
    The related task: https://github.com/scylladb/qa-tasks/issues/1671

--- sdcm/teardown_validators/base.py
+++ sdcm/teardown_validators/base.py
@@ -1,6 +1,5 @@
 import logging
 
-from sdcm.cluster import BaseCluster
 from sdcm.sct_config import SCTConfiguration
 
 LOGGER = logging.getLogger(__name__)
@@ -9,9 +8,9 @@ LOGGER = logging.getLogger(__name__)
 class TeardownValidator:  # pylint: disable=too-few-public-methods
     validator_name = None  # must be defined by child classes
 
-    def __init__(self, params: SCTConfiguration, cluster: BaseCluster):
+    def __init__(self, params: SCTConfiguration, tester: 'sdcm.tester.ClusterTester'):
         self.params = params
-        self.cluster = cluster
+        self.tester = tester
         self.configuration = params.get(f"teardown_validators.{self.validator_name}")
         self.is_enabled = self.configuration.get("enabled", False)
         if not self.is_enabled:
