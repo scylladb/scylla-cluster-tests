96b3eb57a611107ad6a9f64e23fb341f97909c10
Israel Fruchter <fruch@scylladb.com>
2024-06-16 10:12:42 +0300

    fix(wait_cloud_init_completes): ignore warnings
    newer versions of cloud-init, report status code 2, if there
    are warnings, and we should have failures like the:
    ```
    2024-06-15 22:14:22.954: (TestFrameworkEvent Severity.ERROR)
       period_type=one-time event_id=..., source=ArtifactsTest.SetUp()
    exception=Failed to create node: Encountered a bad command exit code!
    Command: 'sudo cloud-init status --wait'
    Exit code: 2
    Stdout:
    status: done
    Stderr:
    ```
    
    in this chaneg we detect the cloud-init version, and adapt the logic
    we do for identifing if cloud-init failed or not, while showing
    the extended information json newer version can supply
    
    (cherry picked from commit 772561c590ec86e0cc462c3f6906efc79a55fb26)

--- sdcm/provision/helpers/cloud_init.py
+++ sdcm/provision/helpers/cloud_init.py
@@ -10,8 +10,11 @@
 # See LICENSE for more details.
 #
 # Copyright (c) 2022 ScyllaDB
+import json
 import logging
 from pathlib import Path
+
+from packaging.version import Version
 
 from sdcm import sct_abs_path
 from sdcm.provision.provisioner import VmInstance
@@ -31,12 +34,24 @@ def wait_cloud_init_completes(remoter: RemoteCmdRunnerBase, instance: VmInstance
     LOGGER.info("Waiting for cloud-init to complete on node %s...", instance.name)
     errors_found = False
     remoter.is_up(60 * 5)
-    result = remoter.sudo("cloud-init status --wait")
-    status = result.stdout
-    LOGGER.debug("cloud-init status: %s", status)
-    if "done" not in status:
-        LOGGER.error("Some error during cloud-init %s", status)
-        errors_found = True
+    # examples: 24.1.3-0ubuntu3.3, 19.3-46.amzn2.0.2
+    cloud_init_version = Version(remoter.sudo("cloud-init --version 2>&1").stdout.split()[1].split('-')[0])
+    # cloud-init supports json output from version 23.4, see:
+    # https://cloudinit.readthedocs.io/en/latest/explanation/return_codes.html#id1
+    if cloud_init_version >= Version("23.4"):
+        result = remoter.sudo("cloud-init status --format=json --wait", ignore_status=True)
+        status = json.loads(result.stdout)
+
+        LOGGER.debug("cloud-init status: %s", status)
+        if status['status'] != "done" or status['errors'] or result.return_code == 1:
+            LOGGER.error("Some errors during cloud-init %s", status)
+            errors_found = True
+    else:
+        result = remoter.sudo("cloud-init status --wait", ignore_status=True)
+        status = result.stdout
+        if "done" not in status or result.return_code == 1:
+            LOGGER.error("Some errors during cloud-init %s", status)
+            errors_found = True
     scripts_errors_found = log_user_data_scripts_errors(remoter=remoter)
     if errors_found or scripts_errors_found:
         raise CloudInitError("Errors during cloud-init provisioning phase. See logs for errors.")
