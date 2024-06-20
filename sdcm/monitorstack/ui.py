241b2f75a65598a8e59b369765af510976bd440e
Israel Fruchter <fruch@scylladb.com>
2024-06-21 01:55:50 +0300

    fix(screenshot): revert to specific resolution
    we were using height=-1 for the renderer to automticlly capture
    all of the dashboard.
    
    seems like with monitoring 4.7, this got broken cause of newer grafana
    version, and dashboard are coming out cut, and some ever completly
    broken and not readable.
    
    couldn't pinpoint which exact version is causing it.
    
    so meanwhile reverting to use exact resolution, that
    seems to be working as expected
    
    Fixes: #7600
    (cherry picked from commit 335a3018145742214212a4b96147289b451d823b)

--- sdcm/monitorstack/ui.py
+++ sdcm/monitorstack/ui.py
@@ -7,12 +7,14 @@ class Dashboard:
     name: str
     path: str
     title: str
+    resolution: tuple[int]
 
 
 class OverviewDashboard(Dashboard):
     name = 'overview'
     path = 'd/overview-{version}/scylla-{dashboard_name}'
     title = 'Overview'
+    resolution = (1920, 4000)
 
 
 class ServerMetricsNemesisDashboard(Dashboard):
@@ -22,9 +24,11 @@ class ServerMetricsNemesisDashboard(Dashboard):
     name = f'{test_name}scylla-per-server-metrics-nemesis'
     title = 'Scylla Per Server Metrics Nemesis'
     path = 'dashboard/db/{dashboard_name}-{version}'
+    resolution = (1920, 15000)
 
 
 class AlternatorDashboard(Dashboard):
     name = 'alternator'
     title = 'Alternator'
     path = 'd/alternator-{version}/{dashboard_name}'
+    resolution = (1920, 4000)
