9954ca6b3422238a5f70a421bc238177c4e6b3dd
Israel Fruchter <fruch@scylladb.com>
2024-06-27 23:47:49 +0300

    fix(jepsen): hack tcpdump to work as jepsen needs
    jepsen is picky about the location of tcpdump in some
    version so we'll symlink to where it's expecting

--- jepsen_test.py
+++ jepsen_test.py
@@ -46,7 +46,12 @@ class JepsenTest(ClusterTester):
                    destination_dir_name="jepsen-scylla",
                    clone_as_root=False)
         for db_node in self.db_cluster.nodes:
-            remoter.run(f"ssh-keyscan -t rsa {db_node.ip_address} >> ~/.ssh/known_hosts")
+            remoter.run(f"ssh-keyscan -t ed25519 {db_node.ip_address} >> ~/.ssh/known_hosts")
+
+            # newer jepsen test is using tcpdump, and expect it in /usr/bin
+            db_node.install_package("tcpdump")
+            db_node.remoter.sudo("ln -s /sbin/tcpdump /usr/bin/tcpdump", ignore_status=True)
+
         remoter.send_files(os.path.expanduser(self.db_cluster.nodes[0].ssh_login_info["key_file"]), DB_SSH_KEY)
 
     def setUp(self):
