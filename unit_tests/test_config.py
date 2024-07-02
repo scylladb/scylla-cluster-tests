6821475b73a5c504803a120943480772f6d46c1c
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2024-07-02 23:09:46 +0300

    ci(latte): bump latte version to '0.26.1-scylladb'
    Use the most recent version of the latte tool - '0.26.1-scylladb'.
    Details: https://github.com/scylladb/latte/releases/tag/0.26.1-scylladb

--- unit_tests/test_config.py
+++ unit_tests/test_config.py
@@ -666,7 +666,7 @@ class ConfigurationTests(unittest.TestCase):  # pylint: disable=too-many-public-
                           'ycsb': 'scylladb/something_else',
                           'kcl': 'scylladb/hydra-loaders:kcl-jdk8-20210526-ShardSyncStrategyType-PERIODIC',
                           'harry': 'scylladb/hydra-loaders:cassandra-harry-jdk11-20220816',
-                          'latte': 'scylladb/hydra-loaders:latte-0.25.2-scylladb',
+                          'latte': 'scylladb/hydra-loaders:latte-0.26.1-scylladb',
                           'cql-stress-cassandra-stress': 'docker.io/scylladb/hydra-loaders:cql-stress-cassandra-stress-20240606'})
 
         self.assertEqual(conf.get('stress_image.gemini'), 'scylladb/hydra-loaders:gemini-v1.8.6')
