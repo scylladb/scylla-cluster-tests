d01670f4eafc68c69527f471ca1c02bb7e016fad
Israel Fruchter <fruch@scylladb.com>
2024-07-02 18:23:32 +0300

    feature(remoter): execuation of long running command
    cause we have issues with long running ssh commands that we can retry,
    if node is a bit load at the time of the operation we might get disconnected,
    and fail the command complectly from SCT end.
    
    in this change we introduce a way to run the command in the background while
    logging stdout, stderr, and exit code.
    
    and poll for the finish of the process by its pid
    
    in the end we'll collect all the logs, and retry results as if it with a
    regular remoter execution
    
    Fixes: #7781
    (cherry picked from commit 2115e8ba6e711424f967397200886ff93c6b4070)

---
+++ unit_tests/test_remote_long_running.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import pytest
+
+from sdcm.remote import LOCALRUNNER
+from sdcm.remote.remote_long_running import run_long_running_cmd
+from sdcm.remote.libssh2_client import UnexpectedExit
+
+
+@pytest.fixture(scope='function', autouse=True)
+def fixture_check_tmp_files_cleared():
+    yield
+    assert list(Path('/tmp').glob('remoter_*')) == []
+
+
+def test_long_command_failing():
+    with pytest.raises(UnexpectedExit,  match=r'.*Exit code: 127.*') as exc_info:
+        run_long_running_cmd(LOCALRUNNER, cmd='sleep 1 && bbb', timeout=100)
+
+    assert exc_info.value.result.command == 'sleep 1 && bbb'
+    assert "line 1: bbb: command not found" in exc_info.value.result.stderr
+    assert exc_info.value.result.stdout == ""
+    assert exc_info.value.result.exited == 127
+    assert exc_info.value.result.return_code == 127
+    assert exc_info.value.result.duration > 1
+
+
+def test_long_command_success():
+    result = run_long_running_cmd(LOCALRUNNER, cmd="sleep 1 && echo 'cmd is done'", timeout=100)
+    assert result.ok
+    assert result.stdout == "cmd is done\n"
+    assert result.stderr == ""
+    assert result.exited == 0
+    assert result.return_code == 0
+    assert result.duration > 1
