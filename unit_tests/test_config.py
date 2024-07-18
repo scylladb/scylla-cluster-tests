547e37833b11962b44616b20d7835497d7232082
Lukasz Sojka <lukasz.sojka@scylladb.com>
2024-07-18 12:29:43 +0200

    fix(unit-test): fix nested config test
    `test_21_nested_values` test fails whenever we change loader image.
    
    Fix by verifying only constant images overriden by test.

--- unit_tests/test_config.py
+++ unit_tests/test_config.py
@@ -655,21 +655,11 @@ class ConfigurationTests(unittest.TestCase):  # pylint: disable=too-many-public-
 
         conf = sct_config.SCTConfiguration()
         conf.verify_configuration()
-        self.assertEqual(conf.get('stress_image'),
-                         {'alternator-dns': 'scylladb/hydra-loaders:alternator-dns-0.1',
-                          'cassandra-stress': 'scylla-bench',
-                          'cdc-stresser': 'scylladb/hydra-loaders:cdc-stresser-A',
-                          'gemini': 'scylladb/hydra-loaders:gemini-v1.8.6',
-                          'ndbench': 'scylladb/hydra-loaders:ndbench-jdk8-A',
-                          'nosqlbench': 'scylladb/hydra-loaders:nosqlbench-A',
-                          'scylla-bench': 'scylladb/something',
-                          'ycsb': 'scylladb/something_else',
-                          'kcl': 'scylladb/hydra-loaders:kcl-jdk8-20210526-ShardSyncStrategyType-PERIODIC',
-                          'harry': 'scylladb/hydra-loaders:cassandra-harry-jdk11-20220816',
-                          'latte': 'scylladb/hydra-loaders:latte-0.26.1-scylladb',
-                          'cql-stress-cassandra-stress': 'docker.io/scylladb/hydra-loaders:cql-stress-cassandra-stress-20240606'})
+        stress_image = conf.get('stress_image')
+        self.assertEqual(stress_image['ndbench'], 'scylladb/hydra-loaders:ndbench-jdk8-A')
+        self.assertEqual(stress_image['nosqlbench'], 'scylladb/hydra-loaders:nosqlbench-A')
 
-        self.assertEqual(conf.get('stress_image.gemini'), 'scylladb/hydra-loaders:gemini-v1.8.6')
+        self.assertEqual(conf.get('stress_image.scylla-bench'), 'scylladb/something')
         self.assertEqual(conf.get('stress_image.non-exist'), None)
 
         self.assertEqual(conf.get('stress_read_cmd'), ['cassandra_stress', 'cassandra_stress'])
