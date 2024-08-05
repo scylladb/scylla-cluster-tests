a985feca3c590af21f4d81d258bcc9d149e0da4f
Israel Fruchter <fruch@scylladb.com>
2024-08-05 22:05:20 +0300

    fix(rolling-upgrade): remove branch filter
    this filter was assuming enterprise repo url, would have `enterprise`
    in them, that is not completly correct, the offical releases urls
    don't have `enterprise` in them.
    
    regardless the code nowdays return the exact list of version needed
    for upgrade, and there no need for this filtering.
    (that was introduce in 2019, by fruch)
    
    (cherry picked from commit 2f99132e8fd69ee289327c219c1f9e6fdf6372de)

--- vars/supportedUpgradeFromVersions.groovy
+++ vars/supportedUpgradeFromVersions.groovy
@@ -14,21 +14,6 @@ def call(List base_versions_list, String linux_distro, String new_scylla_repo, S
         println("Did not find a valid base versions string!")
             throw new Exception("Didn't get valid base versions automatically!")
         }
-
     }
-    if (new_scylla_repo.contains('enterprise')) {
-        return base_versions_list
-    } else {
-        return base_versions_list.findAll{ ! is_enterprise_version(it) }
-    }
-}
-
-def is_enterprise_version(String version) {
-    def first_version = 0
-    try {
-        first_version = version.tokenize('.')[0] as Integer
-    } catch (java.lang.NumberFormatException ex) {
-        println "WARN: non formal/versioned branch '$version', assuming opensource version"
-    }
-    return first_version > 2000
+    return base_versions_list
 }
