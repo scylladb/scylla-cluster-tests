e6a2eec3b0af189d8b18f58075c746e47e1c2f1d
Israel Fruchter <fruch@scylladb.com>
2024-08-06 22:36:51 +0300

    feature(pipelines): read extra environment variables from text paramter
    introducing a way to inject specific `SCT_*` environment variables,
    so we can configure a job with any sct configuration that is usable
    via environment variables, without creating pipeline parameter for
    each one.
    
    we'll need this to pass in docker images to jobs, in the flow of
    drivers CI
    
    (cherry picked from commit 5a6c5f555c57b175ffc437ce5f4cd5c891da6742)

--- vars/longevityPipeline.groovy
+++ vars/longevityPipeline.groovy
@@ -150,7 +150,14 @@ def call(Map pipelineParams) {
             string(defaultValue: '',
                    description: 'Actual user requesting job start, for automated job builds (e.g. through Argus)',
                    name: 'requested_by_user')
-
+            text(defaultValue: "${pipelineParams.get('extra_environment_variables', '')}",
+                    description: (
+                        'Extra environment variables to be set in the test environment, uses the java Properties File Format.\n' +
+                        'Example:\n' +
+                        '\tSCT_STRESS_IMAGE.cassandra-stress=scylladb/cassandra-stress:3.12.1\n' +
+                        '\tSCT_USE_MGMT=false'
+                        ),
+                    name: 'extra_environment_variables')
             // NOTE: Optional parameters for BYO ScyllaDB stage
             string(defaultValue: '',
                    description: (
@@ -196,6 +203,7 @@ def call(Map pipelineParams) {
                 steps {
                     script {
                         completed_stages = [:]
+                        loadEnvFromString(params.extra_environment_variables)
                     }
                     dir('scylla-cluster-tests') {
                         timeout(time: 5, unit: 'MINUTES') {
