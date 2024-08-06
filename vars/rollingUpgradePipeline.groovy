d5851ca0c5ece6bdd96d394f13fc1c7169dd82d3
Israel Fruchter <fruch@scylladb.com>
2024-08-06 22:36:51 +0300

    feature(pipelines): read extra environment variables from text paramter
    introducing a way to inject specific `SCT_*` environment variables,
    so we can configure a job with any sct configuration that is usable
    via environment variables, without creating pipeline parameter for
    each one.
    
    we'll need this to pass in docker images to jobs, in the flow of
    drivers CI
    
    (cherry picked from commit 5a6c5f555c57b175ffc437ce5f4cd5c891da6742)

--- vars/rollingUpgradePipeline.groovy
+++ vars/rollingUpgradePipeline.groovy
@@ -124,6 +124,7 @@ def call(Map pipelineParams) {
                         timeout(time: 10, unit: 'MINUTES') {
                             script {
                                 wrap([$class: 'BuildUser']) {
+                                    loadEnvFromString(params.extra_environment_variables)
                                     dir('scylla-cluster-tests') {
                                         checkout scm
                                         checkoutQaInternal(params)
@@ -196,6 +197,7 @@ def call(Map pipelineParams) {
                                             catchError(stageResult: 'FAILURE') {
                                                 timeout(time: 5, unit: 'MINUTES') {
                                                     script {
+                                                        loadEnvFromString(params.extra_environment_variables)
                                                         wrap([$class: 'BuildUser']) {
                                                             dir('scylla-cluster-tests') {
                                                                 checkout scm
