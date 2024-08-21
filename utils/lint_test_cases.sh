9b9f10e88b0e41076ddf26061e4eb55dd92b8784
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2024-08-21 19:40:08 +0300

    test(customd2): add custom_d2/workload1 test configurations
    First configuration is "small" - 3 DCs, 3 nodes per each.
    Second configuration is "big" - 5DCs, 12 nodes per each.
    
    Ref: #19131

--- utils/lint_test_cases.sh
+++ utils/lint_test_cases.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 OUT=0
-SCT_AMI_ID_DB_SCYLLA=ami-1234 ./sct.py lint-yamls -i '.yaml' -e 'azure,multi-dc,multiDC,multidc,multiple-dc,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,jepsen,repair-based-operations,add-new-dc,baremetal'
+SCT_AMI_ID_DB_SCYLLA=ami-1234 ./sct.py lint-yamls -i '.yaml' -e 'azure,multi-dc,multiDC,multidc,multiple-dc,3dcs,5dcs,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,jepsen,repair-based-operations,add-new-dc,baremetal'
 OUT=$(($OUT + $?))
 
 SCT_AZURE_IMAGE_DB=image SCT_AZURE_REGION_NAME="eastus" ./sct.py lint-yamls --backend azure -i azure
@@ -15,7 +15,11 @@ SCT_GCE_IMAGE_DB=image SCT_GCE_DATACENTER="us-east1 us-west1" SCT_SCYLLA_REPO='h
 OUT=$(($OUT + $?))
 
 echo "multi dc yamls with 3 regions"
-SCT_GCE_IMAGE_DB=image SCT_GCE_DATACENTER="us-east1 us-west1 eu-north1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i '24h-multidc,large-cluster,counters-multidc,cdc-8h-multi-dc' -e 'docker,azure,shutdown'
+SCT_GCE_IMAGE_DB=image SCT_GCE_DATACENTER="us-east1 us-west1 eu-north1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i '3dcs,24h-multidc,large-cluster,counters-multidc,cdc-8h-multi-dc' -e 'docker,azure,shutdown'
+OUT=$(($OUT + $?))
+
+echo "multi dc yamls with 5 regions"
+SCT_AMI_ID_DB_SCYLLA="ami-1 ami-2 ami-3 ami-4 ami-5" SCT_REGION_NAME="eu-central-1 eu-north-1 eu-west-1 eu-west-2 us-east-1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b aws -i '5dcs' -e 'docker,azure,shutdown'
 OUT=$(($OUT + $?))
 
 
