9b6179249febd0c3219393d5ce00f1910c1a465a
Lukasz Sojka <lukasz.sojka@scylladb.com>
2024-08-28 15:53:40 +0200

    improvement(cleanup): update Argus on cloud cleanup
    When cleaning instances with cloud cleanup scripts, Argus is not
    updated. This makes `Resources` tab in Argus outdated and may mislead
    users.
    
    Update Argus when periodical cleanup script is triggering machine
    termination/stop.
    
    Closes: https://github.com/scylladb/scylla-cluster-tests/issues/8299
    (cherry picked from commit 68906cf17c7be4e78e2d50a3347eb367d0915368)

--- unit_tests/provisioner/test_azure_region_definition_builder.py
+++ unit_tests/provisioner/test_azure_region_definition_builder.py
@@ -28,13 +28,17 @@ def test_can_create_basic_scylla_instance_definition_from_sct_config():
                            ["SCT_CLUSTER_BACKEND", "SCT_TEST_ID", "SCT_CONFIG_FILES", "SCT_AZURE_REGION_NAME",
                             "SCT_N_DB_NODES", "SCT_USER_PREFIX",
                             "SCT_AZURE_IMAGE_DB", "SCT_N_LOADERS", "SCT_N_MONITORS_NODES"])
+
+    test_id = "3923f974-bf0e-4c3c-9f52-3f6473b8a0b6"
+    test_config = TestConfig()
+    test_config.set_test_id_only(test_id)
     env_config = EnvConfig(
         SCT_CLUSTER_BACKEND="azure",
-        SCT_TEST_ID="3923f974-bf0e-4c3c-9f52-3f6473b8a0b6",
+        SCT_TEST_ID=test_config.test_id(),
         SCT_CONFIG_FILES=f'["{Path(__file__).parent.absolute()}/azure_default_config.yaml"]',
         SCT_AZURE_REGION_NAME="['eastus', 'easteu']",
         SCT_N_DB_NODES="3 1",
-        SCT_USER_PREFIX="unit-test",
+        SCT_USER_PREFIX="unit",
         SCT_AZURE_IMAGE_DB="/subscriptions/6c268694-47ab-43ab-b306-3c5514bc4112/resourceGroups/scylla-images/providers/"
                            "Microsoft.Compute/images/scylla-5.2.0-dev-x86_64-2022-08-22T04-18-36Z",
         SCT_N_LOADERS="2 0",
@@ -46,12 +50,11 @@ def test_can_create_basic_scylla_instance_definition_from_sct_config():
     tags = TestConfig.common_tags()
     ssh_key = KeyStore().get_gce_ssh_key_pair()
     prefix = config.get('user_prefix')
-    test_config = TestConfig()
-    test_config.set_test_id_only(env_config.SCT_TEST_ID)
     builder = region_definition_builder.get_builder(params=config, test_config=test_config)
     region_definitions = builder.build_all_region_definitions()
 
-    instance_definition = InstanceDefinition(name=f"{prefix}-db-node-3923f974-eastus-1", image_id=env_config.SCT_AZURE_IMAGE_DB,
+    instance_definition = InstanceDefinition(name=f"{prefix}-db-node-{test_config.test_id()[:8]}-eastus-1",
+                                             image_id=env_config.SCT_AZURE_IMAGE_DB,
                                              type="Standard_L8s_v3", user_name="scyllaadm", root_disk_size=30,
                                              tags=tags | {"NodeType": "scylla-db", "keep_action": "",
                                                           'NodeIndex': '1', "TestId": test_config.test_id()},
