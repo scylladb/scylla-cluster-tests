eda4e6e000e496672f3d9b015b5b77b3f2631436
Lukasz Sojka <lukasz.sojka@scylladb.com>
2024-08-28 15:19:05 +0200

    fix(azure): fix resources names
    Azure instances names differ from `node.name` values. This causes
    discrepancy between resources names between Argus-Azure. While in
    testing it does not cause problems, updating resources status in Argus
    raises error about missing resource name (because cleanup scripts base
    on Azure names).
    
    (cherry picked from commit f401161ccb7f22a5b23dcfc5eed38e22ad28a9d0)

--- unit_tests/provisioner/test_azure_region_definition_builder.py
+++ unit_tests/provisioner/test_azure_region_definition_builder.py
@@ -28,13 +28,17 @@ def test_can_create_basic_scylla_instance_definition_from_sct_config():
                            ["SCT_CLUSTER_BACKEND", "SCT_TEST_ID", "SCT_CONFIG_FILES", "SCT_AZURE_REGION_NAME",
                             "SCT_N_DB_NODES", "SCT_USER_PREFIX",
                             "SCT_AZURE_IMAGE_DB", "SCT_N_LOADERS", "SCT_N_MONITORS_NODES"])
+
+    test_id = "3923f974-bf0e-4c3c-9f52-3f6473b8a0b6"
+    test_config = TestConfig()
+    test_config.set_test_id_only(test_id)
     env_config = EnvConfig(
         SCT_CLUSTER_BACKEND="azure",
-        SCT_TEST_ID="example_test_id",
+        SCT_TEST_ID=test_config.test_id(),
         SCT_CONFIG_FILES=f'["{Path(__file__).parent.absolute()}/azure_default_config.yaml"]',
         SCT_AZURE_REGION_NAME="['eastus', 'easteu']",
         SCT_N_DB_NODES="3 1",
-        SCT_USER_PREFIX="unit-test",
+        SCT_USER_PREFIX="unit",
         SCT_AZURE_IMAGE_DB="/subscriptions/6c268694-47ab-43ab-b306-3c5514bc4112/resourceGroups/scylla-images/providers/"
                            "Microsoft.Compute/images/scylla-5.2.0-dev-x86_64-2022-08-22T04-18-36Z",
         SCT_N_LOADERS="2 0",
@@ -46,18 +50,19 @@ def test_can_create_basic_scylla_instance_definition_from_sct_config():
     tags = TestConfig.common_tags()
     ssh_key = KeyStore().get_gce_ssh_key_pair()
     prefix = config.get('user_prefix')
-    test_config = TestConfig()
     builder = region_definition_builder.get_builder(params=config, test_config=test_config)
     region_definitions = builder.build_all_region_definitions()
 
-    instance_definition = InstanceDefinition(name=f"{prefix}-db-node-eastus-1", image_id=env_config.SCT_AZURE_IMAGE_DB,
+    instance_definition = InstanceDefinition(name=f"{prefix}-db-node-{test_config.test_id()[:8]}-eastus-1",
+                                             image_id=env_config.SCT_AZURE_IMAGE_DB,
                                              type="Standard_L8s_v3", user_name="scyllaadm", root_disk_size=30,
-                                             tags=tags | {"NodeType": "scylla-db", "keep_action": "", 'NodeIndex': '1'},
+                                             tags=tags | {"NodeType": "scylla-db", "keep_action": "",
+                                                          'NodeIndex': '1', "TestId": test_config.test_id()},
                                              ssh_key=ssh_key)
     assert len(region_definitions) == 2
     actual_region_definition = region_definitions[0]
 
-    assert actual_region_definition.test_id == env_config.SCT_TEST_ID
+    assert actual_region_definition.test_id == test_config.test_id()
     assert actual_region_definition.backend == "azure"
     assert actual_region_definition.region == "eastus"
     # ignoring user_data in this validation
