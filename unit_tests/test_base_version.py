0784935939b0144e742eabcaf75b4b867496add8
Israel Fruchter <fruch@scylladb.com>
2024-09-10 16:45:59 +0300

    fix(get_all_versions_from_centos_repository): limit only to scylla packages
    since now scylla-doctor was introduced into scylla repositories
    we don't want to retrive it's version, only the scylla versions
    
    if and when we'll need to get it's version, we'll need to refactor
    this code to supply list of package we'll want to look at.
    
    (cherry picked from commit 7cf3f2d156877b9d0c406ff5a6fce9424addca15)

--- unit_tests/test_base_version.py
+++ unit_tests/test_base_version.py
@@ -116,6 +116,12 @@ class TestBaseVersion(unittest.TestCase):
         version_list = general_test(scylla_repo, linux_distro)
         assert set(version_list) == {'5.4', '2022.2', '2023.1', '2024.1'}
 
+    def test_2024_2_ubuntu(self):
+        scylla_repo = self.url_base + '-enterprise/enterprise-2024.2/deb/unified/latest/scylladb-2024.2/scylla.list'
+        linux_distro = 'ubuntu-focal'
+        version_list = general_test(scylla_repo, linux_distro)
+        assert set(version_list) == {'6.0', '2024.1'}
+
 
 if __name__ == "__main__":
     unittest.main()
