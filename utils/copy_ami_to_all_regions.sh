d06172574f67274a0ecde1c9d45a08a03deaa30c
Israel Fruchter <fruch@scylladb.com>
2024-09-10 20:53:25 +0300

    fix(loader): update loader images to `scylla-qa-loader-ami-v22-ubuntu22`
    seems like the loaders were missing the `PREPARED-LOADER` file, to really
    stop installing all bunch of unneeded packages, which are already should
    be in place, and in some cases might lead to errors trying related to identify
    the correct scylla product
    
    (cherry picked from commit 05d619dfa3056595edf9d8e9d2511316636dedfb)

--- utils/copy_ami_to_all_regions.sh
+++ utils/copy_ami_to_all_regions.sh
@@ -1,10 +1,15 @@
-AMI_ID=ami-0c4e5a2331f97fd0d
-AMI_NAME='scylladb-monitor-4-6-2-2024-02-13t08-06-04z'
-SOURCE_REGION=us-east-1
-TARGET_REGIONS='us-west-2 eu-west-1 eu-west-2 eu-north-1 eu-central-1'
+AMI_ID="ami-0d93dc84c27b5864c"
+AMI_NAME="scylla-qa-loader-ami-v22-ubuntu22"
+SOURCE_REGION="us-east-1"
+TARGET_REGIONS="us-west-2 eu-west-1 eu-west-2 eu-north-1 eu-central-1"
 
-for target_region in $TARGET_REGIONS
-do
-	echo "copy AMI to $target_region"
-	aws ec2 copy-image --region $target_region  --name "$AMI_NAME" --source-region $SOURCE_REGION --source-image-id $AMI_ID
+# Retrieve the tags of the source AMI
+TAGS=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$AMI_ID" --region $SOURCE_REGION --query "Tags[*].{Key:Key,Value:Value}")
+
+for target_region in $TARGET_REGIONS; do
+    echo "Copying AMI to $target_region"
+    NEW_AMI_ID=$(aws ec2 copy-image --region "${target_region}" --name "$AMI_NAME" --source-region $SOURCE_REGION --source-image-id $AMI_ID --query 'ImageId' --output text)
+
+    echo "Applying tags to the new AMI in $target_region"
+    aws ec2 create-tags --resources "${NEW_AMI_ID}" --tags "$TAGS" --region "${target_region}"
 done
