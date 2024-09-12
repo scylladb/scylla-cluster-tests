0d909cab966d4abbab57c656f81306ac56d9ab01
Israel Fruchter <fruch@scylladb.com>
2024-09-12 08:34:33 +0300

    fix(oel8): new oel8 image was missing tar
    cause of it missing, offline installer test ontop of it failed
    
    (cherry picked from commit 1dd8c290ff6d6e65110960b31bb25abf4b2ef961)

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -1982,7 +1982,7 @@ class BaseNode(AutoSshContainerMixin):  # pylint: disable=too-many-instance-attr
         if self.distro.is_amazon2:
             self.remoter.sudo('amazon-linux-extras install java-openjdk11')
         elif self.distro.is_rhel_like:
-            self.install_package(package_name='java-11-openjdk-headless')
+            self.install_package(package_name='java-11-openjdk-headless tar')
         elif self.distro.is_sles:
             raise Exception("Offline install on SLES isn't supported")
         elif self.distro.is_debian:
