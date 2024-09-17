dc485920e54c4de5fd1f061c04e3d90898555045
Julia Yakovlev <julia.yakovlev@scylladb.com>
2024-09-17 18:21:43 +0300

    test(performance): add gradual test
    This PR presents new performance test that run gradual increased throughput steps.
    The test run steps with different throughput.
    Throughput of every step is fixed and defined hardcoded according to the load type (write, read
    and mixed). Last step is untrotteled.
    Latency for every step is received from cassandra-stress HDR file and reported in Argus and email.
    This test will allow us to replace both the simple latency test and the throughput test with less
    flakiness.
    
    (cherry picked from commit fc8d3d100130fd5bbb733c73edde3cd0b6ca1c48)

--- performance_regression_gradual_grow_throughput.py
+++ performance_regression_gradual_grow_throughput.py
@@ -1,10 +1,16 @@
 import os
+import pathlib
+import time
 from enum import Enum
 from collections import defaultdict
 
 import json
 from performance_regression_test import PerformanceRegressionTest
+from sdcm.sct_events import Severity
+from sdcm.sct_events.system import TestFrameworkEvent
 from sdcm.results_analyze import ThroughputLatencyGradualGrowPayloadPerformanceAnalyzer
+from sdcm.utils.decorators import latency_calculator_decorator
+from sdcm.utils.latency import calculate_latency, analyze_hdr_percentiles
 
 
 class CSPopulateDistribution(Enum):
@@ -12,31 +18,13 @@ class CSPopulateDistribution(Enum):
     UNIFORM = "uniform"
 
 
-class PerformanceRegressionGradualGrowThroughutTest(PerformanceRegressionTest):  # pylint: disable=too-many-instance-attributes
-
+class PerformanceRegressionGradualGrowThroughputTest(PerformanceRegressionTest):  # pylint: disable=too-many-instance-attributes
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # all parameters were taken from scylla-stress-orch repo
         # Planned data size is 3 TB in total: 1tb per node
-        self.NUM_THREADS = 500  # pylint: disable=invalid-name
         self.CLUSTER_SIZE = self.params.get("n_db_nodes")  # pylint: disable=invalid-name
         self.REPLICATION_FACTOR = 3  # pylint: disable=invalid-name
-        # Planned data size is 3 TB in total: 1tb per node
-        self.DATASET_SIZE = 3000 * 1024 * 1024 * 1024  # pylint: disable=invalid-name
-        # expected row size (was calculated by dev team ~313 bytes)
-        self.ROW_SIZE_BYTES = 210 * 1024 * 1024 * 1024 / 720_000_000  # pylint: disable=invalid-name
-        self.ROW_COUNT = int(self.DATASET_SIZE / self.ROW_SIZE_BYTES /  # pylint: disable=invalid-name
-                             self.REPLICATION_FACTOR)
-        # How many standard deviations should span the cluster's memory
-        self.CONFIDENCE = 6  # pylint: disable=invalid-name
-        # instance i3.4xlarge
-        self.INSTANCE_MEMORY_GB = 122  # pylint: disable=invalid-name
-        self.GAUSS_CENTER = self.ROW_COUNT // 2  # pylint: disable=invalid-name
-        self.GAUSS_SIGMA = int(self.INSTANCE_MEMORY_GB * self.CLUSTER_SIZE * 1024 * 1024 *  # pylint: disable=invalid-name
-                               1024 // (self.CONFIDENCE * self.ROW_SIZE_BYTES * self.REPLICATION_FACTOR))
-
-        self.MAX_95TH_LATENCY = 20.0  # pylint: disable=invalid-name
-        self.MAX_99TH_LATENCY = 400.0  # pylint: disable=invalid-name
 
     def test_mixed_gradual_increase_load(self):  # pylint: disable=too-many-locals
         """
@@ -46,7 +34,8 @@ class PerformanceRegressionGradualGrowThroughutTest(PerformanceRegressionTest): 
         2. Run a mixed workload with gradual increase load
         """
         self._base_test_workflow(cs_cmd_tmpl=self.params.get('stress_cmd_m'),
-                                 test_name="Test 'mixed: read:50%,write:50%'")
+                                 test_name="test_mixed_gradual_increase_load (read:50%,write:50%)",
+                                 sub_type="mixed")
 
     def test_write_gradual_increase_load(self):  # pylint: disable=too-many-locals
         """
@@ -56,7 +45,8 @@ class PerformanceRegressionGradualGrowThroughutTest(PerformanceRegressionTest): 
         2. Run a write workload with gradual increase load
         """
         self._base_test_workflow(cs_cmd_tmpl=self.params.get('stress_cmd_w'),
-                                 test_name="Test 'write 100%'")
+                                 test_name="test_write_gradual_increase_load (100% writes)",
+                                 sub_type="write")
 
     def test_read_gradual_increase_load(self):  # pylint: disable=too-many-locals
         """
@@ -66,44 +56,29 @@ class PerformanceRegressionGradualGrowThroughutTest(PerformanceRegressionTest): 
         2. Run a read workload with gradual increase load
         """
         self._base_test_workflow(cs_cmd_tmpl=self.params.get('stress_cmd_r'),
-                                 test_name="Test 'read 100%'")
+                                 test_name="test_read_gradual_increase_load (100% reads)",
+                                 sub_type="read")
 
-    def _base_test_workflow(self, cs_cmd_tmpl, test_name):
+    def _base_test_workflow(self, cs_cmd_tmpl, test_name, sub_type):
         stress_num = 1
         num_loaders = len(self.loaders.nodes)
         self.run_fstrim_on_all_db_nodes()
         # run a write workload as a preparation
         compaction_strategy = self.params.get('compaction_strategy')
-        self.preload_data(compaction_strategy=compaction_strategy)
-        self.wait_no_compactions_running(n=400, sleep_time=120)
+        if sub_type in ["mixed", "read"]:
+            self.preload_data(compaction_strategy=compaction_strategy)
+            self.wait_no_compactions_running(n=400, sleep_time=120)
+            self.run_fstrim_on_all_db_nodes()
 
-        self.run_fstrim_on_all_db_nodes()
-        # self.disable_autocompaction_on_all_nodes()
         self.run_gradual_increase_load(stress_cmd_templ=cs_cmd_tmpl,
-                                       start_ops=self.start_ops,
-                                       throttle_step=self.throttle_step,
-                                       max_ops=self.max_ops,
                                        stress_num=stress_num,
                                        num_loaders=num_loaders,
                                        compaction_strategy=compaction_strategy,
-                                       test_name=test_name)
+                                       test_name=test_name,
+                                       sub_type=sub_type)
 
     def preload_data(self, compaction_strategy=None):
-        prepare_write_tmpl: str = self.params.get("prepare_write_cmd")
-        num_of_loaders: int = self.params.get("n_loaders")
-        row_count_per_loader: int = self.ROW_COUNT // num_of_loaders
-        range_points = [1]
-
-        for i in range(num_of_loaders):
-            range_points.append(range_points[-1] + row_count_per_loader)
-        range_points[-1] = self.ROW_COUNT
-
-        population_commands = []
-        for i in range(len(range_points) - 1):
-            cmd = prepare_write_tmpl.replace(
-                "$ROW_NUMBER", f"{range_points[i + 1] - range_points[i] + 1}").replace(
-                "$SEQUENCE", f"{range_points[i]}..{range_points[i + 1]}")
-            population_commands.append(cmd)
+        population_commands: list = self.params.get("prepare_write_cmd")
 
         self.log.info("Population c-s commands: %s", population_commands)
         # Check if it should be round_robin across loaders
@@ -128,34 +103,79 @@ class PerformanceRegressionGradualGrowThroughutTest(PerformanceRegressionTest): 
 
         self.log.info("Dataset has been populated")
 
+    def check_latency_during_steps(self, step):
+        with open(self.latency_results_file, encoding="utf-8") as file:
+            latency_results = json.load(file)
+        self.log.debug('Step %s: latency_results were loaded from file %s and its result is %s',
+                       step, self.latency_results_file, latency_results)
+        if latency_results and self.create_stats:
+            latency_results[step]["step"] = step
+            latency_results[step] = calculate_latency(latency_results[step])
+            latency_results = analyze_hdr_percentiles(latency_results)
+            pathlib.Path(self.latency_results_file).unlink()
+            self.log.debug('collected latency values are: %s', latency_results)
+            self.update({"latency_during_ops": latency_results})
+            return latency_results
+
+    def run_step(self, stress_cmds, current_throttle, num_threads):
+        results = []
+        stress_queue = []
+        for stress_cmd in stress_cmds:
+            params = {"round_robin": True, "stats_aggregate_cmds": False}
+            stress_cmd_to_run = stress_cmd.replace(
+                "$threads", f"{num_threads}").replace("$throttle", f"{current_throttle}")
+            params.update({'stress_cmd': stress_cmd_to_run})
+            # Run all stress commands
+            self.log.debug('RUNNING stress cmd: %s', stress_cmd_to_run)
+            stress_queue.append(self.run_stress_thread(**params))
+
+        for stress in stress_queue:
+            results.extend(self.get_stress_results(queue=stress, store_results=False))
+            self.log.debug("One c-s command results: %s", results[-1])
+        return results
+
+    def drop_keyspace(self):
+        self.log.debug(f'Drop keyspace {"keyspace1"}')
+        with self.db_cluster.cql_connection_patient(self.db_cluster.nodes[0]) as session:
+            session.execute(f'DROP KEYSPACE IF EXISTS {"keyspace1"};')
+
     # pylint: disable=too-many-arguments,too-many-locals
-    def run_gradual_increase_load(self, stress_cmd_templ,
-                                  start_ops, max_ops, throttle_step,
-                                  stress_num, num_loaders, compaction_strategy, test_name):
-        self.warmup_cache(compaction_strategy)
-        total_summary = []
-        cs_popuplation_distribution = self.get_cs_distribution()
-        base_stress_cmd = stress_cmd_templ[0].replace("$DIST_PARAMS", cs_popuplation_distribution)
+    def run_gradual_increase_load(self, stress_cmd_templ, stress_num, num_loaders, compaction_strategy, test_name, sub_type):  # noqa: PLR0914
+        if sub_type in ["read", "mixed"]:
+            self.warmup_cache(stress_cmd_templ)
 
-        for current_ops in range(start_ops, max_ops + throttle_step, throttle_step):
-            self.log.info("Run cs command with rate: %s Kops", current_ops)
-            current_throttle = current_ops // (num_loaders * stress_num)
-            stress_cmd = base_stress_cmd.replace("$threads", f"{self.NUM_THREADS}").replace(
-                "$throttle", f"{current_throttle}")
+        if not self.exists():
+            self.log.debug("Create test statistics in ES")
+            self.create_test_stats(sub_type=sub_type, doc_id_with_timestamp=True)
+        total_summary = {}
+        num_threads = self.params["perf_gradual_threads"][sub_type]
+        throttle_steps = self.params["perf_gradual_throttle_steps"]
+        if sub_type not in throttle_steps:
+            TestFrameworkEvent(source=self.__class__.__name__,
+                               message=f"Throttle steps for '{sub_type}' test is not defined in 'perf_gradual_throttle_steps' parameter",
+                               severity=Severity.CRITICAL).publish()
 
-            stress_queue = self.run_stress_thread(stress_cmd=stress_cmd, stress_num=stress_num,
-                                                  compaction_strategy=compaction_strategy, stats_aggregate_cmds=False)
-            results = self.get_stress_results(queue=stress_queue, store_results=False)
-            self.log.debug("All c-s results: %s", results)
-            summary_result = self._calculate_average_latency(results)
-            summary_result["ops"] = current_ops
-            self.log.debug("C-S results for ops: %s. \n Results: \n %s", current_ops, summary_result)
-            total_summary.append(summary_result)
-            if (summary_result["latency 95th percentile"] > self.MAX_95TH_LATENCY or
-                    summary_result["latency 99th percentile"] > self.MAX_99TH_LATENCY):
+        for throttle_step in throttle_steps[sub_type]:
+            self.log.info("Run cs command with rate: %s Kops", throttle_step)
+            current_throttle = f"fixed={int(int(throttle_step) // (num_loaders * stress_num))}/s" if throttle_step != "unthrottled" else ""
+            run_step = ((latency_calculator_decorator(legend=f"Gradual test step {throttle_step} op/s",
+                                                      cycle_name=throttle_step))(self.run_step))
+            results = run_step(stress_cmds=stress_cmd_templ, current_throttle=current_throttle,
+                               num_threads=num_threads)
 
-                self.log.warning("Latency 95th percentile is large that %d", self.MAX_95TH_LATENCY)
-                break
+            calculate_result = self._calculate_average_max_latency(results)
+            self.update_test_details(scylla_conf=True)
+            summary_result = self.check_latency_during_steps(step=throttle_step)
+            summary_result[throttle_step].update({"ops_rate": calculate_result["op rate"] * num_loaders})
+            self.log.debug("summary_result: %s", summary_result)
+            total_summary.update(summary_result)
+            if sub_type == "write":
+                self.drop_keyspace()
+            # We want 3 minutes (180 sec) wait between steps.
+            # In case of "write" / "mixed" workflow - wait for compactions finished.
+            # In case of "read" workflow -  it just will wait for 3 minutes
+            if sub_type != "write" and (wait_time := self.wait_no_compactions_running()[0]) < 180:
+                time.sleep(180 - wait_time)
 
         total_summary_json = json.dumps(total_summary, indent=4, separators=(", ", ": "))
         self.log.debug("---------------------------------")
@@ -182,45 +202,67 @@ class PerformanceRegressionGradualGrowThroughutTest(PerformanceRegressionTest): 
             "job_url": os.environ.get("BUILD_URL"),
             "shard_aware_driver": self.is_shard_awareness_driver,
         }
-
         perf_analyzer = ThroughputLatencyGradualGrowPayloadPerformanceAnalyzer(
             es_index=self._test_index,
             es_doc_type=self._es_doc_type,
             email_recipients=self.params.get('email_recipients'))
-        perf_analyzer.check_regression(test_name=test_name, test_results=total_summary, test_details=setup_details)
-
-    def get_cs_distribution(self):
-        popuplation_distribution = CSPopulateDistribution(self.params.get("cs_populating_distribution"))
-        if popuplation_distribution == CSPopulateDistribution.GAUSS:
-            return f"GAUSSIAN(1..{self.ROW_COUNT},{self.GAUSS_CENTER},{self.GAUSS_SIGMA})"
-        elif popuplation_distribution == CSPopulateDistribution.UNIFORM:
-            return f"UNIFORM(1..{self.ROW_COUNT})"
-        else:
-            self.log.error("Unsupported cs population distribution")
-            return ""
+        # Keep next 3 lines for debug purpose
+        self.log.debug("es_index: %s", self._test_index)
+        self.log.debug("total_summary: %s", total_summary)
+        self.log.debug("setup_details: %s", setup_details)
+        is_gce = bool(self.params.get('cluster_backend') == 'gce')
+        try:
+            perf_analyzer.check_regression(test_id=self.test_id,
+                                           data=total_summary,
+                                           is_gce=is_gce,
+                                           email_subject_postfix=self.params.get('email_subject_postfix'))
+        except Exception as exc:  # noqa: BLE001
+            TestFrameworkEvent(
+                message='Failed to check regression',
+                source=self.__class__.__name__,
+                source_method='check_regression',
+                exception=exc
+            ).publish_or_dump()
 
     @staticmethod
-    def _calculate_average_latency(results):
+    def _calculate_average_max_latency(results):
         status = defaultdict(float).fromkeys(results[0].keys(), 0.0)
+        max_latency = defaultdict(list)
+
         for result in results:
             for key in status:
                 try:
                     status[key] += float(result.get(key, 0.0))
+                    if key in ["latency 95th percentile", "latency 99th percentile"]:
+                        max_latency[f"{key} max"].append(float(result.get(key, 0.0)))
                 except ValueError:
                     continue
 
         for key in status:
             status[key] = round(status[key] / len(results), 2)
 
+        for key, latency in max_latency.items():
+            status[key] = max(latency)
+
         return status
 
-    def warmup_cache(self, compaction_strategy):
-        cmd = f"cassandra-stress read no-warmup cl=QUORUM duration=180m -pop 'dist={self.get_cs_distribution()}' -mode native cql3 -rate 'threads=500 throttle=35000/s'"  # pylint: disable=line-too-long
-        stress_queue = self.run_stress_cassandra_thread(
-            stress_cmd=cmd,
-            stress_num=1,
-            compaction_strategy=compaction_strategy,
-            stats_aggregate_cmds=False,
-            round_robin=True
-        )
-        self.get_stress_results(stress_queue, store_results=False)
+    def warmup_cache(self, stress_cmd_templ):
+        stress_queue = []
+        for stress_cmd in stress_cmd_templ:
+            params = {"round_robin": True, "stats_aggregate_cmds": False}
+            stress_cmd_to_run = stress_cmd.replace("$threads", "1000")
+            # Run c-s command without throttle
+            stress_cmd_to_run = stress_cmd_to_run.replace("$throttle", "")
+            # TODO: get "n" from c-s command or do it configurable
+            stress_cmd_to_run = stress_cmd_to_run.replace(
+                "duration=20m", "n=216666669" if "gauss" in stress_cmd_to_run else "n=20000000")
+            # For mixed workflow run read warm up
+            stress_cmd_to_run = stress_cmd_to_run.replace("mixed", "read")
+            stress_cmd_to_run = stress_cmd_to_run.replace("cl=QUORUM", "cl=ALL")
+            params.update({'stress_cmd': stress_cmd_to_run})
+            # Run all stress commands
+            self.log.debug('RUNNING warm up stress cmd: %s', stress_cmd_to_run)
+            stress_queue.append(self.run_stress_thread(**params))
+
+        for stress in stress_queue:
+            self.get_stress_results(queue=stress, store_results=False)
