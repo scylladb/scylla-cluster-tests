0dae2c0afd6cfb06bf66498159e4452d89e4667a
Lukasz Sojka <lukasz.sojka@scylladb.com>
2024-09-26 08:26:28 +0200

    fix(config): Raise proper error for missing config file
    When user supplies invalid config files setting it causes `anyconfig` to
    raise problem with parsing `yaml` file - which is misleading.
    
    Fix by verification of existence of config files before proceeding to
    anyconfig.
    
    fixes: https://github.com/scylladb/scylla-cluster-tests/issues/8844
    (cherry picked from commit 09ffaf8b92b45c56f63b2f5cc30d3b243658f9b2)

--- sdcm/sct_config.py
+++ sdcm/sct_config.py
@@ -1662,6 +1662,9 @@ class SCTConfiguration(dict):
 
         # 2) load the config files
         try:
+            for conf_file in list(config_files):
+                if not os.path.exists(conf_file):
+                    raise FileNotFoundError(f"Couldn't find config file: {conf_file}")
             files = anyconfig.load(list(config_files))
             anyconfig.merge(self, files)
         except ValueError:
