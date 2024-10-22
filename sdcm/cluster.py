f4135ddd8f4153e1c2a903d7de581922abf14b34
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2024-10-22 19:44:52 +0300

    fix(kms): set proper node regions in multi-dc setups
    The AWS-KMS code in DB node python class uses shared dictionary
    from the DB cluster class for updating the KMS endpoint region.
    
    It was not a problem when DB nodes setup was serial.
    In this case shared object was changed by each node but had proper
    value in needed time frame.
    
    After implementation of the parallel DB nodes setup [1] we started
    getting problems that only one state of that shared object was
    being applied for all nodes.
    
    In single-dc setups everything was correct just because there was no
    diff among DB node's region names.
    But in multi-dc setups values from DB nodes started being applied
    to each other.
    
    So, fix it by just deep-copying that shared dictionary to avoid
    updates of a shared object.
    
    [1] https://github.com/scylladb/scylla-cluster-tests/pull/7383
    
    Closes: https://github.com/scylladb/scylla-cluster-tests/issues/9025
    (cherry picked from commit ce56367bd50ff0bbfbd4e50e81ee517911df8549)
    
    # Conflicts:
    #	sdcm/cluster.py

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -13,6 +13,7 @@
 
 # pylint: disable=too-many-lines
 import contextlib
+import copy
 import queue
 import logging
 import os
@@ -415,9 +416,14 @@ class BaseNode(AutoSshContainerMixin, WebDriverContainerMixin):  # pylint: disab
             scylla_yml.replace_address_first_boot = self.replacement_node_ip
         if self.replacement_host_id:
             scylla_yml.replace_node_first_boot = self.replacement_host_id
+<<<<<<< HEAD
         if append_scylla_yaml := self.parent_cluster.params.get('append_scylla_yaml'):
             append_scylla_yaml = yaml.safe_load(append_scylla_yaml)
             if any(substr in append_scylla_yaml for substr in (
+=======
+        if append_scylla_yaml := copy.deepcopy(self.parent_cluster.params.get('append_scylla_yaml')) or {}:
+            if any(key in append_scylla_yaml for key in (
+>>>>>>> ce56367bd (fix(kms): set proper node regions in multi-dc setups)
                     "system_key_directory", "system_info_encryption", "kmip_hosts")):
                 install_encryption_at_rest_files(self.remoter)
             scylla_yml.update(append_scylla_yaml)
