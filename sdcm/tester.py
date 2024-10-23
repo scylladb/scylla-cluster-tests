34ac92b0344ae2f837c136cca6b9825c4098cb46
Lukasz Sojka <lukasz.sojka@scylladb.com>
2024-10-23 08:53:27 +0200

    fix(monitor): don't call monitor method when no monitors
    In case `self.monitors` is not defined, sct raises exception upon
    `update_default_time_range` invocation.
    
    Add protection by checking if `self.monitors` is not `None`.
    
    fixes: https://github.com/scylladb/scylla-cluster-tests/issues/9023
    (cherry picked from commit 412d820deae5597567da06a45d949fe3b28e1669)

--- sdcm/tester.py
+++ sdcm/tester.py
@@ -2920,7 +2920,8 @@ class ClusterTester(db_stats.TestStatsMixin, unittest.TestCase):  # pylint: disa
                 k8s_cluster.gather_k8s_logs_by_operator()
                 k8s_cluster.gather_k8s_logs()
 
-        self.monitors.update_default_time_range(self.start_time, time.time())
+        if self.monitors is not None:
+            self.monitors.update_default_time_range(self.start_time, time.time())
         if self.params.get('collect_logs'):
             self.collect_logs()
         self.clean_resources()
