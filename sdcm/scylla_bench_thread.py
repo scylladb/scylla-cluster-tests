bf208544930b3b8a9204f66251d636f6dd80019a
Dmitriy Kruglov <dmytro.kruglov@scylladb.com>
2024-12-18 19:55:31 +0100

    fix(nemesis.py): abort nemesis on stress command failure
    Abort the nemesis flow early when a stress command fails, as continuing it is
    often invalid due to the cluster being in an unexpected state. For example, if
    the _prepare_test_table routine fails, subsequent steps that depend on the test
    table (or attempt disruptions on top of it) will also fail.
    
    This change adds a check for results of stress command triggered within the nemesis
    code, ensuring the nemesis halts early if the stress command is unsuccessful.
    
    Fixes: https://github.com/scylladb/scylla-cluster-tests/issues/8722

--- sdcm/scylla_bench_thread.py
+++ sdcm/scylla_bench_thread.py
@@ -144,7 +144,7 @@ class ScyllaBenchThread(DockerBasedStressThread):  # pylint: disable=too-many-in
 
         results = self.get_results()
 
-        for _, result in results:
+        for _, result, _ in results:
             if not result:
                 # Silently skip if stress command threw an error, since it was already reported in _run_stress
                 continue
@@ -251,7 +251,7 @@ class ScyllaBenchThread(DockerBasedStressThread):  # pylint: disable=too-many-in
             except Exception as exc:  # pylint: disable=broad-except  # noqa: BLE001
                 self.configure_event_on_failure(stress_event=scylla_bench_event, exc=exc)
 
-        return loader, result
+        return loader, result, scylla_bench_event
 
     @classmethod
     def _parse_bench_summary(cls, lines):
