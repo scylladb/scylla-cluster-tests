9912e13fde1a8b5a9a226dfe915c931c19d1fc23
Liapkovich <mikita.liapkovich@scylladb.com>
2024-12-27 11:35:15 +0100

    fix(manager): update default Scylla version to 2024.2 for Manager tests
    Default scylla_version is set to 2024.2 instead of 2024.1.
    
    At the same time, to have some coverage for 2024.1 as well, all Debian11
    jobs adjusted to use version 2024.1.
    
    Related test_get_manager_scylla_backend_returns_repo_address test is
    adjusted too.

--- unit_tests/test_sdcm_mgmt_common.py
+++ unit_tests/test_sdcm_mgmt_common.py
@@ -7,7 +7,7 @@ class TestManagerVersions:
     def test_get_manager_scylla_backend_returns_repo_address(self):  # pylint: disable=no-self-use
         url = get_manager_scylla_backend("2024", Distro.UBUNTU22)
 
-        assert url == 'https://downloads.scylladb.com/deb/debian/scylla-2024.1.list'
+        assert url == 'https://downloads.scylladb.com/deb/debian/scylla-2024.2.list'
 
     def test_get_manager_repo_from_defaults_returns_repo_address(self):  # pylint: disable=no-self-use
         url = get_manager_repo_from_defaults("3.2", Distro.UBUNTU22)
