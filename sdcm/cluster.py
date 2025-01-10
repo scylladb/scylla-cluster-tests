da19d860a5984bb58607472c46e652a75cd815bd
Dmitriy Kruglov <dmytro.kruglov@scylladb.com>
2025-01-10 01:03:15 +0100

    fix(nemesis.py): log nemesis start/end on db nodes logs
    Add logging of the following nemesis details in logs of each db node of
    a cluster under test:
    - nemesis start with disruption/nemesis name
    - target node for the nemesis
    - nemesis end with status

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -32,6 +32,7 @@ import traceback
 import itertools
 import json
 import ipaddress
+import shlex
 from importlib import import_module
 from typing import List, Optional, Dict, Union, Set, Iterable, ContextManager, Any, IO, AnyStr, Callable
 from datetime import datetime, timezone
@@ -3138,6 +3139,10 @@ class BaseNode(AutoSshContainerMixin):  # pylint: disable=too-many-instance-attr
         self.remoter.sudo('systemctl stop firewalld', ignore_status=True)
         self.remoter.sudo('systemctl disable firewalld', ignore_status=True)
 
+    def log_message(self, message: str, level: str = 'info', verbose: bool = False) -> None:
+        self.remoter.run(
+            f'scylla-api-client system log POST --level {level} --message {shlex.quote(message)}', verbose=verbose)
+
 
 class FlakyRetryPolicy(RetryPolicy):
 
@@ -5115,6 +5120,10 @@ class BaseScyllaCluster:  # pylint: disable=too-many-public-methods, too-many-in
             for feature in feature_list:
                 enabled_features_state.append(feature in enabled_features)
         return all(enabled_features_state)
+
+    def log_message(self, message: str, level: str = 'info', verbose: bool = False) -> None:
+        for node in self.nodes:
+            node.log_message(message, level, verbose)
 
 
 class BaseLoaderSet():
