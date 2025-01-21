354b654556b7a47839d1cd8e7cce6e441eea47a2
Israel Fruchter <fruch@scylladb.com>
2025-01-21 23:46:20 +0200

    fix(integration-test): update scylla docker image to lastest master
    we had very old docker image, and for some reason it wasn't booting
    when configured with SSL, with latest docker image it's working as expeted

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -70,7 +70,8 @@ def fixture_docker_scylla(request: pytest.FixtureRequest, params):  # pylint: di
     entryfile_path = entryfile_path / 'docker' / 'scylla-sct' / ('entry_ssl.sh' if ssl else 'entry.sh')
 
     alternator_flags = f"--alternator-port {ALTERNATOR_PORT} --alternator-write-isolation=always"
-    docker_version = docker_scylla_args.get('image', "scylladb/scylla-nightly:6.1.0-dev-0.20240605.2c3f7c996f98")
+    docker_version = docker_scylla_args.get(
+        'image', "scylladb/scylla-nightly:2025.2.0-dev-0.20250302.0343235aa269")
     cluster = LocalScyllaClusterDummy(params=params)
 
     ssl_dir = (Path(__file__).parent.parent / 'data_dir' / 'ssl_conf').absolute()
