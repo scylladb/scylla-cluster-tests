d19bb81c05ad6ae03bfbf5c82a51ed161c4a4f1d
Israel Fruchter <fruch@scylladb.com>
2025-01-21 17:05:46 +0200

    fix(createArgusTestRun): retry 3 times
    since we are running into cases docker is failing
    on this first stage, we would retry it 3 times
    with hope it would be enough to avoid those type
    of error
    
    Ref: scylladb/scylla-cluster-tests#9645
    (cherry picked from commit db7b8c6bcaa635e20ca4292dd3edeb4a78f466da)

--- vars/createArgusTestRun.groovy
+++ vars/createArgusTestRun.groovy
@@ -2,20 +2,22 @@
 
 def call(Map params) {
     def test_config = groovy.json.JsonOutput.toJson(params.test_config)
-    sh """#!/bin/bash
-        set -xe
+    retry(3) {
+		sh """#!/bin/bash
+			set -xe
 
-        echo "Creating Argus test run ..."
-        if [[ -n "${params.requested_by_user ? params.requested_by_user : ''}" ]] ; then
-            export BUILD_USER_REQUESTED_BY=${params.requested_by_user}
-        fi
-        export SCT_CLUSTER_BACKEND="${params.backend}"
-        export SCT_CONFIG_FILES=${test_config}
+			echo "Creating Argus test run ..."
+			if [[ -n "${params.requested_by_user ? params.requested_by_user : ''}" ]] ; then
+				export BUILD_USER_REQUESTED_BY=${params.requested_by_user}
+			fi
+			export SCT_CLUSTER_BACKEND="${params.backend}"
+			export SCT_CONFIG_FILES=${test_config}
 
-        ./docker/env/hydra.sh create-argus-test-run
+			./docker/env/hydra.sh create-argus-test-run
 
-        echo " Argus test run created."
-    """
+			echo " Argus test run created."
+		"""
+    }
     if (!currentBuild.description) {
         currentBuild.description = ''
     }
