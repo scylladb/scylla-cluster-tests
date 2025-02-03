4076695819d69bdf760ccf0be492f346dbfe980f
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2025-02-03 12:39:51 +0200

    improvement(hdrh): rename 'csrangehistogram.py' to 'hdrhistogram.py'
    We are going to add support of hdr histogram files for other
    benchmarking tools too.
    So, rename the 'sdcm/utils/csrangehistogram.py' module
    to the 'sdcm/utils/hdrhistogram.py'.
    
    Also, delete the 'sdcm/utils/cshdrhistogram.py' which is dead code.
    
    (cherry picked from commit d66f2f2ce3f0923b431c5161c0cca1532cbb864d)

--- performance_regression_test.py
+++ performance_regression_test.py
@@ -29,7 +29,7 @@ from sdcm.sct_events.filters import EventsSeverityChangerFilter
 from sdcm.sct_events.loaders import CassandraStressEvent
 from sdcm.sct_events.system import HWPerforanceEvent, InfoEvent
 from sdcm.utils.decorators import log_run_info, latency_calculator_decorator
-from sdcm.utils.csrangehistogram import CSHistogramTagTypes
+from sdcm.utils.hdrhistogram import CSHistogramTagTypes
 from sdcm.utils.nemesis_utils.indexes import wait_for_view_to_be_built
 
 KB = 1024
