7cb1fa17f7096bced8d57d9d0a73e765fefaee03
Eugene <yauheni.khatsianevich@scylladb.com>
2025-02-10 16:02:39 +0100

    fix(nemesis): decorator for ignoring mutation errors added
    this commit adds decorator for ignoring mutation writes during
    remove_node_then_add_node nemesis
    refer: https://github.com/scylladb/scylladb/issues/21815

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -3747,7 +3747,13 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
         else:
             host_id = remove_node_host_id
 
-        with ignore_ycsb_connection_refused():
+        if SkipPerIssues('https://github.com/scylladb/scylladb/issues/21815', params=self.tester.params):
+            # TBD: To be removed after https://github.com/scylladb/scylladb/issues/21815 is resolved
+            ignore_stream_mutation_errors_due_to_issue = ignore_stream_mutation_fragments_errors
+        else:
+            ignore_stream_mutation_errors_due_to_issue = contextlib.nullcontext
+
+        with ignore_ycsb_connection_refused(), ignore_stream_mutation_errors_due_to_issue():
             # node stop and make sure its "DN"
             node_to_remove.stop_scylla_server(verify_up=False, verify_down=True)
 
