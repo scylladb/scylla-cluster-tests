03a0711232ba2c4b89ec3f71dcf958e50d2f9d15
Lukasz Sojka <lukasz.sojka@scylladb.com>
2025-02-14 11:46:38 +0100

    chore(deps): update argus client
    Argus client got new release (0.13.1) supporting setting sut_package for
    results metadata (e.g. allowing setting drivers as SUT).
    
    refs: https://github.com/scylladb/qa-tasks/issues/1543

--- argus/client/generic/client.py
+++ argus/client/generic/client.py
@@ -7,6 +7,10 @@ LOGGER = logging.getLogger(__name__)
 class ArgusGenericClient(ArgusClient):
     test_type = "generic"
     schema_version: None = "v1"
+
+    class Routes(ArgusClient.Routes):
+        TRIGGER_JOBS = "/planning/plan/trigger"
+
     def __init__(self, auth_token: str, base_url: str, api_version="v1") -> None:
         super().__init__(auth_token, base_url, api_version)
 
@@ -22,6 +26,24 @@ class ArgusGenericClient(ArgusClient):
         response = self.submit_run(run_type=self.test_type, run_body=request_body)
         self.check_response(response)
 
+    def trigger_jobs(self, common_params: dict[str, str], params: list[dict[str, str]], version: str = None, release: str = None, plan_id: str = None):
+        request_body = {
+            "common_params": common_params,
+            "params": params,
+            "version": version,
+            "release": release,
+            "plan_id": plan_id,
+        }
+        response = self.post(
+            endpoint=self.Routes.TRIGGER_JOBS,
+            location_params={},
+            body={
+                **self.generic_body,
+                **request_body,
+            }
+        )
+        self.check_response(response)
+        return response.json()
 
     def finalize_generic_run(self, run_id: str, status: str, scylla_version: str | None = None):
         response = self.finalize_run(run_type=self.test_type, run_id=run_id, body={
