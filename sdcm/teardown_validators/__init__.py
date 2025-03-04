b0cd0d7ff467748d5e8aa19183b776a3d4123ae9
Julia Yakovlev <julia.yakovlev@scylladb.com>
2025-03-04 12:08:38 +0200

    improvement(teardown): add rack-aware policy validation
    Add new validation for rack-aware policy.
    Rack-aware validation is enabled under these conditions:
    - A rack-aware policy is configured.
    - A single loader is employed (due to Scylla rack metric limitation).
    - The cluster is deployed across multiple availability zones / datacenters.
    
    The validation aims to confirm that no coordination traffic is directed to availability zones
    where loaders are absent or not more then 10% of non-system traffic. These reads, such as those
    performed by nemeses or non-rack-aware test code, may be routed this way.
    
    If the validator is enabled, attempt to run the rack-aware configuration validation.
    This step will involve checking the necessary settings and dependencies required for the
    rack-aware validator to function correctly.

--- sdcm/teardown_validators/__init__.py
+++ sdcm/teardown_validators/__init__.py
@@ -1,4 +1,5 @@
 from sdcm.teardown_validators.events import ErrorEventsValidator
+from sdcm.teardown_validators.rackaware import RackawareValidator
 from sdcm.teardown_validators.sstables import SstablesValidator
 
-teardown_validators_list = [SstablesValidator, ErrorEventsValidator]
+teardown_validators_list = [SstablesValidator, ErrorEventsValidator, RackawareValidator]
