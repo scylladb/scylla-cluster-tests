a41a297a0e99db17fd5a3b115049597fb489d87c
Israel Fruchter <fruch@scylladb.com>
2025-03-10 13:50:21 +0200

    fix(keep_duration): pass along the keep:duration tags as hours
    test duration in SCT is calculated in minutes, while the tags
    are using hours, updating the implmention to pass hours
    and rename the functions arguments to match.
    
    (cherry picked from commit a5b3d9bb6e39517ffe343323983a88c8a79a802b)

--- sdcm/cluster_gce.py
+++ sdcm/cluster_gce.py
@@ -132,11 +132,11 @@ class GCENode(cluster.BaseNode):
                               zone=self.zone) and \
             super()._set_keep_alive()
 
-    def _set_keep_duration(self, duration_in_minutes: int) -> None:
+    def _set_keep_duration(self, duration_in_hours: int) -> None:
         self._refresh_instance_state()
         gce_set_labels(instances_client=self._gce_service,
                        instance=self._instance,
-                       new_labels={"keep": str(duration_in_minutes)},
+                       new_labels={"keep": str(duration_in_hours)},
                        project=self.project,
                        zone=self.zone)
 
