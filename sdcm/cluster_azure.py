386163284351ff3b05c8c321309def7cc883a9cd
Israel Fruchter <fruch@scylladb.com>
2025-03-10 13:50:21 +0200

    fix(keep_duration): pass along the keep:duration tags as hours
    test duration in SCT is calculated in minutes, while the tags
    are using hours, updating the implmention to pass hours
    and rename the functions arguments to match.
    
    (cherry picked from commit a5b3d9bb6e39517ffe343323983a88c8a79a802b)

--- sdcm/cluster_azure.py
+++ sdcm/cluster_azure.py
@@ -78,8 +78,16 @@ class AzureNode(cluster.BaseNode):
         return super()._set_keep_alive()
 
     @retrying(n=6, sleep_time=1)
+<<<<<<< HEAD
     def _set_keep_duration(self, duration_in_minutes) -> None:
         self._instance.add_tags({"keep": str(duration_in_minutes)})
+||||||| parent of a5b3d9bb6 (fix(keep_duration): pass along the keep:duration tags as hours)
+    def _set_keep_duration(self, duration_in_minutes: int) -> None:
+        self._instance.add_tags({"keep": str(duration_in_minutes)})
+=======
+    def _set_keep_duration(self, duration_in_hours: int) -> None:
+        self._instance.add_tags({"keep": str(duration_in_hours)})
+>>>>>>> a5b3d9bb6 (fix(keep_duration): pass along the keep:duration tags as hours)
 
     def _refresh_instance_state(self):
         ip_tuple = ([self._instance.public_ip_address], [self._instance.private_ip_address])
