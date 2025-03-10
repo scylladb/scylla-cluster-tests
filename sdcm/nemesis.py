1cfbc73c8709f4b2466d4330213b17ef4c7af87f
Alexey Kartashov <alexey.kartashov@scylladb.com>
2025-03-10 16:23:44 +0100

    fix(nemesis.py): Prevent static columns for MVs
    This commit disables disrupt_add_remove_mv from using a static column
    inside a materialized view by introducing additional flag to both
    get_column_names and get_random_column_name to disable selecting
    'static' column types.
    
    Fixes #10202

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -5185,7 +5185,7 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
                     session=session, ks=ks_name, cf=base_table_name, is_primary_key=True)
                 # selecting a supported column for creating a materialized-view (not a collection type).
                 column = get_random_column_name(session=session, ks=ks_name,
-                                                cf=base_table_name, filter_out_collections=True)
+                                                cf=base_table_name, filter_out_collections=True, filter_out_static_columns=True)
                 if not column:
                     raise UnsupportedNemesis(
                         'A supported column for creating MV is not found. nemesis can\'t run')
