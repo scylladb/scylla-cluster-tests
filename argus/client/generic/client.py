3ed17e0ec4dc2f93a155cdeedd8183cd72341737
Israel Fruchter <fruch@scylladb.com>
2025-03-23 19:48:43 +0200

    feature(argus): call client with extra headers
    since argus serivce will need more headers for it's to be operational,
    we'll need to change the calls to it, to pass the needed headers.
    
    (cherry picked from commit 6e4e1fbc09114c1b8e9cd8773d170f1952990818)

--- argus/client/generic/client.py
+++ argus/client/generic/client.py
@@ -7,8 +7,12 @@ LOGGER = logging.getLogger(__name__)
 class ArgusGenericClient(ArgusClient):
     test_type = "generic"
     schema_version: None = "v1"
-    def __init__(self, auth_token: str, base_url: str, api_version="v1") -> None:
-        super().__init__(auth_token, base_url, api_version)
+
+    class Routes(ArgusClient.Routes):
+        TRIGGER_JOBS = "/planning/plan/trigger"
+
+    def __init__(self, auth_token: str, base_url: str, api_version="v1", extra_headers: dict | None = None) -> None:
+        super().__init__(auth_token, base_url, api_version, extra_headers=extra_headers)
 
     def submit_generic_run(self, build_id: str, run_id: str, started_by: str, build_url: str, scylla_version: str | None = None):
         request_body = {
@@ -22,6 +26,24 @@ class ArgusGenericClient(ArgusClient):
         response = self.submit_run(run_type=self.test_type, run_body=request_body)
         self.check_response(response)
 
+    def trigger_jobs(self, common_params: dict[str, str], params: list[dict[str, str]], version: str = None, release: str = None, plan_id: str = None):
+        request_body = {
+            "common_params": common_params,
+            "params": params,
+            "version": version,
+            "release": release,
+            "plan_id": plan_id,
+        }
+        response = self.post(
+            endpoint=self.Routes.TRIGGER_JOBS,
+            location_params={},
+            body={
+                **self.generic_body,
+                **request_body,
+            }
+        )
+        self.check_response(response)
+        return response.json()
 
     def finalize_generic_run(self, run_id: str, status: str, scylla_version: str | None = None):
         response = self.finalize_run(run_type=self.test_type, run_id=run_id, body={
