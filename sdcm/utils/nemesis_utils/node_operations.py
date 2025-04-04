2c059763b32a0d68104c7846a1c32b6361fc45a6
Aleksandr Bykov <alex.bykov@scylladb.com>
2025-04-04 21:13:05 +0700

    fix(nemesis): add support ipv6 for refuse connection for banned node
    disrupt_refuse_connection_with_* nemesises doesn't support ipv6.
     - Added command for blocking ports for ipv6 stack.
    
    When node is banned and alive, c-s/s-b could connect to it
    and failed with critical error, because banned node return
    that other node cluster is down.
     - Added new node_operation: block_loader_workload_for_scyllanode.
     This allow to block connections to scylla node from loaders
     and aboid critical error of c-s/s-b if them connect to
     banned node and failed to run
    
    Fixes: #10434
    (cherry picked from commit 8691403ddf30abac25c2832c473c23419be91e72)

--- sdcm/utils/nemesis_utils/node_operations.py
+++ sdcm/utils/nemesis_utils/node_operations.py
@@ -9,7 +9,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 @contextlib.contextmanager
-def block_scylla_ports(target_node: "BaseNode", ports: list[int] | None = None):
+def block_scylla_ports(target_node: BaseNode, ports: list[int] | None = None):
     ports = ports or [7001, 7000, 9042, 9142, 19042, 19142]
     target_node.install_package("iptables")
     target_node.start_service("iptables", ignore_status=True)
@@ -17,21 +17,56 @@ def block_scylla_ports(target_node: "BaseNode", ports: list[int] | None = None):
     for port in ports:
         target_node.remoter.sudo(f"iptables -A INPUT -p tcp --dport {port} -j DROP")
         target_node.remoter.sudo(f"iptables -A OUTPUT -p tcp --dport {port} -j DROP")
+        target_node.remoter.sudo(f"ip6tables -A INPUT -p tcp --dport {port} -j DROP")
+        target_node.remoter.sudo(f"ip6tables -A OUTPUT -p tcp --dport {port} -j DROP")
     yield
     target_node.log.debug("Remove all iptable rules %s", target_node.name)
     for port in ports:
         target_node.remoter.sudo(f"iptables -D INPUT -p tcp --dport {port} -j DROP")
         target_node.remoter.sudo(f"iptables -D OUTPUT -p tcp --dport {port} -j DROP")
+        target_node.remoter.sudo(f"ip6tables -D INPUT -p tcp --dport {port} -j DROP")
+        target_node.remoter.sudo(f"ip6tables -D OUTPUT -p tcp --dport {port} -j DROP")
     target_node.stop_service("iptables", ignore_status=True)
 
 
 @contextlib.contextmanager
-def pause_scylla_with_sigstop(target_node: "BaseNode"):
+def pause_scylla_with_sigstop(target_node: BaseNode):
     target_node.log.debug("Send signal SIGSTOP to scylla process on node %s", target_node.name)
     target_node.remoter.sudo("pkill --signal SIGSTOP -e scylla", timeout=60)
     yield
     target_node.log.debug("Send signal SIGCONT to scylla process on node %s", target_node.name)
     target_node.remoter.sudo(cmd="pkill --signal SIGCONT -e scylla", timeout=60)
+
+
+@contextlib.contextmanager
+def block_loaders_payload_for_scylla_node(scylla_node: BaseNode, loader_nodes: list[BaseNode]):
+    """ Block connections from loaders to cql ports on scylla node
+
+    Make the Scylla node inaccessible to loaders by blocking
+    any subsequent connections to the Scylla node.
+    This ensures that the stress tool can continue to operate without failure
+    even if the Scylla node is banned and removed from the cluster.
+    """
+    ports = [9042, 9142, 19042, 19142]
+    scylla_node.install_package("iptables")
+    scylla_node.start_service("iptables", ignore_status=True)
+    loader_nodes_names = [node.name for node in loader_nodes]
+    blocking_ips = [node.ip_address for node in loader_nodes]
+    scylla_node.log.debug("Block connections on %s from loader nodes %s", scylla_node.name, loader_nodes_names)
+    for port in ports:
+        scylla_node.remoter.sudo(
+            f"iptables -A INPUT -s {','.join(blocking_ips)} -p tcp --dport {port} -j DROP", ignore_status=True)
+        scylla_node.remoter.sudo(
+            f"ip6tables -A INPUT -s {','.join(blocking_ips)} -p tcp --dport {port} -j DROP", ignore_status=True)
+    yield
+    # if scylla_node is alive, then delete the iptables rules
+    if scylla_node.remoter.is_up():
+        for port in ports:
+            scylla_node.remoter.sudo(
+                f"iptables -D INPUT -s {','.join(blocking_ips)} -p tcp --dport {port} -j DROP", ignore_status=True)
+            scylla_node.remoter.sudo(
+                f"ip6tables -D INPUT -s {','.join(blocking_ips)} -p tcp --dport {port} -j DROP", ignore_status=True)
+        scylla_node.stop_service("iptables", ignore_status=True)
 
 
 def is_node_removed_from_cluster(removed_node: BaseNode, verification_node: BaseNode) -> bool:
@@ -48,4 +83,6 @@ def is_node_removed_from_cluster(removed_node: BaseNode, verification_node: Base
 
 def is_node_seen_as_down(down_node: BaseNode, verification_node: BaseNode) -> bool:
     LOGGER.debug("Verification node %s", verification_node.name)
-    return down_node not in verification_node.parent_cluster.get_nodes_up_and_normal(verification_node)
+    nodes_status = verification_node.parent_cluster.get_nodetool_status(verification_node, dc_aware=False)
+    down_node_status = nodes_status.get(down_node.ip_address)
+    return (not down_node_status or down_node_status["state"] == "DN")
