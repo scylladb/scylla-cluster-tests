afeb8f03d5862ded4d24e605b04485fdb753fe79
Lukasz Sojka <lukasz.sojka@scylladb.com>
2025-04-29 16:06:28 +0200

    fix(perf-pipeline): add Argus step to pipeline
    Perf pipeline does not create argus run. Added this step to support
    linking Argus to job builds and have them even if fails at early stage.

--- vars/perfRegressionParallelPipeline.groovy
+++ vars/perfRegressionParallelPipeline.groovy
@@ -272,6 +272,19 @@ def call(Map pipelineParams) {
                                                 }
                                             }
                                         }
+                                        stage('Create Argus Test Run') {
+                                            catchError(stageResult: 'FAILURE') {
+                                                script {
+                                                    wrap([$class: 'BuildUser']) {
+                                                        dir('scylla-cluster-tests') {
+                                                            timeout(time: 5, unit: 'MINUTES') {
+                                                                createArgusTestRun(params)
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
                                         stage("Create SCT Runner for ${sub_test}") {
                                             wrap([$class: 'BuildUser']) {
                                                 dir('scylla-cluster-tests') {
