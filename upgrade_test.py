e066c380b29be8b7a30404f579f4c281dea38fb3
Aleksandr Bykov <alex.bykov@scylladb.com>
2025-05-07 16:45:40 +0700

    fix(upgrade_test): Check limited voters enabled after upgrade
    Check that feature LIMITED Voters enabled after upgrade and
    number of voters for cluster with nodes > 5, not all nodes are voters
    There is not reason to raise exception, but trigger info event with
    Severity error if all nodes are voters after feature was enabled

--- upgrade_test.py
+++ upgrade_test.py
@@ -64,6 +64,8 @@ from sdcm.utils.features import CONSISTENT_TOPOLOGY_CHANGES_FEATURE, is_tablets_
 from sdcm.wait import wait_for
 from sdcm.rest.raft_upgrade_procedure import RaftUpgradeProcedure
 from test_lib.sla import create_sla_auth
+from sdcm.exceptions import Group0LimitedVotersFeatureNotEnableOnNodes
+
 
 NUMBER_OF_ROWS_FOR_TRUNCATE_TEST = 10
 
@@ -771,6 +773,8 @@ class UpgradeTest(FillDatabaseData, loader_utils.LoaderUtilsMixin):
                                                               step=step)
         InfoEvent(message='Step5.1 - run raft topology upgrade procedure').publish()
         self.run_raft_topology_upgrade_procedure()
+        InfoEvent(message="Step5.2 - check limited voters feature").publish()
+        self.validate_limited_voters_feature_enabled()
 
         InfoEvent(message='Step6 - Verify stress results after upgrade ').publish()
         InfoEvent(message='Waiting for stress threads to complete after upgrade').publish()
@@ -1004,6 +1008,7 @@ class UpgradeTest(FillDatabaseData, loader_utils.LoaderUtilsMixin):
 
         InfoEvent(message='Run raft topology upgrade procedure').publish()
         self.run_raft_topology_upgrade_procedure()
+        self.validate_limited_voters_feature_enabled()
 
         InfoEvent(message="Waiting for stress_during_entire_upgrade to finish").publish()
         for stress_thread_pool in stress_thread_pools:
@@ -1438,6 +1443,41 @@ class UpgradeTest(FillDatabaseData, loader_utils.LoaderUtilsMixin):
                 self.log.warning("Couldn't extract version from %s", new_version)
         except Exception as exc:  # pylint: disable=broad-except
             self.log.exception("Failed to save upgraded Scylla version in Argus", exc_info=exc)
+
+    def validate_limited_voters_feature_enabled(self):
+        InfoEvent(message="Check that limited voters feature enabled after upgrade for all nodes")
+        max_voter_number = 5
+        feature_state_per_node = []
+        for node in self.db_cluster.nodes:
+            result = wait_for(func=self.db_cluster.is_features_enabled_on_node,
+                              timeout=60,
+                              step=1,
+                              text=f"Check feature enabled on node {node.name}",
+                              throw_exc=False,
+                              feature_list=["GROUP0_LIMITED_VOTERS"],
+                              node=node)
+            feature_state_per_node.append(result)
+        if all(not enabled for enabled in feature_state_per_node):
+            InfoEvent(message="Limited voters feature is not supported or enabled on all nodes").publish()
+            return
+        elif all(feature_state_per_node):
+            InfoEvent(message="Limited voters feature enabled on all nodes").publish()
+            num_of_nodes = len(self.db_cluster.nodes)
+            num_of_non_voters = len(self.db_cluster.nodes[0].raft.get_group0_non_voters())
+            if num_of_nodes <= max_voter_number and num_of_non_voters > 0:
+                InfoEvent(message=f'Number of voters: {num_of_nodes - num_of_non_voters} are less than expected: {max_voter_number}',
+                          severity=Severity.ERROR).publish()
+            elif num_of_nodes > max_voter_number and (num_of_nodes - num_of_non_voters) > max_voter_number:
+                InfoEvent(message=f'Number of voters: {num_of_nodes - num_of_non_voters} is higher than expected: {max_voter_number}',
+                          severity=Severity.ERROR).publish()
+            else:
+                InfoEvent(message="Feature limited voters is enabled correctly").publish()
+            return
+        else:
+            nodes_without_feature = [node.name for _, node in filter(
+                lambda state: not state[0], zip(feature_state_per_node, self.db_cluster.nodes))]
+            raise Group0LimitedVotersFeatureNotEnableOnNodes(
+                f"Limited voters feature not enabled on all nodes: {','.join(nodes_without_feature)}")
 
 
 class UpgradeCustomTest(UpgradeTest):
