b9bccea40271f6fa4dac844d6a6789af5f2cf51f
Israel Fruchter <fruch@scylladb.com>
2025-05-12 13:38:57 +0300

    fix(decode_backtrace): handle non-exist debug info better
    scylladb/scylla-cluster-tests#10646 introduced some unsafe code
    
    the when debug info doesn't it fail like the following:
    ```
    20:58:29      raise Exception("Couldn't find scylla debug information")
    20:58:29  Exception: Couldn't find scylla debug information
    20:58:29
    20:58:29  During handling of the above exception, another exception occurred:
    20:58:29
    20:58:29  Traceback (most recent call last):
    20:58:29    File "/usr/local/lib/python3.10/threading.py", line 1016, in _bootstrap_inner
    20:58:29      self.run()
    20:58:29    File "/usr/local/lib/python3.10/threading.py", line 953, in run
    20:58:29      self._target(*self._args, **self._kwargs)
    20:58:29    File "/home/ubuntu/scylla-cluster-tests/sdcm/cluster.py", line 1600, in decode_backtrace
    20:58:29      if "is closed" in details:
    20:58:29  TypeError: argument of type 'Exception' is not iterable
    ```
    
    the mistake was to treat Exception as string, with turn it into a string
    
    (cherry picked from commit c2b8ce434dbc23fa4d99b696c2baf36b20dd2cbc)

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -1557,7 +1557,7 @@ class BaseNode(AutoSshContainerMixin):  # pylint: disable=too-many-instance-attr
                 pass
             except Exception as details:  # pylint: disable=broad-except  # noqa: BLE001
                 self.log.error("failed to decode backtrace %s", details)
-                if "is closed" in details:
+                if "is closed" in str(details):
                     break
             finally:
                 if event:
