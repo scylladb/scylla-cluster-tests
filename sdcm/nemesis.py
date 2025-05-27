8fd2f963c48db0c137be6ccc86ef6df10a030c94
Petr Hála <petr.hala@scylladb.com>
2025-05-27 16:13:51 +0200

    feature(nemesis): Update CategoricalMonkey for new format
    * Change it to be compatible with nemesis rework
    * Improve it to use nemesis filtering and seed.

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -18,7 +18,6 @@ Classes that introduce disruption in clusters.
 import contextlib
 import copy
 import datetime
-import inspect
 import logging
 import math
 import os
@@ -5926,8 +5925,7 @@ class CategoricalMonkey(Nemesis):
     In particular if the default weight is 0 then the unlisted disruptions won't be executed.
     """
 
-    @staticmethod
-    def get_disruption_distribution(dist: dict, default_weight: float) -> Tuple[List[Callable], List[float]]:
+    def get_disruption_distribution(self, dist: dict, default_weight: float) -> Tuple[List[Callable], List[float]]:
         def is_nonnegative_number(val):
             try:
                 val = float(val)
@@ -5936,12 +5934,7 @@ class CategoricalMonkey(Nemesis):
             else:
                 return val >= 0
 
-        def prefixed(pref: str, val: str) -> str:
-            if val.startswith(pref):
-                return val
-            return pref + val
-
-        all_methods = CategoricalMonkey.get_disrupt_methods()
+        all_methods = {method.__name__: method for method in self.build_disruptions_by_selector(self.nemesis_selector)}
 
         population: List[Callable] = []
         weights: List[float] = []
@@ -5949,8 +5942,7 @@ class CategoricalMonkey(Nemesis):
 
         for _name, _weight in dist.items():
             name = str(_name)
-            prefixed_name = prefixed('disrupt_', name)
-            if prefixed_name not in all_methods:
+            if _name not in all_methods.keys():
                 raise ValueError(f"'{name}' is not a valid disruption. All methods: {all_methods.keys()}")
 
             if not is_nonnegative_number(_weight):
@@ -5959,9 +5951,9 @@ class CategoricalMonkey(Nemesis):
 
             weight = float(_weight)
             if weight > 0:
-                population.append(all_methods[prefixed_name])
+                population.append(all_methods[_name])
                 weights.append(weight)
-            listed_methods.add(prefixed_name)
+            listed_methods.add(_name)
 
         if default_weight > 0:
             for method_name, method in all_methods.items():
@@ -5974,25 +5966,20 @@ class CategoricalMonkey(Nemesis):
 
         return population, weights
 
-    @staticmethod
-    def get_disrupt_methods() -> Dict[str, Callable]:
-        return {attr[0]: attr[1] for attr in inspect.getmembers(CategoricalMonkey) if
-                attr[0].startswith('disrupt_') and
-                callable(attr[1])}
-
     def __init__(self, tester_obj, termination_event, dist: dict, *args, default_weight: float = 1, **kwargs):
         super().__init__(tester_obj, termination_event, *args, **kwargs)
-        self.disruption_distribution = CategoricalMonkey.get_disruption_distribution(dist, default_weight)
+        self.random = random.Random(self.nemesis_seed)
+        self.disruption_distribution = self.get_disruption_distribution(dist, default_weight)
 
-    def disrupt(self):
-        self._random_disrupt()
-
-    def _random_disrupt(self):
+    def select_next_nemesis(self):
         population, weights = self.disruption_distribution
         assert len(population) == len(weights) and population
 
-        method = random.choices(population, weights=weights)[0]
-        self.execute_nemesis(method)
+        return self.random.choices(population, weights=weights)[0]
+
+    def call_next_nemesis(self):
+        """Override parent method to change how nemesis are executed"""
+        self.execute_nemesis(self.select_next_nemesis())
 
 
 class ScyllaCloudLimitedChaosMonkey(Nemesis):
