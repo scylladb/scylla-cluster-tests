62f1b7e738092fb5f3b137a8474ee5a1440f8e07
Lakshmipathi.Ganapathi <Lakshmipathi.Ganapathi@scylladb.com>
2025-06-03 13:27:15 +0530

    test(collect_diagnosis_data_test): Add DiagnosisReport teardown validator
    Adds a new DiagnosisReport teardown validator to send signal to scylla
    and dump diagnostics then verify it.
    
    Fixes: https://github.com/scylladb/scylla-cluster-tests/issues/9443
    
    Signed-off-by: Lakshmipathi.Ganapathi <Lakshmipathi.Ganapathi@scylladb.com>

--- sdcm/teardown_validators/__init__.py
+++ sdcm/teardown_validators/__init__.py
@@ -1,5 +1,6 @@
 from sdcm.teardown_validators.events import ErrorEventsValidator
 from sdcm.teardown_validators.rackaware import RackawareValidator
 from sdcm.teardown_validators.sstables import SstablesValidator
+from sdcm.teardown_validators.diagnosis_report import DiagnosisReportValidator
 
-teardown_validators_list = [SstablesValidator, ErrorEventsValidator, RackawareValidator]
+teardown_validators_list = [SstablesValidator, ErrorEventsValidator, RackawareValidator, DiagnosisReportValidator]
