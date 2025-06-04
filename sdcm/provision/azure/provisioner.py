bc2306a84191601179e7112a9b469303565801ea
Lukasz Sojka <lukasz.sojka@scylladb.com>
2025-06-04 17:02:23 +0200

    fix(azure): improved handling of spot instances provision
    When provisioning spot instances it may happen that instance is
    preempted during creation. In that case it destroys all related
    resources like ip's, nic's etc. causing cached values to be invalid.
    Probably it also breaks fallback to `on_demand` due the same.
    
    fix is by detecting preemption errors and recreate cache.
    Used retrying from SCT's utils to allow retry on provision error and
    skip when there's problem with preemption straight to fallback.
    
    fixes: https://github.com/scylladb/scylla-cluster-tests/issues/5198
    (cherry picked from commit 2f6a4015f10405575317a2ed1ed7077499ad63d2)

--- sdcm/provision/azure/provisioner.py
+++ sdcm/provision/azure/provisioner.py
@@ -27,7 +27,7 @@ from sdcm.provision.azure.resource_group_provider import ResourceGroupProvider
 from sdcm.provision.azure.subnet_provider import SubnetProvider
 from sdcm.provision.azure.virtual_machine_provider import VirtualMachineProvider
 from sdcm.provision.azure.virtual_network_provider import VirtualNetworkProvider
-from sdcm.provision.provisioner import Provisioner, InstanceDefinition, VmInstance, PricingModel
+from sdcm.provision.provisioner import Provisioner, InstanceDefinition, VmInstance, PricingModel, OperationPreemptedError
 from sdcm.provision.security import ScyllaOpenPorts
 from sdcm.utils.azure_utils import AzureService
 
@@ -132,10 +132,19 @@ class AzureProvisioner(Provisioner):  # pylint: disable=too-many-instance-attrib
         vnet_name = self._vnet_provider.get_or_create().name
         subnet_id = self._subnet_provider.get_or_create(vnet_name, sec_group_id).id
         ip_addresses = self._ip_provider.get_or_create(names=[d.name for d in definitions_to_provision], version="IPV4")
-        nics = self._nic_provider.get_or_create(subnet_id, ip_addresses_ids=[address.id for address in ip_addresses], names=[
-                                                definition.name for definition in definitions_to_provision])
-        v_ms = self._vm_provider.get_or_create(definitions=definitions_to_provision, nics_ids=[
-                                               nic.id for nic in nics], pricing_model=pricing_model)
+        nics = self._nic_provider.get_or_create(subnet_id, ip_addresses_ids=[address.id for address in ip_addresses],
+                                                names=[definition.name for definition in definitions_to_provision])
+        try:
+            v_ms = self._vm_provider.get_or_create(definitions=definitions_to_provision, nics_ids=[
+                                                   nic.id for nic in nics], pricing_model=pricing_model)
+        except OperationPreemptedError:
+            # upon preemption error, need to recreate providers to rediscover resources as cache might be invalid.
+            self._ip_provider = IpAddressProvider(
+                self._resource_group_name, self._region, self._az, self._azure_service)
+            self._nic_provider = NetworkInterfaceProvider(self._resource_group_name, self._region, self._azure_service)
+            self._vm_provider = VirtualMachineProvider(
+                self._resource_group_name, self._region, self._az, self._azure_service)
+            raise
         for definition, v_m in zip(definitions, v_ms):
             instance = self._vm_to_instance(v_m)
             self._cache[definition.name] = instance
