4983c60556d108db203831926113e4f9abbb1f81
Israel Fruchter <fruch@scylladb.com>
2025-06-04 18:03:52 +0300

    test(adptive_timeouts): add all metric into report to argus

--- sdcm/utils/adaptive_timeouts/load_info_store.py
+++ sdcm/utils/adaptive_timeouts/load_info_store.py
@@ -290,6 +290,15 @@ class ArgusAdaptiveTimeoutStore(AdaptiveTimeoutStore):
                     ColumnMetadata(name="duration", unit="HH:MM:SS", type=ResultType.DURATION, higher_is_better=False),
                     ColumnMetadata(name="timeout", unit="HH:MM:SS", type=ResultType.DURATION),
                     ColumnMetadata(name="end_time", unit="", type=ResultType.TEXT),
+                    ColumnMetadata(name="cpu_load_5", unit="%", type=ResultType.FLOAT, higher_is_better=False),
+                    ColumnMetadata(name="shards_count", unit="", type=ResultType.INTEGER),
+                    ColumnMetadata(name="read_bandwidth_mb", unit="MB/s", type=ResultType.FLOAT),
+                    ColumnMetadata(name="write_bandwidth_mb", unit="MB/s", type=ResultType.FLOAT),
+                    ColumnMetadata(name="read_iops", unit="op/s", type=ResultType.INTEGER),
+                    ColumnMetadata(name="write_iops", unit="op/s", type=ResultType.INTEGER),
+                    ColumnMetadata(name="node_data_size_mb", unit="MB", type=ResultType.INTEGER),
+                    ColumnMetadata(name="node_name", unit="", type=ResultType.TEXT),
+
                 ]
         cycle = next(self.cycle_counters[result.operation]) + 1
         table = AdaptiveTimeoutResultsTable()
@@ -297,6 +306,23 @@ class ArgusAdaptiveTimeoutStore(AdaptiveTimeoutStore):
                          status=Status.PASS if not result.timeout_occurred else Status.ERROR)
         table.add_result(column="timeout", row=f"#{cycle}", value=result.timeout, status=Status.UNSET)
         table.add_result(column="end_time", row=f"#{cycle}", value=result.end_time, status=Status.UNSET)
+        table.add_result(column="cpu_load_5", row=f"#{cycle}",
+                         value=result.metrics.get('cpu_load_5'), status=Status.UNSET)
+        table.add_result(column="shards_count", row=f"#{cycle}",
+                         value=result.metrics.get('shards_count'), status=Status.UNSET)
+        table.add_result(column="read_bandwidth_mb", row=f"#{cycle}", value=result.metrics.get(
+            'read_bandwidth_mb'), status=Status.UNSET)
+        table.add_result(column="write_bandwidth_mb", row=f"#{cycle}", value=result.metrics.get(
+            'write_bandwidth_mb'), status=Status.UNSET)
+        table.add_result(column="read_iops", row=f"#{cycle}",
+                         value=result.metrics.get('read_iops'), status=Status.UNSET)
+        table.add_result(column="write_iops", row=f"#{cycle}",
+                         value=result.metrics.get('write_iops'), status=Status.UNSET)
+        table.add_result(column="node_data_size_mb", row=f"#{cycle}", value=result.metrics.get(
+            'node_data_size_mb'), status=Status.UNSET)
+        table.add_result(column="node_name", row=f"#{cycle}",
+                         value=result.metrics.get('node_name'), status=Status.UNSET)
+
         logging.debug("Submitting adaptive timeout results to Argus: %s", table.as_dict())
         submit_results_to_argus(argus_client, table)
 
