c31b8b3e6e3805f417f7c661b8f580399c73ae40
Cezar Moise <cezar.moise@scylladb.com>
2025-06-06 16:32:44 +0300

    fix(upgrade_test): increase timeout for truncates
    The truncate operation may take several minutes.
    Increase the timeout of the truncate itself to prevent the session
    from retrying the truncate (this is mostly a cosmetic change, but will result
    in less errors in log).
    Increase the timeout of the execute to match that of the truncat to ensure
    the operation has enough time to finish.
    
    closes: #10677
    closes: #4866
    (cherry picked from commit 2733a6e32417b0126529b1b8bb8302a5a335d5b3)

--- sdcm/fill_db_data.py
+++ sdcm/fill_db_data.py
@@ -3052,7 +3052,7 @@ class FillDatabaseData(ClusterTester):
 
     @staticmethod
     def cql_truncate_simple_tables(session, rows):
-        truncate_query = 'TRUNCATE TABLE truncate_table%d'
+        truncate_query = 'TRUNCATE TABLE truncate_table%d USING TIMEOUT 300s'
         for i in range(rows):
             session.execute(truncate_query % i)
 
@@ -3199,6 +3199,8 @@ class FillDatabaseData(ClusterTester):
 
     @retrying(n=3, sleep_time=20, allowed_exceptions=ProtocolException)
     def truncate_table(self, session, truncate):
+        if "using timeout" not in truncate.lower():
+            truncate = f"{truncate} USING TIMEOUT 300s"
         LOGGER.debug(truncate)
         session.execute(truncate)
 
