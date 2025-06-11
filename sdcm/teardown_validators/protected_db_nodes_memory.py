028f477f42fa25fa6c1bf379754d5313471a6a48
Israel Fruchter <fruch@scylladb.com>
2025-06-11 15:55:21 +0300

    feature(tear_validator): introduce validation for memory of `protected_db_nodes`
    this take all of the memory uasge information from prometheus for the protected nodes
    and validates there wasn't any drops in memory usage

---
+++ sdcm/teardown_validators/protected_db_nodes_memory.py
@@ -0,0 +1,69 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright (c) 2025 ScyllaDB
+
+import time
+import logging
+
+from sdcm.cluster import BaseNode
+from sdcm.sct_events import Severity
+from sdcm.sct_events.teardown_validators import ValidatorEvent
+from sdcm.teardown_validators.base import TeardownValidator
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ProtectedDbNodesMemoryValidator(TeardownValidator):
+    validator_name = "protected_db_nodes_memory"
+
+    def validate(self):
+        """
+        Validate memory usage for protected_db_nodes using Prometheus.
+        """
+        protected_nodes = self.params.get("protected_db_nodes")
+        if not protected_nodes:
+            LOG.info("No protected_db_nodes configured, skipping memory validation.")
+            return
+
+        # Assume protected_nodes is a list of node indexes
+        # Map node indexes to node names/ips as needed
+        nodes = [node for node in self.tester.db_cluster.nodes if node.is_protected]
+        for node in nodes:
+            mem_usage = self._get_memory_usage(node)
+            LOG.info(f"Node {node} memory usage: {mem_usage} bytes")
+
+    def _get_memory_usage(self, node: BaseNode) -> int:
+        """
+        This method queries Prometheus for the scylla_memory_unspooled_dirty_bytes metric
+        for the given node, summing the values over the test run period.
+        Returns the total memory usage in bytes as an integer.
+        """
+        query = 'sum(scylla_memory_unspooled_dirty_bytes{instance="%s"})' % node.ip_address
+
+        if results := self.tester.prometheus_db.query(query=query, start=self.tester.start_time, end=time.time()):
+            values = [int(float(value[1])) for value in results[0]["values"]] if results[0]["values"] else []
+            if values:
+                mean = sum(values) / len(values)
+                max_val = max(values)
+                min_val = min(values)
+                stddev = (sum((v - mean) ** 2 for v in values) / len(values)) ** 0.5
+                spike_detected = any(abs(v - mean) > 3 * stddev for v in values)
+                if spike_detected:
+                    LOG.warning(
+                        f"Memory usage anomaly detected on node {node}: mean={mean}, stddev={stddev}, max={max_val}, min={min_val}")
+                    ValidatorEvent(
+                        message=f"Memory usage anomaly detected on node {node}: mean={mean}, stddev={stddev}, max={max_val}, min={min_val}",
+                        severity=Severity.ERROR,
+                    ).publish()
+                return sum(values)
+        return 0
