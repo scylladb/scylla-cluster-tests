527f78cf7202e03e87e8fc0adff737718622aa1e
Israel Fruchter <fruch@scylladb.com>
2025-06-13 00:55:05 +0300

    test(runner): temporary trying to support running tests with pytest
    subtest are more it a bit more complicated then it should be

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -177,3 +177,29 @@ def pytest_sessionfinish():
     # to silence the warnings, let's just prevent logging from raising
     # exceptions.
     logging.raiseExceptions = False
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """
+    Hook to capture the test report and attach it to the item.
+    so it can be accessed during
+    """
+    outcome = yield
+    report = outcome.get_result()
+
+    setattr(item, "rep_" + report.when, report)
+
+
+from sdcm.utils.subtest_utils import SUBTESTS_FAILURES
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_runtest_logreport(report: pytest.TestReport):
+    """
+    Hook to log subtest failures and their context.
+    """
+    if report.when == "call" and getattr(report, "context", None):
+        print(f"Report context: {report.context}")
+        if report.failed:
+            SUBTESTS_FAILURES[report.nodeid].append(report)
