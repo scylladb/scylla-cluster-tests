cd8c3a97d83328a6ea694300a6b88a1f22b1c0b9
Israel Fruchter <fruch@scylladb.com>
2025-06-18 15:51:40 +0300

    fix(pipelines): don't set `instance_provision` with null
    pipeline were setting that regardless if it set of not,
    and causing situation where it get up to SCT configuration
    with `null` value that isn't handled
    
    (cherry picked from commit 12f9dfea42909449d9aa73a1caa2a4709ea1cfb1)

--- vars/runCleanupResource.groovy
+++ vars/runCleanupResource.groovy
@@ -16,9 +16,9 @@ def call(Map params, String region){
 
     export SCT_CONFIG_FILES=${test_config}
     export SCT_CLUSTER_BACKEND="${params.backend}"
-
-    export SCT_INSTANCE_PROVISION=${params.provision_type}
-
+    if [[ -n "${params.provision_type ? params.provision_type : ''}" ]] ; then
+        export SCT_INSTANCE_PROVISION=${params.provision_type}
+    fi
     if [[ -n "${params.region ? params.region : ''}" ]] ; then
         export SCT_REGION_NAME=${current_region}
     fi
