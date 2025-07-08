2cb3e6dc5d75b76e74bd95bf21c5d6acc967a9fd
Liapkovich <mikita.liapkovich@scylladb.com>
2025-07-08 15:43:20 +0200

    ci(manager): up Scylla version used in Manager tests
    - get rid of not anymore supported Scylla 2024.2
    - use Scylla 2025.2 by default if other version isn't provided

--- vars/managerPipeline.groovy
+++ vars/managerPipeline.groovy
@@ -74,9 +74,10 @@ def call(Map pipelineParams) {
                description: 'Availability zone',
                name: 'availability_zone')
 
+            separator(name: 'SCYLLA_DB', sectionHeader: 'ScyllaDB Configuration Selection')
+            string(defaultValue: '', description: 'AMI ID for ScyllaDB ', name: 'scylla_ami_id')
+            string(defaultValue: "${pipelineParams.get('scylla_version', '2025.2')}", description: '', name: 'scylla_version')
 
-            string(defaultValue: '', description: '', name: 'scylla_ami_id')
-            string(defaultValue: "${pipelineParams.get('scylla_version', '2025.1')}", description: '', name: 'scylla_version')
             // When branching to manager version branch, set scylla_version to the latest release
             string(defaultValue: '', description: '', name: 'scylla_repo')
             string(defaultValue: "${pipelineParams.get('gce_image_db', '')}",
