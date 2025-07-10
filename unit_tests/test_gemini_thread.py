042a6cca4b693183b842e4acb07ee9aa59f0ec79
Dusan Malusev <dusan@dusanmalusev.dev>
2025-07-10 16:00:58 +0200

    feature(gemini): use new gemini version 2.0
    - Enable Statement Logger on all workloads
       statements are logged now in Oracle Cluster,
       this removes the problem, performance bottlenck of logging to file
       and gives us better tracability
    
    - Removing --fail-fast flag, this flag still exists in gemini but is not
      used for anything, every workload should migrate to
      --max-errors-to-store flags, which gives better control if and how
      gemini will fail
    
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- unit_tests/test_gemini_thread.py
+++ unit_tests/test_gemini_thread.py
@@ -61,41 +61,9 @@ def test_01_gemini_thread(request, docker_scylla, params):
         gemini_thread.kill()
 
     request.addfinalizer(cleanup_thread)
-
-    default_options = [
-        "--oracle-replication-strategy=\"{'class': 'NetworkTopologyStrategy', 'replication_factor': '1'}\"",
-        "--table-options=\"gc_grace_seconds=60\"",
-        "--table-options=\"cdc = {'enabled': true, 'ttl': 0}\"",
-        "--level=info",
-        "--request-timeout=3s",
-        "--connect-timeout=60s",
-        "--consistency=QUORUM",
-        "--async-objects-stabilization-backoff=10ms",
-        "--async-objects-stabilization-attempts=10",
-        "--dataset-size=large",
-        "--oracle-host-selection-policy=token-aware",
-        "--test-host-selection-policy=token-aware",
-        "--drop-schema=true",
-        "--fail-fast=true",
-        "--materialized-views=false",
-        "--use-lwt=false",
-        "--use-counters=false",
-        "--max-tables=1",
-        "--max-columns=16",
-        "--min-columns=8",
-        "--max-partition-keys=6",
-        "--min-partition-keys=2",
-        "--max-clustering-keys=4",
-        "--min-clustering-keys=2",
-        "--partition-key-distribution=uniform",
-        "--token-range-slices=10000",
-        "--partition-key-buffer-reuse-size=128",
-        "--statement-log-file-compression=zstd",
-    ]
-
     gemini_cmd = gemini_thread._generate_gemini_command()
 
-    for option in default_options + options:
+    for option in options:
         assert option in gemini_cmd
 
     gemini_thread.run()
