820711b24294e2bd2e30661adee71c32b609d761
Israel Fruchter <fruch@scylladb.com>
2025-07-13 10:41:13 +0300

    feature(loaders): update all loader to ubuntu 24.04 latest
    since the version being used are not supported anymore, and failing
    to install offical docker packages.
    
    we are updating all of the loader in all cloud and aligning them
    to take latest release of ubunutu 24.04
    
    (cherry picked from commit 8cafb01b479e3847d3b8832347a35c65de71c842)

--- unit_tests/test_aws_services.py
+++ unit_tests/test_aws_services.py
@@ -18,6 +18,7 @@ from sdcm.sct_provision.common.layout import SCTProvisionLayout, create_sct_conf
 from sdcm.utils.common import get_scylla_ami_versions
 from sdcm.ec2_client import EC2ClientWrapper
 from sdcm.utils.aws_utils import tags_as_ec2_tags
+from sdcm.utils.context_managers import environment
 
 AWS_REGION = "us-east-1"
 
@@ -101,23 +102,25 @@ def test_03_provision(aws_region: AwsRegion, instance_provision: str) -> None:
     # test AWS provision flow
 
     # TODO: switch all this to configuration yaml, it would be clear and easier to maintain
-    os.environ['SCT_CLUSTER_BACKEND'] = 'aws'
-    os.environ['SCT_REGION_NAME'] = 'us-east-1'
-    os.environ['SCT_AMI_ID_DB_SCYLLA'] = 'ami-760aaa0f'
-    os.environ['SCT_INSTANCE_TYPE_DB'] = 'm5.xlarge'
-    os.environ['SCT_N_DB_NODES'] = '3'
-    os.environ['SCT_N_MONITORS_NODES'] = '1'
-    os.environ['SCT_N_LOADERS'] = '1'
-    os.environ['SCT_LOGS_TRANSPORT'] = 'ssh'
-    os.environ['SCT_INSTANCE_PROVISION'] = instance_provision
+    with environment(
+        SCT_CLUSTER_BACKEND='aws',
+        SCT_REGION_NAME='us-east-1',
+        SCT_AMI_ID_DB_SCYLLA='ami-760aaa0f',
+        SCT_INSTANCE_TYPE_DB='m5.xlarge',
+        SCT_N_DB_NODES='3',
+        SCT_N_MONITORS_NODES='1',
+        SCT_N_LOADERS='1',
+        SCT_LOGS_TRANSPORT='ssh',
+        SCT_INSTANCE_PROVISION=instance_provision,
+        # we need to set the monitor id, otherwise it will fail on every update of it
+        SCT_AMI_ID_MONITOR='scylladb-monitor-4-8-0-2024-08-06t03-34-43z',
+        # switch to a specific image that has ssm information backed into moto itself
+            SCT_AMI_ID_LOADER='resolve:ssm:/aws/service/ami-amazon-linux-latest/amzn-ami-hvm-x86_64-ebs:73'):
 
-    # we need to set the monitor id, otherwise it will fail on every update of it
-    os.environ['SCT_AMI_ID_MONITOR'] = 'scylladb-monitor-4-8-0-2024-08-06t03-34-43z'
+        params = create_sct_configuration('test_04_provision')
 
-    params = create_sct_configuration('test_04_provision')
-
-    layout = SCTProvisionLayout(params=params)
-    layout.provision()
+        layout = SCTProvisionLayout(params=params)
+        layout.provision()
 
     # TODO: add some checks here, like checking if the instances are existing running in moto
 
