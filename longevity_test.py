2384b4fdd35fcfb477f4bb0366e557c9c82ebdde
Aleksandr Bykov <alex.bykov@scylladb.com>
2025-07-22 14:50:45 +0700

    fix(test): add infoevent with stats)

--- longevity_test.py
+++ longevity_test.py
@@ -587,6 +587,8 @@ class LongevityTest(ClusterTester, loader_utils.LoaderUtilsMixin):
                 nodes_by_dcx = group_nodes_by_dc_idx(self.db_cluster.data_nodes)
                 current_cluster_size = [len(nodes_by_dcx[dcx]) for dcx in sorted(nodes_by_dcx)]
                 if current_cluster_size[0] % 10 == 0 and current_cluster_size[0] < 199:
+                    InfoEvent(f"Nodes startup stats: {getattr(self.db_cluster, 'nodeup_stats', {})}").publish()
+                    InfoEvent("----------------Truncate cdc tables---------------").publish()
                     with self.db_cluster.cql_connection_patient(node=self.db_cluster.nodes[0]) as session:
                         session.execute("TRUNCATE system_distributed.cdc_generation_timestamps;")
                         session.execute("TRUNCATE system_distributed.cdc_streams_descriptions_v2;")
