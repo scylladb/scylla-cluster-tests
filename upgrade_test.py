a46f7466ce72ccee365e6d06e6e2091eeba310ae
Dusan Malusev <dusan@dusanmalusev.dev>
2025-07-29 12:27:43 +0200

    fix(gemini): metrics not beining read properly after 2.0
    In 2.0 gemini, more thing were added to `gemini_cql_requests` metrics,
    mainly the `method` which contained what statement type is executed.
    This broke the upgrade tests, as in upgrade test the first value
    returned from prometheus is used to check `> 0`, but the prometheus
    ordered the metric labels in alphabetical order, making the metric
    currently checked to be *almost* always zero, thus failing the check and
    killing gemini thinking it's not running (but it actually is).
    
    Now fetching the `InsertStatement` label makes sure that this check will
    succeed (maybe not on the first try as it is not yet inserting, but after
    a couple of try will succeed).
    
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- upgrade_test.py
+++ upgrade_test.py
@@ -298,7 +298,7 @@ class UpgradeTest(FillDatabaseData, loader_utils.LoaderUtilsMixin):
                         self.params.scylla_version_upgrade_target)):
                 InfoEvent(
                     message=f'upgrade_node - orig is enterprise and version {self.orig_ver} < 2025.1.0~dev and '
-                            f'scylla_version_upgrade_target is {self.params.scylla_version_upgrade_target}').publish()
+                    f'scylla_version_upgrade_target is {self.params.scylla_version_upgrade_target}').publish()
                 scylla_pkg = 'scylla'
                 scylla_pkg_ver = f"{scylla_pkg}{ver_suffix}"
                 InfoEvent(message='Rollback mode is set to reinstall').publish()
@@ -562,7 +562,7 @@ class UpgradeTest(FillDatabaseData, loader_utils.LoaderUtilsMixin):
         if idx_token_error:
             IndexSpecialColumnErrorEvent(
                 message=f'Node: {node.name}. Step: {step}. '
-                        f'Found error: index special column "idx_token" is not recognized'
+                f'Found error: index special column "idx_token" is not recognized'
             ).publish()
 
     @staticmethod
@@ -657,7 +657,7 @@ class UpgradeTest(FillDatabaseData, loader_utils.LoaderUtilsMixin):
             gemini_cmd += " --table-options \"cdc={'enabled': true}\""
         gemini_thread = self.run_gemini(gemini_cmd)
         self.metric_has_data(
-            metric_query='gemini_cql_requests', n=10)
+            metric_query='sum(increase(gemini_cql_requests[1m]))', n=10)
 
         with ignore_upgrade_schema_errors():
 
