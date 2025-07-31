36ee16717715ac684fc415801120c0641697a82d
yarongilor <yarongilor@scylla.com>
2025-07-31 15:13:41 +0300

    fix(logdir import): use lazy import of sdcm.mgmt.operations
    Since nemesis.py importing of sdcm.mgmt.operations also imports tester.py,
    it causes re-linking sct logdir to a wrong directory of other pipeline step.
    Avoiding that by using a lazy import inside _run_manager_backup function.
    Fixes: #11546

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -71,7 +71,6 @@ from sdcm.db_stats import PrometheusDBStats
 from sdcm.log import SDCMAdapter
 from sdcm.logcollector import save_kallsyms_map
 from sdcm.mgmt.common import TaskStatus, ScyllaManagerError, get_persistent_snapshots, ObjectStorageUploadMode
-from sdcm.mgmt.operations import run_manager_backup
 from sdcm.mgmt.argus_report import report_manager_backup_results_to_argus
 from sdcm.mgmt.helpers import get_dc_name_from_ks_statement, get_schema_create_statements_from_snapshot
 from sdcm.nemesis_publisher import NemesisElasticSearchPublisher
@@ -2856,6 +2855,7 @@ class Nemesis(NemesisFlags):
         disrupt_func()
 
     def _run_manager_backup(self, mgr_cluster, object_storage_upload_mode: ObjectStorageUploadMode, timeout: int) -> BackupTask:
+        from sdcm.mgmt.operations import run_manager_backup
         task = run_manager_backup(mgr_cluster, self.tester.locations, object_storage_upload_mode, timeout)
         return task
 
