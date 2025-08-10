scylladb/scylla-cluster-tests

commit c2ae668cf9193ae7e3bf88ce2047557828225435

improvement(trigger): send ami name instead of scylla_version for master
If scylla_version is 'master:latest', an AMI name must be provided.
Subtests currently run sequentially, not in parallel. This can lead to inconsistencies where
initial subtests use an older AMI while subsequent ones use a newly created AMI (if an update
occurs during the first subtest's execution). To ensure all subtests run with the same version,
we should send an AMI ID and execute all subtests with that specific version.

vars/perfRegressionParallelPipelinebyRegion.groovy
@@ -6,6 +6,11 @@ def call(Map pipelineParams) {
                    label builder.label
             }
         }
+        environment {
+            AWS_ACCESS_KEY_ID     = credentials('qa-aws-secret-key-id')
+            AWS_SECRET_ACCESS_KEY = credentials('qa-aws-secret-access-key')
+        }
+
         parameters {
             string(name: 'scylla_version', defaultValue: '', description: 'Scylla version to test')
             string(name: 'base_versions', defaultValue: '', description: 'Base versions')
@@ -21,19 +26,20 @@ def call(Map pipelineParams) {
                 '''
             )
         }
+
         stages {
             stage('Get Scylla Version') {
                 steps {
                     script {
                         def scylla_version = params.scylla_version?.trim()
                         def labels_selector = params.labels_selector?.trim()
-                        println("Scylla version: ${scylla_version}")
-                        println("Labels selector: $labels_selector")
                         if (scylla_version == "master:latest") {
                             scylla_version = "master"
                             if (!labels_selector) {
                                 error "Labels selector is not set. Please provide one of a valid 'labels_selector' values: 'master-weekly' OR 'master-daily' OR 'master-3weeks'."
                             }
+
+
                         }
                         def testRegionMatrix = [
                             [
@@ -170,21 +176,34 @@ def call(Map pipelineParams) {
                         println("testRegionMatrix: $testRegionMatrix")
                         def jobs_names = testRegionMatrix*.job_name.toSet()
                         println("Jobs names: $jobs_names")
+                        def image_name = null
                         for (job_name in jobs_names) {
                             println("Job name: $job_name")
                             for (def entry in testRegionMatrix) {
-                                def version = null
-                                def sub_tests = []
-                                def region = null
+                                 def cloud_provider = entry.cloud_provider ?: 'aws'
+                                 def version = null
+                                 def sub_tests = []
+                                 def region = null
+                                 if (scylla_version == "master" && !image_name){
+                                    region = entry.region ?: 'us-east-1'
+                                    def output = sh(script: "./docker/env/hydra.sh list-images -c ${cloud_provider} -r ${region} -o text", returnStdout: true).trim()
+                                    println("Output from hydra list-images: $output")
+                                    def image_name_json = output.split('\n')[-1].trim()
+                                    println("Image name json: $image_name_json")
+                                    if (!image_name_json){
+                                        error "Image name is empty. Please check the hydra.sh command output."
+                                    }
+
+                                    image_name = new groovy.json.JsonSlurper().parseText(image_name_json).keySet()[0]
+                                    println("Image name: $image_name")
+                                 }
+
                                 if (entry.job_name == job_name) {
                                     println("job_name: ${entry.job_name}, sub_tests: ${entry.sub_tests}")
                                     for (def ver in entry.versions) {
-                                        println("Checking version: $ver against scylla_version: $scylla_version")
-                                        println("If scylla_version is needed: ${scylla_version?.trim() == ver || scylla_version?.trim().startsWith(ver + ".")}")
                                         if (scylla_version?.trim() == ver || scylla_version?.trim().startsWith(ver + ".")) {
                                             version = params.scylla_version
                                         }
-                                        println("Is scylla_version supported: $version")
                                     if (version) {
                                         if (labels_selector && !(entry.labels.contains(labels_selector))) {
                                             println("Skipping job $job_name for labels_selector: $labels_selector")
@@ -202,7 +221,8 @@ def call(Map pipelineParams) {
                                 catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
                                     println("Building job: $job_name with sub_test: ${sub_tests}, region: ${region}")
                                         build job: job_name, wait: false, parameters: [
-                                            string(name: 'scylla_version', value: params.scylla_version),
+                                            string(name: 'scylla_version', value: image_name ? null : params.scylla_version),
+                                            string(name: 'scylla_ami_id', value: image_name ? image_name : null),
                                             string(name: 'base_versions', value: rolling_upgrade_test ? params.base_versions : null),
                                             string(name: 'provision_type', value: 'on_demand'),
                                             string(name: 'new_scylla_repo', value: rolling_upgrade_test ? params.new_scylla_repo : null),

sct.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
@@ -84,7 +83,7 @@ from sdcm.utils.common import (
     list_resources_docker,
     list_parallel_timelines_report_urls,
     search_test_id_in_latest,
-    get_latest_scylla_release,
+    get_latest_scylla_release, images_dict_in_json_format,
 )
 from sdcm.utils.nemesis_generation import generate_nemesis_yaml, NemesisJobGenerator
 from sdcm.utils.open_with_diff import OpenWithDiff, ErrorCarrier
@@ -697,7 +696,11 @@ def list_resources(ctx, user, test_id, get_all, get_all_running, verbose, backen
               type=click.Choice(AwsArchType.__args__),
               default='x86_64',
               help="architecture of the AMI (default: x86_64)")
-def list_images(cloud_provider: str, branch: str, version: str, regions: List[str], arch: AwsArchType):
+@click.option('-o', '--output-format',
+              type=str,
+              default='table',
+              help="")
+def list_images(cloud_provider: str, branch: str, version: str, regions: List[str], arch: AwsArchType, output_format: str = "table"):  # noqa: PLR0912
     if len(regions) == 0:
         regions = [NemesisJobGenerator.BACKEND_TO_REGION[cloud_provider]]
     add_file_logger()
@@ -719,20 +722,28 @@ def list_images(cloud_provider: str, branch: str, version: str, regions: List[st
             match cloud_provider:
                 case "aws":
                     rows = get_ami_images_versioned(region_name=region, arch=arch, version=version)
-                    click.echo(
-                        create_pretty_table(rows=rows, field_names=version_fields_with_tag_name).get_string(
-                            title=f"AWS Machine Images by Version in region {region}")
-                    )
+                    if output_format == "table":
+                        click.echo(
+                            create_pretty_table(rows=rows, field_names=version_fields_with_tag_name).get_string(
+                                title=f"AWS Machine Images by Version in region {region}")
+                        )
+                    elif output_format == "text":
+                        ami_images_json = images_dict_in_json_format(
+                            rows=rows, field_names=version_fields_with_tag_name)
+                        click.echo(ami_images_json)
                 case "gce":
                     if arch:
                         #  TODO: align branch and version fields once scylla-pkg#2995 is resolved
                         click.echo("WARNING:--arch option not implemented currently for GCE machine images.")
                     rows = get_gce_images_versioned(version=version)
-
-                    click.echo(
-                        create_pretty_table(rows=rows, field_names=version_fields).get_string(
-                            title="GCE Machine Images by version")
-                    )
+                    if output_format == "table":
+                        click.echo(
+                            create_pretty_table(rows=rows, field_names=version_fields).get_string(
+                                title="GCE Machine Images by version")
+                        )
+                    elif output_format == "text":
+                        gce_images_json = images_dict_in_json_format(rows=rows, field_names=version_fields)
+                        click.echo(gce_images_json)
                 case "azure":
                     if arch:
                         click.echo("WARNING:--arch option not implemented currently for Azure machine images.")
@@ -740,10 +751,15 @@ def list_images(cloud_provider: str, branch: str, version: str, regions: List[st
                     rows = []
                     for image in azure_images:
                         rows.append(['Azure', image.name, image.unique_id, 'N/A'])
-                    click.echo(
-                        create_pretty_table(rows=rows, field_names=version_fields).get_string(
-                            title="Azure Machine Images by version")
-                    )
+
+                    if output_format == "table":
+                        click.echo(
+                            create_pretty_table(rows=rows, field_names=version_fields).get_string(
+                                title="Azure Machine Images by version")
+                        )
+                    elif output_format == "text":
+                        azure_images_json = images_dict_in_json_format(rows=rows, field_names=version_fields)
+                        click.echo(azure_images_json)
 
                 case _:
                     click.echo(f"Cloud provider {cloud_provider} is not supported")
@@ -755,18 +771,27 @@ def list_images(cloud_provider: str, branch: str, version: str, regions: List[st
             match cloud_provider:
                 case "aws":
                     ami_images = get_ami_images(branch=branch, region=region, arch=arch)
-                    click.echo(
-                        create_pretty_table(rows=ami_images, field_names=branch_fields_with_tag_name).get_string(
-                            title=f"AMI Machine Images for {branch} in region {region}"
+                    if output_format == "table":
+                        click.echo(
+                            create_pretty_table(rows=ami_images, field_names=branch_fields_with_tag_name).get_string(
+                                title=f"AMI Machine Images for {branch} in region {region}"
+                            )
                         )
-                    )
+                    elif output_format == "text":
+                        ami_images_json = images_dict_in_json_format(
+                            rows=ami_images, field_names=branch_fields_with_tag_name)
+                        click.echo(ami_images_json)
                 case "gce":
                     gce_images = get_gce_images(branch=branch, arch=arch)
-                    click.echo(
-                        create_pretty_table(rows=gce_images, field_names=branch_fields).get_string(
-                            title=f"GCE Machine Images for {branch}"
+                    if output_format == "table":
+                        click.echo(
+                            create_pretty_table(rows=gce_images, field_names=branch_fields).get_string(
+                                title=f"GCE Machine Images for {branch}"
+                            )
                         )
-                    )
+                    elif output_format == "text":
+                        gce_images_json = images_dict_in_json_format(rows=gce_images, field_names=branch_fields)
+                        click.echo(gce_images_json)
                 case "azure":
                     if arch:
                         click.echo("WARNING:--arch option not implemented currently for Azure machine images.")
@@ -774,11 +799,15 @@ def list_images(cloud_provider: str, branch: str, version: str, regions: List[st
                     rows = []
                     for image in azure_images:
                         rows.append(['Azure', image.name, image.id, 'N/A'])
-                    click.echo(
-                        create_pretty_table(rows=rows, field_names=version_fields).get_string(
-                            title="Azure Machine Images by version")
-                    )
 
+                    if output_format == "table":
+                        click.echo(
+                            create_pretty_table(rows=rows, field_names=version_fields).get_string(
+                                title="Azure Machine Images by version")
+                        )
+                    elif output_format == "text":
+                        azure_images_json = images_dict_in_json_format(rows=rows, field_names=version_fields)
+                        click.echo(azure_images_json)
                 case _:
                     click.echo(f"Cloud provider {cloud_provider} is not supported")
 

sdcm/utils/common.py
@@ -16,6 +16,7 @@ from __future__ import absolute_import, annotations
 
 import atexit
 import itertools
+import json
 import os
 import logging
 import random
@@ -1545,6 +1546,24 @@ def create_pretty_table(rows: list[str] | list[list[str]], field_names: list[str
         tbl.add_row(row)
 
     return tbl
+
+
+def images_dict_in_json_format(rows: list[str] | list[list[str]], field_names: list[str]) -> str:
+    """
+    Convert the rows to a JSON format string.
+    :param rows: list of rows to convert
+    :param field_names: list of field names for the JSON keys
+    :return: JSON formatted string
+    """
+    if not rows:
+        return "{"
+
+    images_dict = {}
+    for image in rows:
+        images_dict.update({image[1]: {field: image[num] for num, field in enumerate(field_names)}})
+    images_json = json.dumps(images_dict)
+
+    return images_json
 
 
 def get_branched_gce_images(

commit 689bf4e55d467c73fcb78416fbb17d4aee9f5a9d

fix(trigger): correct job parameters for rolling upgrade and microbenchmark tests
This change fixes two issues introduced in PR https://github.com/scylladb/scylla-cluster-tests/pull/11574
that caused test failures.

1.  Rolling Upgrade Test: The 'scylla_ami_id parameter was being passed incorrectly. It should be
'null' to ensure the test uses the correct AMI.
2.  Microbenchmark AMD Test: The wrong image ('x86') was being used for this test. The fix ensures
the correct AMD image is used.

Additionally, since the microbenchmark test is short, it no longer needs to wait for another run
to complete and can now be started directly with 'master:latest'.

vars/perfRegressionParallelPipelinebyRegion.groovy
@@ -90,28 +90,32 @@ def call(Map pipelineParams) {
                                 region: 'us-east-1',
                                 versions: ['2024.1', '2024.2', '2025.1', '2025.2', 'master'],
                                 sub_tests: ['microbenchmark'],
-                                labels: ['master-weekly']
+                                labels: ['master-weekly'],
+                                microbenchmark: true
                             ],
                             [
                                 job_name: 'scylla-enterprise/perf-regression/scylla-enterprise-perf-simple-query-weekly-microbenchmark_arm64-write',
                                 region: 'us-east-1',
                                 versions: ['2024.1', '2024.2', '2025.1', '2025.2', 'master'],
                                 sub_tests: ['microbenchmark'],
-                                labels: ['master-weekly']
+                                labels: ['master-weekly'],
+                                microbenchmark: true
                             ],
                             [
                                 job_name: 'scylla-enterprise/perf-regression/scylla-enterprise-perf-simple-query-weekly-microbenchmark_x86_64',
                                 region: 'us-east-1',
                                 versions: ['2024.1', '2024.2', '2025.1', '2025.2', 'master'],
                                 sub_tests: ['microbenchmark'],
-                                labels: ['master-weekly']
+                                labels: ['master-weekly'],
+                                microbenchmark: true
                             ],
                             [
                                 job_name: 'scylla-enterprise/perf-regression/scylla-enterprise-perf-simple-query-weekly-microbenchmark_x86_64-write',
                                 region: 'us-east-1',
                                 versions: ['2024.1', '2024.2', '2025.1', '2025.2', 'master'],
                                 sub_tests: ['microbenchmark'],
-                                labels: ['master-weekly']
+                                labels: ['master-weekly'],
+                                microbenchmark: true
                             ],
                             [
                                 job_name: 'scylla-master/perf-regression/perf-regression-predefined-throughput-steps-sanity-vnodes',
@@ -184,6 +188,7 @@ def call(Map pipelineParams) {
                                  def version = null
                                  def sub_tests = []
                                  def region = null
+                                 def image_name_for_job = null
                                  if (scylla_version == "master" && !image_name){
                                     region = entry.region ?: 'us-east-1'
                                     def output = sh(script: "./docker/env/hydra.sh list-images -c ${cloud_provider} -r ${region} -o text", returnStdout: true).trim()
@@ -215,14 +220,21 @@ def call(Map pipelineParams) {
                                         break
                                     }
                                     rolling_upgrade_test = entry.rolling_upgrade_test
+                                    microbenchmark = entry.microbenchmark
+                                    if (rolling_upgrade_test || entry.microbenchmark) {
+                                        image_name_for_job = null
+                                    } else {
+                                        image_name_for_job = image_name
+                                    }
                                 }
                             }
                             if (region && version && sub_tests) {
                                 catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
-                                    println("Building job: $job_name with sub_test: ${sub_tests}, region: ${region}")
+                                    println("Building job: $job_name with sub_test: ${sub_tests}, region: ${region}, image_name_for_job: ${image_name_for_job}, scylla_version: ${version}")
+                                    println("Send to job: scylla_version: ${rolling_upgrade_test ? null : (image_name_for_job ? null : params.scylla_version)}; scylla_ami_id: ${image_name_for_job ? image_name_for_job : null}")
                                         build job: job_name, wait: false, parameters: [
-                                            string(name: 'scylla_version', value: image_name ? null : params.scylla_version),
-                                            string(name: 'scylla_ami_id', value: image_name ? image_name : null),
+                                            string(name: 'scylla_version', value: rolling_upgrade_test ? null : (image_name_for_job ? null : params.scylla_version)),
+                                            string(name: 'scylla_ami_id', value: image_name_for_job ? image_name_for_job : null),
                                             string(name: 'base_versions', value: rolling_upgrade_test ? params.base_versions : null),
                                             string(name: 'provision_type', value: 'on_demand'),
                                             string(name: 'new_scylla_repo', value: rolling_upgrade_test ? params.new_scylla_repo : null),