986944ea70f2cc3a985fe4175611d52ec02a8fd7
Szymon Malewski <szymon.malewski@scylladb.com>
2025-08-19 13:10:36 +0200

    feature(mgmt_cli_test.py): adds test of Scylla Manager Alternator restore
    This patch adds `test_alternator_backup_feature` which checks Scylla Manager's feature
    for backing up and restoring Alternator data.
    In general it copies workflow of previous CQL backup test `test_restore_backup_with_task`, but:
    - it pre-creates Alternator table,
    - it fills table and runs post-restore check using YSCB,
    - table is deleted between backup and restore
    
    Besides plain table+data restore, it is ready to check additional features:
    - restoring table tags
    - table name including special characters
    - restoring table with secondary indexes
    
    Restored index content could be tested with:
        - YCSB updated to support index in read command (https://github.com/scylladb/YCSB/pull/20)
        - stress commands in `test-cases/manager/manager-regression-alternator-singleDC-set-distro.yaml`.
    Before running stress commands, test waits for the indexes to achieve expected number of records.
    
    When defined in configuration, the tests passes to Scylla Manager additional command line parameters
     `--alternator-access-key-id` `--alternator-secret-access-key`.
    
    Fixes #11696

--- sdcm/utils/alternator/__init__.py
+++ sdcm/utils/alternator/__init__.py
@@ -3,4 +3,4 @@ from sdcm.utils.alternator import consts
 from sdcm.utils.alternator import enums
 from sdcm.utils.alternator import schemas
 
-__all__ = ["api", "consts", "enums", "schemas"]
+__all__ = ["api", "consts", "enums", "schemas", "table_setup"]
