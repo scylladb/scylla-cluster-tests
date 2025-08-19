946c44928183d693a0f344c7f79e3ab4252b3110
Liapkovich <mikita.liapkovich@scylladb.com>
2025-08-19 13:22:21 +0200

    fix(manager): make 3.6.0 a default version of Manager
    As the new minor release of Manager - 3.6.0 - is here, it is set to
    be a default one used in tests.
    
    test_sdcm_mgmt_common.py tests and managerPipeline.groovy have been
    updated accordingly.
    
    (cherry picked from commit b514c3938ab063d114ace02fcb5054c4c95c7ac3)

--- vars/managerPipeline.groovy
+++ vars/managerPipeline.groovy
@@ -109,7 +109,7 @@ def call(Map pipelineParams) {
                    name: 'ip_ssh_connections')
             separator(name: 'MANAGER_CONFIG', sectionHeader: 'Manager Configuration')
             string(defaultValue: "${pipelineParams.get('manager_version', 'master_latest')}",
-                   description: 'master_latest|3.5|3.4',
+                   description: 'master_latest|3.6|3.5',
                    name: 'manager_version')
 
             string(defaultValue: "${pipelineParams.get('scylla_mgmt_address', '')}",
@@ -125,7 +125,7 @@ def call(Map pipelineParams) {
                    name: 'scylla_mgmt_pkg')
 
             string(defaultValue: "${pipelineParams.get('target_manager_version', '')}",
-                   description: 'master_latest|3.4|3.3. Only for upgrade test',
+                   description: 'master_latest|3.6|3.5. Only for upgrade test',
                    name: 'target_manager_version')
 
             string(defaultValue: "${pipelineParams.get('target_scylla_mgmt_server_address', '')}",
