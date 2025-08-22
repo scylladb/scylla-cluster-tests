b1e6d14f5c99e838cca7f9adda6dd86f5968aa6e
Eugene Abramchuk <yevhen.abramchuk@scylladb.com>
2025-08-22 15:02:23 +0200

    fix: Inject Docker authentication secret on helm install
    Part of https://github.com/scylladb/scylla-cluster-tests/issues/11553

--- sdcm/utils/k8s/__init__.py
+++ sdcm/utils/k8s/__init__.py
@@ -758,6 +758,8 @@ class HelmContainerMixin:
         if timeout is not None:
             # timeout in Go duration format
             command.extend(("--timeout", timeout))
+        # Inject Docker Hub authentication secrets
+        command.extend("--set 'imagePullSecrets[0].name=docker-auth'")
         return self.helm(
             kluster,
             *command,
