556564809d107dd7d8e7a92b5c13e10f2dc0673c
Israel Fruchter <fruch@scylladb.com>
2025-08-28 12:18:57 +0300

    test(internal): use private fork/branch, TODO: remove

--- vars/checkoutQaInternal.groovy
+++ vars/checkoutQaInternal.groovy
@@ -2,8 +2,8 @@
 
 def call(Map params = [:]) {
     dir("scylla-qa-internal") {
-        git(url: 'git@github.com:scylladb/scylla-qa-internal.git',
+        git(url: 'git@github.com:fruch/scylla-qa-internal.git',
             credentialsId:'b8a774da-0e46-4c91-9f74-09caebaea261',
-            branch: 'master')
+            branch: 'xcloud_connectivity')
     }
 }
