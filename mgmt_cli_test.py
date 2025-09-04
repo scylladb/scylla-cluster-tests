ea6efa31c7bc475fb14304ea952f57e3da3b1fcb
Liapkovich <mikita.liapkovich@scylladb.com>
2025-09-04 15:48:07 +0200

    fix(mgmt_cli_test): conditionally restart Scylla after schema restore
    Restart Scylla only after schema restoration for versions 2024.1
    or lower. For the newest versions of Scylla it's not required.

--- mgmt_cli_test.py
+++ mgmt_cli_test.py
@@ -57,6 +57,7 @@ from sdcm.utils.compaction_ops import CompactionOps
 from sdcm.utils.gce_utils import get_gce_storage_client
 from sdcm.utils.azure_utils import AzureService
 from sdcm.utils.tablets.common import TabletsConfiguration
+from sdcm.utils.version_utils import ComparableScyllaVersion
 from sdcm.exceptions import FilesNotCorrupted
 
 
@@ -764,8 +765,11 @@ class ManagerTestFunctionsMixIn(
         assert restore_task.status == TaskStatus.DONE, f"Restoration of {snapshot_tag} has failed!"
         InfoEvent(message=f'The restore task has ended successfully. '
                   f'Restore run time: {restore_task.duration}.').publish()
-        if restore_schema:
-            self.db_cluster.restart_scylla()  # After schema restoration, you should restart the nodes
+
+        is_restart = restore_schema and ComparableScyllaVersion(self.db_cluster.nodes[0].scylla_version) <= "2024.1"
+        if is_restart:
+            self.db_cluster.restart_scylla()
+
         return restore_task
 
     def create_repair_and_alter_it_with_repair_control(self):
