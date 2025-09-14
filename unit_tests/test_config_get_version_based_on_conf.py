f7c9cc8f6fd73486ee8e92ac8d3045c5419c47d5
Israel Fruchter <fruch@scylladb.com>
2025-09-14 12:13:29 +0300

    fix(test_config_get_version_based_on_conf): remove old releases
    since there was no new builds for 6.2 for quite some time, the images
    for Azure are now gone.
    
    changing all of those test to retrive only from live releases, and mostly
    from LTS ones.
    
    (cherry picked from commit b7c60f9813a2041792e3dd23942e21700ffdea72)

--- unit_tests/test_config_get_version_based_on_conf.py
+++ unit_tests/test_config_get_version_based_on_conf.py
@@ -47,8 +47,8 @@ def function_setup():
 @pytest.mark.parametrize(
     argnames="scylla_version, expected_docker_image, expected_outcome",
     argvalues=[
-        pytest.param("6.1", "scylladb/scylla", ("6.1", False), id="6.1"),
         pytest.param("2024.1", "scylladb/scylla-enterprise", ("2024.1", True), id="2024.1"),
+        pytest.param("2025.1", "scylladb/scylla-enterprise", ("2025.1", True), id="2025.1"),
         pytest.param("latest", "scylladb/scylla-nightly", (None, False), id="latest"),
         pytest.param("master:latest", "scylladb/scylla-nightly", (None, False), id="master:latest"),
         pytest.param("enterprise", "scylladb/scylla-enterprise-nightly", (None, True), id="enterprise"),
@@ -74,12 +74,10 @@ def test_docker(scylla_version, expected_docker_image, expected_outcome):
 @pytest.mark.parametrize(
     argnames="scylla_version, expected_outcome",
     argvalues=[
-        pytest.param("6.1", ("6.1", False), id="6.1"),
         pytest.param("2024.1", ("2024.1", True), id="2024.1"),
         pytest.param("master:latest", (None, True), id="master"),
-        pytest.param("branch-6.0:latest", (None, False), id="branch-6.0"),
-        pytest.param("enterprise-2023.1:latest", (None, True), id="enterprise-2023.1"),
         pytest.param("enterprise-2024.1:latest", (None, True), id="enterprise-2024.1"),
+        pytest.param("branch-2025.1:latest", (None, True), id="branch-2025.1"),
     ],
 )
 def test_scylla_repo(scylla_version, expected_outcome, distro):
@@ -103,12 +101,11 @@ def test_scylla_repo(scylla_version, expected_outcome, distro):
 @pytest.mark.parametrize(
     argnames="scylla_version, expected_outcome",
     argvalues=[
-        pytest.param("6.2", ("6.2", False), id="6.2"),
-        pytest.param("2024.2", ("2024.2", True), id="2024.2"),
+        pytest.param("2024.1", ("2024.1", True), id="2024.1"),
+        pytest.param("2025.1", ("2025.1", True), id="2025.1"),
         pytest.param("master:latest", (None, True), id="master"),
-        pytest.param("branch-6.2:latest", (None, False), id="branch-6.2"),
         pytest.param("branch-2024.1:latest", (None, True), id="branch-2024.1"),
-        pytest.param("branch-2024.2:latest", (None, True), id="branch-2024.2"),
+        pytest.param("branch-2025.1:latest", (None, True), id="branch-2025.1"),
     ],
 )
 @pytest.mark.parametrize(argnames="backend", argvalues=("aws", "gce", "azure"))
