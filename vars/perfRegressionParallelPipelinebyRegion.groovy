d0921f898b4e53b96a75367c11eb131895edcd70
Israel Fruchter <fruch@scylladb.com>
2025-09-21 15:09:14 +0300

    feature(triggers): add owner to all weekly triggers
    since we want the resources of those test to be tagged base on
    actual users, and not `timer` anymore, we are adding those
    into all of the triggers for the weekly runs

--- vars/perfRegressionParallelPipelinebyRegion.groovy
+++ vars/perfRegressionParallelPipelinebyRegion.groovy
@@ -17,13 +17,16 @@ def call(Map pipelineParams) {
             string(name: 'new_scylla_repo', defaultValue: 'https://downloads.scylladb.com/unstable/scylla/master/deb/unified/latest/scylladb-master/scylla.list', description: 'New Scylla repo')
             booleanParam(name: 'use_job_throttling', defaultValue: true, description: 'if true, use job throttling to limit the number of concurrent builds')
             string(name: 'labels_selector', defaultValue: '', description: 'This parameter is used for trigger with Scylla master version only. It points how to trigger the test: daily, weekly ot once in 3 weeks. Expected values: master-3weeks OR master-weekly OR master-daily')
+            string(defaultValue: '',
+                   description: 'Actual user requesting job start, for automated job builds (e.g. through Argus)',
+                   name: 'requested_by_user')
         }
         triggers {
             parameterizedCron (
                 '''
-                    0 8 * * * %scylla_version=master:latest;labels_selector=alternator-daily
-                    00 6 * * 0 %scylla_version=master:latest;labels_selector=master-weekly
-                    0 23 */21 * * %scylla_version=master:latest;labels_selector=master-3weeks
+                    0 8 * * * %scylla_version=master:latest;labels_selector=alternator-daily;requested_by_user=radoslawcybulski
+                    00 6 * * 0 %scylla_version=master:latest;labels_selector=master-weekly;requested_by_user=juliayakovlev
+                    0 23 */21 * * %scylla_version=master:latest;labels_selector=master-3weeks;requested_by_user=juliayakovlev
                 '''
             )
         }
@@ -250,7 +253,8 @@ def call(Map pipelineParams) {
                                             string(name: 'new_scylla_repo', value: rolling_upgrade_test ? params.new_scylla_repo : null),
                                             booleanParam(name: 'use_job_throttling', value: params.use_job_throttling),
                                             string(name: 'sub_tests', value: groovy.json.JsonOutput.toJson(sub_tests)),
-                                            string(name: 'region', value: region)
+                                            string(name: 'region', value: region),
+                                            string(name: 'requested_by_user', value: params.requested_by_user)
                                         ]
                                     }
                                 }
