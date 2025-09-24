b2da515d5764a2cb870a65f21c9ece719bacb9b5
Yauheni Khatsianevich <yauheni.khatsianevich@scylladb.com>
2025-09-24 12:34:27 +0200

    fix(nemesis): changed the order of repair and remove node invocation
    Remove the dead node first and only then run repair. This avoids
    "Error: create repair target... "
    fixes:https://github.com/scylladb/scylla-cluster-tests/issues/11603

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -3769,6 +3769,10 @@ class Nemesis(NemesisFlags):
             node_to_remove.stop_scylla_server(verify_up=False, verify_down=True)
             self._terminate_cluster_node(node_to_remove)
 
+        wait_for(node_operations.is_node_seen_as_down, step=5, timeout=600, throw_exc=True,
+                 down_node=node_to_remove, verification_node=verification_node,
+                 text=f"Wait other nodes see {node_to_remove.name} as DOWN...")
+
         @retrying(n=3, sleep_time=5, message="Removing node from cluster...")
         def remove_node():
             removenode_reject_msg = r"Rejected removenode operation.*the node being removed is alive"
@@ -3782,13 +3786,6 @@ class Nemesis(NemesisFlags):
                     raise Exception(f"Removenode was rejected {res.stdout}\n{res.stderr}")
 
             return res.exit_status
-
-        # full cluster repair
-        up_normal_nodes = self.cluster.get_nodes_up_and_normal(verification_node)
-        # Repairing will result in a best effort repair due to the terminated node,
-        # and as a result requires ignoring repair errors
-        with DbEventsFilter(db_event=DatabaseLogEvent.RUNTIME_ERROR, line="failed to repair"):
-            self.run_repair_on_nodes(nodes=up_normal_nodes, ignore_down_hosts=True)
 
         with self.action_log_scope(f"Remove {node_to_remove.name} node"):
             exit_status = remove_node()
@@ -3834,6 +3831,13 @@ class Nemesis(NemesisFlags):
         # In order to eliminate cases that we miss a "data loss" bug because of it, we cleanup this data.
         # This fix important when just user profile is run in the test and "keyspace1" doesn't exist.
         self.nodetool_cleanup_on_all_nodes_parallel()
+
+        # full cluster repair
+        up_normal_nodes = self.cluster.get_nodes_up_and_normal(verification_node)
+        # Repairing will result in a best effort repair due to the terminated node,
+        # and as a result requires ignoring repair errors
+        with DbEventsFilter(db_event=DatabaseLogEvent.RUNTIME_ERROR, line="failed to repair"):
+            self.run_repair_on_nodes(nodes=up_normal_nodes, ignore_down_hosts=True)
 
     @target_all_nodes
     def disrupt_network_reject_inter_node_communication(self):
