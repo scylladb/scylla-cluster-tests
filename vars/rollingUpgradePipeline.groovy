c4c3f650438f289db002af90a37fd2da3bd514cf
Israel Fruchter <fruch@scylladb.com>
2025-11-23 11:33:41 +0200

    fix(rolling-upgrade-ami-arm): force iotune run
    since we start the test with version that doesn't support yet
    the i8g instances out of the box, we need to manauly run iotune
    
    we should continue doing so until 2026.1 is out and becomes the LTS
    
    (cherry picked from commit c0a14cd4ce85b3040583771326fff756efad92d6)

--- vars/rollingUpgradePipeline.groovy
+++ vars/rollingUpgradePipeline.groovy
@@ -108,7 +108,7 @@ def call(Map pipelineParams) {
             string(defaultValue: 'next',
                    description: 'Default branch to be used for scylla and other repositories. Default is "next".',
                    name: 'byo_default_branch')
-            string(defaultValue: '',
+            string(defaultValue: "${pipelineParams.get('extra_environment_variables', '')}",
                    description: 'Extra environment variables to inject (format: KEY1=VAL1\nKEY2=VAL2)',
                    name: 'extra_environment_variables')
         }
