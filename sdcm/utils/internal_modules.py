c3b98683c0e8a8d299ed8f27801a6b1a2e8ffe38
Dmitriy Kruglov <dmytro.kruglov@scylladb.com>
2025-12-03 13:35:27 +0100

    improvement(xcloud): add SSH connectivity support for VS nodes
    Add SSH connectivity for Vector Store nodes in a cloud cluster.
    Additionally, enable collecting logs from Vector Store nodes.
    
    Refs: https://github.com/scylladb/qa-tasks/issues/1991
    (cherry picked from commit a124f76175c04dc2128a46e54602f1b7b55d74c3)

--- sdcm/utils/internal_modules.py
+++ sdcm/utils/internal_modules.py
@@ -38,7 +38,10 @@ except ImportError:
         def xcloud_connect_get_ssh_address(self, node):
             raise NotImplementedError(not_supported_message)
 
-        def xcloud_connect_get_manager_ssh_address(self, cluster_id, sdm_environment):
+        def xcloud_connect_get_manager_ssh_address(self, node):
+            raise NotImplementedError(not_supported_message)
+
+        def xcloud_connect_get_vs_ssh_address(self, node):
             raise NotImplementedError(not_supported_message)
 
 __all__ = ["XCloudConnectivityContainerMixin"]
