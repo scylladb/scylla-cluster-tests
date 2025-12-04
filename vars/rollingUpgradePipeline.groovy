107b3e7a337f8e777e064f2f090cd131ddf58fca
Julia Yakovlev <julia.yakovlev@scylladb.com>
2025-12-04 10:57:57 +0200

    fix(pipeline): remove default availability zone
    There are 3 more pipelines have a default availability zone.
    Remove it
    
    (cherry picked from commit 57d23ec87dd27bc3c2aae4e0f046bcc1eb10b0db)

--- vars/rollingUpgradePipeline.groovy
+++ vars/rollingUpgradePipeline.groovy
@@ -31,7 +31,7 @@ def call(Map pipelineParams) {
             string(defaultValue: "${pipelineParams.get('gce_datacenter', 'us-east1')}",
                    description: 'GCE datacenter',
                    name: 'gce_datacenter')
-            string(defaultValue: "a",
+            string(defaultValue: "",
                description: 'Availability zone',
                name: 'availability_zone')
 
