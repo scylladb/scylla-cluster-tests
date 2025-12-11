4acb45e9c8ba6f2d4227e89ec5c9f9aa6f09403e
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2025-12-11 19:47:28 +0200

    fix(latte): do not copy non-file items in 'build_stress_cmd' method
    Fixes: https://github.com/scylladb/scylla-cluster-tests/issues/12734
    (cherry picked from commit 91137cb4c166eac12be5d99781094483cbf4a23f)

--- sdcm/stress/latte_thread.py
+++ sdcm/stress/latte_thread.py
@@ -104,7 +104,10 @@ class LatteStressThread(DockerBasedStressThread):
         script_name = script_name_regx.search(self.stress_cmd).group(0)
 
         for src_file in (Path(get_sct_root_path()) / script_name).parent.iterdir():
-            cmd_runner.send_files(str(src_file), str(Path(script_name).parent / src_file.name))
+            if src_file.is_file():
+                remote_path = str(Path(script_name).parent / src_file.name)
+                if not cmd_runner.run(f"test -f {remote_path}", ignore_status=True, verbose=False).ok:
+                    cmd_runner.send_files(str(src_file), remote_path)
 
         ssl_config = ''
         if self.params['client_encrypt']:
