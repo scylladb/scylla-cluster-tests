db687079bf21c6920f1985a52ecc1e8da772c088
Julia Yakovlev <julia.yakovlev@scylladb.com>
2026-01-05 09:55:06 +0200

    fix(collection log): builder log collection
    Fix a failure in the builder collection log when a mandatory parameter is defined in
    'extra_environment_variables'.
    Previously, the log failed with a 'ValueError' such as:
    'ValueError: perf_gradual_threads should be defined for performance throughput test.'
    This occurred because test configuration parameters were not passed to
    'vars/collectBuilderLogs.groovy'.
    The issue is resolved by passing the missing parameters.

--- vars/collectBuilderLogs.groovy
+++ vars/collectBuilderLogs.groovy
@@ -1,10 +1,13 @@
 #!groovy
 
-def call(){
+def call(Map params){
+    def test_config = groovy.json.JsonOutput.toJson(params.test_config)
     sh """#!/bin/bash
 
 	set -xe
 
+	echo "${params.test_config}"
+	export SCT_CONFIG_FILES=${test_config}
 	SHORT_SCT_TEST_ID=\$(echo \$SCT_TEST_ID | cut -c1-8)
     sudo journalctl --no-tail --no-pager -o short-precise > builder-\$SHORT_SCT_TEST_ID.log
     tar -zcvf builder-\$SHORT_SCT_TEST_ID.log.tar.gz builder-\$SHORT_SCT_TEST_ID.log
