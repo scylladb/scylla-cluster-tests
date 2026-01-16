1a67ae526f2e303068e94f28431a594ebf4f5f52
Dmitriy Kruglov <dmytro.kruglov@scylladb.com>
2026-01-16 17:16:04 +0100

    fix(temp): strongDM limit
    Temp commit to test strongDM client container limits.

--- vars/checkoutQaInternal.groovy
+++ vars/checkoutQaInternal.groovy
@@ -2,8 +2,8 @@
 
 def call(Map params = [:]) {
     dir("scylla-qa-internal") {
-        git(url: 'git@github.com:scylladb/scylla-qa-internal.git',
+        git(url: 'git@github.com:dimakr/scylla-qa-internal.git',
             credentialsId:'b8a774da-0e46-4c91-9f74-09caebaea261',
-            branch: 'master')
+            branch: 'add_limits_for_strongdm_container')  // TEMPORARY: testing StrongDM limits
     }
 }
