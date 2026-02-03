6de4fee10ba84d2c55281498253bfff873bd3914
Alexey Kartashov <alexey.kartashov@scylladb.com>
2026-02-03 14:47:04 +0100

    chore(argus): Update client to 0.15.12
    This update brings SCT Stress Command Support, SCT Configuration support
    and SCT Nemesis Description support
    
    (cherry picked from commit d0f6ec06efb69e9b28f3722817a821d1630606ab)

--- argus/_version.py
+++ argus/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.15.10'
-__version_tuple__ = version_tuple = (0, 15, 10)
+__version__ = version = '0.15.12'
+__version_tuple__ = version_tuple = (0, 15, 12)
 
-__commit_id__ = commit_id = 'gfb79cf01d'
+__commit_id__ = commit_id = 'g11df55d0a'
