cf298a760ce82adf78698b638746d90907927e87
Petr Hála <petr.hala@scylladb.com>
2026-02-17 15:26:03 +0100

    feat(nemesis): Extract methods from disrupt_method_wrapper
    Reworks Argus and validation logic, which was in inner function inside this wrapper
    
    (cherry picked from commit 9913e6462d9f59a0a2c703e64d0c00ba58994b94)

--- sdcm/sct_events/nemesis.py
+++ sdcm/sct_events/nemesis.py
@@ -12,6 +12,7 @@
 # Copyright (c) 2020 ScyllaDB
 from functools import cached_property
 
+from argus.common.enums import NemesisStatus
 from sdcm.sct_events import Severity
 from sdcm.sct_events.continuous_event import ContinuousEvent
 
@@ -44,3 +45,11 @@ class DisruptionEvent(ContinuousEvent):
         if self.full_traceback:
             fmt += "\n{0.full_traceback}"
         return fmt
+
+    @property
+    def nemesis_status(self) -> NemesisStatus:
+        if self.severity == Severity.ERROR:
+            return NemesisStatus.FAILED
+        if self.is_skipped:
+            return NemesisStatus.SKIPPED
+        return NemesisStatus.SUCCEEDED
