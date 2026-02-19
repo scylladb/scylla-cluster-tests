19ea41841bf059ee5e7ace451384566a31f15b34
Aleksandr Bykov <alex.bykov@scylladb.com>
2026-02-19 15:54:05 +0700

    use scale loader

--- sdcm/utils/adaptive_timeouts/__init__.py
+++ sdcm/utils/adaptive_timeouts/__init__.py
@@ -17,7 +17,7 @@ from sdcm.utils.features import is_tablets_feature_enabled
 LOGGER = logging.getLogger(__name__)
 
 TABLETS_SOFT_TIMEOUT = 1 * 60 * 60
-TABLETS_HARD_TIMEOUT = 3 * 60 * 60
+TABLETS_HARD_TIMEOUT = 8 * 60 * 60
 
 
 def _get_decommission_timeout(
