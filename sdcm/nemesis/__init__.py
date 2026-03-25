c0192baf835310a601c13f0256c234411882d570
Liapkovich <mikita.liapkovich@scylladb.com>
2026-03-25 16:24:32 +0100

    feature(scylla_yaml): add 'rack' as valid internode_compression value
    ScyllaDB supports 'rack' as an internode compression mode alongside
    'none', 'all', and 'dc'. Add it to the Literal type in ScyllaYaml and
    the nemesis rolling-config-change toggle list
    
    Refs: scylladb/scylladb#27099

--- sdcm/nemesis/__init__.py
+++ sdcm/nemesis/__init__.py
@@ -922,7 +922,7 @@ class NemesisRunner:
     def disrupt_rolling_config_change_internode_compression(self):
         def get_internode_compression_new_value_randomly(current_compression):
             self.log.debug(f"Current compression is {current_compression}")
-            values = ["dc", "all", None]
+            values = ["dc", "all", "rack", None]
             values_to_toggle = list(filter(lambda value: value != current_compression, values))
             return random.choice(values_to_toggle)
 
