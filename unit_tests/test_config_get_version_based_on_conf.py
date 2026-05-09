b5362afab0906b48f1f5efbf84b64c771f0efa39
Israel Fruchter <fruch@scylladb.com>
2026-05-10 00:31:53 +0300

    fix(test-config): remove enterprise-prefixed version tests
    Enterprise-prefixed versions (enterprise-LATEST:latest, scylla-enterprise
    docker images) are no longer relevant since Scylla has unified its
    versioning. Remove test cases that expect enterprise docker images and
    enterprise branch repo lookups, and update remaining tests to expect
    non-enterprise (is_enterprise=False) results.
    
    (cherry picked from commit 08a3d288fd7a66dc47534f5293eabc31157dff4a)

--- unit_tests/test_config_get_version_based_on_conf.py
+++ unit_tests/test_config_get_version_based_on_conf.py
@@ -11,16 +11,22 @@
 #
 # Copyright (c) 2023 ScyllaDB
 
-import os
 import logging
 import unittest.mock
+
 import pytest
 
 from sdcm import sct_config
+from sdcm.utils.common import get_latest_scylla_release
 
 pytestmark = [
     pytest.mark.integration,
 ]
+
+
+def _get_latest_branch() -> str:
+    full_version = get_latest_scylla_release("scylla-enterprise")
+    return ".".join(full_version.split(".")[:2])
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -33,105 +39,111 @@ def setup():
     yield
 
 
+@pytest.fixture(scope="session")
+def latest_release():
+    return _get_latest_branch()
+
+
 @pytest.fixture(scope="function", autouse=True)
-def function_setup():
-    os.environ["SCT_CONFIG_FILES"] = "internal_test_data/minimal_test_case.yaml"
-
-    yield
-
-    for k in os.environ:
-        if k.startswith("SCT_"):
-            del os.environ[k]
+def function_setup(monkeypatch):
+    monkeypatch.setenv("SCT_CONFIG_FILES", "internal_test_data/minimal_test_case.yaml")
 
 
 @pytest.mark.parametrize(
-    argnames="scylla_version, expected_docker_image, expected_outcome",
+    argnames="scylla_version, expected_docker_image, expected_version, expected_is_enterprise",
     argvalues=[
-        pytest.param("6.1", "scylladb/scylla", ("6.1", False), id="6.1"),
-        pytest.param("2024.1", "scylladb/scylla-enterprise", ("2024.1", True), id="2024.1"),
-        pytest.param("latest", "scylladb/scylla-nightly", (None, False), id="latest"),
-        pytest.param("master:latest", "scylladb/scylla-nightly", (None, False), id="master:latest"),
-        pytest.param("enterprise", "scylladb/scylla-enterprise-nightly", (None, True), id="enterprise"),
+        pytest.param("LATEST", "scylladb/scylla", "LATEST", False, id="latest-release"),
+        pytest.param("latest", "scylladb/scylla-nightly", None, False, id="latest"),
+        pytest.param("master:latest", "scylladb/scylla-nightly", None, False, id="master:latest"),
     ],
 )
-def test_docker(scylla_version, expected_docker_image, expected_outcome):
-    os.environ["SCT_CLUSTER_BACKEND"] = "docker"
-    os.environ["SCT_USE_MGMT"] = "false"
-    os.environ["SCT_SCYLLA_VERSION"] = scylla_version
+def test_docker(
+    scylla_version, expected_docker_image, expected_version, expected_is_enterprise, monkeypatch, latest_release
+):
+    if scylla_version == "LATEST":
+        scylla_version = latest_release
+    if expected_version == "LATEST":
+        expected_version = latest_release
+
+    monkeypatch.setenv("SCT_CLUSTER_BACKEND", "docker")
+    monkeypatch.setenv("SCT_USE_MGMT", "false")
+    monkeypatch.setenv("SCT_SCYLLA_VERSION", scylla_version)
 
     conf = sct_config.SCTConfiguration()
     conf.verify_configuration()
     assert "docker_image" in conf.dump_config()
     assert conf.get("docker_image") == expected_docker_image
     _version, _is_enterprise = conf.get_version_based_on_conf()
-    if expected_outcome[0] is None:
-        assert _is_enterprise == expected_outcome[1]
+    if expected_version is None:
+        assert _is_enterprise == expected_is_enterprise
     else:
-        assert (_version, _is_enterprise) == expected_outcome
+        assert (_version, _is_enterprise) == (expected_version, expected_is_enterprise)
 
 
 @pytest.mark.parametrize(argnames="distro", argvalues=("ubuntu-xenial", "centos", "debian-jessie"))
 @pytest.mark.parametrize(
-    argnames="scylla_version, expected_outcome",
+    argnames="scylla_version, expected_version, expected_is_enterprise",
     argvalues=[
-        pytest.param("6.1", ("6.1", False), id="6.1"),
-        pytest.param("2024.1", ("2024.1", True), id="2024.1"),
-        pytest.param("master:latest", (None, True), id="master"),
-        pytest.param("branch-6.0:latest", (None, False), id="branch-6.0"),
-        pytest.param("enterprise-2023.1:latest", (None, True), id="enterprise-2023.1"),
-        pytest.param("enterprise-2024.1:latest", (None, True), id="enterprise-2024.1"),
+        pytest.param("LATEST", "LATEST", True, id="latest-release"),
+        pytest.param("master:latest", None, True, id="master"),
+        pytest.param("branch-LATEST:latest", None, True, id="branch"),
     ],
 )
-def test_scylla_repo(scylla_version, expected_outcome, distro):
-    os.environ["SCT_CLUSTER_BACKEND"] = "gce"
-    os.environ["SCT_SCYLLA_VERSION"] = scylla_version
-    os.environ["SCT_GCE_IMAGE_DB"] = (
-        "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/family/centos-7"
+def test_scylla_repo(scylla_version, expected_version, expected_is_enterprise, distro, monkeypatch, latest_release):
+    scylla_version = scylla_version.replace("LATEST", latest_release)
+    if expected_version == "LATEST":
+        expected_version = latest_release
+
+    monkeypatch.setenv("SCT_CLUSTER_BACKEND", "gce")
+    monkeypatch.setenv("SCT_SCYLLA_VERSION", scylla_version)
+    monkeypatch.setenv(
+        "SCT_GCE_IMAGE_DB", "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/family/centos-7"
     )
-    os.environ["SCT_USE_PREINSTALLED_SCYLLA"] = "false"
-    os.environ["SCT_SCYLLA_LINUX_DISTRO"] = distro
+    monkeypatch.setenv("SCT_USE_PREINSTALLED_SCYLLA", "false")
+    monkeypatch.setenv("SCT_SCYLLA_LINUX_DISTRO", distro)
 
     conf = sct_config.SCTConfiguration()
     conf.verify_configuration()
     _version, _is_enterprise = conf.get_version_based_on_conf()
 
-    if expected_outcome[0] is not None:
-        assert expected_outcome[0] in _version
-    assert _is_enterprise == expected_outcome[1]
+    if expected_version is not None:
+        assert expected_version in _version
+    assert _is_enterprise == expected_is_enterprise
 
 
 @pytest.mark.parametrize(
-    argnames="scylla_version, expected_outcome",
+    argnames="scylla_version, expected_version, expected_is_enterprise",
     argvalues=[
-        pytest.param("6.2", ("6.2", False), id="6.2"),
-        pytest.param("2024.2", ("2024.2", True), id="2024.2"),
-        pytest.param("master:latest", (None, True), id="master"),
-        pytest.param("branch-6.2:latest", (None, False), id="branch-6.2"),
-        pytest.param("branch-2024.1:latest", (None, True), id="branch-2024.1"),
-        pytest.param("branch-2024.2:latest", (None, True), id="branch-2024.2"),
+        pytest.param("LATEST", "LATEST", True, id="latest-release"),
+        pytest.param("master:latest", None, True, id="master"),
+        pytest.param("branch-LATEST:latest", None, True, id="branch"),
     ],
 )
 @pytest.mark.parametrize(argnames="backend", argvalues=("aws", "gce", "azure"))
-def test_images(backend, scylla_version, expected_outcome):
-    os.environ["SCT_CLUSTER_BACKEND"] = backend
-    os.environ["SCT_SCYLLA_VERSION"] = scylla_version
+def test_images(backend, scylla_version, expected_version, expected_is_enterprise, monkeypatch, latest_release):
+    scylla_version = scylla_version.replace("LATEST", latest_release)
+    if expected_version == "LATEST":
+        expected_version = latest_release
+
+    monkeypatch.setenv("SCT_CLUSTER_BACKEND", backend)
+    monkeypatch.setenv("SCT_SCYLLA_VERSION", scylla_version)
 
     conf = sct_config.SCTConfiguration()
     conf.verify_configuration()
 
     _version, _is_enterprise = conf.get_version_based_on_conf()
 
-    if expected_outcome[0] is not None:
-        assert expected_outcome[0] in _version
-    assert _is_enterprise == expected_outcome[1]
+    if expected_version is not None:
+        assert expected_version in _version
+    assert _is_enterprise == expected_is_enterprise
 
 
-def test_baremetal():
-    os.environ["SCT_CLUSTER_BACKEND"] = "baremetal"
-    os.environ["SCT_SCYLLA_VERSION"] = "6.1"
-    os.environ["SCT_S3_BAREMETAL_CONFIG"] = "some_config"
-    os.environ["SCT_DB_NODES_PRIVATE_IP"] = '["127.0.0.1"]'
-    os.environ["SCT_DB_NODES_PUBLIC_IP"] = '["127.0.0.1"]'
+def test_baremetal(monkeypatch):
+    monkeypatch.setenv("SCT_CLUSTER_BACKEND", "baremetal")
+    monkeypatch.setenv("SCT_SCYLLA_VERSION", "6.1")
+    monkeypatch.setenv("SCT_S3_BAREMETAL_CONFIG", "some_config")
+    monkeypatch.setenv("SCT_DB_NODES_PRIVATE_IP", '["127.0.0.1"]')
+    monkeypatch.setenv("SCT_DB_NODES_PUBLIC_IP", '["127.0.0.1"]')
 
     conf = sct_config.SCTConfiguration()
     conf.verify_configuration()
@@ -142,14 +154,18 @@ def test_baremetal():
     assert not _is_enterprise
 
 
-def test_unified_package():
-    os.environ["SCT_CLUSTER_BACKEND"] = "gce"
-    os.environ["SCT_UNIFIED_PACKAGE"] = (
-        "https://downloads.scylladb.com/unstable/scylla/master/relocatable/2023-11-13T03:04:27Z/"
-        "scylla-unified-5.5.0~dev-0.20231113.7b08886e8dd8.x86_64.tar.gz"
+def test_unified_package(monkeypatch):
+    monkeypatch.setenv("SCT_CLUSTER_BACKEND", "gce")
+    monkeypatch.setenv(
+        "SCT_UNIFIED_PACKAGE",
+        (
+            "https://downloads.scylladb.com/unstable/scylla/master/relocatable/2023-11-13T03:04:27Z/"
+            "scylla-unified-5.5.0~dev-0.20231113.7b08886e8dd8.x86_64.tar.gz"
+        ),
     )
-    os.environ["SCT_GCE_IMAGE_DB"] = (
-        "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/family/centos-7"
+    monkeypatch.setenv(
+        "SCT_GCE_IMAGE_DB",
+        ("https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/family/centos-7"),
     )
 
     conf = sct_config.SCTConfiguration()
