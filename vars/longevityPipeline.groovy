ea4e5aefae2b5d0e50e67074c8205afcf186a50c
Israel Fruchter <fruch@scylladb.com>
2026-05-11 22:31:44 +0300

    ci(pipelines): update AWS region descriptions to show all supported regions
    Update pipeline parameter descriptions to list all currently supported
    AWS regions including us-east-2, eu-west-3, and ca-central-1.
    
    [QAINFRA-103]

--- vars/longevityPipeline.groovy
+++ vars/longevityPipeline.groovy
@@ -36,7 +36,7 @@ def call(Map pipelineParams) {
                    name: 'xcloud_env')
 
             string(defaultValue: "${pipelineParams.get('region', 'eu-west-1')}",
-               description: 'Supported: us-east-1 | eu-west-1 | eu-west-2 | eu-north-1 | eu-central-1 | us-west-2 | random (randomly select region)',
+               description: 'Supported: us-east-1 | us-east-2 | us-west-2 | eu-west-1 | eu-west-2 | eu-west-3 | eu-north-1 | eu-central-1 | ca-central-1 | random (randomly select region)',
                name: 'region')
             string(defaultValue: "${pipelineParams.get('gce_datacenter', 'us-east1')}",
                    description: 'GCE datacenter',
