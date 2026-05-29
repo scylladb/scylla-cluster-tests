b3e3f0ca6757e94aebffe21d68a6808d7112cdff
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2026-05-29 20:03:54 +0300

    fix(oci): fix regex for the rack name parsing
    OCI rack names contain hyphens (-) but our regex was not expecting this
    symbol. And it led to the situation where we were getting only first
    part of a rack name.
    Example: instead of the "iad-ad-1" we were taking "iad".
    
    So, fix the regex by adding hyphen symbol into the expected ones.
    
    Closes: #SCT-386

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -4263,7 +4263,7 @@ class BaseCluster:
             r"(?P<tokens>[\d]+)\s+"
             r"(?P<owns>[\w?]+)\s+"
             r"(?P<host_id>[\w-]+)\s+"
-            r"(?P<rack>[\w]+|$)"
+            r"(?P<rack>[\w-]+|$)"
         )
 
         for dc in data_centers:
