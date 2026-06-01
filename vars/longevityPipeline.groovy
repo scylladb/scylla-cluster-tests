36ae4b930e91b8bbdca2c2a2cebe344f3d3bf775
Liapkovich <mikita.liapkovich@scylladb.com>
2026-06-01 10:40:41 +0200

    fix(manager): bump default Manager version to 3.11 across framework
    (cherry picked from commit bb41d7a03eec448f186e8b16f3b1154313ea34a1)

--- vars/longevityPipeline.groovy
+++ vars/longevityPipeline.groovy
@@ -124,7 +124,7 @@ def call(Map pipelineParams) {
             // Manager Configuration
             separator(name: 'MANAGER_CONFIG', sectionHeader: 'Manager Configuration')
             string(defaultValue: "${pipelineParams.get('manager_version', '')}",
-                   description: 'master_latest|3.10|3.9',
+                   description: 'master_latest|3.11|3.10',
                    name: 'manager_version')
 
             string(defaultValue: '',
