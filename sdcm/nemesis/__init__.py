72f0e6289eff7af2d9d77b341be37c0dd4b300dd
Yauheni Khatsianevich <yauheni.khatsianevich@scylladb.com>
2026-06-17 11:41:33 +0200

    fix(nemesis): downgrade expected raft gate-closed errors
    Add event filters for Raft errors that may occur while a node is shutting
    down and internal Seastar gates are being closed.
    
    (cherry picked from commit c4269e819f8dcfd6d25ce6dde5bde92bbd1c168d)

--- sdcm/nemesis/__init__.py
+++ sdcm/nemesis/__init__.py
@@ -95,6 +95,7 @@ from sdcm.sct_events.group_common_events import (
     suppress_expected_unavailability_errors,
     ignore_raft_topology_cmd_failing,
     ignore_drop_table_during_repair_errors,
+    ignore_raft_applier_gate_closed_errors,
 )
 
 from sdcm.sct_events.loaders import CassandraStressLogEvent
@@ -4642,7 +4643,7 @@ class NemesisRunner:
             bootstrap_node=new_node, verification_node=self.target_node, actions_log=self.actions_log
         )
 
-        with suppress_expected_unavailability_errors():
+        with suppress_expected_unavailability_errors(), ignore_raft_applier_gate_closed_errors():
             bootstrapabortmanager.run_bootstrap_and_abort_with_action(
                 terminate_pattern, abort_action=new_node.stop_scylla
             )
