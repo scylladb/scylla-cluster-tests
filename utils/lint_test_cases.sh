5675139870f363d636364f327d7580922d66569c
Israel Fruchter <fruch@scylladb.com>
2026-07-07 20:40:33 +0300

    TEMP: resolved conflicts

--- utils/lint_test_cases.sh
+++ utils/lint_test_cases.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 
 OUT=0
-SCT_AMI_ID_DB_SCYLLA=ami-1234 ./sct.py lint-yamls -i '.yaml' -e 'azure,oci,multi-dc,multiDC,multidc,multiple-dc,3dcs,5dcs,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,custom-d3,jepsen,repair-based-operations,add-new-dc,baremetal,x86-to-arm,oel'
+SCT_AMI_ID_DB_SCYLLA=ami-1234 SCT_AMI_ID_VECTOR_STORE=ami-placeholder ./sct.py lint-yamls -i '.yaml' -e 'azure,oci,multi-dc,multiDC,multidc,multiple-dc,3dcs,5dcs,rolling,docker,artifacts,private-repo,ics/long,scylla-operator,gce,custom-d3,jepsen,repair-based-operations,add-new-dc,baremetal,x86-to-arm,cassandra,oel'
 OUT=$(($OUT + $?))
 
-SCT_AZURE_IMAGE_DB=image SCT_AZURE_REGION_NAME="eastus" ./sct.py lint-yamls --backend azure -i azure
+SCT_AZURE_IMAGE_DB=image SCT_AZURE_REGION_NAME="eastus" SCT_AZURE_INSTANCE_TYPE_DB=Standard_E4s_v5 ./sct.py lint-yamls --backend azure -i azure
 OUT=$(($OUT + $?))
 
-SCT_OCI_IMAGE_DB=image SCT_OCI_REGION_NAME="us-ashburn-1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2026.1.repo' ./sct.py lint-yamls --backend oci -i 'oci,oel'
+SCT_OCI_IMAGE_DB=image SCT_OCI_REGION_NAME="us-ashburn-1" SCT_OCI_INSTANCE_TYPE_DB=VM.Standard3.Flex ./sct.py lint-yamls --backend oci -i 'oci,oel'
 OUT=$(($OUT + $?))
 
-SCT_GCE_IMAGE_DB=image SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'rolling,artifacts,private-repo,gce,custom-d3,jepsen' -e 'multi-dc,multiDC,docker,azure,oci,5dcs,oel'
+SCT_GCE_IMAGE_DB=image SCT_GCE_INSTANCE_TYPE_DB=n2-standard-2 SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'rolling,artifacts,private-repo,gce,custom-d3,jepsen' -e 'multi-dc,multiDC,docker,azure,oci,5dcs,oel'
 OUT=$(($OUT + $?))
 
 echo "multi dc yamls with 2 regions"
-SCT_GCE_IMAGE_DB=image SCT_GCE_DATACENTER="us-east1 us-west1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'multi-dc-rolling,3h-multidc,multidc-4h,multidc-parallel,snitch-multi-dc,manager-regression-multiDC' -e 'docker,azure,shutdown'
+SCT_GCE_IMAGE_DB=image SCT_GCE_INSTANCE_TYPE_DB=n2-standard-2 SCT_GCE_DATACENTER="us-east1 us-west1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i 'multi-dc-rolling,3h-multidc,multidc-4h,multidc-parallel,snitch-multi-dc,manager-regression-multiDC' -e 'docker,azure,shutdown'
 OUT=$(($OUT + $?))
 
 echo "multi dc yamls with 3 regions"
-SCT_GCE_IMAGE_DB=image SCT_GCE_DATACENTER="us-east1 us-west1 eu-north1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i '3dcs,24h-multidc,large-cluster,counters-multidc,cdc-8h-multi-dc' -e 'docker,azure,shutdown'
+SCT_GCE_IMAGE_DB=image SCT_GCE_INSTANCE_TYPE_DB=n2-standard-2 SCT_GCE_DATACENTER="us-east1 us-west1 eu-north1" SCT_SCYLLA_REPO='http://downloads.scylladb.com.s3.amazonaws.com/rpm/centos/scylla-2021.1.repo' ./sct.py lint-yamls -b gce -i '3dcs,24h-multidc,large-cluster,counters-multidc,cdc-8h-multi-dc' -e 'docker,azure,shutdown'
 OUT=$(($OUT + $?))
 
 echo "multi dc yamls with 5 regions"
