1babfdb45e9a4c27f3a2a0e101e653188a14c856
Valerii Ponomarov <valerii.ponomarov@scylladb.com>
2026-07-21 18:28:32 +0300

    ci(oci): use different region by default in OCI due to capacity issues
    Ref: #SCT-651

--- vars/longevityPipeline.groovy
+++ vars/longevityPipeline.groovy
@@ -44,7 +44,7 @@ def call(Map pipelineParams) {
             string(defaultValue: "${pipelineParams.get('azure_region_name', 'eastus')}",
                    description: 'Azure location',
                    name: 'azure_region_name')
-            string(defaultValue: "${pipelineParams.get('oci_region_name', 'us-ashburn-1')}",
+            string(defaultValue: "${pipelineParams.get('oci_region_name', 'us-phoenix-1')}",
                    description: 'Oracle location',
                    name: 'oci_region_name')
             string(defaultValue: "${pipelineParams.get('availability_zone', '')}",
